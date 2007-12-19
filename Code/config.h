500ac09f1072e39a96996e74938cae7563c0a421
Steven Manos <s.manos@ucl.ac.uk>
2007-12-19 17:07:19 +0000

    New  (big!) source changes made to the tree by Marco.
    --HG--
    branch : development

--- Code/config.h
+++ Code/config.h
@@ -6,64 +6,166 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
-#include <rpc/xdr.h>
 
-int STABLE   = 1;
-int UNSTABLE = 0;
+#ifndef int64_t
+#define int64_t long int
+#endif
+
+#ifdef XT3
+#include "types.h"
+#include "xdr.h"
+#else
+#include <rpc/types.h>
+#include <rpc/xdr.h>
+#endif
+
+
+#ifdef BENCH
+#undef RG
+#undef STEER
+#endif
+
+
+#ifdef RG
+#include <pthread.h>
+#endif // RG
+
+
+#ifdef STEER
+#include "ReG_Steer_types.h"
+#include "ReG_Steer_Appside.h"
+#endif // STEER
+
+
+#define MACROSCOPIC_PARS   5
+#define DENSITY            0
+#define VELOCITY           1
+#define STRESS             2
+
+#define NEIGHBOUR_PROCS_MAX            52
+#define SHARED_DISTRIBUTIONS_MAX       4000000
+#define COMMS_LEVELS                   2
+#define COLLISION_TYPES                6
+#define MACHINES_MAX                   4
+#define PROCS_MAX                      8 * 1024
+
+#define STREAMLINES_MAX                1000
+
+#define PIXELS_X                       1024
+#define PIXELS_Y                       1024
+#define COLOURED_PIXELS_PER_PROC_MAX   PIXELS_X * PIXELS_Y
+#define IMAGE_SIZE                     PIXELS_X * PIXELS_Y
+
+#define SSE_SIZE                       4
+
+extern float EPSILON;
+
+extern int STABLE;
+extern int UNSTABLE;
 
 // the constants needed to define the configuration of the lattice
 // sites follow
 
-unsigned int SOLID_TYPE  = 0U;
-unsigned int FLUID_TYPE  = 1U;
-unsigned int INLET_TYPE  = 2U;
-unsigned int OUTLET_TYPE = 3U;
-unsigned int NULL_TYPE   = 4U;
+extern unsigned int SOLID_TYPE;
+extern unsigned int FLUID_TYPE;
+extern unsigned int INLET_TYPE;
+extern unsigned int OUTLET_TYPE;
+extern unsigned int NULL_TYPE;
 
-unsigned int BOUNDARIES              = 4U;
-unsigned int INLET_BOUNDARY          = 0U;
-unsigned int OUTLET_BOUNDARY         = 1U;
-unsigned int WALL_BOUNDARY           = 2U;
-unsigned int CHARACTERISTIC_BOUNDARY = 3U;
+extern unsigned int BOUNDARIES;
+extern unsigned int INLET_BOUNDARY;
+extern unsigned int OUTLET_BOUNDARY;
+extern unsigned int WALL_BOUNDARY;
+extern unsigned int CHARACTERISTIC_BOUNDARY;
 
-unsigned int SITE_TYPE_BITS       = 2U;
-unsigned int BOUNDARY_CONFIG_BITS = 14U;
-unsigned int BOUNDARY_DIR_BITS    = 4U;
-unsigned int BOUNDARY_ID_BITS     = 10U;
+extern unsigned int SITE_TYPE_BITS;
+extern unsigned int BOUNDARY_CONFIG_BITS;
+extern unsigned int BOUNDARY_DIR_BITS;
+extern unsigned int BOUNDARY_ID_BITS;
 
-unsigned int BOUNDARY_CONFIG_SHIFT = 2U;   // SITE_TYPE_BITS;
-unsigned int BOUNDARY_DIR_SHIFT    = 16U;  // BOUNDARY_CONFIG_SHIFT + BOUNDARY_CONFIG_BITS;
-unsigned int BOUNDARY_ID_SHIFT     = 20U;  // BOUNDARY_DIR_SHIFT + BOUNDARY_DIR_BITS;
+extern unsigned int BOUNDARY_CONFIG_SHIFT ;   // SITE_TYPE_BITS;
+extern unsigned int BOUNDARY_DIR_SHIFT;  // BOUNDARY_CONFIG_SHIFT + BOUNDARY_CONFIG_BITS;
+extern unsigned int BOUNDARY_ID_SHIFT;  // BOUNDARY_DIR_SHIFT + BOUNDARY_DIR_BITS;
 
-unsigned int SITE_TYPE_MASK       = ((1U <<  2U) - 1U);         // ((1U << SITE_TYPE_BITS) - 1U);
-unsigned int BOUNDARY_CONFIG_MASK = ((1U << 14U) - 1U) << 2U;   // ((1U << BOUNDARY_CONFIG_BITS) - 1U) << BOUNDARY_CONFIG_SHIFT;
-unsigned int BOUNDARY_DIR_MASK    = ((1U <<  4U) - 1U) << 16U;  //((1U << BOUNDARY_DIR_BITS) - 1U)    << BOUNDARY_DIR_SHIFT;
-unsigned int BOUNDARY_ID_MASK     = ((1U << 10U) - 1U) << 20U;  // ((1U << BOUNDARY_ID_BITS) - 1U)     << BOUNDARY_ID_SHIFT
-unsigned int CHARACTERISTIC_MASK  = 1U << 31U;
+extern unsigned int SITE_TYPE_MASK;         // ((1U << SITE_TYPE_BITS) - 1U);
+extern unsigned int BOUNDARY_CONFIG_MASK;   // ((1U << BOUNDARY_CONFIG_BITS) - 1U) << BOUNDARY_CONFIG_SHIFT;
+extern unsigned int BOUNDARY_DIR_MASK;  //((1U << BOUNDARY_DIR_BITS) - 1U)    << BOUNDARY_DIR_SHIFT;
+extern unsigned int BOUNDARY_ID_MASK;  // ((1U << BOUNDARY_ID_BITS) - 1U)     << BOUNDARY_ID_SHIFT
+extern unsigned int CHARACTERISTIC_MASK;
 
 // square of the speed of sound
 
-double Cs2 = 3.0 / 8.0;
+extern double Cs2;
 
 
 // parameters related to the lattice directions
 
-int e_x[] = { 0, 1,-1, 0, 0, 0, 0, 1,-1, 1,-1, 1,-1, 1,-1};
-int e_y[] = { 0, 0, 0, 1,-1, 0, 0, 1,-1, 1,-1,-1, 1,-1, 1};
-int e_z[] = { 0, 0, 0, 0, 0, 1,-1, 1,-1,-1, 1, 1,-1,-1, 1};
-int inv_dir[] = {0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13};
+extern int e_x[15];
+extern int e_y[15];
+extern int e_z[15];
+extern int inv_dir[15];
 
-
-#define MACROSCOPIC_PARS   5
-
-#define NEIGHBOUR_PROCS_MAX        52
-#define SHARED_DISTRIBUTIONS_MAX   4000000
+extern MPI_Datatype MPI_col_pixel_type;
 
 
 // data structures useful to define the simulation set-up, construct
 // the system and partition it
+
+#ifdef RG
+
+extern pthread_mutex_t network_buffer_copy_lock;
+extern pthread_cond_t network_send_frame;
+
+extern int send_array_length;
+
+#endif // RG
+
+
+#ifdef STEER
+
+// this is here so that I can transfer all params and data in one
+// chunk in one MPI_Bcast rather than loads of separate ones...
+struct SteerParams
+{
+  // reg
+  int    status;
+  int    num_recvd_cmds;
+  int    recvd_cmds[REG_MAX_NUM_STR_CMDS];
+  int    num_params_changed;
+  
+  // lbm
+  double tau;
+  double tolerance;
+  int    max_time_steps;
+  int    conv_freq;
+  int    check_freq;
+  
+  // vis
+  float  ctr_x, ctr_y, ctr_z;
+  float  longitude;
+  float  latitude;
+  float  zoom;
+  int    image_freq;
+  int    flow_field_type;
+  int    mode;
+  float  abs_factor;
+  float  cutoff;
+  float  max_density;
+  float  max_velocity;
+  float  max_stress;
+};
+
+#endif // STEER
+
+
+struct DataBlock
+{
+  unsigned int *site_data;
+};
+
 
 struct BlockLocation
 {
@@ -74,7 +176,6 @@ struct BlockLocation
 struct LBM
 {
   char *system_file_name;
-  char *parameters_file_name;
   char *checkpoint_file_name;
   
   double tau;
@@ -84,6 +185,10 @@ struct LBM
   double *inlet_density;
   double *outlet_density;
   double tolerance;
+  double conv_error;
+  double density_min, density_max;
+  double velocity_min, velocity_max;
+  double stress_min, stress_max;
   
   int sites_x, sites_y, sites_z;
   int blocks_x, blocks_y, blocks_z;
@@ -96,8 +201,12 @@ struct LBM
   int site_max_x, site_max_y, site_max_z;
   int inlets, outlets;
   int time_steps_max;
-  int checkpoint_frequency, convergence_frequency;
+  int checkpoint_freq, conv_freq;
   int is_checkpoint;
+  
+  float *block_density;
+  
+  int *block_map;
   
   short int *fluid_sites_per_block;
 };
@@ -115,18 +224,8 @@ struct NeighProc
   
   int *f_send_id;
   int *f_recv_iv;
-};
-
-
-struct DataBlock
-{
-  unsigned int *site_data;
-};
-
-
-struct Velocity
-{
-  double x, y, z;
+  
+  double **d_to_send_p;
 };
 
 
@@ -140,13 +239,20 @@ struct Net
   int sites_in_a_block;
   int id;
   int procs, machines;
-  int inter_m_neigh_procs, neigh_procs;
+  int neigh_procs;
   int err;
   int my_inter_sites, my_inner_sites;
+  int my_inner_collisions[COLLISION_TYPES];
+  int my_inter_collisions[COLLISION_TYPES];
+  int my_inner_collisions_sse[COLLISION_TYPES];
+  int my_inter_collisions_sse[COLLISION_TYPES];
+  int my_sites;
+  int shared_fs;
   
   int *machine_id;
   int *procs_per_machine;
   
+  short int *from_proc_id_to_neigh_proc_index;
   short int *proc_id;
   
   unsigned int *site_data;
@@ -154,31 +260,160 @@ struct Net
   DataBlock *data_block;
   DataBlock *map_block;
   
-  NeighProc inter_m_neigh_proc[NEIGHBOUR_PROCS_MAX];
   NeighProc neigh_proc[NEIGHBOUR_PROCS_MAX];
   
   MPI_Status status[4];
   
   MPI_Request **req;
   
-  double timing[7];
+  double dd_time, bm_time, fr_time, fo_time;
 };
 
 
-double *f_old, *f_new;
+struct BlockData
+{
+  double min[3]; int id[3];///
+  DataBlock *p;
+  
+  float t;
+  float min_x, min_y, min_z;
+  
+  int i, j, k;
+};
 
-int *f_id;
 
-Velocity *vel;
+struct ColPixel
+{
+  float r, g, b;
+  float t;
+  
+  short int i, j;
+};
 
 
-short int f_data[4*SHARED_DISTRIBUTIONS_MAX];
+struct Cluster
+{
+  float block_min_x, block_min_y, block_min_z;
+  
+  int blocks_yz, blocks;
+  
+  unsigned short int blocks_x, blocks_y, blocks_z;
+  unsigned short int block_min_i, block_min_j, block_min_k;
+};
 
-double f_to_send[SHARED_DISTRIBUTIONS_MAX];
-double f_to_recv[SHARED_DISTRIBUTIONS_MAX];
 
-int f_send_id[SHARED_DISTRIBUTIONS_MAX];
-int f_recv_iv[SHARED_DISTRIBUTIONS_MAX];
+struct Vis
+{
+  char *image_file_name;
+  
+  int image_freq;
+  int flow_field_type;
+  int mode;
+  int blocks_x, blocks_y, blocks_z;
+  int blocks_yz, blocks;
+  int block_size, block_size2, block_size3, block_size_1;
+  int shift, shift2;
+  int col_pixels, col_pixels_max, col_pixels_locked;
+  int col_pixels_recv[ MACHINES_MAX-1 ];
+  int *col_pixel_id;
+  int pixels_max;
+  int seeds, seeds_max;
+  int streamlines, streamlines_max;
+  
+  short int clusters;
+  
+  float flow_field_value_max_inv[3];
+  float absorption_factor;
+  float cutoff;
+  float t_min;
+  float block_size_inv;
+  float system_size;
+  float half_dim_x, half_dim_y, half_dim_z;
+  
+  float *seed;
+  float *streamline;
+  
+  Cluster *cluster;
+  
+  ColPixel col_pixel_send[ (MACHINES_MAX-1)*COLOURED_PIXELS_PER_PROC_MAX ];
+  ColPixel *col_pixel_recv;
+  ColPixel *col_pixel_locked;
+};
+
+
+struct Screen
+{
+  float ctr_x, ctr_y, ctr_z;
+  float max_x, max_y;
+  
+  float dir1x, dir1y, dir1z;
+  float dir2x, dir2y, dir2z;
+  float par_x, par_y, par_z;
+  
+  float zoom, dist;
+  
+  int pixels_x, pixels_y;
+};
+
+
+struct Viewpoint
+{
+  float pos_x, pos_y, pos_z;
+  float sin_1, cos_1;
+  float sin_2, cos_2;
+};
+
+
+struct Ray
+{
+  double dir[3], inv[3]; ///
+  float dir_x, dir_y, dir_z;
+  float inv_x, inv_y, inv_z;
+  float col_r, col_g, col_b;
+};
+
+
+struct AABB
+{
+  float acc[6];///
+  float acc_1, acc_2, acc_3, acc_4, acc_5, acc_6;
+};
+
+
+extern double *f_old, *f_new;
+
+extern int *f_id;
+
+extern double *vel;
+
+extern float *flow_field;
+
+extern double *d;
+
+extern double **nd_p;
+
+
+extern short int f_data[4*SHARED_DISTRIBUTIONS_MAX];
+extern double f_to_send[SHARED_DISTRIBUTIONS_MAX];
+extern double f_to_recv[SHARED_DISTRIBUTIONS_MAX];
+
+extern int f_send_id[SHARED_DISTRIBUTIONS_MAX];
+extern int f_recv_iv[SHARED_DISTRIBUTIONS_MAX];
+
+extern float streamline_to_send[NEIGHBOUR_PROCS_MAX][3*STREAMLINES_MAX];
+extern float streamline_to_recv[NEIGHBOUR_PROCS_MAX][1+3*STREAMLINES_MAX];
+
+extern int streamlines_to_send[NEIGHBOUR_PROCS_MAX];
+extern int streamlines_to_recv[NEIGHBOUR_PROCS_MAX];
+
+
+extern Screen screen;
+
+extern Viewpoint viewpoint;
+
+extern Ray ray;
+
+extern Vis vis;
 
 
 // declarations of all the functions used
@@ -192,53 +427,77 @@ unsigned int *netSiteMapPointer (int site_i, int site_j, int site_k, Net *net);
 
 void lbmFeq (double f[], double *density, double *v_x, double *v_y, double *v_z, double f_eq[]);
 void lbmFeq (double density, double v_x, double v_y, double v_z, double f_eq[]);
+void lbmVelocity (double f[], double *v_x, double *v_y, double *v_z);
 void lbmDensityAndVelocity (double f[], double *density, double *v_x, double *v_y, double *v_z);
-double lbmStress (double f[]);
-void CalculateBC (double f[], unsigned int site_data,double  *vx, double *vy, double *vz, LBM *lbm);
+void lbmStressSSE (double f[], double stress[], LBM *lbm);
+void lbmStress (double f[], double *stress, LBM *lbm);
+void lbmCalculateBC (double f[], unsigned int site_data, double *density, double *vx, double *vy, double *vz, LBM *lbm);
+int lbmCollisionType (unsigned int site_data);
+void lbmInit (char *system_file_name, char *checkpoint_file_name, LBM *lbm, Net *net);
+void lbmSetOptimizedInitialConditions (LBM *lbm, Net *net);
+int lbmCycle (int write_checkpoint, int check_conv, int perform_rt, int *is_converged, LBM *lbm, Net *net);
+void lbmEnd (LBM *lbm);
 
-void netFindTopology (Net *net);
-void netInit (LBM *lbm, Net *net);
-void netEnd (Net *net);
+int netFindTopology (Net *net, int *depths);
+void netInit (LBM *lbm, Net *net, Vis *vis, int proc_sites[]);
+void netEnd (Net *net, Vis *vis);
 
-void lbmReadAndSetConfig (LBM *lbm, Net *net);
-void lbmReadParameters (LBM *lbm);
-void lbmSetInitialConditions (LBM *lbm, Net *net);
+void lbmReadConfig (LBM *lbm, Net *net);
+
+#ifdef STEER
+void lbmReadParameters (char *parameters_file_name, LBM *lbm, Net *net, SteerParams *steer);
+#else
+void lbmReadParameters (char *parameters_file_name, LBM *lbm, Net *net);
+#endif
+
+#ifdef STEER
+void lbmUpdateParameters (LBM *lbm, SteerParams *steer);
+#endif
+
 void lbmWriteConfig (int stability, char *output_file_name, int is_checkpoint, LBM *lbm, Net *net);
+void lbmSetInitialConditionsWithCheckpoint (LBM *lbm, Net *net);
+
+void rtInit (Net *net, Vis *vis);
+void rtRayTracing (void (*AbsorptionCoefficients) (float flow_field_data, float t1, float t2,
+						   float cutoff, float *r, float *g, float *b),
+		   Net *net, Vis *vis);
+void rtEnd (Vis *vis);
+
+void slInit (Net *net, Vis *vis);
+void slStreamlines (Net *net, Vis *vis);
+void slEnd (Vis *vis);
 
 
+void visProject (float px1, float py1, float pz1, float *px2, float *py2, float *pz2);
+void visWritePixel (float r, float g, float b, float t, int i, int j, Vis *vis);
+void visRotate (float sin_1, float cos_1,
+		float sin_2, float cos_2,
+		float  x1, float  y1, float  z1,
+		float *x2, float *y2, float *z2);
+void visProjection (float ortho_x, float ortho_y,
+		    int pixels_x, int pixels_y,
+		    float ctr_x, float ctr_y, float ctr_z,
+		    float rad,
+		    float longitude, float latitude,
+		    float dist,
+		    float zoom);
+void visInit (char *image_file_name, Net *net, Vis *vis);
+void visRenderA (void (*rtAbsorptionCoefficients) (float flow_field_data, float t1, float t2,
+						 float cutoff, float *r, float *g, float *b),
+		 void ColourPalette (float vel_m, float *r, float *g, float *b),
+		 Net *net, Vis *vis);
+void visRenderB (Net *net, Vis *vis);
 
-// some simple functions
+#ifdef STEER
+void visReadParameters (char *parameters_file_name, Net *net, Vis *vis, SteerParams *steer);
+#else
+void visReadParameters (char *parameters_file_name, Net *net, Vis *vis);
+#endif
 
-int min (int a, int b)
-{
-  if (a < b)
-    {
-      return a;
-    }
-  else
-    {
-      return b;
-    }
-}
+#ifdef STEER
+void visUpdateParameters (Vis *vis, SteerParams *steer);
+#endif
 
-
-int max (int a, int b)
-{
-  if (a > b)
-    {
-      return a;
-    }
-  else
-    {
-      return b;
-    }
-}
-
-
-double myClock ()
-{
-  return (double)clock () / (double)CLOCKS_PER_SEC;
-}
-
+void visEnd (Net *net, Vis *vis);
 
 #endif                  // __config_h__
