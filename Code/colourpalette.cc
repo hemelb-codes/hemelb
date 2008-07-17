92e6f209314a667a3cca3c6144b8616a853b78c5
Steven Manos <s.manos@ucl.ac.uk>
2008-07-17 18:11:38 +0100

    code refactoring

---
+++ Code/colourpalette.cc
@@ -0,0 +1,10 @@
+#include <math.h>
+#include "colourpalette.h"
+
+void ColourPalette (float value, float col[])
+{
+  col[0] = fminf(1.F, value);
+  col[1] = 0.;
+  col[2] = fmaxf(0.F, 1.F - value);
+}
+
---
+++ Code/colourpalette.h
@@ -0,0 +1,1 @@
+void ColourPalette (float value, float col[]);
