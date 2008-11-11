905ad35a2cc180d782af701fb794639c919714ab
Robert Haines <rhaines@manchester.ac.uk>
2008-11-11 15:35:39 +0000

    VizClient updated to draw hemelb frames via glDrawPixels rather than as separate points. The way in which the frame data is stored has changed as a result and the format of the frame data from hemelb has also changed.
    Files changed: DirectBiConnection.java, VizFrameData.java and VizGui.java
    
    --HG--
    branch : clients

--- Tools/visclients/src/uk/ac/ucl/chem/ccs/vizclient/VizFrameData.java
+++ Tools/visclients/src/uk/ac/ucl/chem/ccs/vizclient/VizFrameData.java
@@ -3,32 +3,27 @@
  * Project: VIZ-GUI 
  * File: VizFrameData.java
  * 
- * @author stefan
+ * @author Stefan Zasada
+ * @author Robert Haines
  * 
  * TODO: 
  */
 package uk.ac.ucl.chem.ccs.vizclient;
+
+import java.nio.IntBuffer;
 
 public class VizFrameData {
 
 	//private long frameNo = 0;
 	//private long realFrameNo = 0;
 	private int bufferSize = 0;
-	private int length = 0;
 	//private double dataRate = 0.f;
 	//private double framePerSec = 0.f;
-	private int r[][];
-	private int g[][];
-	private int b[][];
-	
-	private int x[];
-	private int y[];
-	
 	
     private double vis_pressure_min;
     private double vis_pressure_max;
     private double vis_velocity_min;
-   private  double vis_velocity_max;
+    private double vis_velocity_max;
     private double vis_stress_min;
     private double vis_stress_max;
     private int vis_time_step;
@@ -39,20 +34,42 @@ public class VizFrameData {
     private double vis_mouse_pressure;
     private double vis_stess_pressure;
 	
-
+    private int[][] pixels;
+    private int width;
+    private int height;
+    
 	/**
 	 * @param length
 	 */
-	public VizFrameData(int length) {
-		this.length = length;
-		r = new int[length][4];
-		 g = new int[length][4];
-		 b = new int[length][4];
-		 x = new int[length];
-		 y = new int[length];
+    public VizFrameData(int width, int height) {
+		this.width = width;
+		this.height = height;
+		 
+		pixels = new int[4][width * height];
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < (width * height); j ++) {
+				pixels[i][j] = (255 << 24) | (255 << 16) | (255 << 8);
+			}
+		}
 	}
 	
+	public int getWidth() {
+		return width;
+	}
 
+	public int getHeight() {
+		return height;
+	}
+
+	public void setPixel(int index, int view, int red, int green, int blue) {
+		int pixel = (red << 24) | (green << 16) | (blue << 8);
+		pixels[view][index] = pixel;
+	}
+	
+	public IntBuffer getBuffer(int view) {
+		return IntBuffer.wrap(pixels[view]);
+	}
+	
 //	public double getDataRate() {
 //		return dataRate;
 //	}
@@ -245,70 +262,4 @@ public class VizFrameData {
 //	public void setFrameNo(long frame_no) {
 //		this.frameNo = frame_no;
 //	}
-	/**
-	 * @return Returns the b.
-	 */
-	public int getB(int i, int view) {
-		return b[i][view];
-	}
-	/**
-	 * @param b The b to set.
-	 */
-	public void setB(int i, int view, int b) {
-		this.b[i][view] = b;
-	}
-	/**
-	 * @return Returns the g.
-	 */
-	public int getG(int i, int view) {
-		return g[i][view];
-	}
-	/**
-	 * @param g The g to set.
-	 */
-	public void setG(int i, int view, int g) {
-		this.g[i][view] = g;
-	}
-	/**
-	 * @return Returns the r.
-	 */
-	public int getR(int i, int view) {
-		return r[i][view];
-	}
-	/**
-	 * @param r The r to set.
-	 */
-	public void setR(int i, int view, int r) {
-		this.r[i][view] = r;
-	}
-	/**
-	 * @return Returns the x.
-	 */
-	public int getX(int i) {
-		return x[i];
-	}
-	/**
-	 * @param x The x to set.
-	 */
-	public void setX(int i, int x) {
-		this.x[i] = x;
-	}
-	/**
-	 * @return Returns the y.
-	 */
-	public int getY(int i) {
-		return y[i];
-	}
-	/**
-	 * @param y The y to set.
-	 */
-	public void setY(int i, int y) {
-		this.y[i] = y;
-	}
-	/**
-	 * @return Returns the length.
-	 */
-	public int getLength() {
-		return length;
-	}
 }
