db85cc0fc7d75b110fb18ac91c044bc356b9def9
Marco Mazzeo <md.mazzeo@libero.it>
2009-04-06 17:33:15 +0100

    it has been changed because of the new requirements for benchmarking purposes.

--- Code/usage.cc
+++ Code/usage.cc
@@ -2,12 +2,13 @@
 #include "usage.h"
 
 void usage(char *progname) {
-  printf("Usage: %s path of the input files and minutes for benchmarking\n", progname);
-  printf("if one wants to do a benchmark or\n");
+  printf("Usage: %s path of the input files\n", progname);
+  printf("number of time steps per cycle, voxel size in metres and\n");
+  printf("minutes for benchmarking if one wants to do a benchmark or\n");
   printf("number of pulsaticle cycles, time steps per cycle, \n");
   printf("voxel size in metres, #snapshots and #images otherwise.\n");
   printf("The following files must be present in the path specified:\n");
-  printf("config.dat, pars.asc rt_pars.asc\n");
+  printf("config.dat, pars.asc and rt_pars.asc\n");
 
 }
 
