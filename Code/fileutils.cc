c70819a538ed80826693f08b7a88dc59504833fa
Unknown <hywel@planck.chem.ucl.ac.uk>
2010-06-17 14:01:57 +0100

    More small refactorings, trying to neaten things up and encapsulate functions.
    Nothing broken (i.e. tests are passed).

--- Code/fileutils.cc
+++ Code/fileutils.cc
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/dir.h>
+#include <sys/stat.h>
 
 #include "fileutils.h"
 
@@ -18,5 +23,48 @@ void FileUtils::check_file(const char * filename) {
                 fprintf(stderr,"Cannot open file %s\nExiting.\n", filename);
                 exit(0);
         }
+}
+
+// Function to select directory contents that are not "." or ".."
+// The hack is necessary because of different versions of 'scandir'
+// used by different compilers.
+#ifdef DARWIN
+int FileUtils::SelectOnlyContents (struct direct *entry)
+#else
+int FileUtils::SelectOnlyContents (const struct direct *entry)
+#endif
+{
+  if ((strcmp(entry->d_name, ".") == 0) || (strcmp(entry->d_name, "..") == 0))
+    {
+      return 0;
+    }
+  else
+    {
+      return 1;
+    }
+}
+
+// Delete all files within a directory.
+int FileUtils::DeleteDirContents (char *pathname)
+{
+  struct direct **files;
+  
+  int file_count = scandir(pathname, &files, SelectOnlyContents, alphasort);
+  
+  char filename[1024];
+  
+  for (int i = 0; i < file_count; i++)
+    {
+      snprintf (filename, 1024, "%s/%s", pathname, files[i]->d_name);	
+      unlink (filename);
+    }
+  return 0;
+}
+
+// Function to create the directory of given path, which user group and anyone
+// can read write and execute.
+void FileUtils::MakeDirAllRXW(char* dirPath)
+{
+  mkdir(dirPath, 0777);
 }
 
--- Code/fileutils.h
+++ Code/fileutils.h
@@ -4,7 +4,23 @@ class FileUtils
     // Returns true if the file with the given name exists for reading, false otherwise.
     static bool file_exists(const char * filename);
 
+    // Function to select directory contents that are not "." or ".."
+    // The hack is necessary because of different versions of 'scandir'
+    // used by different compilers.
+    #ifdef DARWIN
+      static int SelectOnlyContents (struct direct *entry);
+    #else
+      static int SelectOnlyContents (const struct direct *entry);
+    #endif
+
   public:
     // Exits if the named file doesn't exist or can't be opened for reading.
     static void check_file(const char * filename);
+
+    // Delete all files within a directory.
+    static int DeleteDirContents (char *pathname);
+
+    // Function to create the directory of given path, which user group and anyone
+    // can read write and execute.
+    static void MakeDirAllRXW(char* dirPath);
 };
