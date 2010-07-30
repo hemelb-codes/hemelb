ab8ec70e6d24fc1882c6399d49308f105d8a105f
Rupert Nash <rupert.nash@ed.ac.uk>
2010-07-30 17:11:19 +0100

    Added include guards for xdr* family of files and tidied a little. Moved the old vis client out of the main Code/ directory (probably can delet it as superceeded by Java visclient
    Removed config.h and fixed the includes.

--- Code/xdrReader.cc
+++ Code/xdrReader.cc
@@ -1,30 +1,25 @@
 #include "xdrReader.h"
 
 // Constructor to create an Xdr object based on a file.
-XdrReader::XdrReader(FILE* xdrFile)
-{
+XdrReader::XdrReader(FILE* xdrFile) {
   xdrstdio_create (&myXdr, xdrFile, XDR_DECODE);
 }
 
 // Functions to read out the next bit of the file as a certain type.
-void XdrReader::readDouble(double& outDouble)
-{
+void XdrReader::readDouble(double& outDouble) {
   xdr_double(&myXdr, &outDouble);
 }
 
-void XdrReader::readInt(int& outInt)
-{
+void XdrReader::readInt(int& outInt) {
   xdr_int(&myXdr, &outInt);
 }
 
-void XdrReader::readUnsignedInt(unsigned int& outUInt)
-{
+void XdrReader::readUnsignedInt(unsigned int& outUInt) {
   xdr_u_int (&myXdr, &outUInt);
 }
 
 // Destructor to get rid of any resources used by the Xdr object. This class doesn't create
 // the file object, so it doesn't free it either.
-XdrReader::~XdrReader()
-{
+XdrReader::~XdrReader() {
   xdr_destroy (&myXdr);
 }
--- Code/xdrWriter.h
+++ Code/xdrWriter.h
@@ -5,27 +5,25 @@
 #include <rpc/xdr.h>
 
 #include "rt.h"
-#include "xdrWriter.h"
 
-class XdrWriter
-{
-  protected:
-    // Constructor and member variable. This class should never be instantiated directly, so the 
-    // constructor is only available to subclasses.
-    XDR myXdr;
-    XdrWriter();
-
-  public:
-    // Functions to write basic types to the Xdr object.
-    void writePixel (ColPixel *col_pixel_p, void (*ColourPalette) (float value, float col[]));
-    void writeInt(int* intToWrite);
-    void writeDouble(double* doubleToWrite);
-    void writeShort(short* shortToWrite);
-    void writeFloat(float* floatToWrite);
-    void writeUnsignedInt(unsigned int* unsignedIntToWrite);    
-
-    // Function to get the current position of writing in the stream.
-    unsigned int getCurrentStreamPosition();
+class XdrWriter {
+ protected:
+  // Constructor and member variable. This class should never be instantiated directly, so the 
+  // constructor is only available to subclasses.
+  XDR myXdr;
+  XdrWriter();
+  
+ public:
+  // Functions to write basic types to the Xdr object.
+  void writePixel (ColPixel *col_pixel_p, void (*ColourPalette) (float value, float col[]));
+  void writeInt(int* intToWrite);
+  void writeDouble(double* doubleToWrite);
+  void writeShort(short* shortToWrite);
+  void writeFloat(float* floatToWrite);
+  void writeUnsignedInt(unsigned int* unsignedIntToWrite);    
+  
+  // Function to get the current position of writing in the stream.
+  unsigned int getCurrentStreamPosition();
 };
 
 #endif //__xdrWriter_h_
