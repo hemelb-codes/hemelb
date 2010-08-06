eeba2fc5f29bedc65e7cce6e25cc9877f993836e
Rupert Nash <rupert.nash@ed.ac.uk>
2010-08-06 14:53:47 +0100

    FileUtils now in util:: namespace
    --HG--
    branch : 2010-refactor

--- Code/fileutils.h
+++ Code/fileutils.h
@@ -6,31 +6,24 @@
 // Define a suitable type for the system we're on
 // (scandir has slightly different definitions on different
 // architectures)
+namespace util {
 #ifdef DARWIN
-typedef struct direct direct_t;
+  typedef struct direct direct_t;
 #else
-typedef const struct direct direct_t;
+  typedef const struct direct direct_t;
 #endif // DARWIN
-
-class FileUtils
-{
-  private:
-    // Returns true if the file with the given name exists for reading, false otherwise.
-    static bool file_exists(const char * filename);
-
-    // Function to select directory contents that are not "." or ".."
-    static int SelectOnlyContents (direct_t *entry);
-    
-  public:
-    // Exits if the named file doesn't exist or can't be opened for reading.
-    static void check_file(const char * filename);
-
-    // Delete all files within a directory.
-    static int DeleteDirContents (char *pathname);
-
-    // Function to create the directory of given path, which user group and anyone
-    // can read write and execute.
-    static void MakeDirAllRXW(char* dirPath);
-};
+  
+  // Exits if the named file doesn't exist or can't be opened for
+  // reading.
+  void check_file(const char * filename);
+  
+  // Delete all files within a directory.
+  int DeleteDirContents (char *pathname);
+  
+  // Function to create the directory of given path, which user group
+  // and anyone can read write and execute.
+  void MakeDirAllRXW(char* dirPath);
+  
+}
 
 #endif // __fileutils_h_
