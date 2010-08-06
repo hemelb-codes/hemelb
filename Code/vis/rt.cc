7e32be6f91da502109e224a6bd495fdf3bf6df8c
Rupert Nash <rupert.nash@ed.ac.uk>
2010-08-06 12:17:43 +0100

    Merged the PixelI/PixelJ/RT/STREAKLINE/GLYPH macro-bitfiddling horror into a bitfield struct + methods
    --HG--
    branch : 2010-refactor

--- Code/vis/rt.cc
+++ Code/vis/rt.cc
@@ -126,104 +126,110 @@ namespace vis {
 
   void visMergePixels (ColPixel *col_pixel1, ColPixel *col_pixel2)
   {
-    // merge raytracing data
-    if ((col_pixel1->i & RT) && (col_pixel2->i & RT))
-      {
-	col_pixel2->vel_r += col_pixel1->vel_r;
-	col_pixel2->vel_g += col_pixel1->vel_g;
-	col_pixel2->vel_b += col_pixel1->vel_b;
+    // Merge raytracing data
+    
+    if (col_pixel1->i.isRt && col_pixel2->i.isRt) {
+      // Both are ray-tracing
+      col_pixel2->vel_r += col_pixel1->vel_r;
+      col_pixel2->vel_g += col_pixel1->vel_g;
+      col_pixel2->vel_b += col_pixel1->vel_b;
       
-	if (lbm_stress_type != SHEAR_STRESS)
-	  {
-	    col_pixel2->stress_r += col_pixel1->stress_r;
-	    col_pixel2->stress_g += col_pixel1->stress_g;
-	    col_pixel2->stress_b += col_pixel1->stress_b;
-	  }
-	col_pixel2->dt += col_pixel1->dt;
-      
-	if (col_pixel1->t < col_pixel2->t)
-	  {
-	    col_pixel2->t       = col_pixel1->t;
-	    col_pixel2->density = col_pixel1->density;
-	    col_pixel2->stress  = col_pixel1->stress;
-	  }
+      if (lbm_stress_type != SHEAR_STRESS) {
+	col_pixel2->stress_r += col_pixel1->stress_r;
+	col_pixel2->stress_g += col_pixel1->stress_g;
+	col_pixel2->stress_b += col_pixel1->stress_b;
       }
-    else if ((col_pixel1->i & RT) && !(col_pixel2->i & RT))
-      {
-	col_pixel2->vel_r = col_pixel1->vel_r;
-	col_pixel2->vel_g = col_pixel1->vel_g;
-	col_pixel2->vel_b = col_pixel1->vel_b;
       
-	if (lbm_stress_type != SHEAR_STRESS)
-	  {
-	    col_pixel2->stress_r = col_pixel1->stress_r;
-	    col_pixel2->stress_g = col_pixel1->stress_g;
-	    col_pixel2->stress_b = col_pixel1->stress_b;
-	  }
+      col_pixel2->dt += col_pixel1->dt;
+      
+      if (col_pixel1->t < col_pixel2->t) {
 	col_pixel2->t       = col_pixel1->t;
-	col_pixel2->dt      = col_pixel1->dt;
 	col_pixel2->density = col_pixel1->density;
 	col_pixel2->stress  = col_pixel1->stress;
+      }
       
-	col_pixel2->i |= RT;
+    } else if (col_pixel1->i.isRt && !col_pixel2->i.isRt) {
+      // Only 1 is ray-tracing
+      col_pixel2->vel_r = col_pixel1->vel_r;
+      col_pixel2->vel_g = col_pixel1->vel_g;
+      col_pixel2->vel_b = col_pixel1->vel_b;
+      
+      if (lbm_stress_type != SHEAR_STRESS) {
+	col_pixel2->stress_r = col_pixel1->stress_r;
+	col_pixel2->stress_g = col_pixel1->stress_g;
+	col_pixel2->stress_b = col_pixel1->stress_b;
       }
-    if (lbm_stress_type != SHEAR_STRESS && (mode == 0 || mode == 1))
-      {
-	// merge glyph data
-	if (col_pixel1->i & GLYPH)
-	  {
-	    col_pixel2->i |= GLYPH;
-	  }
+      
+      col_pixel2->t       = col_pixel1->t;
+      col_pixel2->dt      = col_pixel1->dt;
+      col_pixel2->density = col_pixel1->density;
+      col_pixel2->stress  = col_pixel1->stress;
+      
+      col_pixel2->i.isRt = true;
+    }
+    // Done merging ray-tracing
+    
+    // Now merge glyph data
+    if (lbm_stress_type != SHEAR_STRESS && (mode == 0 || mode == 1)) {
+      
+      if (col_pixel1->i.isGlyph) {
+	col_pixel2->i.isGlyph = true;
       }
-    else
-      {
+      
+    } else {
+      
 #ifndef NO_STREAKLINES
-	// merge streakline data
-	if ((col_pixel1->i & STREAKLINE) && (col_pixel2->i & STREAKLINE))
-	  {
-	    if (col_pixel1->particle_z < col_pixel2->particle_z)
-	      {
-		col_pixel2->particle_z        = col_pixel1->particle_z;
-		col_pixel2->particle_vel      = col_pixel1->particle_vel;
-		col_pixel2->particle_inlet_id = col_pixel1->particle_inlet_id;
-	      }
-	  }
-	else if ((col_pixel1->i & STREAKLINE) && !(col_pixel2->i & STREAKLINE))
-	  {
-	    col_pixel2->particle_z        = col_pixel1->particle_z;
-	    col_pixel2->particle_vel      = col_pixel1->particle_vel;
-	    col_pixel2->particle_inlet_id = col_pixel1->particle_inlet_id;
-	  
-	    col_pixel2->i |= STREAKLINE;
-	  }
-#endif
+      // merge streakline data
+      if (col_pixel1->i.isStreakline &&
+	  col_pixel2->i.isStreakline) {
+	
+	if (col_pixel1->particle_z < col_pixel2->particle_z) {
+	  col_pixel2->particle_z        = col_pixel1->particle_z;
+	  col_pixel2->particle_vel      = col_pixel1->particle_vel;
+	  col_pixel2->particle_inlet_id = col_pixel1->particle_inlet_id;
+	}
+	
+      }	else if (col_pixel1->i.isStreakline &&
+		 !col_pixel2->i.isStreakline) {
+	col_pixel2->particle_z        = col_pixel1->particle_z;
+	col_pixel2->particle_vel      = col_pixel1->particle_vel;
+	col_pixel2->particle_inlet_id = col_pixel1->particle_inlet_id;
+	
+	col_pixel2->i.isStreakline = true;
       }
+#endif
+    }
+    
   }
 
 
-  void visWritePixel (ColPixel *col_pixel_p)
-  {
+  void visWritePixel (ColPixel *col_pixel_p) {
     int *col_pixel_id_p, i, j;
-  
-  
-    i = PixelI(col_pixel_p->i);
-    j = PixelJ(col_pixel_p->i);
-  
-    if (*(col_pixel_id_p = &col_pixel_id[ i*screen.pixels_y+j ]) != -1)
-      {
-	visMergePixels (col_pixel_p, &col_pixel_send[ *col_pixel_id_p ]);
-      }
-    else
-      {
-	if (col_pixels >= COLOURED_PIXELS_MAX)
-	  {
-	    return;
-	  }
-	*col_pixel_id_p = col_pixels;
+    
+    
+    i = col_pixel_p->i.i;
+    j = col_pixel_p->i.j;
+    
+    col_pixel_id_p = &col_pixel_id[ i*screen.pixels_y+j ];
+    
+    if (*col_pixel_id_p != -1) {
+      visMergePixels(col_pixel_p,
+		     &col_pixel_send[ *col_pixel_id_p ]);
       
-	memcpy (&col_pixel_send[ col_pixels ], col_pixel_p, sizeof(ColPixel));
-	++col_pixels;
+    } else { // col_pixel_id_p == -1
+      
+      if (col_pixels >= COLOURED_PIXELS_MAX) {
+	return;
       }
+      
+      *col_pixel_id_p = col_pixels;
+      
+      memcpy (&col_pixel_send[ col_pixels ],
+	      col_pixel_p,
+	      sizeof(ColPixel));
+      ++col_pixels;
+    }
+    
   }
 
 
@@ -236,8 +242,7 @@ namespace vis {
     blue  = (unsigned char)util::enforceBounds(rawBlue, 0, 255);
   }
 
-  void visRenderLine (float p1[], float p2[])
-  {
+  void visRenderLine (float p1[], float p2[]) {
     int pixels_x, pixels_y;
     int x, y;
     int x1, y1;
@@ -249,121 +254,113 @@ namespace vis {
   
     ColPixel col_pixel;
   
-  
     pixels_x = screen.pixels_x;
     pixels_y = screen.pixels_y;
   
-    x1 = (int)p1[0];
-    y1 = (int)p1[1];
+    x1 = int(p1[0]);
+    y1 = int(p1[1]);
   
-    x2 = (int)p2[0];
-    y2 = (int)p2[1];
+    x2 = int(p2[0]);
+    y2 = int(p2[1]);
   
-    if (x2 < x1)
-      {
-	x = x1;
-	y = y1;
-	x1 = x2;
-	y1 = y2;
-	x2 = x;
-	y2 = y;
-      }
+    if (x2 < x1) {
+      x = x1;
+      y = y1;
+      x1 = x2;
+      y1 = y2;
+      x2 = x;
+      y2 = y;
+    }
+    
     x = x1;
     y = y1;
-  
-    if (y1 < y2)
-      {
-	dy = y2 - y1;
-	m = 1;
-      }
-    else
-      {
-	dy = y1 - y2;
-	m = -1;
-      }
+    
+    if (y1 < y2) {
+      dy = y2 - y1;
+      m = 1;
+      
+    } else {
+      dy = y1 - y2;
+      m = -1;
+    }
+    
     dx = x2 - x1;
     
-    if (dx > dy)
-      {
-	incE = dy;
-	d = dy - dx;
-	incNE = d;
+    if (dx > dy) {
+      incE = dy;
+      d = dy - dx;
+      incNE = d;
       
-	while (x <= x2)
-	  {
-	    if (!(x < 0 || x >= pixels_x ||
-		  y < 0 || y >= pixels_y))
-	      {
-		col_pixel.i = PixelId(x, y) | GLYPH;
-	      
-		visWritePixel (&col_pixel);
-	      }
-	    if (d < 0)
-	      {
-		d += incE;
-	      }
-	    else
-	      {
-		d += incNE;
-		y += m;
-	      }
-	    ++x;
-	  }
-      }
-    else if (y1 < y2)
-      {
-	incE = dx;
-	d = dx - dy;
-	incNE = d;
+      while (x <= x2) {
+	if (!(x < 0 || x >= pixels_x ||
+	      y < 0 || y >= pixels_y)) {
+	  
+	  col_pixel.i = PixelId(x, y);
+	  col_pixel.i.isGlyph = true;
+	  
+	  visWritePixel (&col_pixel);
+	}
+	
+	if (d < 0) {
+	  d += incE;
+	} else {
+	  d += incNE;
+	  y += m;
+	}
+	++x;
+	
+      } // end while
       
-	while (y <= y2)
-	  {
-	    if (!(x < 0 || x >= pixels_x ||
-		  y < 0 || y >= pixels_y))
-	      {
-		col_pixel.i = PixelId(x, y) | GLYPH;
-	      
-		visWritePixel (&col_pixel);
-	      }
-	    if (d < 0)
-	      {
-		d += incE;
-	      }
-	    else
-	      {
-		d += incNE;
-		++x;
-	      }
-	    ++y;
-	  }
-      }
-    else
-      {
-	incE = dx;
-	d = dx - dy;
-	incNE = d;
+    } else if (y1 < y2) {
+      incE = dx;
+      d = dx - dy;
+      incNE = d;
       
-	while (y >= y2)
-	  {
-	    if (!(x < 0 || x >= pixels_x ||
-		  y < 0 || y >= pixels_y))
-	      {
-		col_pixel.i = PixelId(x, y) | GLYPH;
-	      
-		visWritePixel (&col_pixel);
-	      }
-	    if (d < 0)
-	      {
-		d += incE;
-	      }
-	    else
-	      {
-		d += incNE;
-		++x;
-	      }
-	    --y;
-	  }
-      }
+      while (y <= y2) {
+	if (!(x < 0 || x >= pixels_x ||
+	      y < 0 || y >= pixels_y)) {
+	  
+	  col_pixel.i = PixelId(x, y);
+	  col_pixel.i.isGlyph = true;
+	  
+	  visWritePixel (&col_pixel);
+	}
+	
+	if (d < 0) {
+	  d += incE;
+	} else {
+	  d += incNE;
+	  ++x;
+	}
+	++y;
+	
+      } // while
+      
+    } else {
+      incE = dx;
+      d = dx - dy;
+      incNE = d;
+      
+      while (y >= y2) {
+	if (!(x < 0 || x >= pixels_x ||
+	      y < 0 || y >= pixels_y)) {
+	  
+	  col_pixel.i = PixelId(x, y);
+	  col_pixel.i.isGlyph = true;
+	  
+	  visWritePixel (&col_pixel);
+	}
+	
+	if (d < 0) {
+	  d += incE;
+	} else {
+	  d += incNE;
+	  ++x;
+	}
+	--y;
+	
+      } // while
+    }
   }
 
 
@@ -577,125 +574,120 @@ namespace vis {
     int comm_inc, send_id, recv_id;
     int i, j;
     int m, n;
-  
+    
     ColPixel *col_pixel1, *col_pixel2;
-  
-  
+    
 #ifndef NEW_COMPOSITING
-    memcpy (col_pixel_recv[recv_buffer_id], col_pixel_send, col_pixels * sizeof(ColPixel));
+    memcpy (col_pixel_recv[recv_buffer_id],
+	    col_pixel_send,
+	    col_pixels * sizeof(ColPixel));
 #else
-    if (net->id != 0)
-      {
-	memcpy (col_pixel_recv[recv_buffer_id], col_pixel_send, col_pixels * sizeof(ColPixel));
-      }
+    if (net->id != 0) {
+      memcpy (col_pixel_recv[recv_buffer_id],
+	      col_pixel_send,
+	      col_pixels * sizeof(ColPixel));
+    }
 #endif
+    
     comm_inc = 1;
     m = 1;
-  
-    while (m < net->procs)
-      {
-	m <<= 1;
+    
+    while (m < net->procs) {
+      m <<= 1;
 #ifndef NEW_COMPOSITING
-	int start_id = 0;
+      int start_id = 0;
 #else
-	int start_id = 1;
+      int start_id = 1;
 #endif
-	for (recv_id = start_id; recv_id < net->procs;)
-	  {
-	    send_id = recv_id + comm_inc;
+      for (recv_id = start_id; recv_id < net->procs;) {
+	send_id = recv_id + comm_inc;
+	
+	if (net->id != recv_id && net->id != send_id) {
+	  recv_id += comm_inc << 1;
+	  continue;
+	}
+	
+	if (send_id >= net->procs || recv_id == send_id) {
+	  recv_id += comm_inc << 1;
+	  continue;
+	}
+	
+	if (net->id == send_id) {
+#ifndef NOMPI
+	  MPI_Send(&col_pixels, 1,
+		   MPI_INT, recv_id, 20, MPI_COMM_WORLD);
+#endif
+	  if (col_pixels > 0) {
+#ifndef NOMPI
+	    MPI_Send (col_pixel_send, col_pixels, MPI_col_pixel_type,
+		      recv_id, 20, MPI_COMM_WORLD);
+#endif
+	  }
 	  
-	    if (net->id != recv_id && net->id != send_id)
-	      {
-		recv_id += comm_inc << 1;
-		continue;
-	      }
-	    if (send_id >= net->procs || recv_id == send_id)
-	      {
-		recv_id += comm_inc << 1;
-		continue;
-	      }
-	    if (net->id == send_id)
-	      {
+	} else {
 #ifndef NOMPI
-		MPI_Send (&col_pixels, 1, MPI_INT, recv_id, 20, MPI_COMM_WORLD);
-#endif
-		if (col_pixels > 0)
-		  {
-#ifndef NOMPI
-		    MPI_Send (col_pixel_send, col_pixels, MPI_col_pixel_type,
-			      recv_id, 20, MPI_COMM_WORLD);
-#endif
-		  }
-	      }
-	    else
-	      {
-#ifndef NOMPI
-		MPI_Recv (&col_pixels_temp, 1, MPI_INT, send_id, 20, MPI_COMM_WORLD, net->status);
-	      
-		if (col_pixels_temp > 0)
-		  {
-		    MPI_Recv (col_pixel_send, col_pixels_temp, MPI_col_pixel_type,
-			      send_id, 20, MPI_COMM_WORLD, net->status);
-		  }
+	  MPI_Recv (&col_pixels_temp, 1, MPI_INT, send_id, 20, MPI_COMM_WORLD, net->status);
+	  
+	  if (col_pixels_temp > 0) {
+	    MPI_Recv (col_pixel_send, col_pixels_temp, MPI_col_pixel_type,
+		      send_id, 20, MPI_COMM_WORLD, net->status);
+	  }
 #else
-		col_pixels_temp = 0;
+	  col_pixels_temp = 0;
 #endif
-		for (n = 0; n < col_pixels_temp; n++)
-		  {
-		    col_pixel1 = &col_pixel_send[ n ];
-		    i = PixelI(col_pixel1->i);
-		    j = PixelJ(col_pixel1->i);
-		  
-		    if (*(col_pixel_id_p = &col_pixel_id[ i * screen.pixels_y + j ]) == -1)
-		      {
-			col_pixel2 = &col_pixel_recv[recv_buffer_id][ *col_pixel_id_p = col_pixels ];
-		      
-			memcpy (col_pixel2, col_pixel1, sizeof(ColPixel));
-			++col_pixels;
-		      }
-		    else
-		      {
-			col_pixel2 = &col_pixel_recv[recv_buffer_id][ *col_pixel_id_p ];
-		      
-			visMergePixels (col_pixel1, col_pixel2);
-		      }
-		  }
-	      }
-	    if (m < net->procs && net->id == recv_id)
-	      {
-		memcpy (col_pixel_send, col_pixel_recv[recv_buffer_id], col_pixels * sizeof(ColPixel));
-	      }
-	    recv_id += comm_inc << 1;
+	  for (n = 0; n < col_pixels_temp; n++) {
+	    col_pixel1 = &col_pixel_send[ n ];
+	    i = col_pixel1->i.i;
+	    j = col_pixel1->i.j;
+	    
+	    if (*(col_pixel_id_p = &col_pixel_id[ i * screen.pixels_y + j ]) == -1) {
+	      col_pixel2 = &col_pixel_recv[recv_buffer_id][ *col_pixel_id_p = col_pixels ];
+	      
+	      memcpy (col_pixel2, col_pixel1, sizeof(ColPixel));
+	      ++col_pixels;
+	      
+	    } else {
+	      col_pixel2 = &col_pixel_recv[recv_buffer_id][ *col_pixel_id_p ];
+	      
+	      visMergePixels (col_pixel1, col_pixel2);
+	    }
+	    
 	  }
-	comm_inc <<= 1;
+	}
+	if (m < net->procs && net->id == recv_id) {
+	  memcpy(col_pixel_send,
+		 col_pixel_recv[recv_buffer_id],
+		 col_pixels * sizeof(ColPixel));
+	}
+	
+	recv_id += comm_inc << 1;
       }
+      comm_inc <<= 1;
+    }
 #ifdef NEW_COMPOSITING
-    if (net->id == 1)
-      {
+    if (net->id == 1) {
 #ifndef NOMPI
-	MPI_Send (&col_pixels, 1, MPI_INT, 0, 20, MPI_COMM_WORLD);
+      MPI_Send (&col_pixels, 1, MPI_INT, 0, 20, MPI_COMM_WORLD);
 #endif
-	if (col_pixels > 0)
-	  {
+      if (col_pixels > 0) {
 #ifndef NOMPI
-	    MPI_Send (col_pixel_recv[recv_buffer_id], col_pixels, MPI_col_pixel_type,
-		      0, 20, MPI_COMM_WORLD);
+	MPI_Send (col_pixel_recv[recv_buffer_id], col_pixels, MPI_col_pixel_type,
+		  0, 20, MPI_COMM_WORLD);
 #endif
-	  }
       }
-    else if (net->id == 0)
-      {
-	MPI_Recv (&col_pixels, 1, MPI_INT, 1, 20, MPI_COMM_WORLD, net->status);
       
-	if (col_pixels > 0)
-	  {
-	    MPI_Recv (col_pixel_recv[recv_buffer_id], col_pixels, MPI_col_pixel_type,
-		      1, 20, MPI_COMM_WORLD, net->status);
-	  }
+    } else if (net->id == 0) {
+      MPI_Recv (&col_pixels, 1, MPI_INT, 1, 20, MPI_COMM_WORLD, net->status);
+      
+      if (col_pixels > 0) {
+	MPI_Recv (col_pixel_recv[recv_buffer_id], col_pixels, MPI_col_pixel_type,
+		  1, 20, MPI_COMM_WORLD, net->status);
       }
+      
+    }
 #endif // NEW_COMPOSITING
   }
-
+  
 
   void visRender (int recv_buffer_id, void (*ColourPalette) (float value, float col[]), Net *net)
   {
@@ -731,7 +723,7 @@ namespace vis {
 #endif
     for (int m = 0; m < col_pixels_recv[recv_buffer_id]; m++)
       {
-	col_pixel_id[ (PixelI(col_pixel_send[m].i)) * screen.pixels_y + (PixelJ(col_pixel_send[m].i)) ] = -1;
+	col_pixel_id[ col_pixel_send[m].i.i * screen.pixels_y + col_pixel_send[m].i.j ] = -1;
       }
   }
 
