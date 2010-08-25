9e2c9e53603c6bfad2de934fe54348c1543de216
Rupert Nash <rupert.nash@ed.ac.uk>
2010-08-25 17:52:30 +0100

    Minor fixes in io:: class hierarchy, making dtors virtual

--- Code/io/xdrMemWriter.h
+++ Code/io/xdrMemWriter.h
@@ -7,9 +7,9 @@ namespace heme
 {
   namespace io
   {
-    // Class for writing to an Xdr object in memory. Derives from a base
-    // XdrWriter which implements all the reading functions.
-
+    // Class for writing to an Xdr object in memory. Derives from a
+    // base XdrWriter which implements all the writing functions.
+    
     class XdrMemWriter : public XdrWriter {
     public:
       // Constructor and destructor for the in-memory Xdr writer.
