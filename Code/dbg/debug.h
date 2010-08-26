95003c658c865961a7bc9b30475e65de6479c877
Rupert Nash <rupert.nash@ed.ac.uk>
2010-08-26 13:35:52 +0100

    Added debugging lib

---
+++ Code/dbg/debug.h
@@ -0,0 +1,13 @@
+#ifndef HEMELB_DEBUG_DEBUG_H
+#define HEMELB_DEBUG_DEBUG_H
+
+namespace hemelb
+{
+  namespace dbg
+  {
+    void attach();
+  }
+}
+
+
+#endif // HEMELB_DEBUG_DEBUG_H
---
+++ Code/dbg/none/debug.h
@@ -0,0 +1,6 @@
+#ifndef HEMELB_DBG_NONE_DEBUG_H
+#define HEMELB_DBG_NONE_DEBUG_H
+
+// Any platform specific declarations here
+
+#endif // HEMELB_DBG_NONE_DEBUG_H
