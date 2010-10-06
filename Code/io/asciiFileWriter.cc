6425bab958c47619082c324eb70bfeddaf64f93a
Rupert Nash <rupert.nash@ed.ac.uk>
2010-10-06 12:33:05 +0100

    Fixed naughty "using namespace" bits in io namespace

--- Code/io/asciiFileWriter.cc
+++ Code/io/asciiFileWriter.cc
@@ -2,23 +2,29 @@
 
 #include "io/AsciiFileWriter.h"
 
-using namespace hemelb::io;
+namespace hemelb
+{
+  namespace io
+  {
 
-// Implement a constructor that opens the file and creates the Xdr
-// object to write to it.
-AsciiFileWriter::AsciiFileWriter(char* fileName) {
-  outStream = new std::ofstream(fileName);
-}
+    // Implement a constructor that opens the file and creates the Xdr
+    // object to write to it.
+    AsciiFileWriter::AsciiFileWriter(char* fileName) {
+      outStream = new std::ofstream(fileName);
+    }
 
-// Destructor closes the file and cleans up the stream object.
-AsciiFileWriter::~AsciiFileWriter() {
-  if (outStream != NULL) {
-    // Since we know the member std::ostream* outStream is actually an
-    // instance of std::ofstream, cast it to that so we can close it.
-    std::ofstream* outFileStream = 
-      static_cast<std::ofstream *>(outStream);
+    // Destructor closes the file and cleans up the stream object.
+    AsciiFileWriter::~AsciiFileWriter() {
+      if (outStream != NULL) {
+	// Since we know the member std::ostream* outStream is actually an
+	// instance of std::ofstream, cast it to that so we can close it.
+	std::ofstream* outFileStream = 
+	  static_cast<std::ofstream *>(outStream);
     
-    outFileStream->close();
-    delete outFileStream;
+	outFileStream->close();
+	delete outFileStream;
+      }
+    }
+
   }
 }
--- Code/io/xdrReader.cc
+++ Code/io/xdrReader.cc
@@ -4,28 +4,34 @@
 
 #include "io/XdrReader.h"
 
-using namespace hemelb::io;
+namespace hemelb
+{
+  namespace io
+  {
 
-// Constructor to create an Xdr object based on a file.
-XdrReader::XdrReader(FILE* xdrFile) {
-  xdrstdio_create (&myXdr, xdrFile, XDR_DECODE);
-}
+    // Constructor to create an Xdr object based on a file.
+    XdrReader::XdrReader(FILE* xdrFile) {
+      xdrstdio_create (&myXdr, xdrFile, XDR_DECODE);
+    }
 
-// Functions to read out the next bit of the file as a certain type.
-void XdrReader::readDouble(double& outDouble) {
-  xdr_double(&myXdr, &outDouble);
-}
+    // Functions to read out the next bit of the file as a certain type.
+    void XdrReader::readDouble(double& outDouble) {
+      xdr_double(&myXdr, &outDouble);
+    }
 
-void XdrReader::readInt(int& outInt) {
-  xdr_int(&myXdr, &outInt);
-}
+    void XdrReader::readInt(int& outInt) {
+      xdr_int(&myXdr, &outInt);
+    }
 
-void XdrReader::readUnsignedInt(unsigned int& outUInt) {
-  xdr_u_int (&myXdr, &outUInt);
-}
+    void XdrReader::readUnsignedInt(unsigned int& outUInt) {
+      xdr_u_int (&myXdr, &outUInt);
+    }
 
-// Destructor to get rid of any resources used by the Xdr object. This class doesn't create
-// the file object, so it doesn't free it either.
-XdrReader::~XdrReader() {
-  xdr_destroy (&myXdr);
+    // Destructor to get rid of any resources used by the Xdr object. This class doesn't create
+    // the file object, so it doesn't free it either.
+    XdrReader::~XdrReader() {
+      xdr_destroy (&myXdr);
+    }
+
+  }
 }
