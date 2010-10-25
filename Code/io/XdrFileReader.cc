f7b556a3ed1469fd5541354662288541ab5fdad6
Rupert Nash <rupert.nash@ed.ac.uk>
2010-10-25 15:11:53 +0100

    Class hierarcy for XdrReaders

---
+++ Code/io/XdrFileReader.cc
@@ -0,0 +1,23 @@
+/*
+ * XdrFileReader.cc
+ *
+ *  Created on: Oct 25, 2010
+ *      Author: rupert
+ */
+
+#include "XdrFileReader.h"
+
+namespace hemelb
+{
+
+  namespace io
+  {
+    // Constructor to create an Xdr object based on a file.
+    XdrFileReader::XdrFileReader(FILE* xdrFile) {
+      mXdr = new XDR;
+      xdrstdio_create (mXdr, xdrFile, XDR_DECODE);
+    }
+
+  }
+
+}
--- Code/io/xdrReader.cc
+++ Code/io/xdrReader.cc
@@ -9,28 +9,32 @@ namespace hemelb
   namespace io
   {
 
-    // Constructor to create an Xdr object based on a file.
-    XdrReader::XdrReader(FILE* xdrFile) {
-      xdrstdio_create (&myXdr, xdrFile, XDR_DECODE);
+    XdrReader::XdrReader() :
+      mXdr(NULL)
+    {
     }
 
     // Functions to read out the next bit of the file as a certain type.
-    void XdrReader::readDouble(double& outDouble) {
-      xdr_double(&myXdr, &outDouble);
+    void XdrReader::readDouble(double& outDouble)
+    {
+      xdr_double(mXdr, &outDouble);
     }
 
-    void XdrReader::readInt(int& outInt) {
-      xdr_int(&myXdr, &outInt);
+    void XdrReader::readInt(int& outInt)
+    {
+      xdr_int(mXdr, &outInt);
     }
 
-    void XdrReader::readUnsignedInt(unsigned int& outUInt) {
-      xdr_u_int (&myXdr, &outUInt);
+    void XdrReader::readUnsignedInt(unsigned int& outUInt)
+    {
+      xdr_u_int(mXdr, &outUInt);
     }
 
     // Destructor to get rid of any resources used by the Xdr object. This class doesn't create
     // the file object, so it doesn't free it either.
-    XdrReader::~XdrReader() {
-      xdr_destroy (&myXdr);
+    XdrReader::~XdrReader()
+    {
+      xdr_destroy (mXdr);
     }
 
   }
