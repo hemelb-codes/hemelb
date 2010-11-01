c0c9228a460415d8580fe5bb6dd96d22af791ba8
Hywel Carver <hywelc@gmail.com>
2010-11-01 10:45:48 +0000

    Loading and saving both work, with multiple inputs and outputs. This isn't being used in the actual simulations yet, but will be soon.
    --HG--
    branch : 2010-refactor

--- Code/SimConfig.h
+++ Code/SimConfig.h
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <vector>
 #include "xml/ticpp.h"
-#include "xml/xpath_static.h"
 
 namespace hemelb
 {
@@ -47,14 +46,24 @@ namespace hemelb
 
     private:
       SimConfig();
-      void DoIO(TiXmlNode *iXmlNode, bool iIsLoading);
-      void DoIO(TiXmlAttribute *iXmlNode, bool iIsLoading, float &value);
-      void DoIO(TiXmlAttribute *iXmlNode, bool iIsLoading, std::string &value);
-      void DoIO(TiXmlNode *iXmlNode,
+      void DoIO(TiXmlElement *iXmlNode, bool iIsLoading);
+      void DoIO(TiXmlElement* iXmlNode,
+                std::string iAttributeName,
                 bool iIsLoading,
-                std::vector<InOutLet*> &value);
-      void DoIO(TiXmlNode *iXmlNode, bool iIsLoading, InOutLet *value);
-      void DoIO(TiXmlNode *iXmlNode, bool iIsLoading, Vector &iValue);
+                float &value);
+      void DoIO(TiXmlElement *iXmlNode,
+                std::string iAttributeName,
+                bool iIsLoading,
+                std::string &iValue);
+      void DoIO(TiXmlElement *iXmlNode,
+                bool iIsLoading,
+                std::vector<InOutLet*> &value,
+                std::string iChildNodeName);
+      void DoIO(TiXmlElement *iXmlNode, bool iIsLoading, InOutLet *value);
+      void DoIO(TiXmlElement *iXmlNode, bool iIsLoading, Vector &iValue);
+      TiXmlElement* GetChild(TiXmlElement *iParent,
+                             std::string iChildNodeName,
+                             bool iIsLoading);
   };
 }
 
