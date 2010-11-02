7c455c6d6ef1e1910eb54074a4c11a1f8c8fd6c8
Hywel Carver <hywelc@gmail.com>
2010-11-02 18:23:51 +0000

    Tested the xml I/O and made sure we don't overwrite existing data. Details below. Tested: correctly uses output dir given over command line and correctly balks for .. Tested: Existing output dir is detected. Tested: correct default output directory. Tested: config xml file is copied. Tested: most parameters. Tested: Different formats for the input file.
    --HG--
    branch : 2010-refactor

--- Code/fileutils.h
+++ Code/fileutils.h
@@ -15,18 +15,24 @@ namespace hemelb
 #else // HEMELB_CFG_ON_BSD
     typedef const struct direct direct_t;
 #endif // HEMELB_CFG_ON_BSD
-  
+    // Copy a file from the source path destination path to the destination
+    // path.
+    bool FileCopy(const char* iOriginalPath, const char* iNewPath);
+
     // Exits if the named file doesn't exist or can't be opened for
     // reading.
     void check_file(const char * filename);
-  
+
     // Delete all files within a directory.
-    int DeleteDirContents (char *pathname);
-  
+    int DeleteDirContents(char *pathname);
+
+    // Check whether a directory exists.
+    bool DoesDirectoryExist(const char *pathname);
+
     // Function to create the directory of given path, which user group
     // and anyone can read write and execute.
     void MakeDirAllRXW(const char* dirPath);
-  
+
   }
 }
 #endif // HEMELB_FILEUTILS_H
