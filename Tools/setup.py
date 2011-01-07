8d9a3afea4aa69c4b97068f4c9e5aea25e587f6f
Rupert Nash <rupert.nash@ed.ac.uk>
2011-01-07 11:02:05 +0000

    Added the python setup script originally omitted from import.

---
+++ Tools/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='HemeTools',
+      version='0.2',
+      description='HemeLB tools',
+      author='Rupert Nash',
+      author_email='rupert.nash@ucl.ac.uk',
+      packages=['hemeTools', 'hemeTools.parsers'],
+     )
