858dfef6696b9b9011aca65c425acd694448396f
Rupert Nash <rupert.nash@ed.ac.uk>
2011-01-27 16:17:14 +0000

    Make parmetis build on OSX
    --HG--
    branch : 2010-refactor

--- Code/parmetis/stdheaders.h
+++ Code/parmetis/stdheaders.h
@@ -14,7 +14,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
+/* This include is redundant as malloc etc are in stdlib.h and
+   malloc.h not guaranteed present on POSIX */
+/* #include <malloc.h> */
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
