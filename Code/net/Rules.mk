9ea20aa3c99dbb2b30e124c0420983ad219d0067
Hywel <unknown>
2011-02-02 13:51:32 +0000

    Move Net to its own sub-namespace.

---
+++ Code/net/Rules.mk
@@ -0,0 +1,10 @@
+include $(MK)/header.mk
+
+TARGETS := libHemeLbNet.$(LIBEXT)
+SRCS := net.cc
+
+$(TARGETS)_DEPS := $(subst .cc,.$(OBJEXT), $(SRCS))
+
+INCLUDES_$(d) := $(INCLUDES_$(parent))
+
+include $(MK)/footer.mk
