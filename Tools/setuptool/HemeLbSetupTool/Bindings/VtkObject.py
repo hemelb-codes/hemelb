c961a87c18c87465a51cccd0efe3bf54e1b03224
Rupert Nash <rupert.nash@ed.ac.uk>
2011-02-04 17:41:15 +0000

    More progress towards making the VTK stuf work

---
+++ Tools/setuptool/HemeLbSetupTool/Bindings/VtkObject.py
@@ -0,0 +1,72 @@
+import types
+from vtk import vtkObject
+
+from ..Util.Observer import Change, ChangeTimes
+from .ObjectController import ObjectController
+import pdb
+
+class VtkObjectController(ObjectController):
+    def __init__(self, delegate):
+        """The delegate will typically be the model or another controller.
+        """
+        
+        assert isinstance(delegate, vtkObject)
+        
+        delegate._GetLocalValueForKey = types.MethodType(getattr, delegate, type(delegate))
+        delegate._SetLocalValueForKey = types.MethodType(setattr, delegate, type(delegate))
+        self.delegate = delegate
+        self._values = dict()
+        self._actions = set()
+        self._observedKeys = dict()
+
+        self.delegate.AddObserver('ModifiedEvent', self._HandleModifiedEvent)
+        return
+    
+    def _HandleModifiedEvent(self, obj, evt):
+        for cbsForKey in self._observedKeys.itervalues():
+            for cb in cbsForKey:
+                cb(Change(time=ChangeTimes.AFTER, obj=self))
+                continue
+            continue
+        
+        return
+    
+    def _AddObserverToLocalKey(self, keyPath, callback, options):
+        if hasattr(self, keyPath) or '.' in keyPath:
+            # If its our attribute or a dotted path, add to self
+            Observable._AddObserverToLocalKey(self, keyPath, callback, options)
+        else:
+            # Delegate it
+            try:
+                cbSet = self._observedKeys[keyPath]
+            except KeyError:
+                cbSet = self._observedKeys[keyPath] = set()
+                pass
+            cbSet.add(callback)
+            pass
+        return
+
+    pass
+
+class HasVtkObjectKeys(object):
+    """Mixin for ObjectController subclasses with VtkObject keys.
+    """
+    BindFunctionDispatchTable = ((VtkObjectController, 'BindVtkObject'),)
+    
+    def BindVtkObject(self, top, key, mapper):
+        """Each component of the vector should be appropriately bound.
+        """
+        
+        return
+
+    def DefineVtkObjectKey(self, name):
+        """Typically used in the subclass __init__ method to easily
+        mark a key as being a VtkObject and hence needing a
+        VtkObjectrController to manage it.
+        """
+        setattr(self, name,
+                VtkObjectController(getattr(self.delegate, name))
+                )
+        return
+    
+    pass
