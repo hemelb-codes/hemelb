dd6329f5e568ce379ffb4fa18e881770cfbe09cc
Rupert Nash <rupert.nash@ed.ac.uk>
2011-02-14 14:04:53 +0000

    SetupTool: More progress for making the VTK iolets work

--- Tools/setuptool/HemeLbSetupTool/Controller/PlacedIoletController.py
+++ Tools/setuptool/HemeLbSetupTool/Controller/PlacedIoletController.py
@@ -1,9 +1,10 @@
-from ..Bindings.Translators import QuickTranslator
+from ..Bindings.Translators import QuickTranslator, UnitTranslator
 from ..Bindings.ListController import ObjectController, ListController, HasListKeys
 from ..Bindings.Mappers import SimpleObservingMapper
 from ..Bindings.VtkObject import HasVtkObjectKeys
 
-from ..Model.PlacedIolet import PlacedIolet
+from ..Model.PlacedIolet import PlacedIolet, PlacedInlet, PlacedOutlet
+from ..Model.Iolets import Inlet, Outlet
 
 from .IoletController import IoletController
 
@@ -19,6 +20,82 @@ class PlacedIoletController(HasVtkObjectKeys, ObjectController):
     
     pass
 
+# class ComponentTranslator(Translator):
+#     def __init__(self, model, component, inner=None):
+#         Translator.__init__(self, inner)
+#         self.component = component
+#         self.model = model
+#         self.index = {'x': 0,
+#                       'y': 1,
+#                       'z': 2}[component.lower()]
+#         return
+#     def TranslateStage(self, val):
+#         orig = self.model.GetValueForKey(self.key)
+#         orig[self.index] = val
+#         self.model.SetValueForKey(self.key, orig)
+#         return
+    
+class VectorMapper(SimpleObservingMapper):
+    """Mapper for Vectors to components of a list.
+    """
+    
+    # def __init__(self, model, key, translator=UnitTranslator()):
+    #     SimpleObservingMapper.__init__(self, translator=translator)
+        
+    #     self.model = model
+    #     self.key = key
+        
+    #     return
+    
+    def CreateSubMapper(self, component):
+        return VectorComponentMapper(self.model,
+                                     self.key+'.'+component,
+                                     translator=self.translator)
+    
+    pass
+
+class VectorComponentMapper(SimpleObservingMapper):
+    def __init__(self, model, key, translator=UnitTranslator()):
+        self.model = model
+        self.fullkey = key
+        baseKey, self.component = key.rsplit('.', 1)
+        self.index = {'x': 0,
+                      'y': 1,
+                      'z': 2}[self.component.lower()]
+        
+        SimpleObservingMapper.__init__(self, model, baseKey, translator)
+        return
+    
+    def _Get(self):
+        return self.model.GetValueForKey(self.key)[self.index]
+    
+    def _Set(self, val):
+        # Can't set a single component. So get the coords (it's a
+        # tuple so must convert to a list to allow item assignment)
+        # update and set them all.
+        new = list(self.model.GetValueForKey(self.key))
+        new[self.index] = val
+        self.model.SetValueForKey(self.key, new)
+        return
+
+    # We add the index to the change options here so our sibling
+    # VCMappers can ignore us.
+    def WillChangeValueForKey(self, key):
+        return SimpleObservingMapper.WillChangeValueForKey(self, key, index=self.index)
+    def DidChangeValueForKey(self, key):
+        return SimpleObservingMapper.DidChangeValueForKey(self, key, index=self.index)
+    # Here we ignore the update if we have an index in the
+    # ChangeOptions object and it's not ours.
+    def HandleUpdate(self, change=None, alsoIgnored=None):
+        try:
+            index = change.index
+            if index != self.index:
+                return
+        except:
+            pass
+        return SimpleObservingMapper.HandleUpdate(self, change, alsoIgnored)
+    pass
+
 class PlacedIoletListController(ListController):
     """Controller for a list of PlacedIolet objects.
     """
@@ -28,9 +105,17 @@ class PlacedIoletListController(ListController):
         return
 
     def IoletToPlacedIolet(self, iolet):
-        pi = PlacedIolet(iolet)
+        if isinstance(iolet, Inlet):
+            pi = PlacedInlet()
+        elif isinstance(iolet, Outlet):
+            pi = PlacedOutlet()
+        else:
+            raise ValueError("Must be an Inlet or Outlet")
+        
         controller = IoletController(iolet)
-        # controller.BindValue('Radius', SimpleObservingMapper(pi, 'Radius'))
+        controller.BindValue('Radius', SimpleObservingMapper(pi, 'Radius'))
+        controller.BindValue('Centre', VectorMapper(pi, 'Centre'))
+        controller.BindValue('Normal', VectorMapper(pi, 'Normal'))
         return pi
     
     pass
--- Tools/setuptool/HemeLbSetupTool/Model/PlacedIolet.py
+++ Tools/setuptool/HemeLbSetupTool/Model/PlacedIolet.py
@@ -6,7 +6,7 @@ from vtk import vtkPlaneWidget, vtkPolyData, vtkPolyDataMapper, \
 from ..Util.Observer import Observable, ObservableList, NotifyOptions
 
 from .Iolets import Inlet, Outlet
-
+import pdb
 class PlacedIoletList(ObservableList):
     def __init__(self, *args, **kwargs):
         ObservableList.__init__(self, *args, **kwargs)
@@ -16,7 +16,8 @@ class PlacedIoletList(ObservableList):
         # happens, so that we can get the item to remove the observer.
         self.AddObserver("@INSERTION", self.HandleInsertion)
         self.AddObserver("@REMOVAL", self.HandlePreRemoval,
-                         options=NotifyOptions(BEFORE_CHANGE=True))
+                         options=NotifyOptions(BEFORE_CHANGE=True,
+                                               AFTER_CHANGE=False))
         return
     
     def SetItemEnabledChangeHandler(self, handler):
@@ -39,17 +40,10 @@ class PlacedIoletList(ObservableList):
 
 class PlacedIolet(Observable):
     
-    def __init__(self, iolet):
-        self.iolet = iolet
-        if isinstance(iolet, Inlet):
-            self.colour = (0.,1.,0.)
-        elif isinstance(iolet, Outlet):
-            self.colour = (1.,0.,0.)
-        else:
-            raise ValueError("IOlet must be either an inlet or outlet!")
-        
+    def __init__(self):
         # Working arrays, constructed once for speed.
         self._c = N.zeros(3)
+        self._n = N.zeros(3)
         self._p1 = N.zeros(3)
         self._p2 = N.zeros(3)
 
@@ -72,11 +66,24 @@ class PlacedIolet(Observable):
         self.actor.GetProperty().SetColor(self.colour)
         
         self.Enabled = False
+        
         self.AddObserver('Enabled', self.EnabledSet)
-
-        return
-    
+        self.AddDependency('IsCentreValid', 'Centre')
+        self.AddDependency('IsNormalValid', 'Normal')
+        self.AddDependency('IsRadiusValid', 'Radius')
+    #     self.iolet.AddObserver('Centre.@ANY', self.MyIoletCentreChanged)
+    #     self.iolet.AddObserver('Normal.@ANY', self.MyIoletNormalChanged)
+    #     self.iolet.AddObserver('Radius', self.MyIoletRadiusChanged)
+    #     return
+    # def MyIoletCentreChanged(self, change):
+    #     self.SetCentre(self.iolet.Centre.x,
+    #                    self.iolet.Centre.y,
+    #                    self.iolet.Centre.z)
+        
     def EnabledSet(self, change):
+        if self.widget.GetInteractor() is None:
+            return
+        
         if self.Enabled:
             self.widget.On()
         else:
@@ -96,38 +103,53 @@ class PlacedIolet(Observable):
     def GetCentre(self):
         return self.widget.GetCenter()
     Centre = property(GetCentre, SetCentre)
+    @property
+    def IsCentreValid(self):
+        return N.alltrue(N.isfinite(self.Centre))
     
     def SetNormal(self, normal):
-        # self.widget.SetNormal(normal)
-        # # Force the plane to be updated
-        # self.widget.InvokeEvent("InteractionEvent")
+        self.widget.SetNormal(normal)
+        # Force the plane to be updated
+        self.widget.InvokeEvent("InteractionEvent")
         return
     def GetNormal(self):
         return self.widget.GetNormal()
     Normal = property(GetNormal, SetNormal)
+    @property
+    def IsNormalValid(self):
+        normal = self.Normal
+        return N.alltrue(N.isfinite(normal)) and \
+               N.dot(normal, normal) >1e-6
     
     def SetRadius(self, radius):
-        # if not N.isfinite(radius):
-        #     self.Enabled = False
-        #     return
+        pdb.set_trace()
+        if not N.isfinite(radius):
+            self.Enabled = False
+            return
         
-        # # Get into numpy vectors
-        # self.widget.GetCenter(self._c)
-        # self.widget.GetPoint1(self._p1)
-        # self.widget.GetPoint2(self._p2)
-
-        # # p1/2 now are relative to centre
-        # self._p1 -= self._c
-        # self._p2 -= self._c
-        # # Rescale so that they have length == radius
-        # self._p1 *= radius / N.sqrt(N.dot(self._p1, self._p1))
-        # self._p2 *= radius / N.sqrt(N.dot(self._p2, self._p2))
+        if not (self.IsCentreValid and self.IsNormalValid):
+            return
         
-        # # Set
-        # self.widget.SetPoint1(self._p1)
-        # self.widget.SetPoint1(self._p2)
-        # # Force the plane to be updated
-        # self.widget.InvokeEvent("InteractionEvent")
+        # Get into numpy vectors
+        self.widget.GetCenter(self._c)
+        self.widget.GetNormal(self._n)
+        self.widget.GetPoint1(self._p1)
+        if not N.alltrue(N.isfinite(self._p1))
+        
+        self.widget.GetPoint2(self._p2)
+        
+        # p1/2 now are relative to centre
+        self._p1 -= self._c
+        self._p2 -= self._c
+        # Rescale so that they have length == radius
+        self._p1 *= radius / N.sqrt(N.dot(self._p1, self._p1))
+        self._p2 *= radius / N.sqrt(N.dot(self._p2, self._p2))
+        
+        # Set
+        self.widget.SetPoint1(self._p1)
+        self.widget.SetPoint1(self._p2)
+        # Force the plane to be updated
+        self.widget.InvokeEvent("InteractionEvent")
         return
     def GetRadius(self):
         # Get into numpy vectors
@@ -145,5 +167,24 @@ class PlacedIolet(Observable):
         r2 = N.sqrt(self._p1.sum())
         return 0.5*(r1+r2)
     Radius = property(GetRadius, SetRadius)
+    @property
+    def IsRadiusValid(self):
+        radius = self.Radius
+        return N.isfinite(radius) and radius > 1e-6
     
     pass
+
+class PlacedInlet(PlacedIolet):    
+    def __init__(self):
+        self.colour = (0.,1.,0.)
+        PlacedIolet.__init__(self)
+        return
+    pass
+
+class PlacedOutlet(PlacedIolet):    
+    def __init__(self):
+        self.colour = (1.,0.,0.)
+        PlacedIolet.__init__(self)
+        return
+    pass
+
