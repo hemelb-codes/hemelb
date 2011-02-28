d915d098881f13d5a691f0b3f76eece8d0529ae7
Hywel Carver <hywelc@gmail.com>
2011-02-28 17:18:39 +0000

    Make domain decomposition and file-reading part of the same function in the TopologyReader.

--- Code/Rules.mk
+++ Code/Rules.mk
@@ -1,7 +1,7 @@
 # -*- mode: makefile;-*-
 include $(MK)/header.mk
 
-TARGETS = $(EXE) hlbTest
+TARGETS = $(EXE)
 SUBDIRS = steering vis lb net debug topology io xml parmetis util
 
 $(EXE)_DEPS = D3Q15.o \
@@ -9,12 +9,6 @@ $(EXE)_DEPS = D3Q15.o \
         SimConfig.o \
 	main.o \
 	$(SUBDIRS_TGTS)
-
-hlbTest_DEPS = D3Q15.o \
-        SimulationMaster.o \
-        SimConfig.o \
-        tests.o \
-        $(SUBDIRS_TGTS)
 
 HEMELB_INCLUDEPATHS += $(TOP)
 
--- Code/topology/TopologyReader.cpp
+++ Code/topology/TopologyReader.cpp
@@ -282,9 +282,15 @@ namespace hemelb
                                           int &totalFluidSites,
                                           unsigned int siteMins[3],
                                           unsigned int siteMaxes[3],
+                                          bool iReserveSteeringCore,
+                                          NetworkTopology* bNetTop,
                                           lb::LbmParameters* bLbmParams,
-                                          SimConfig* bSimConfig)
+                                          SimConfig* bSimConfig,
+                                          double* oReadTime,
+                                          double* oDecomposeTime)
     {
+      double lStart = MPI_Wtime();
+
       MPI_File lFile;
       int lError;
 
@@ -336,10 +342,18 @@ namespace hemelb
 
       //TODO   ReadInBlocks
 
+      MPI_File_close(&lFile);
 
       //OptimiseDomainDecomposition();
 
-      MPI_File_close(&lFile);
+      double lMiddle = MPI_Wtime();
+
+      DecomposeDomain(totalFluidSites, iReserveSteeringCore, bNetTop, bGlobalLatticeData);
+
+      double lEnd = MPI_Wtime();
+
+      *oReadTime = lMiddle - lStart;
+      *oDecomposeTime = lEnd - lMiddle;
     }
 
     /**
@@ -525,7 +539,7 @@ namespace hemelb
     void TopologyReader::DecomposeDomain(int iTotalFluidSites,
                                          bool iReserveSteeringCore,
                                          NetworkTopology* bNetTop,
-                                         const lb::GlobalLatticeData & bGlobLatDat)
+                                         const lb::GlobalLatticeData* bGlobLatDat)
     {
       // Allocations.  fluid sites will store actual number of fluid
       // sites per proc.  Site location will store up to 10000 of some
@@ -603,7 +617,7 @@ namespace hemelb
                                                       const int iMarker,
                                                       const bool iIsMachineLevel,
                                                       NetworkTopology* bNetTop,
-                                                      const lb::GlobalLatticeData &iGlobLatDat)
+                                                      const lb::GlobalLatticeData* iGlobLatDat)
     {
       std::vector<SiteLocation*> *lSiteLocationA = new std::vector<SiteLocation*>;
       std::vector<SiteLocation*> *lSiteLocationB = new std::vector<SiteLocation*>;
@@ -622,17 +636,17 @@ namespace hemelb
       int lSitesOnCurrentProc = 0;
 
       // Iterate over all blocks.
-      for (unsigned int lBlockCoordI = 0; lBlockCoordI < iGlobLatDat.GetXBlockCount(); lBlockCoordI++)
+      for (unsigned int lBlockCoordI = 0; lBlockCoordI < iGlobLatDat->GetXBlockCount(); lBlockCoordI++)
       {
-        for (unsigned int lBlockCoordJ = 0; lBlockCoordJ < iGlobLatDat.GetYBlockCount(); lBlockCoordJ++)
+        for (unsigned int lBlockCoordJ = 0; lBlockCoordJ < iGlobLatDat->GetYBlockCount(); lBlockCoordJ++)
         {
-          for (unsigned int lBlockCoordK = 0; lBlockCoordK < iGlobLatDat.GetZBlockCount(); lBlockCoordK++)
+          for (unsigned int lBlockCoordK = 0; lBlockCoordK < iGlobLatDat->GetZBlockCount(); lBlockCoordK++)
           {
             // Block number is the number of the block we're currently on.
             lBlockNumber++;
 
             // Point to a block of ProcessorRankForEachBlockSite.  If we are in a block of solids, move on.
-            int *lProcRankForSite = iGlobLatDat.Blocks[lBlockNumber].ProcessorRankForEachBlockSite;
+            int *lProcRankForSite = iGlobLatDat->Blocks[lBlockNumber].ProcessorRankForEachBlockSite;
 
             // If the array of proc rank for each site is NULL, we're on an all-solid block.
             if (lProcRankForSite == NULL)
@@ -646,14 +660,14 @@ namespace hemelb
 
             // For each dimension of the site co-ordinates, iterate over all values of the site
             // co-ordinates on the current block.
-            for (unsigned int lSiteCoordI = lBlockCoordI * iGlobLatDat.GetBlockSize(); lSiteCoordI
-                < lBlockCoordI * iGlobLatDat.GetBlockSize() + iGlobLatDat.GetBlockSize(); lSiteCoordI++)
+            for (unsigned int lSiteCoordI = lBlockCoordI * iGlobLatDat->GetBlockSize(); lSiteCoordI
+                < lBlockCoordI * iGlobLatDat->GetBlockSize() + iGlobLatDat->GetBlockSize(); lSiteCoordI++)
             {
-              for (unsigned int lSiteCoordJ = lBlockCoordJ * iGlobLatDat.GetBlockSize(); lSiteCoordJ
-                  < lBlockCoordJ * iGlobLatDat.GetBlockSize() + iGlobLatDat.GetBlockSize(); lSiteCoordJ++)
+              for (unsigned int lSiteCoordJ = lBlockCoordJ * iGlobLatDat->GetBlockSize(); lSiteCoordJ
+                  < lBlockCoordJ * iGlobLatDat->GetBlockSize() + iGlobLatDat->GetBlockSize(); lSiteCoordJ++)
               {
-                for (unsigned int lSiteCoordK = lBlockCoordK * iGlobLatDat.GetBlockSize(); lSiteCoordK
-                    < lBlockCoordK * iGlobLatDat.GetBlockSize() + iGlobLatDat.GetBlockSize(); lSiteCoordK++)
+                for (unsigned int lSiteCoordK = lBlockCoordK * iGlobLatDat->GetBlockSize(); lSiteCoordK
+                    < lBlockCoordK * iGlobLatDat->GetBlockSize() + iGlobLatDat->GetBlockSize(); lSiteCoordK++)
                 {
                   // Keep track of the site number.
                   lSiteNumber++;
@@ -709,11 +723,11 @@ namespace hemelb
                         int neigh_k = lNew->k + D3Q15::CZ[l];
 
                         // Move on if neighbour is outside the bounding box.
-                        if (neigh_i == -1 || neigh_i == (int) iGlobLatDat.GetXSiteCount())
+                        if (neigh_i == -1 || neigh_i == (int) iGlobLatDat->GetXSiteCount())
                           continue;
-                        if (neigh_j == -1 || neigh_j == (int) iGlobLatDat.GetYSiteCount())
+                        if (neigh_j == -1 || neigh_j == (int) iGlobLatDat->GetYSiteCount())
                           continue;
-                        if (neigh_k == -1 || neigh_k == (int) iGlobLatDat.GetZSiteCount())
+                        if (neigh_k == -1 || neigh_k == (int) iGlobLatDat->GetZSiteCount())
                           continue;
 
                         // Move on if the neighbour is in a block of solids (in which case
@@ -723,8 +737,8 @@ namespace hemelb
 
                         // Pointer to the rank on which a particular fluid site
                         // resides.
-                        int * proc_id_p = iGlobLatDat.GetProcIdFromGlobalCoords(neigh_i, neigh_j,
-                                                                                neigh_k);
+                        int * proc_id_p = iGlobLatDat->GetProcIdFromGlobalCoords(neigh_i, neigh_j,
+                                                                                 neigh_k);
 
                         if (proc_id_p == NULL || *proc_id_p != iMarker)
                         {
