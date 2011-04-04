ae57503323ef1a2f3f1edd2aa3e9c13bd54d6812
Hywel Carver <hywelc@gmail.com>
2011-04-04 10:48:16 +0100

    Minor changes to GlyphDrawer.

--- Code/vis/GlyphDrawer.cc
+++ Code/vis/GlyphDrawer.cc
@@ -17,15 +17,16 @@ namespace hemelb
       mLatDat(iLatDat), mScreen(iScreen), mDomainStats(iDomainStats), mViewpoint(iViewpoint),
           mVisSettings(iVisSettings)
     {
-      int n = -1;
+      unsigned int n = 0;
 
+      // Iterate over the first site in each block.
       for (unsigned int i = 0; i < mLatDat->GetXSiteCount(); i += mLatDat->GetBlockSize())
       {
         for (unsigned int j = 0; j < mLatDat->GetYSiteCount(); j += mLatDat->GetBlockSize())
         {
           for (unsigned int k = 0; k < mLatDat->GetZSiteCount(); k += mLatDat->GetBlockSize())
           {
-            n++;
+            // Get the block data for this block - if it has no site data, move on.
             geometry::LatticeData::BlockData * map_block_p = mLatDat->GetBlock(n);
 
             if (map_block_p->site_data == NULL)
@@ -33,14 +34,15 @@ namespace hemelb
               continue;
             }
 
-            unsigned int site_i = (mLatDat->GetBlockSize() >> 1);
-            unsigned int site_j = (mLatDat->GetBlockSize() >> 1);
-            unsigned int site_k = (mLatDat->GetBlockSize() >> 1);
+            // We put the glyph at the site at the centre of the block.
+            const unsigned int site_i = (mLatDat->GetBlockSize() >> 1);
+            const unsigned int site_j = (mLatDat->GetBlockSize() >> 1);
+            const unsigned int site_k = (mLatDat->GetBlockSize() >> 1);
 
-            unsigned int m = ( ( (site_i << mLatDat->GetLog2BlockSize()) + site_j)
+            const unsigned int siteIdOnBlock = ( ( (site_i << mLatDat->GetLog2BlockSize()) + site_j)
                 << mLatDat->GetLog2BlockSize()) + site_k;
 
-            if (map_block_p->site_data[m] & BIG_NUMBER3)
+            if (map_block_p->site_data[siteIdOnBlock] & BIG_NUMBER3)
             {
               continue;
             }
@@ -51,10 +53,11 @@ namespace hemelb
             lGlyph->y = float (j + site_j) - 0.5F * float (mLatDat->GetYSiteCount());
             lGlyph->z = float (k + site_k) - 0.5F * float (mLatDat->GetZSiteCount());
 
-            lGlyph->f = mLatDat->GetFOld(map_block_p->site_data[m] * D3Q15::NUMVECTORS);
+            lGlyph->f = mLatDat->GetFOld(map_block_p->site_data[siteIdOnBlock] * D3Q15::NUMVECTORS);
 
             mGlyphs.push_back(lGlyph);
 
+            ++n;
           } // for k
         } // for j
       } // for i
@@ -70,51 +73,40 @@ namespace hemelb
       }
     }
 
-    // Function to perform the rendering.
+    /**
+     * Perform the rendering for each glyph.
+     */
     void GlyphDrawer::render()
     {
-      float screen_max[4];
-      screen_max[0] = mScreen->MaxXValue;
-      screen_max[1] = mScreen->MaxXValue;
-      screen_max[2] = mScreen->MaxYValue;
-      screen_max[3] = mScreen->MaxYValue;
-
-      float scale[4];
-      scale[0] = scale[1] = mScreen->ScaleX;
-      scale[2] = scale[3] = mScreen->ScaleY;
-
-      double density;
-      double vx, vy, vz;
-      double temp;
-
-      float p1[3], p2[3], p3[3], p4[3];
-
+      // For each glyph...
       for (unsigned int n = 0; n < mGlyphs.size(); n++)
       {
+        // ... get the density and velocity at that point...
+        double density;
+        double vx, vy, vz;
         D3Q15::CalculateDensityAndVelocity(mGlyphs[n]->f, density, vx, vy, vz);
 
-        temp = glyph_length * mLatDat->GetBlockSize() * mDomainStats->velocity_threshold_max_inv
-            / density;
+        // ... calculate the velocity vector multiplier...
+        const double temp = glyph_length * mLatDat->GetBlockSize()
+            * mDomainStats->velocity_threshold_max_inv / density;
 
-        vx *= temp;
-        vy *= temp;
-        vz *= temp;
-
+        // ... calculate the two ends of the line we're going to draw...
+        float p1[3], p2[3];
         p1[0] = mGlyphs[n]->x;
         p1[1] = mGlyphs[n]->y;
         p1[2] = mGlyphs[n]->z;
 
-        p2[0] = mGlyphs[n]->x + vx;
-        p2[1] = mGlyphs[n]->y + vy;
-        p2[2] = mGlyphs[n]->z + vz;
+        p2[0] = mGlyphs[n]->x + vx * temp;
+        p2[1] = mGlyphs[n]->y + vy * temp;
+        p2[2] = mGlyphs[n]->z + vz * temp;
 
+        // ... transform to the location on the screen, and render.
+        float p3[3], p4[3];
         mViewpoint->Project(p1, p3);
         mViewpoint->Project(p2, p4);
 
-        p3[0] = scale[0] * (p3[0] + screen_max[0]);
-        p3[1] = scale[1] * (p3[1] + screen_max[1]);
-        p4[0] = scale[2] * (p4[0] + screen_max[2]);
-        p4[1] = scale[3] * (p4[1] + screen_max[3]);
+        mScreen->Transform<float> (p3, p3);
+        mScreen->Transform<float> (p4, p4);
 
         mScreen->RenderLine(p3, p4, mVisSettings->mStressType, mVisSettings->mode);
       }
