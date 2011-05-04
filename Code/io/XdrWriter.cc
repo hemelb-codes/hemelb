5b489386b68c97e05997f7deb87f4cc300f99224
Rupert Nash <rupert.nash@ed.ac.uk>
2011-05-04 17:57:50 +0100

    Deal with XDR API differences across platforms

--- Code/io/XdrWriter.cc
+++ Code/io/XdrWriter.cc
@@ -10,8 +10,20 @@ namespace hemelb
     // Functions to write simple types out to the Xdr stream.
     // Sadly templating is non-trivial due to XDR's naming.
 
-    // The const_cast in these is safe as the XDR writer won't
-    // change the values but doesn't specify in the headers.
+    /* The const_cast in these is safe as the XDR writer won't
+     * change the values but doesn't specify in the headers.
+     */
+
+    /* The XDR implementation on BSD based machines uses
+     * xdr_u_int??_t() to pack/unpack these types, while Linux
+     * based machines have xdr_uint??_t(). Use the config macro to
+     * create aliases on BSD.
+     */
+#ifdef HEMELB_CFG_ON_BSD
+#define xdr_uint16_t xdr_u_int16_t
+#define xdr_uint32_t xdr_u_int32_t
+#define xdr_uint64_t xdr_u_int64_t
+#endif //  HEMELB_CFG_ON_BSD
 
     void XdrWriter::_write(int16_t const& shortToWrite)
     {
@@ -20,7 +32,7 @@ namespace hemelb
 
     void XdrWriter::_write(uint16_t const& shortToWrite)
     {
-      xdr_u_int16_t(&mXdr, const_cast<uint16_t *> (&shortToWrite));
+      xdr_uint16_t(&mXdr, const_cast<uint16_t *> (&shortToWrite));
     }
 
     void XdrWriter::_write(int32_t const& value)
@@ -30,7 +42,7 @@ namespace hemelb
 
     void XdrWriter::_write(uint32_t const& uIntToWrite)
     {
-      xdr_u_int32_t(&mXdr, const_cast<uint32_t *> (&uIntToWrite));
+      xdr_uint32_t(&mXdr, const_cast<uint32_t *> (&uIntToWrite));
     }
 
     void XdrWriter::_write(int64_t const& longToWrite)
@@ -40,7 +52,7 @@ namespace hemelb
 
     void XdrWriter::_write(uint64_t const& longToWrite)
     {
-      xdr_u_int64_t(&mXdr, const_cast<uint64_t*> (&longToWrite));
+      xdr_uint64_t(&mXdr, const_cast<uint64_t*> (&longToWrite));
     }
 
     void XdrWriter::_write(float const& floatToWrite)
