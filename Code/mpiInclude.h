21d0bb4c6563bcc1d3e48f778c7aaf21e437f6cd
Rupert Nash <rupert.nash@ed.ac.uk>
2011-05-05 19:48:49 +0100

    Enhanced the prrevious to use traits and start using it for the ColPixel class

--- Code/mpiInclude.h
+++ Code/mpiInclude.h
@@ -1,104 +1,64 @@
 #ifndef HEMELB_MPIINCLUDE_H
 #define HEMELB_MPIINCLUDE_H
 
-#ifdef XT3
 #include <mpi.h>
-#else
-#include "mpi.h"
-#endif
 
-/*
- * Templated function for getting the MPI_Datatype corresponding to a type.
- * The generic version, for custom classes, expects that the class T has a
- * static member function const MPI_DataType T::GetMpiDataType().
- *
- * Builtin MPI types have specialisations defined.
- */
-template<typename T>
-inline const MPI_Datatype MpiDataType()
+namespace hemelb
 {
-  return T::GetMpiDataType();
-}
+  /*
+   * There are templated functions for getting the MPI_Datatype
+   * corresponding to a type or variable below. Use as:
+   *     Foo bar;
+   *     MPI_Datatype tp = MpiDataType(bar);
+   *     // or alternatively
+   *     tp = MpiDataType<Foo>();
+   *
+   * The generic version, for custom classes, is implemented using traits.
+   * You must specialize the template, e.g.
+   *
+   *     template<>
+   *     MPI_Datatype hemelb::MpiDataTypeTraits<Foo>::RegisterMpiDataType()
+   *     {
+   *       // Create the type
+   *       MPI_Type_commit(&type);
+   *       return type;
+   *     }
+   *
+   * Built-in MPI types have specializations defined in the implementation.
+   */
 
-// These mappings are taken directly from the MPI standard version 2.2,
-// table 3.2 on page 28.
-template<>
-inline const MPI_Datatype MpiDataType<char> ()
-{
-  return MPI_CHAR;
-}
-template<>
-inline const MPI_Datatype MpiDataType<signed short int> ()
-{
-  return MPI_SHORT;
-}
-template<>
-inline const MPI_Datatype MpiDataType<signed int> ()
-{
-  return MPI_INT;
-}
-template<>
-inline const MPI_Datatype MpiDataType<signed long int> ()
-{
-  return MPI_LONG;
-}
-// Strictly, C++ doesn't have long long
-//template<>
-//inline const MPI_Datatype MpiDataType<signed long long int> ()
-//{
-//  return MPI_LONG_LONG_INT;
-//}
-template<>
-inline const MPI_Datatype MpiDataType<signed char> ()
-{
-  return MPI_SIGNED_CHAR;
-}
-template<>
-inline const MPI_Datatype MpiDataType<unsigned char> ()
-{
-  return MPI_UNSIGNED_CHAR;
-}
-template<>
-inline const MPI_Datatype MpiDataType<unsigned short int> ()
-{
-  return MPI_UNSIGNED_SHORT;
-}
-template<>
-inline const MPI_Datatype MpiDataType<unsigned int> ()
-{
-  return MPI_UNSIGNED;
-}
-template<>
-inline const MPI_Datatype MpiDataType<unsigned long int> ()
-{
-  return MPI_UNSIGNED_LONG;
-}
-// Strictly, C++ doesn't have long long
-//template<>
-//inline const MPI_Datatype MpiDataType<unsigned long long int> ()
-//{
-//  return MPI_UNSIGNED_LONG_LONG;
-//}
-template<>
-inline const MPI_Datatype MpiDataType<float> ()
-{
-  return MPI_FLOAT;
-}
-template<>
-inline const MPI_Datatype MpiDataType<double> ()
-{
-  return MPI_DOUBLE;
-}
-template<>
-inline const MPI_Datatype MpiDataType<long double> ()
-{
-  return MPI_LONG_DOUBLE;
-}
-template<>
-inline const MPI_Datatype MpiDataType<wchar_t> ()
-{
-  return MPI_WCHAR;
-}
+  template<typename T>
+  class MpiDataTypeTraits
+  {
+    public:
+      inline static const MPI_Datatype& GetMpiDataType()
+      {
+        if (mpiType == MPI_DATATYPE_NULL)
+        {
+          mpiType = RegisterMpiDataType();
+        }
+        return mpiType;
+      }
+    protected:
+      static MPI_Datatype mpiType;
+      static MPI_Datatype RegisterMpiDataType();
+  };
 
+  // Define the initial value
+  template<typename T>
+  MPI_Datatype MpiDataTypeTraits<T>::mpiType = MPI_DATATYPE_NULL;
 
+  // Generic getters of data type info
+  template<typename T>
+  inline const MPI_Datatype& MpiDataType()
+  {
+    return MpiDataTypeTraits<T>::GetMpiDataType();
+  }
+  template<typename T>
+  inline const MPI_Datatype& MpiDataType(T& var)
+  {
+    return MpiDataTypeTraits<T>::GetMpiDataType();
+  }
+
+}
 #endif // HEMELB_MPIINCLUDE_H
