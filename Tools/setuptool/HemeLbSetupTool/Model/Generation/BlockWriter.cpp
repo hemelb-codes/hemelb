170fb7406945906db8c2a03721432cbffd5413e6
Rupert Nash <rupert.nash@ed.ac.uk>
2011-06-06 13:35:08 +0100

    Examining along links to determing inside/outsideness. some problems remain

--- Tools/setuptool/HemeLbSetupTool/Model/Generation/BlockWriter.cpp
+++ Tools/setuptool/HemeLbSetupTool/Model/Generation/BlockWriter.cpp
@@ -1,24 +1,40 @@
+#include <cstdio>
+
 #include "BlockWriter.h"
 #include "ConfigWriter.h"
 
 BlockWriter::BlockWriter(ConfigWriter &cfg) :
-	configWriter(&cfg) {
-	this->count = 0;
-	this->blockStart
-			= this->configWriter->bodyEncoder->getCurrentStreamPosition();
+	configWriter(&cfg), nFluidSites(0) {
+	this->maxBufferSize = (4 * 1 // unsigned int, site config
+			+ 8 * 4 // doubles, boundary
+			+ 8 * 4 // doubles, wall
+			+ 8 * 14) // doubles, cut distances
+			* cfg.BlockSize * cfg.BlockSize * cfg.BlockSize; // number of blocks
+	this->buffer = new char[this->maxBufferSize];
+	this->memWriter = new hemelb::io::XdrMemWriter(this->buffer,
+			this->maxBufferSize);
+}
+
+BlockWriter::~BlockWriter() {
+	delete this->memWriter;
+	delete this->buffer;
 }
 
 void BlockWriter::IncrementFluidSitesCount() {
-	this->count++;
+	this->nFluidSites++;
 }
 
 void BlockWriter::Finish() {
-	// Work out how much space our block occupies
-	unsigned int blockLength =
-			this->configWriter->bodyEncoder->getCurrentStreamPosition()
-					- this->blockStart;
+	unsigned int blockLength = 0;
+	if (this->nFluidSites > 0) {
+		// Work out how much space we've written to the buffer.
+		blockLength = this->memWriter->getCurrentStreamPosition();
 
-	// Write this to the header
-	(*this->configWriter->headerEncoder) << this->count << blockLength;
+		// Write the buffer contents to the file.
+		std::fwrite(this->buffer, 1, blockLength, this->configWriter->bodyFile);
+	}
 
+	// If there are no fluid sites, write nothing except two zeros to the header.
+	// If there are fluid sites, blockLength was set above.
+	(*this->configWriter->headerEncoder) << this->nFluidSites << blockLength;
 }
