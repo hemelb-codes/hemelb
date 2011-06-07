f8427f30ebce5f457a714a986980f91ef7fbb6bf
Hywel Carver <hywelc@gmail.com>
2011-06-07 13:34:18 +0100

    Repair the Hector build, fix a usage of a non-standard function.

--- Code/make/config-HectorXT6.mk
+++ Code/make/config-HectorXT6.mk
@@ -7,4 +7,4 @@ HEMELB_STEERING_LIB := none
 HEMELB_DEFS += NO_STREAKLINES
 
 $(EXE)_LIBS = parmetis/ParMetis-3.1.1/libparmetis.a parmetis/ParMetis-3.1.1/libmetis.a
-HEMELB_INCLUDEPATHS := parmetis/ParMetis-3.1.1
+HEMELB_INCLUDEPATHS += parmetis/ParMetis-3.1.1
