aa0ed184f494cf181dbbce51d96435915ce2f176
Hywel Carver <hywelc@gmail.com>
2011-06-17 16:14:53 +0100

    Get HemeLB to build on Hector. Fix some bugs concerning MPI datatypes.

--- Code/make/config-HectorXT6.mk
+++ Code/make/config-HectorXT6.mk
@@ -1,10 +1,13 @@
 include $(MK)/config-linux.mk
 
 CXX := CC
-HEMELB_CXXFLAGS := -g -fastsse --display_error_number --diag_suppress 611
+#HEMELB_CXXFLAGS := -Mnounderscoring --one_instantiation_per_object --prelink_objects
+#-g -fastsse --display_error_number --diag_suppress 611
 
 HEMELB_STEERING_LIB := none
 HEMELB_DEFS += NO_STREAKLINES
+
+HEMELB_LOG_LEVEL := info
 
 #$(EXE)_LIBS = parmetis/ParMetis-3.1.1/libparmetis.a parmetis/ParMetis-3.1.1/libmetis.a
 #HEMELB_INCLUDEPATHS += parmetis/ParMetis-3.1.1
