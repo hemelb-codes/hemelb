cf36e5327c926cd1bf99cb07cda979736cc44f3f
Hywel Carver <hywelc@gmail.com>
2011-06-22 18:01:40 +0100

    Fix a bug in the GeometryReader, make HeCTOR builds work again.

--- Code/make/config-HectorXT6.mk
+++ Code/make/config-HectorXT6.mk
@@ -1,7 +1,8 @@
 include $(MK)/config-linux.mk
 
 CXX := CC
-#HEMELB_CXXFLAGS := -Mnounderscoring --one_instantiation_per_object --prelink_objects
+HEMELB_CXXFLAGS := 
+#-Mnounderscoring --one_instantiation_per_object --prelink_objects
 #-g -fastsse --display_error_number --diag_suppress 611
 
 HEMELB_STEERING_LIB := none
