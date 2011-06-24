c4c3868fc5c5a60a987be29be5f7b7ebf70e1c19
Hywel Carver <hywelc@gmail.com>
2011-06-24 11:06:30 +0100

    Revert the ParMetis tolerance. Increase the amount of logging on HeCTOR.

--- Code/make/config-HectorXT6.mk
+++ Code/make/config-HectorXT6.mk
@@ -8,7 +8,7 @@ HEMELB_CXXFLAGS :=
 HEMELB_STEERING_LIB := none
 HEMELB_DEFS += NO_STREAKLINES
 
-HEMELB_LOG_LEVEL := info
+HEMELB_LOG_LEVEL := warning
 
 #$(EXE)_LIBS = parmetis/ParMetis-3.1.1/libparmetis.a parmetis/ParMetis-3.1.1/libmetis.a
 #HEMELB_INCLUDEPATHS += parmetis/ParMetis-3.1.1
