92ae722623fff430db37d7d5e1fcb69fd088c7a7
Hywel Carver <unknown>
2011-07-26 18:19:48 +0100

    Remove ParMetis as a sub-repo - from now we will use the latest release version which supports 64-bit integers. Update code to match latest ParMetis version and add a build config for 32-bit machines.

--- Code/make/config-default.mk
+++ Code/make/config-default.mk
@@ -6,7 +6,7 @@ EXE := hemelb
 HEMELB_DEBUG_LEVEL := 0
 HEMELB_STEERING_LIB := $(or $(HEMELB_STEERING_LIB),basic)
 
-PMETIS_INCLUDE_DIR := $(TOP)/parmetis
+PMETIS_INCLUDE_DIR := $(TOP)/parmetis/include $(TOP)/parmetis/metis/include
 PMETIS_LIBRARY_DIR := $(TOP)/parmetis
 HEMELB_CFLAGS :=
 HEMELB_CXXFLAGS := -g -pedantic -Wall -Wextra -Wno-unused
