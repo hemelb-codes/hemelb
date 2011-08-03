0446642de359604e13f5083322140148141a3018
Rupert Nash <rupert.nash@ed.ac.uk>
2011-08-03 15:00:31 +0100

    Remove empty Location.cc file as class is fully templated

--- Code/vis/rayTracer/Rules.mk
+++ Code/vis/rayTracer/Rules.mk
@@ -2,11 +2,10 @@ include $(MK)/header.mk
 
 SRCS :=	BlockIterator.cc \
 	ClusterBuilder.cc \
-	Location.cc \
 	RayTracer.cc \
 	RectangularIterator.cc \
 	SiteIterator.cc \
-	
+
 INCLUDES_$(d) := $(INCLUDES_$(parent))
 
 include $(MK)/footer.mk
