3cffbca520a2e6ed329b6c129875233dd0d288c3
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2011-08-10 15:48:44 +0100

    Collision operator modification and Casson model

--- Code/D3Q15.h
+++ Code/D3Q15.h
@@ -57,6 +57,12 @@ namespace hemelb
                                        const double nor[],
                                        distribn_t &stress,
                                        const double &iStressParameter);
+      static double CalculateStrainRateTensorComponent(const unsigned iRow,
+                                                       const unsigned iColumn,
+                                                       const double &iTau,
+                                                       const distribn_t iFNeq[]);
+      static double CalculateShearRate(const double &iTau,
+                                       const distribn_t &iFNeq);
   };
 }
 #endif /* HEMELB_D3Q15_H */
