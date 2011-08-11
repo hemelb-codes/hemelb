715023093d414f4e05e63e343cc88d785cb199be
Rupert Nash <rupert.nash@ed.ac.uk>
2011-08-11 14:39:20 +0100

    Updated the snapshot output format to include a magic number, versioning metadata and the origin of the grid coordinates. The snapshot reader has been updated to cope with this.

--- Tools/hemeTools/parsers/__init__.py
+++ Tools/hemeTools/parsers/__init__.py
@@ -1,1 +1,12 @@
-import pars
+"""Package for reading/writing HemeLB file formats.
+
+Beginning August 2011, binary formats will be identified with magic numbers
+http://en.wikipedia.org/wiki/File_format#Magic_number
+
+Here we define the generic HemeLB binary file identifier. It should be
+the first 4 bytes of every (binary) file used for IO. It will be then
+followed by another 4 bytes identifying the particular type/version,
+that number being terminated by the EOF character (0x04).
+"""
+
+HemeLbMagicNumber = 0x686c6221
