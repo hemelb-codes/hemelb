740be308b5bef4b6bc664da52008aecebc3f6757
Nick Westlake <nw292@cam.ac.uk>
2011-08-26 15:01:41 +0100

    Extracted BlockTraverserWithVisitedBlockTracker class to provide a basic BlockTraverser for the ray tracer

--- Code/vis/rayTracer/BlockTraverser.h
+++ Code/vis/rayTracer/BlockTraverser.h
@@ -11,10 +11,6 @@ namespace hemelb
     namespace raytracer 
     {
       //BlockTraverser is used to traverse the blocks in a lattice sequentially.
-      //The class also contains a record of which blocks have been visited, which
-      //is neccessary for the algoritm which uses this. No locations are automatically
-      //marked visited, and methods have been created to assist with random access
-      //of the lattice data as required by the algorithm
       class BlockTraverser : public VolumeTraverser
       {
       public:
@@ -25,9 +21,6 @@ namespace hemelb
 
 	Vector3D<site_t> GetSiteCoordinatesOfLowestSiteInCurrentBlock();
 
-	//Tranverses the block until the next unvisited block is reached.
-	//Returns false if the end of the Volume is reached
-	bool GoToNextUnvisitedBlock();
 
 	geometry::LatticeData::BlockData * GetCurrentBlockData();
 
@@ -47,22 +40,11 @@ namespace hemelb
 
 	bool IsValidLocation(Vector3D<site_t> block);
 
-	bool IsCurrentBlockVisited();
 
-	bool IsBlockVisited(site_t n);
-	bool IsBlockVisited(Vector3D<site_t> n);
-
-	void MarkCurrentBlockVisited();
-
-	void MarkBlockVisited(site_t n);
-	void MarkBlockVisited(Vector3D<site_t> location);
-
-      private:
+      protected:
 	bool GoToNextBlock();
 
 	const geometry::LatticeData * mLatticeData;
-
-	bool* mBlockVisited;
       }; 
     }
   }
---
+++ Code/vis/rayTracer/BlockTraverserWithVisitedBlockTracker.cc
@@ -0,0 +1,86 @@
+//#define NDEBUG;
+#include <assert.h>
+
+#include "geometry/LatticeData.h"
+#include "vis/Vector3D.h"
+#include "vis/rayTracer/BlockTraverserWithVisitedBlockTracker.h"
+#include "vis/rayTracer/RayTracer.h"
+
+namespace hemelb
+{
+  namespace vis
+  {
+    namespace raytracer
+    {
+      BlockTraverserWithVisitedBlockTracker::BlockTraverserWithVisitedBlockTracker
+      (const geometry::LatticeData* iLatDat)
+	: BlockTraverser(iLatDat)
+      {
+	//Initially no blocks have been visited
+	mBlockVisited = new bool[mLatticeData->GetBlockCount()];
+	for (site_t n = 0; 
+	     n < mLatticeData->GetBlockCount(); 
+	     n++)
+	{
+	  mBlockVisited[n] = false;
+	}		
+      }
+
+      BlockTraverserWithVisitedBlockTracker::~BlockTraverserWithVisitedBlockTracker()
+      {
+	delete[] mBlockVisited;
+      }
+      
+           		
+      bool BlockTraverserWithVisitedBlockTracker::GoToNextUnvisitedBlock()
+      {
+	assert(IsCurrentBlockVisited());
+	do 
+	{
+	  bool validBlock = GoToNextBlock();
+	  if(!validBlock)
+	  {
+	    return false;
+	  }
+	}
+	while (IsCurrentBlockVisited());
+
+	return true;
+      }
+
+      bool BlockTraverserWithVisitedBlockTracker::IsBlockVisited(site_t iN)
+      {
+	return mBlockVisited[iN];
+      }
+
+      bool BlockTraverserWithVisitedBlockTracker::IsBlockVisited(Vector3D<site_t>iLocation)
+      {
+
+	return mBlockVisited[GetIndexFromLocation(iLocation)];
+      }
+	    
+
+      bool BlockTraverserWithVisitedBlockTracker::IsCurrentBlockVisited()
+      {
+	return IsBlockVisited(CurrentBlockNumber());
+      }
+
+      void BlockTraverserWithVisitedBlockTracker::MarkCurrentBlockVisited()
+      {
+	MarkBlockVisited(CurrentBlockNumber());
+      }
+	    
+      void BlockTraverserWithVisitedBlockTracker::MarkBlockVisited(site_t iBlockId)
+      {
+	mBlockVisited[iBlockId] = true;
+      }
+
+      void BlockTraverserWithVisitedBlockTracker::MarkBlockVisited(Vector3D<site_t> iLocation)
+      {
+	site_t lNumber = GetIndexFromLocation(iLocation);
+	assert(lNumber < mLatticeData->GetBlockCount());
+	MarkBlockVisited(lNumber);
+      }	   
+    }
+  }
+}
