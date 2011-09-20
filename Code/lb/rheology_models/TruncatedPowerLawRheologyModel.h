827d38481817d5b573b6216f301d900798499a78
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2011-09-20 15:25:56 +0100

    making power-law look more like the rest of models

--- Code/lb/rheology_models/TruncatedPowerLawRheologyModel.h
+++ Code/lb/rheology_models/TruncatedPowerLawRheologyModel.h
@@ -13,10 +13,10 @@ namespace hemelb
        * GAMMA_{ZERO,INF} are computed with the power-law formula: nu = m * gamma^{n-1} for
        * given values of nu_{max,min}, m, and n.
        */
-      static const double GAMMA_ZERO = 6.25e-4; // TODO units, gamma=6.25e-4 for m=BLOOD_VISCOSITY_Pa_s / BLOOD_DENSITY_Kg_per_m3 n=0.5 and nu_max=0.16 Pa_s / BLOOD_DENSITY_Kg_per_m3
-      static const double GAMMA_INF = 1.3061; // TODO units, gamma=1 for m=BLOOD_VISCOSITY_Pa_s / BLOOD_DENSITY_Kg_per_m3 n=0.5 and nu_min=0.0035 Pa_s / BLOOD_DENSITY_Kg_per_m3
+      static const double GAMMA_ZERO = 6.25e-4; // s^{-1}, gamma=6.25e-4 for m=BLOOD_VISCOSITY_Pa_s, n=0.5 and eta_max=0.16 Pa_s
+      static const double GAMMA_INF = 1.3061; // s^{-1}, gamma=1.3061 for m=BLOOD_VISCOSITY_Pa_s, n=0.5 and eta_min=0.0035 Pa_s
 
-      static const double M_CONSTANT = BLOOD_VISCOSITY_Pa_s / BLOOD_DENSITY_Kg_per_m3; // m^2/s
+      static const double M_CONSTANT = BLOOD_VISCOSITY_Pa_s; // Pa_s
       static const double N_CONSTANT = 0.5; // dimensionless, n<1 shear thinning, n>1 shear thickening, n=1 newtonian with nu=M_CONSTANT
 
       class TruncatedPowerLawRheologyModel : public AbstractRheologyModel<TruncatedPowerLawRheologyModel>
@@ -25,9 +25,9 @@ namespace hemelb
           /*
            *  Compute nu for a given shear rate according to the truncated power-law:
            *
-           *                     { M_CONSTANT * GAMMA_ZERO^{N_CONSTANT - 1},  if iShearRate<GAMMA_ZERO
-           *  nu = eta/density = { M_CONSTANT *  GAMMA_INF^{N_CONSTANT - 1},  if iShearRate>GAMMA_INF
-           *                     { M_CONSTANT * iShearRate^{N_CONSTANT - 1},  otherwise
+           *        { M_CONSTANT * GAMMA_ZERO^{N_CONSTANT - 1},  if iShearRate<GAMMA_ZERO
+           *  eta = { M_CONSTANT *  GAMMA_INF^{N_CONSTANT - 1},  if iShearRate>GAMMA_INF
+           *        { M_CONSTANT * iShearRate^{N_CONSTANT - 1},  otherwise
            *
            *  @param iShearRate local shear rate value (s^{-1}).
            *  @param iDensity local density. TODO at the moment this value is not used
