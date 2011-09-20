a84922087daaefd2b81ff41710627bd7c262f68e
Nick Westlake <nw292@cam.ac.uk>
2011-09-20 13:41:34 +0100

    Implemented depth cuing into darkness

--- Code/vis/ColPixelMPIDataTypes.cc
+++ Code/vis/ColPixelMPIDataTypes.cc
@@ -62,7 +62,9 @@ namespace hemelb
   }
 
   template<>
-  MPI_Datatype MpiDataTypeTraits<hemelb::vis::ColPixel<hemelb::vis::raytracer::RayDataEnhanced<true> > >::RegisterMpiDataType()
+  MPI_Datatype MpiDataTypeTraits<hemelb::vis::ColPixel
+				 <hemelb::vis::raytracer::RayDataEnhanced
+				  <vis::raytracer::DepthCuing::MIST> > >::RegisterMpiDataType()
   {
     int col_pixel_count = 9;
     int col_pixel_blocklengths[9] = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -72,7 +74,8 @@ namespace hemelb
       MPI_UNSIGNED,
       MPI_UNSIGNED,
       MPI_UNSIGNED,
-      MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced<true> >
+      MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced
+      <vis::raytracer::DepthCuing::MIST> >
       ::GetMpiDataType(),
       MPI_FLOAT,
       MPI_FLOAT,
@@ -98,10 +101,12 @@ namespace hemelb
       {
 	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(unsigned) * col_pixel_blocklengths[i - 1]);
       }
-      else if (col_pixel_types[i - 1] == MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced<true> >
+      else if (col_pixel_types[i - 1] == MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced
+	       <vis::raytracer::DepthCuing::MIST> >
 	       ::GetMpiDataType())
       {
-	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(hemelb::vis::raytracer::RayDataEnhanced<true>)
+	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(hemelb::vis::raytracer::RayDataEnhanced
+							      <vis::raytracer::DepthCuing::MIST>)
 						       * col_pixel_blocklengths[i - 1]);
       }
       
@@ -116,8 +121,10 @@ namespace hemelb
     return type;
   }
 
-template<>
-  MPI_Datatype MpiDataTypeTraits<hemelb::vis::ColPixel<hemelb::vis::raytracer::RayDataEnhanced<false> > >::RegisterMpiDataType()
+ template<>
+  MPI_Datatype MpiDataTypeTraits<hemelb::vis::ColPixel
+				 <hemelb::vis::raytracer::RayDataEnhanced
+				  <vis::raytracer::DepthCuing::DARKNESS> > >::RegisterMpiDataType()
   {
     int col_pixel_count = 9;
     int col_pixel_blocklengths[9] = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -127,7 +134,8 @@ template<>
       MPI_UNSIGNED,
       MPI_UNSIGNED,
       MPI_UNSIGNED,
-      MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced<false> >
+      MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced
+      <vis::raytracer::DepthCuing::DARKNESS> >
       ::GetMpiDataType(),
       MPI_FLOAT,
       MPI_FLOAT,
@@ -153,10 +161,12 @@ template<>
       {
 	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(unsigned) * col_pixel_blocklengths[i - 1]);
       }
-      else if (col_pixel_types[i - 1] == MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced<false> >
+      else if (col_pixel_types[i - 1] == MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced
+	       <vis::raytracer::DepthCuing::DARKNESS> >
 	       ::GetMpiDataType())
       {
-	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(hemelb::vis::raytracer::RayDataEnhanced<false>)
+	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(hemelb::vis::raytracer::RayDataEnhanced
+							      <vis::raytracer::DepthCuing::DARKNESS>)
 						       * col_pixel_blocklengths[i - 1]);
       }
       
@@ -171,4 +181,63 @@ template<>
     return type;
   }
 
+ template<>
+  MPI_Datatype MpiDataTypeTraits<hemelb::vis::ColPixel
+				 <hemelb::vis::raytracer::RayDataEnhanced
+				  <vis::raytracer::DepthCuing::NONE> > >::RegisterMpiDataType()
+  {
+    int col_pixel_count = 9;
+    int col_pixel_blocklengths[9] = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+    MPI_Datatype col_pixel_types[9] = { 
+      MPI_UNSIGNED,
+      MPI_UNSIGNED,
+      MPI_UNSIGNED,
+      MPI_UNSIGNED,
+      MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced
+      <vis::raytracer::DepthCuing::NONE> >
+      ::GetMpiDataType(),
+      MPI_FLOAT,
+      MPI_FLOAT,
+      MPI_INT,
+      MPI_UB };
+    
+    MPI_Aint col_pixel_disps[9];
+
+    col_pixel_disps[0] = 0;
+
+    for (int i = 1; i < col_pixel_count; i++)
+    {
+      if (col_pixel_types[i - 1] == MPI_FLOAT)
+      {
+        col_pixel_disps[i] = col_pixel_disps[i - 1]
+	  + (sizeof(float) * col_pixel_blocklengths[i - 1]);
+      }
+      else if (col_pixel_types[i - 1] == MPI_INT)
+      {
+        col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(int) * col_pixel_blocklengths[i - 1]);
+      }
+      else if (col_pixel_types[i - 1] == MPI_UNSIGNED)
+      {
+	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(unsigned) * col_pixel_blocklengths[i - 1]);
+      }
+      else if (col_pixel_types[i - 1] == MpiDataTypeTraits<hemelb::vis::raytracer::RayDataEnhanced
+	       <vis::raytracer::DepthCuing::NONE> >
+	       ::GetMpiDataType())
+      {
+	col_pixel_disps[i] = col_pixel_disps[i - 1] + (sizeof(hemelb::vis::raytracer::RayDataEnhanced
+							      <vis::raytracer::DepthCuing::NONE>)
+						       * col_pixel_blocklengths[i - 1]);
+      }
+      
+    }
+    MPI_Datatype type;
+    MPI_Type_struct(col_pixel_count,
+                    col_pixel_blocklengths,
+                    col_pixel_disps,
+                    col_pixel_types,
+                    &type);
+    MPI_Type_commit(&type);
+    return type;
+  }
 }
