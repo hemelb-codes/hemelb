5ce8fef8da9c7ccf365eba3462b4fb1154edb961
Rupert Nash <rupert.nash@ed.ac.uk>
2011-10-07 15:58:56 +0100

    Refactor the Scripts/CfgParser.
    Turn it into a generic event driven parser (which is moved to
    hemeTools package) and a specialisation that checks the config files
    as before. Further it can now analyse blocks asynchronously and in
    parallel. See ticket:12

---
+++ Tools/hemeTools/parsers/config/multiprocess.py
@@ -0,0 +1,232 @@
+"""ConfigLoader which will process blocks asynchronously in parallel.
+
+For debugging, add the following to the top of your __main__:
+
+import multiprocessing
+logger = multiprocessing.log_to_stderr()
+logger.setLevel(multiprocessing.SUBDEBUG)
+
+and use multiprocessing.util.debug to print messages within the code
+to be executed per-block. This will also print tracebacks from any
+uncaught exceptions from your per-block code.
+"""
+import multiprocessing
+import copy
+import signal
+import traceback
+import numpy as np
+import pdb
+from .generic import NdIndexConverter, Domain, Block, OutOfDomainBlock, AllSolidBlock
+from .freeing import FreeingConfigLoader
+
+from multiprocessing.util import debug
+
+class SubDomain(Domain):
+    """A Domain subclass that only has a subset of the full
+    domain. This is useful to avoid sending very large messages over
+    interprocess Queues.
+    """
+    
+    def __init__(self, parent, bIdx):
+        """Constructor; requires the domain it's mimicking and the 3D
+        index of the block at the centre.
+        """
+        self.StressType = parent.StressType
+        self._BlockCounts = parent._BlockCounts
+        self._BlockSize = parent._BlockSize
+        
+        self.VoxelSize = parent.VoxelSize
+        self.Origin = parent.Origin
+        
+        self.BlockIndexer = parent.BlockIndexer
+        self.BlockSiteIndexer = parent.BlockSiteIndexer
+
+        self._Index = bIdx
+        self._Shift = bIdx - 1
+        
+        self.Blocks = np.empty(27, dtype=object)
+        self.LocalBlockIndexer = NdIndexConverter((3,3,3))
+        for lbIjk, lbIdx in self.LocalBlockIndexer.IterBoth():
+            pbIdx = lbIdx + self._Shift
+            pb = parent.GetBlock(pbIdx)
+            if isinstance(pb, OutOfDomainBlock):
+                lb = OutOfDomainBlock(self, pbIdx)
+            elif isinstance(pb, AllSolidBlock):
+                lb = AllSolidBlock(self, pbIdx)
+            else:
+                lb = PartialBlock(self, pb, lbIdx - 1)
+            self.Blocks[lbIjk] = lb
+            continue
+        
+        return
+    
+    def GetBlock(self, pbIndex):
+        # The block index is from the parent Domain's array, must
+        # convert to our local index.
+        lbIndex = pbIndex - self._Shift
+        if np.all(lbIndex >= 0) and np.all(lbIndex < 3):
+            return self.Blocks[self.LocalBlockIndexer.NdToOne(lbIndex)]
+        else:
+            raise ValueError("Don't have that block")
+        
+    def SetBlock(self, bIndex, block):
+        raise ValueError("Can't set blocks!")
+        
+    def GetSite(self, sIndex):
+        bIndex = sIndex / self.BlockSize
+        block = self.GetBlock(bIndex)
+        return block.GetLocalSite(sIndex % self.BlockSize)
+
+    pass
+
+class PartialBlock(Block):
+    """A Block that only actually has sites for the parts needed by
+    the Block at the centre of the SubDomain.  Useful to further
+    reduce data transferring over IPC. Viz. a block with a <100> type
+    offset from the centre only has one face of Sites, <110> offset
+    has only one edge and a <111> offset has only a single corner.
+    """
+    def __init__(self, newDomain, parent, targetToThis):
+        """Constructor; requires the SubDomain this is part of, the
+        Block this is mirroring and the vector from the block at the
+        centre of the SubDomain to this Block.
+        """
+        self.Domain = newDomain
+        self.Index = parent.Index
+        self.nFluidSites = parent.nFluidSites
+        
+        self.Sites = np.empty(parent.Sites.shape, dtype=object)
+
+        # We only want to copy the bits we care about
+        
+        # Here, set up iterators for the accessible subset of of this
+        # block, from the target block
+        bs = newDomain.BlockSize
+        iters = []
+        for diff in targetToThis:
+            if diff < 0:
+                it = xrange(bs - 1, bs)
+            elif diff == 0:
+                it = xrange(0, bs)
+            else:
+                it = xrange(0, 1)
+                pass
+            iters.append(it)
+            continue
+        
+        # Now iterate over the interesting bit
+        sIdx = np.zeros(3, dtype=np.int)
+        for sIdx[0] in iters[0]:
+            for sIdx[1] in iters[1]:
+                for sIdx[2] in iters[2]:
+                    sIjk = newDomain.BlockSiteIndexer.NdToOne(sIdx)
+                    site = copy.copy(parent.Sites[sIjk])
+                    site.Block = self
+                    self.Sites[sIjk] = site
+                    continue
+                continue
+            continue
+        
+        return
+
+    def GetLocalSite(self, sIndex):
+        assert np.all(sIndex >= 0) and np.all(sIndex < self.Domain.BlockSize)
+        return self.Sites[self.Domain.BlockSiteIndexer.NdToOne(sIndex)]
+
+    def GetSite(self, sIndex):
+        return self.Domain.GetSite(sIndex)
+    
+    pass
+
+class AsyncBlockProcessingLoader(FreeingConfigLoader):
+    """A ConfigLoader that will asynchronously and in parallel apply a
+    processing function to each Block in the Domain.
+
+    You must make the following attributes callables with the
+    specified signatures in your subclass.
+
+    You must assign thProcessBlock(block) -> result
+    
+    HandleBlockProcessingResult(result) -> None
+
+    This class will ensure that OnBlockProcessed is called
+    appropriately once HandleBlockProcessingResult has returned.
+    
+    """
+    @staticmethod
+    def InitializeWorker():
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        return
+    
+    def __init__(self, *args, **kwargs):
+        self._Workers = multiprocessing.Pool(initializer=self.InitializeWorker)
+        FreeingConfigLoader.__init__(self, *args, **kwargs)
+        return
+    
+    def SetBlockProcessor(self, bp):
+        """Specify the object to process each Block. This MUST be a
+        pickable object, unfortunately instance methods are not
+        pickable. Further, this object will be serialised and sent to
+        the worker processes so if should be as small as possible.
+        """
+        self._BlockProcessor = BlockProcessorWrapper(bp)
+        return
+    
+    def OnBlockNeighboursAvailable(self, bIdx):
+        debug('Submit block ' + str(bIdx))
+        sd = SubDomain(self.Domain, bIdx)
+        b = sd.GetBlock(bIdx)
+        self._Workers.apply_async(self._BlockProcessor,
+                                  args=(b, ),
+                                  callback=self._HandleBlockResult)
+        return
+    
+    def _HandleBlockResult(self, args):
+        bIdx, result = args
+        debug('Got results for block ' + str(bIdx))
+        self.HandleBlockProcessingResult(result)
+        self.OnBlockProcessed(bIdx)
+        return
+
+    def _LoadBody(self):
+        FreeingConfigLoader._LoadBody(self)
+        self._Workers.close()
+        self._Workers.join()
+        return
+    
+    pass
+
+class BlockProcessorWrapper(object):
+    """Wrapper around the Block-processing callable.
+
+    This ensures the result is packaged up with the block index for
+    AsyncBlockProcessingLoader._HandleBlockResults above and ensures
+    any uncaught exceptions are logged.
+    """
+    
+    def __init__(self, callable):
+        self.callable = callable
+        return
+    
+    def __call__(self, block):
+        """Executed in the worker process. This deals with calling the
+        user function and returning the result packaged up with the
+        block index.
+        """
+        
+        debug('Process block %s' % str(block.Index))
+        try:
+            result = self.callable(block)
+            
+        except Exception as e:
+            # Here we add some debugging help. If multiprocessing's
+            # debugging is on, it will arrange to log the traceback
+            debug(traceback.format_exc())
+            # Re-raise the original exception so the Pool worker can
+            # clean up
+            raise
+        
+        # It was fine, give a normal answer
+        return (block.Index, result)
+    
+    pass
---
+++ Tools/hemeTools/parsers/config/simple.py
@@ -0,0 +1,184 @@
+import numpy as np
+import xdrlib
+
+from . import cfg
+from .generic import Domain, Block, AllSolidBlock, Site
+
+class FakeUnpacker(object):
+    """Fake xdrlib.Unpacker, for use when all the Sites in a Block are
+    solid. Just returns zeros.
+    """
+    
+    def unpack_uint(self):
+        return cfg.SOLID_TYPE
+    pass
+
+class ConfigLoader(object):
+    """Loads a HemeLB config file.
+
+    Offers a large number of hooks, in a manner similar to an
+    event-driver XML parser for examining data on the fly.
+    """
+    
+    def __init__(self, filename):
+        self.FileName = filename
+        self.File = file(filename)
+        self.Domain = d = Domain()
+        return
+    
+    def Load(self):
+        """Load and return the Domain encoded by the config file.
+        """
+        self._LoadPreamble()
+        self._LoadHeader()
+        self._LoadBody()
+        return self.Domain
+    
+    def _LoadPreamble(self):
+        """Deal with the preamble (contains global stuff about the
+        lattice: size, shape etc). Triggers OnBeginPreamble and
+        OnEndPreamble events.
+        """
+        self.OnBeginPreamble()
+        
+        self.PreambleBytes = 5*4 + 4*8
+        preambleLoader = xdrlib.Unpacker(self.File.read(self.PreambleBytes))
+        
+        self.Domain.StressType = preambleLoader.unpack_uint()
+        
+        self.Domain.BlockCounts = np.array([preambleLoader.unpack_uint() for i in xrange(3)], dtype=np.uint)
+        self.Domain.BlockSize = preambleLoader.unpack_uint()
+        
+        self.Domain.VoxelSize = preambleLoader.unpack_double()
+        self.Domain.Origin = np.array([preambleLoader.unpack_double() for i in xrange(3)], dtype=np.double)
+        
+        self.OnEndPreamble()
+        return
+
+    def _LoadHeader(self):
+        """Deal with the header containing per-block metadata (number
+        of fluid sites and bytes occupied). Triggers OnBeginHeader and
+        OnEndHeader events.
+        """
+        self.OnBeginHeader()
+        self.HeaderBytes = self.Domain.TotalBlocks * 2 * 4
+
+        headerLoader = xdrlib.Unpacker(self.File.read(self.HeaderBytes))
+        
+        nBlocks = self.Domain.TotalBlocks
+        BlockCounts = self.Domain.BlockCounts
+        
+        BlockFluidSiteCounts = np.zeros(nBlocks, dtype=np.uint)
+        BlockDataLength = np.zeros(nBlocks, dtype=np.uint)
+        BlockStarts = np.zeros(nBlocks, dtype=np.uint)
+        BlockEnds = np.zeros(nBlocks, dtype=np.uint)
+        
+        BlockEnds[-1] = self.PreambleBytes + self.HeaderBytes
+        for bIjk in self.Domain.BlockIndexer.IterOne():
+            BlockFluidSiteCounts[bIjk] = headerLoader.unpack_uint()
+            BlockDataLength[bIjk] = headerLoader.unpack_uint()
+            BlockStarts[bIjk] = BlockEnds[bIjk-1]
+            BlockEnds[bIjk] = BlockStarts[bIjk] + BlockDataLength[bIjk]
+            continue
+        
+        self.Domain.BlockFluidSiteCounts = BlockFluidSiteCounts
+        self.BlockDataLength = BlockDataLength
+        self.BlockStarts = BlockStarts
+        self.BlockEnds = BlockEnds
+
+        self.OnEndHeader()
+        return
+    
+    def _LoadBody(self):
+        """Reads all the Blocks from the file, iterating in a z-index
+        fastest fashion. Triggers OnBeginBody and OnEndBody.
+        """
+        self.OnBeginBody()
+        
+        dom = self.Domain
+        
+        nBlocks = dom.TotalBlocks
+        BlockCounts = dom.BlockCounts
+        
+        for bIjk, bIdx in self.Domain.BlockIndexer.IterBoth():
+            self._LoadBlock(dom, bIdx, bIjk)
+            continue
+        
+        self.OnEndBody()
+        
+    def _LoadBlock(self, domain, bIdx, bIjk):
+        """Loads a single Block. Iterating over Sites in a z-index
+        fastest fashion.
+        """
+        self.OnBeginBlock(bIdx, bIjk)
+        if domain.BlockFluidSiteCounts[bIjk] == 0:
+            b = AllSolidBlock(domain, bIdx)
+        else:
+            b = Block(domain, bIdx)
+        
+            b.nFluidSites = domain.BlockFluidSiteCounts[bIjk]
+            b.Sites = np.zeros(domain.BlockSize**3, dtype=object)
+        
+            blockLoader = xdrlib.Unpacker(self.File.read(self.BlockDataLength[bIjk]))
+            
+            # sl = site local
+            for slIjk, slIdx in domain.BlockSiteIndexer.IterBoth():
+                # sg = site global
+                sgIdx = domain.BlockSize * bIdx + slIdx
+                b.Sites[slIjk] = self._LoadSite(b, sgIdx, blockLoader)
+                continue
+            pass
+        
+        domain.SetBlock(bIdx, b)
+        
+        self.OnEndBlock(bIdx, bIjk)
+        return
+
+    def _LoadSite(self, block, sgIdx, loader):
+        """Loads a single site.
+        """
+        self.OnBeginSite(block, sgIdx)
+        
+        s = Site(block, sgIdx)
+        
+        s.Config = loader.unpack_uint()
+        # Solid and simple fluid, we are done loading
+        if not (s.Config == cfg.SOLID_TYPE or s.Config == cfg.FLUID_TYPE):
+            
+            if s.Type == cfg.INLET_TYPE or s.Type == cfg.OUTLET_TYPE:
+                s.BoundaryNormal = np.array([loader.unpack_double() for i in xrange(3)], dtype=np.double)
+                s.BoundaryDistance = loader.unpack_double()
+                            
+            if s.Edge:
+                s.WallNormal = np.array([loader.unpack_double() for i in xrange(3)], dtype=np.double)
+                s.WallDistance = loader.unpack_double()
+                                        
+            s.CutDistances = np.array([loader.unpack_double() for i in xrange(14)], dtype=np.double)
+            pass
+        
+        self.OnEndSite(block, s)
+        return s
+    
+    # Override these methods in your subclass.
+    def OnBeginPreamble(self):
+        return
+    def OnEndPreamble(self):
+        return
+    def OnBeginHeader(self):
+        return
+    def OnEndHeader(self):
+        return
+    def OnBeginBody(self):
+        return
+    def OnEndBody(self):
+        return
+    def OnBeginBlock(self, bIdx, bIjk):
+        return
+    def OnEndBlock(self, bIdx, bIjk):
+        return
+    def OnBeginSite(self, block, sgIdx):
+        return
+    def OnEndSite(self, block, site):
+        return
+    
+    pass
