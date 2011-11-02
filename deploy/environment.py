76b84e86d706e53e011440c3d98f483ba92ff2e5
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-02 18:23:16 +0000

    Example of using Python Fabric with HemeLB

---
+++ deploy/environment.py
@@ -0,0 +1,19 @@
+from fabric.api import *
+import os
+import posixpath
+env.hg='ssh://hg@entropy.chem.ucl.ac.uk'
+env.repository='hemelb'
+env.modules=[]
+env.pather=posixpath
+env.remote_directory='FabricHemeLb'
+
+def complete_environment():
+	env.repository_path=env.pather.join(env.remote_directory,env.repository)
+	env.tools_path=env.pather.join(env.repository_path,"Tools")
+	env.code_path=env.pather.join(env.repository_path,"Code")
+	env.regression_test_path=env.pather.join(env.repository_path,"RegressionTests","diffTest")
+	env.tools_build_path=env.pather.join(env.tools_path,'build',env.tools_build)
+	env.parmetis_path=env.pather.join(env.code_path,"parmetis")
+	module_commands=["module %s"%module for module in env.modules]
+	env.build_prefix=" && ".join(module_commands+["export HEMELB_MACHINE=%s"%env.HEMELB_MACHINE])
+	env.python_prefix="export PYTHONPATH=$PYTHONPATH:%s"%env.pather.join("~",env.tools_build_path)
