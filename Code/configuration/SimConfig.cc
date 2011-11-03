a934db8c940071f27d08970588ef318fbe8d30ec
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-03 18:54:20 +0000

    Add new SimConfig configuration folder

---
+++ Code/configuration/SimConfig.cc
@@ -0,0 +1,317 @@
+#include "SimConfig.h"
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+
+#include "util/fileutils.h"
+#include "debug/Debugger.h"
+
+namespace hemelb
+{
+  namespace configuration
+  {
+    // TODO really, the functions that take a TiXML object, a bool, a string and some basic type
+    // should go into some Tiny XML abstraction layer. This would remove the need for the IOLet
+    // types to know about the SimConfig object (and get rid of a circular dependency).
+
+    SimConfig::SimConfig()
+    {
+      // This constructor only exists to prevent instantiation without
+      // using the static load method.
+    }
+
+    SimConfig::~SimConfig()
+    {
+    }
+
+    SimConfig *SimConfig::Load(const char *iPath)
+    {
+      util::check_file(iPath);
+      TiXmlDocument *lConfigFile = new TiXmlDocument();
+      lConfigFile->LoadFile(iPath);
+
+      SimConfig *lRet = new SimConfig();
+
+      lRet->DoIO(lConfigFile->FirstChildElement(), true);
+      lRet->DataFilePath = util::NormalizePathRelativeToPath(lRet->DataFilePath, iPath);
+      delete lConfigFile;
+
+      return lRet;
+    }
+
+    void SimConfig::Save(std::string iPath)
+    {
+      TiXmlDocument lConfigFile;
+      TiXmlDeclaration * lDeclaration = new TiXmlDeclaration("1.0", "", "");
+      TiXmlElement *lTopElement = new TiXmlElement("hemelbsettings");
+
+      lConfigFile.LinkEndChild(lDeclaration);
+      lConfigFile.LinkEndChild(lTopElement);
+
+      DoIO(lTopElement, false);
+
+      lConfigFile.SaveFile(iPath);
+    }
+
+    void SimConfig::DoIO(TiXmlElement *iTopNode, bool iIsLoading)
+    {
+      TiXmlElement* lSimulationElement = GetChild(iTopNode, "simulation", iIsLoading);
+      DoIO(lSimulationElement, "cycles", iIsLoading, NumCycles);
+      DoIO(lSimulationElement, "cyclesteps", iIsLoading, StepsPerCycle);
+
+      TiXmlElement* lGeometryElement = GetChild(iTopNode, "geometry", iIsLoading);
+
+      if (lGeometryElement != NULL)
+      {
+        DoIO(GetChild(lGeometryElement, "datafile", iIsLoading), "path", iIsLoading, DataFilePath);
+      }
+
+      DoIO(GetChild(iTopNode, "inlets", iIsLoading), iIsLoading, Inlets, "inlet");
+
+      DoIO(GetChild(iTopNode, "outlets", iIsLoading), iIsLoading, Outlets, "outlet");
+
+      TiXmlElement* lVisualisationElement = GetChild(iTopNode, "visualisation", iIsLoading);
+      DoIO(GetChild(lVisualisationElement, "centre", iIsLoading), iIsLoading, VisCentre);
+      TiXmlElement *lOrientationElement = GetChild(lVisualisationElement, "orientation", iIsLoading);
+      DoIO(lOrientationElement, "longitude", iIsLoading, VisLongitude);
+      DoIO(lOrientationElement, "latitude", iIsLoading, VisLatitude);
+
+      TiXmlElement *lDisplayElement = GetChild(lVisualisationElement, "display", iIsLoading);
+
+      DoIO(lDisplayElement, "zoom", iIsLoading, VisZoom);
+      DoIO(lDisplayElement, "brightness", iIsLoading, VisBrightness);
+
+      TiXmlElement *lRangeElement = GetChild(lVisualisationElement, "range", iIsLoading);
+
+      DoIO(lRangeElement, "maxvelocity", iIsLoading, MaxVelocity);
+      DoIO(lRangeElement, "maxstress", iIsLoading, MaxStress);
+    }
+
+    void SimConfig::DoIO(TiXmlElement* iParent,
+                         std::string iAttributeName,
+                         bool iIsLoading,
+                         float &value)
+    {
+      if (iIsLoading)
+      {
+        char *dummy;
+        value = (float) std::strtod(iParent->Attribute(iAttributeName)->c_str(), &dummy);
+      }
+      else
+      {
+        // This should be ample.
+        char lStringValue[20];
+
+        // %g uses the shorter of decimal / mantissa-exponent notations.
+        // 6 significant figures will be written.
+        sprintf(lStringValue, "%.6g", value);
+
+        iParent->SetAttribute(iAttributeName, lStringValue);
+      }
+    }
+
+    void SimConfig::DoIO(TiXmlElement* iParent,
+                         std::string iAttributeName,
+                         bool iIsLoading,
+                         double &value)
+    {
+      if (iIsLoading)
+      {
+        const std::string* data = iParent->Attribute(iAttributeName);
+
+        if (data != NULL)
+        {
+          char *dummy;
+          value = std::strtod(iParent->Attribute(iAttributeName)->c_str(), &dummy);
+        }
+        else
+        {
+          value = 0.0;
+        }
+      }
+      else
+      {
+        // This should be ample.
+        char lStringValue[20];
+
+        // %g uses the shorter of decimal / mantissa-exponent notations.
+        // 6 significant figures will be written.
+        sprintf(lStringValue, "%.6g", value);
+
+        iParent->SetAttribute(iAttributeName, lStringValue);
+      }
+    }
+
+    void SimConfig::DoIO(TiXmlElement* iParent,
+                         std::string iAttributeName,
+                         bool iIsLoading,
+                         std::string &iValue)
+    {
+      if (iIsLoading)
+      {
+        // Compare to 0 not NULL, because that's what Attribute in TinyXml returns
+        if (iParent->Attribute(iAttributeName) == 0)
+        {
+          iValue = "";
+        }
+        else
+        {
+          iValue = std::string(iParent->Attribute(iAttributeName)->c_str());
+        }
+      }
+      else
+      {
+        if (iValue != "")
+        {
+          iParent->SetAttribute(iAttributeName, iValue);
+        }
+      }
+    }
+
+    void SimConfig::DoIO(TiXmlElement* iParent,
+                         std::string iAttributeName,
+                         bool iIsLoading,
+                         long &bValue)
+    {
+      if (iIsLoading)
+      {
+        char *dummy;
+        // Read in, in base 10.
+        bValue = std::strtol(iParent->Attribute(iAttributeName)->c_str(), &dummy, 10);
+      }
+      else
+      {
+        // This should be ample.
+        char lStringValue[20];
+
+        // %ld specifies long integer style.
+        sprintf(lStringValue, "%ld", bValue);
+
+        iParent->SetAttribute(iAttributeName, lStringValue);
+      }
+    }
+
+    void SimConfig::DoIO(TiXmlElement* iParent,
+                         std::string iAttributeName,
+                         bool iIsLoading,
+                         unsigned long &bValue)
+    {
+      if (iIsLoading)
+      {
+        char *dummy;
+        // Read in, in base 10.
+        bValue = std::strtoul(iParent->Attribute(iAttributeName)->c_str(), &dummy, 10);
+      }
+      else
+      {
+        // This should be ample.
+        char lStringValue[20];
+
+        // %ld specifies long integer style.
+        sprintf(lStringValue, "%ld", bValue);
+
+        iParent->SetAttribute(iAttributeName, lStringValue);
+      }
+    }
+
+    void SimConfig::DoIO(TiXmlElement *iParent, bool iIsLoading, std::vector<
+        lb::boundaries::iolets::InOutLet*> &bResult, std::string iChildNodeName)
+    {
+      if (iIsLoading)
+      {
+        TiXmlElement *lCurrentLet = iParent->FirstChildElement(iChildNodeName);
+
+        while (lCurrentLet != NULL)
+        {
+          // Determine which InOutlet to create
+          // This is done by checking if a path is specified
+          std::string PFilePath;
+          DoIO(GetChild(GetChild(iParent, iChildNodeName, iIsLoading), "pressure", iIsLoading),
+               "path",
+               iIsLoading,
+               PFilePath);
+          lb::boundaries::iolets::InOutLet *lNew;
+          if (PFilePath == "")
+          {
+            // If no file is specified we use a cosine trace
+            lNew = new lb::boundaries::iolets::InOutLetCosine();
+          }
+          else
+          {
+            // If there is a file specified we use it
+            lNew = new lb::boundaries::iolets::InOutLetFile();
+          }
+
+          lNew->DoIO(GetChild(iParent, iChildNodeName, iIsLoading), iIsLoading, this);
+          bResult.push_back(lNew);
+          lCurrentLet = lCurrentLet->NextSiblingElement(iChildNodeName);
+        }
+      }
+      else
+      {
+        for (unsigned int ii = 0; ii < bResult.size(); ii++)
+        {
+          // NB we're good up to 99 io-lets here.
+          bResult[ii]->DoIO(GetChild(iParent, iChildNodeName, iIsLoading), iIsLoading, this);
+        }
+      }
+    }
+
+    void SimConfig::DoIO(TiXmlElement *iParent,
+                         bool iIsLoading,
+                         lb::boundaries::iolets::InOutLetCosine* const value)
+    {
+      TiXmlElement* lPositionElement = GetChild(iParent, "position", iIsLoading);
+      TiXmlElement* lNormalElement = GetChild(iParent, "normal", iIsLoading);
+      TiXmlElement* lPressureElement = GetChild(iParent, "pressure", iIsLoading);
+
+      DoIO(lPressureElement, "mean", iIsLoading, value->PressureMeanPhysical);
+      DoIO(lPressureElement, "amplitude", iIsLoading, value->PressureAmpPhysical);
+      DoIO(lPressureElement, "phase", iIsLoading, value->Phase);
+      value->PressureMinPhysical = value->PressureMeanPhysical - value->PressureAmpPhysical;
+      value->PressureMaxPhysical = value->PressureMeanPhysical + value->PressureAmpPhysical;
+
+      DoIO(lPositionElement, iIsLoading, value->Position);
+      DoIO(lNormalElement, iIsLoading, value->Normal);
+    }
+
+    void SimConfig::DoIO(TiXmlElement *iParent,
+                         bool iIsLoading,
+                         lb::boundaries::iolets::InOutLetFile* const value)
+    {
+      TiXmlElement* lPositionElement = GetChild(iParent, "position", iIsLoading);
+      TiXmlElement* lNormalElement = GetChild(iParent, "normal", iIsLoading);
+      TiXmlElement* lPressureElement = GetChild(iParent, "pressure", iIsLoading);
+
+      DoIO(lPressureElement, "path", iIsLoading, value->PressureFilePath);
+
+      DoIO(lPositionElement, iIsLoading, value->Position);
+      DoIO(lNormalElement, iIsLoading, value->Normal);
+    }
+
+    void SimConfig::DoIO(TiXmlElement *iParent, bool iIsLoading, util::Vector3D &iValue)
+    {
+      DoIO(iParent, "x", iIsLoading, iValue.x);
+      DoIO(iParent, "y", iIsLoading, iValue.y);
+      DoIO(iParent, "z", iIsLoading, iValue.z);
+    }
+
+    TiXmlElement *SimConfig::GetChild(TiXmlElement *iParent,
+                                      std::string iChildNodeName,
+                                      bool iIsLoading)
+    {
+      if (iIsLoading)
+      {
+        return iParent->FirstChildElement(iChildNodeName);
+      }
+      else
+      {
+        TiXmlElement* lNewChild = new TiXmlElement(iChildNodeName);
+        iParent->LinkEndChild(lNewChild);
+        return lNewChild;
+      }
+    }
+  }
+}
---
+++ Code/configuration/SimConfig.h
@@ -0,0 +1,67 @@
+#ifndef HEMELB_SIMCONFIG_H
+#define HEMELB_SIMCONFIG_H
+
+#include <string.h>
+#include <vector>
+#include "xml/tinyxml.h"
+#include "util/Vector3D.h"
+#include "lb/boundaries/iolets/InOutLets.h"
+
+namespace hemelb
+{
+  namespace configuration
+  {
+    class SimConfig
+    {
+      public:
+        static SimConfig *Load(const char *iPath);
+        ~SimConfig();
+
+        void Save(std::string iPath);
+
+        std::string DataFilePath;
+        // These have to contain pointers because there are multiple derived types that might be
+        // instantiated.
+        std::vector<lb::boundaries::iolets::InOutLet*> Inlets;
+        std::vector<lb::boundaries::iolets::InOutLet*> Outlets;
+        util::Vector3D VisCentre;
+        float VisLongitude;
+        float VisLatitude;
+        float VisZoom;
+        float VisBrightness;
+        float MaxVelocity;
+        float MaxStress;
+        unsigned long NumCycles;
+        long StepsPerCycle;
+
+        void DoIO(TiXmlElement *iXmlNode,
+                  bool iIsLoading,
+                  lb::boundaries::iolets::InOutLetCosine* value);
+        void DoIO(TiXmlElement *iXmlNode,
+                  bool iIsLoading,
+                  lb::boundaries::iolets::InOutLetFile* value);
+      protected:
+        SimConfig();
+
+      private:
+        void DoIO(TiXmlElement *iXmlNode, bool iIsLoading);
+        void DoIO(TiXmlElement* iXmlNode, std::string iAttributeName, bool iIsLoading, long &value);
+        void DoIO(TiXmlElement* iXmlNode,
+                  std::string iAttributeName,
+                  bool iIsLoading,
+                  unsigned long &value);
+        void DoIO(TiXmlElement* iXmlNode, std::string iAttributeName, bool iIsLoading, float &value);
+        void DoIO(TiXmlElement* iXmlNode, std::string iAttributeName, bool iIsLoading, double &value);
+        void DoIO(TiXmlElement* iXmlNode,
+                  std::string iAttributeName,
+                  bool iIsLoading,
+                  std::string &iValue);
+        void DoIO(TiXmlElement *iXmlNode, bool iIsLoading, std::vector<
+            lb::boundaries::iolets::InOutLet*> &value, std::string iChildNodeName);
+        void DoIO(TiXmlElement *iXmlNode, bool iIsLoading, util::Vector3D &iValue);
+        TiXmlElement* GetChild(TiXmlElement *iParent, std::string iChildNodeName, bool iIsLoading);
+    };
+  }
+}
+
+#endif /* HEMELB_SIMCONFIG_H */
