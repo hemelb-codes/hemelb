922452ff39bef73c1d4687f554da602c678bca05
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-04 17:14:42 +0000

    Refactor phase 4: Create fileManager class to handle paths and names

---
+++ Code/reporting/FileManager.cc
@@ -0,0 +1,123 @@
+#include "FileManager.h"
+namespace hemelb
+{
+  namespace reporting
+  {
+    FileManager::FileManager(configuration::CommandLine &commandLine, const bool & io, const int & processorCount):
+                                        options(commandLine), ok(false),doIo(io)
+    {
+
+      inputFile=options.GetInputFile();
+      outputDir=options.GetOutputDir();
+
+      GuessOutputDir();
+
+      imageDirectory = outputDir + "/Images/";
+      snapshotDirectory = outputDir + "/Snapshots/";
+
+      if (doIo)
+      {
+        if (hemelb::util::DoesDirectoryExist(outputDir.c_str()))
+        {
+          hemelb::log::Logger::Log<hemelb::log::Info, hemelb::log::Singleton>("\nOutput directory \"%s\" already exists. Exiting.",
+                                                                              outputDir.c_str());
+          return;
+        }
+
+
+        hemelb::util::MakeDirAllRXW(outputDir);
+        hemelb::util::MakeDirAllRXW(imageDirectory);
+        hemelb::util::MakeDirAllRXW(snapshotDirectory);
+
+        InitialiseReport(processorCount);
+      }
+      ok=true;
+    }
+
+    FileManager::~FileManager()
+    {
+      if (doIo)
+      {
+        fclose(mTimingsFile);
+      }
+    }
+    const std::string & FileManager::GetInputFile() const {
+      return(inputFile);
+    }
+    const std::string & FileManager::GetSnapshotDirectory() const {
+      return(snapshotDirectory);
+    }
+    const std::string & FileManager::GetImageDirectory() const {
+      return(imageDirectory);
+    }
+
+
+    void FileManager::EmptyOutputDirectories()
+    {
+      hemelb::util::DeleteDirContents(snapshotDirectory);
+      hemelb::util::DeleteDirContents(imageDirectory);
+    }
+
+    void FileManager::InitialiseReport(const int & processorCount)
+    {
+      char timings_name[256];
+      char procs_string[256];
+
+      sprintf(procs_string, "%i", processorCount);
+      strcpy(timings_name, outputDir.c_str());
+      strcat(timings_name, "/timings");
+      strcat(timings_name, procs_string);
+      strcat(timings_name, ".asc");
+
+      mTimingsFile = fopen(timings_name, "w");
+      ReportHeader();
+    }
+
+    hemelb::io::XdrFileWriter * FileManager::XdrImageWriter(const long int time){
+      char filename[255];
+      snprintf(filename, 255, "%08li.dat", time);
+      return(new hemelb::io::XdrFileWriter(imageDirectory + std::string(filename)));
+    }
+
+    const std::string FileManager::SnapshotPath(unsigned long time) const {
+      char snapshot_filename[255];
+      snprintf(snapshot_filename, 255, "snapshot_%06li.dat", time);
+      return(snapshotDirectory+std::string(snapshot_filename)); // by copy
+    }
+
+    void FileManager::SaveConfiguration(configuration::SimConfig * simConfig)
+    {
+      if (doIo) {
+        simConfig->Save(outputDir + "/" + configLeafName);
+      }
+    }
+
+    void FileManager::ReportHeader()
+    {
+      fprintf(mTimingsFile, "***********************************************************\n");
+      fprintf(mTimingsFile, "Opening config file:\n %s\n", inputFile.c_str());
+    }
+
+    void FileManager::GuessOutputDir()
+    {
+      unsigned long lLastForwardSlash = inputFile.rfind('/');
+      if (lLastForwardSlash == std::string::npos)
+      {
+        // input file supplied is in current folder
+        configLeafName= inputFile;
+        if (outputDir.length() == 0)
+        {
+          // no output dir given, defaulting to local.
+          outputDir="./results";
+        }
+      } else {
+        // input file supplied is a path to the input file
+        configLeafName=  inputFile.substr(lLastForwardSlash);
+        if (outputDir.length() == 0) {
+          // no output dir given, defaulting to location of input file.
+          outputDir=inputFile.substr(0, lLastForwardSlash)+"results";
+        }
+      }
+    }
+  }
+}
