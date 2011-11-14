725d6f7e629b9ccd82416f0b0bd25766c63f39eb
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-14 18:38:38 +0000

    Factor report file into its own class

--- Code/reporting/FileManager.h
+++ Code/reporting/FileManager.h
@@ -5,6 +5,7 @@
 #include "configuration/CommandLine.h"
 #include "util/fileutils.h"
 #include "log/Logger.h"
+#include "reporting/Reporter.h"
 #include "configuration/SimConfig.h"
 #include "io/XdrFileWriter.h"
 namespace hemelb
@@ -20,14 +21,11 @@ namespace hemelb
         {
           return(!ok);
         }
-        void ReportPhase1(long int site_count, int total_time_steps, long int cycle_id, double simulation_time, bool unstable,
-                          unsigned long time_steps_per_cycle,
-                          double decomposition_time, double initialise_time, double read_time, double creation_time);
-        void ReportProcessorTimings(std::string *const names,double *const mins,double *const means,double *const maxes);
+        Reporter *Report() {return report;}
+
         const std::string & GetInputFile() const;
         const std::string & GetSnapshotDirectory() const;
         const std::string & GetImageDirectory() const;
-        FILE * ReportFile() {return(mTimingsFile);}
         void SaveConfiguration(configuration::SimConfig * simConfig);
         void EmptyOutputDirectories();
         hemelb::io::XdrFileWriter * XdrImageWriter(const long int time);
@@ -36,7 +34,7 @@ namespace hemelb
         void ReportHeader();
         void GuessOutputDir();
         void InitialiseReport(const int & processorCount);
-        FILE *mTimingsFile;
+        Reporter *report;
         std::string outputDir;
         std::string inputFile;
         std::string snapshotDirectory;
