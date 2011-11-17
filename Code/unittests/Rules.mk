2d9b79b78b980a806b59cca7ca85f39595e58f77
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-17 17:48:45 +0000

    Don't build the unittests on legion until cppunit is there

--- Code/unittests/Rules.mk
+++ Code/unittests/Rules.mk
@@ -1,9 +1,9 @@
 include $(MK)/header.mk
-
+ifndef NOTESTS
 TARGETS := libHemeLbUnitTests.$(LIBEXT)
 
 $(TARGETS)_DEPS = main.o
 
 INCLUDES_$(d) := $(INCLUDES_$(parent))
-
+endif
 include $(MK)/footer.mk
