95967aefd6627b737a5b236db8f379a3fa13d653
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-17 17:54:09 +0000

    Update fabric to build OK on legion

--- deploy/machines.py
+++ deploy/machines.py
@@ -12,10 +12,7 @@ def legion():
 	env.hosts=['ucgajhe@legion.rc.ucl.ac.uk']
 	env.HEMELB_MACHINE='Legion'
 	env.tools_build="lib.linux-x86_64-2.6"
-	#env.modules.append("purge")
-	#env.modules.append("load ucl")
+	env.modules.append("remove ofed/qlogic/intel/64/1.2.7")
+	env.modules.append("add ofed/openmpi/gcc/64/1.4.1")
 	env.modules.append("load cmake")
-	#env.modules.append("load gcc")
-	env.modules.append("unload ofed/qlogic")
-	env.modules.append("load ofed/openmpi/intel/64/1.4.1")
 	complete_environment()
