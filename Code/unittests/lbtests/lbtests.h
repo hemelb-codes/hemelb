5fd5cfd01df157024d38714e665e66025637d613
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-21 13:19:32 +0000

    Move simulation master tests skeleton to top level, as it is not part of lb package

--- Code/unittests/lbtests/lbtests.h
+++ Code/unittests/lbtests/lbtests.h
@@ -5,7 +5,6 @@
 #include "unittests/lbtests/CollisionTests.h"
 #include "unittests/lbtests/StreamerTests.h"
 #include "unittests/lbtests/RheologyModelTests.h"
-#include "unittests/lbtests/SimulationMasterTests.h"
 
 
 #endif /* HEMELB_UNITTESTS_LBTESTS_LBTESTS_H */
