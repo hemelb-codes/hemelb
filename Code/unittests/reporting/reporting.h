7d693fbb39d0ab7860514de0085ccc3005fba188
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-22 14:12:33 +0000

    First test for reporter

--- Code/unittests/reporting/reporting.h
+++ Code/unittests/reporting/reporting.h
@@ -2,5 +2,6 @@
 #define HEMELB_UNITTESTS_REPORTING_REPORTING_H
 
 #include "unittests/reporting/TimerTests.h"
+#include "unittests/reporting/ReporterTests.h"
 
 #endif /* HEMELB_UNITTESTS_REPORTING_REPORTING_H */
