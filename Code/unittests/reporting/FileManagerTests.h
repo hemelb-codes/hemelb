b51fb029ff6c2864aa6643273285c7ed4102eb23
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-23 15:38:53 +0000

    Test to better exercise name invention, and a correction for functionality tested

--- Code/unittests/reporting/FileManagerTests.h
+++ Code/unittests/reporting/FileManagerTests.h
@@ -13,26 +13,26 @@ namespace hemelb
       class FileManagerTests : public FolderTestFixture
       {
           CPPUNIT_TEST_SUITE(FileManagerTests);
-          CPPUNIT_TEST(TestCreate);
+          CPPUNIT_TEST(TestCreateLocalConfig);
+          CPPUNIT_TEST(TestNameInventionLocalConfig);
+          CPPUNIT_TEST(TestCreatePathConfig);
+          CPPUNIT_TEST(TestNameInventionPathConfig);
           CPPUNIT_TEST_SUITE_END();
         public:
           void setUp()
           {
             FolderTestFixture::setUp();
-            int argc=9;
-            const char* argv[9];
-            int processorCount=5;
+            argc=9;
+            processorCount=5;
             argv[0]="hemelb";
-            argv[1]="-in";
             argv[2]="config.xml";
+            argv[1]="-in";
             argv[3]="-i";
             argv[4]="1";
             argv[5]="-s";
             argv[6]="1";
             argv[7]="-ss";
             argv[8]="1111";
-            configuration::CommandLine cl=configuration::CommandLine(argc,argv);
-            fileManager=new FileManager(cl,true,processorCount);
           }
 
           void tearDown()
@@ -41,16 +41,51 @@ namespace hemelb
             delete fileManager;
           }
 
-          void TestCreate(){
+          void TestCreateLocalConfig(){
+            ConstructManager();
             AssertPresent("results");
             AssertPresent("results/Images");
             AssertPresent("results/Snapshots");
           }
 
+          void TestNameInventionLocalConfig(){
+            ConstructManager();
+            CPPUNIT_ASSERT_EQUAL(std::string("./results/timings5.asc"),fileManager->GetReportPath());
+          }
+
+          void TestCreatePathConfig(){
+            ConstructPathConfigManager();
+            AssertPresent("results");
+            AssertPresent("results/Images");
+            AssertPresent("results/Snapshots");
+          }
+
+          void TestNameInventionPathConfig(){
+            ConstructPathConfigManager();
+            CPPUNIT_ASSERT_EQUAL(GetTempdir()+"/results/timings5.asc",fileManager->GetReportPath());
+          }
+
         private:
 
+          void ConstructManager(){
+            configuration::CommandLine cl=configuration::CommandLine(argc,argv);
+            fileManager=new FileManager(cl,true,processorCount);
+          }
+
+          void ConstructPathConfigManager(){
+            std::string targetConfig=GetTempdir()+"/config.xml"; // note this resource doesn't exist -- not a problem
+            argv[2]=targetConfig.c_str();
+            ReturnToOrigin(); // even if we're not in current dir, explicit path should cause the results to be created in the tmpdir
+            ConstructManager();
+            MoveToTempdir(); // go back to the tempdir and check the files were created in the right place
+          }
+
+          int argc;
+          int processorCount;
+          const char* argv[9];
           FileManager *fileManager;
       };
+
 
       CPPUNIT_TEST_SUITE_REGISTRATION(FileManagerTests);
     }
