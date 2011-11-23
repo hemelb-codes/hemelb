4f62e339e9029604f1d63d03862ad3bdc9baec90
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-23 16:56:18 +0000

    Resource infrastructure for tests, exemplar for SimConfig

---
+++ Code/unittests/configuration/SimConfigTests.h
@@ -0,0 +1,36 @@
+#ifndef HEMELB_UNITTESTS_CONFIGURATION_SIMCONFIGTESTS_H
+#define HEMELB_UNITTESTS_CONFIGURATION_SIMCONFIGTESTS_H
+#include "configuration/SimConfig.h"
+#include "unittests/resources/Resource.h"
+namespace hemelb{
+  namespace unittests{
+    namespace configuration{
+      using namespace hemelb::configuration;
+
+      class SimConfigTests : public CppUnit::TestFixture
+      {
+        CPPUNIT_TEST_SUITE( SimConfigTests );
+        CPPUNIT_TEST( TestConstruct );
+        CPPUNIT_TEST_SUITE_END();
+        public:
+        void setUp(){
+          exemplar=Resource("config.xml").Path();
+          config = SimConfig::Load( exemplar.c_str());
+        }
+        void tearDown(){
+          delete config;
+        }
+
+        void TestConstruct(){
+          // smoke test the configuration as having loaded OK
+          CPPUNIT_ASSERT_EQUAL(3lu,config->NumCycles);
+        }
+        private:
+        std::string exemplar;
+        SimConfig * config;
+      };
+      CPPUNIT_TEST_SUITE_REGISTRATION( SimConfigTests );
+    }
+  }
+}
+#endif // ONCE
