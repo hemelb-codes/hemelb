ef7d70ec56455a74b795dbe6c55ddc9ef8c4109c
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-23 12:42:33 +0000

    FileManager tests

--- Code/unittests/reporting/reporting.h
+++ Code/unittests/reporting/reporting.h
@@ -3,5 +3,6 @@
 
 #include "unittests/reporting/TimerTests.h"
 #include "unittests/reporting/ReporterTests.h"
+#include "unittests/reporting/FileManagerTests.h"
 
 #endif /* HEMELB_UNITTESTS_REPORTING_REPORTING_H */
