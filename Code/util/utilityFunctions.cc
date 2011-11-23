44814df67edd5595b2450b1a22491b57730ace16
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-23 16:13:19 +0000

    Fix platform nonportability

--- Code/util/utilityFunctions.cc
+++ Code/util/utilityFunctions.cc
@@ -10,14 +10,6 @@ namespace hemelb
       return MPI_Wtime();
     }
 
-    std::string GetUUID(){
-      uuid_t uuid;
-      uuid_generate_random ( uuid );
-      char s[37];
-      uuid_unparse ( uuid, s );
-      return std::string(s);
-    }
-
   }
 
 }
