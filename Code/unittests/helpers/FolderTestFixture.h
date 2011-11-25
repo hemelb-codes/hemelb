96bc5c756ea7effdccd5eeb9a21995bed469af40
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-25 10:54:00 +0000

    Move helper into correct namespace

--- Code/unittests/helpers/FolderTestFixture.h
+++ Code/unittests/helpers/FolderTestFixture.h
@@ -5,47 +5,50 @@
 #include <iomanip>
 namespace hemelb{
   namespace unittests{
-    class FolderTestFixture: public CppUnit::TestFixture {
+    namespace helpers{
+      class FolderTestFixture: public CppUnit::TestFixture {
 
-      public:
-      void setUp(){
-        std::stringstream temp_path_stream;
-        // next line is a hack to get the build working again
-        // TODO: find a portable uuid solution. BOOST?
-        temp_path_stream<< util::GetTemporaryDir()<<"/"<<"HemeLBTest"<< std::fixed << floor(util::myClock()*100000) << std::flush;
-        temp_path=temp_path_stream.str();
-        // store current location
-        origin=util::GetCurrentDir();
+        public:
+        void setUp(){
+          std::stringstream temp_path_stream;
+          // next line is a hack to get the build working again
+          // TODO: find a portable uuid solution. BOOST?
+          temp_path_stream<< util::GetTemporaryDir()<<"/"<<"HemeLBTest"<< std::fixed << floor(util::myClock()*100000) << std::flush;
+          temp_path=temp_path_stream.str();
+          // store current location
+          origin=util::GetCurrentDir();
 
-        // create a folder to work in
-        util::MakeDirAllRXW(temp_path);
-        MoveToTempdir();
-      }
+          // create a folder to work in
+          util::MakeDirAllRXW(temp_path);
+          MoveToTempdir();
+        }
 
-      void tearDown(){
-        ReturnToOrigin();
-        // doesn't matter not to clean up in tempdir.
-      }
+        void tearDown(){
+          ReturnToOrigin();
+          // doesn't matter not to clean up in tempdir.
+        }
 
-      protected:
-      void ReturnToOrigin(){
-        // return to origin
-        util::ChangeDirectory(origin);
-      }
+        protected:
+        void ReturnToOrigin(){
+          // return to origin
+          util::ChangeDirectory(origin);
+        }
 
-      void MoveToTempdir(){
-        util::ChangeDirectory(GetTempdir());
-      }
-      void AssertPresent(const std::string &fname){
-        CPPUNIT_ASSERT(util::DoesDirectoryExist(fname.c_str()));
-      }
-      const std::string & GetTempdir(){
-        return temp_path;
-      }
-      private:
-      std::string origin;
-      std::string temp_path;
-    };
+        void MoveToTempdir(){
+          util::ChangeDirectory(GetTempdir());
+        }
+        void AssertPresent(const std::string &fname){
+          // "does directory exist" actually works for files too.
+          CPPUNIT_ASSERT(util::DoesDirectoryExist(fname.c_str()));
+        }
+        const std::string & GetTempdir(){
+          return temp_path;
+        }
+        private:
+        std::string origin;
+        std::string temp_path;
+      };
+    }
   }
 }
 #endif // ONCE
