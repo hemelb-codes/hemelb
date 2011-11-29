3d0d4c58bd8a7de8da0d436269056e0c5324698f
James Hetherington <j.hetherington@ucl.ac.uk>
2011-11-29 10:47:39 +0000

    Run simulation master during unittests

--- Code/unittests/configuration/configuration.h
+++ Code/unittests/configuration/configuration.h
@@ -2,5 +2,6 @@
 #define HEMELB_UNITTESTS_CONFIGURATION_H
 
 #include "unittests/configuration/SimConfigTests.h"
+#include "unittests/configuration/CommandLineTests.h"
 
 #endif /* HEMELB_UNITTESTS_CONFIGURATION_CONFIGURATION_H */
