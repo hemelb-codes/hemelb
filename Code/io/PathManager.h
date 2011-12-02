410580ab99311d78cf1b78c2b92f734ed0d7c79d
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-02 17:47:23 +0000

    Doxygen

--- Code/io/PathManager.h
+++ Code/io/PathManager.h
@@ -11,27 +11,76 @@ namespace hemelb
 {
   namespace io
   {
+    /**
+     * Manage the input and output file system locations for HemeLB images, snapshots, reports, and input xml and dat files.
+     */
     class PathManager
     {
       public:
+        /**
+         * During construction, the path manager will guess an appropriate path from the input file path given at the command line,
+         * if one was not specified explicitly.
+         * @param commandLine The command-line parser object instantiated from the arguments to the C main function.
+         * @param io Set to true if this MPI node is the I/O process and files should be written from this node.
+         * @param processorCount The total count of processors, used in generating file-names for report files.
+         */
         PathManager(const configuration::CommandLine & commandLine,
                     const bool &io,
                     const int &processorCount);
+        /**
+         * After construction, check this to see if a problem occurred. We are not using exceptions.
+         * @return True if the path manager could not construct input and output locations OK.
+         */
+        // TODO replace with an exception
         bool HasProblems() const
         {
           return (!ok);
         }
+        /**
+         * A local or full path to the input xml configuration file.
+         * @return A local or full path to the input xml configuration file.
+         */
         const std::string & GetInputFile() const;
+        /**
+         * Path to the directory where snapshot files should be written.
+         * @return Reference to path to the directory where snapshot files should be written.
+         */
         const std::string & GetSnapshotDirectory() const;
+        /**
+         * Path to the directory where image files should be written.
+         * @return Reference to path to the directory where image files should be written.
+         */
         const std::string & GetImageDirectory() const;
+        /**
+         * Path to where a run report file should be created.
+         * @return Referene to path to where a run report file should be created.
+         */
         const std::string & GetReportPath() const;
+        /**
+         * Save the current configuration as a configuration xml file to the output folder.
+         * @param simConfig The input configuration instance, constructed from the input xml file.
+         */
         void SaveConfiguration(configuration::SimConfig * const simConfig) const;
+        /**
+         * Delete the output data.
+         * This deletes snapshots, images, config files, and the report.
+         * It is used if the simulation needs to reset.
+         */
         void EmptyOutputDirectories() const;
+        /**
+         * Generate an xdr file writer to use to save an image file.
+         * @param time The current time, used to generate a unique filename.
+         * @return Pointer to an XDR file writer -- this is allocated on free store, and should be deleted by the client code.
+         */
         hemelb::io::writers::xdr::XdrFileWriter * XdrImageWriter(const long int time) const;
+        /**
+         * Returns the file path that should be used for writing a snapshot file
+         * @param time The current time, used to generate a unique filename.
+         * @return The string path to use for writing a snapshot file, returned by copy.
+         */
         const std::string SnapshotPath(unsigned long time) const;
       private:
-        void GuessOutputDir();
-        void InitialiseReport(const int & processorCount);
+        void GuessOutputDir(); //! String processing to generate an appropriate outptu folder name.
         std::string outputDir;
         std::string inputFile;
         std::string snapshotDirectory;
@@ -40,7 +89,7 @@ namespace hemelb
         std::string reportName;
         const configuration::CommandLine &options;
         bool ok;
-        bool doIo;
+        bool doIo; //! Am I the input/output node?
     };
   }
 }
