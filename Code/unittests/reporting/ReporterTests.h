f654a4b17c386bfaa9ee3a7cac1ec5f2ffca4006
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-02 15:16:02 +0000

    Coding convention compliance

--- Code/unittests/reporting/ReporterTests.h
+++ Code/unittests/reporting/ReporterTests.h
@@ -25,8 +25,7 @@ namespace hemelb
           CPPUNIT_TEST(TestCycle);
           CPPUNIT_TEST(TestImage);
           CPPUNIT_TEST(TestSnapshot);
-          CPPUNIT_TEST(TestMainReport);
-          CPPUNIT_TEST_SUITE_END();
+          CPPUNIT_TEST(TestMainReport);CPPUNIT_TEST_SUITE_END();
         public:
           void setUp()
           {
@@ -48,35 +47,20 @@ namespace hemelb
 
           void TestCycle()
           {
-            can_count(std::mem_fun(&ReporterMock::Cycle), std::string("cycle id"));
+            CheckRepeatedCallIncrementsCounter(std::mem_fun(&ReporterMock::Cycle),
+                                               std::string("cycle id"));
           }
           void TestImage()
           {
-            can_count(std::mem_fun(&ReporterMock::Image), std::string("Image written"));
+            CheckRepeatedCallIncrementsCounter(std::mem_fun(&ReporterMock::Image),
+                                               std::string("Image written"));
           }
           void TestSnapshot()
           {
-            can_count(std::mem_fun(&ReporterMock::Snapshot), std::string("Snapshot written"));
+            CheckRepeatedCallIncrementsCounter(std::mem_fun(&ReporterMock::Snapshot),
+                                               std::string("Snapshot written"));
           }
 
-          void can_count(std::mem_fun_t<void, ReporterMock> method,const std::string &message)
-          {
-            for (int times = 0; times < 5; times++)
-            {
-              method(reporter);
-              std::stringstream expectation;
-              expectation << message << ": " << times + 1 << std::endl;
-              back_has_substring(expectation.str());
-            }
-          }
-
-          void back_has_substring(const std::string &expectation)
-          {
-            std::string &actual = reporter->Results().back();
-            CPPUNIT_ASSERT_MESSAGE(expectation + " : " + actual,
-                                   std::string::npos != actual.find(expectation));
-            reporter->Results().pop_back();
-          }
           void TestMainReport()
           {
             // Mock up some timings
@@ -107,49 +91,71 @@ namespace hemelb
             // now we validate the lines from the report
             for (int j = Timers::numberOfTimers - 1; j >= 0; j--)
             {
-              table_expectation(j);
+              CheckTimingsTable(j);
             }
-            back_has_substring("Local \tMin \tMean \tMax");
-            back_has_substring("Per-proc timing data");
+            CheckLastMessageContains("Local \tMin \tMean \tMax");
+            CheckLastMessageContains("Per-proc timing data");
             for (int j = 4; j >= 0; j--)
             {
               std::stringstream expectation;
               expectation << "rank: " << j << ", fluid sites: " << j * 1000 << std::flush;
-              back_has_substring(expectation.str());
+              CheckLastMessageContains(expectation.str());
             }
-            back_has_substring("Sub-domains info:");
-            back_has_substring("total time (s):                            0");
+            CheckLastMessageContains("Sub-domains info:");
+            CheckLastMessageContains("total time (s):                            0");
             reporter->Results().pop_back();
             reporter->Results().pop_back();
             reporter->Results().pop_back();
-            back_has_substring("time steps per cycle: 500");
-            back_has_substring("time steps per second: 11.111");
-            back_has_substring("cycles and total time steps: 2, 1000");
-            back_has_substring("fluid sites: 1234");
-            back_has_substring("topology depths checked: 3");
-            back_has_substring("threads: 5, machines checked: 4");
+            CheckLastMessageContains("time steps per cycle: 500");
+            CheckLastMessageContains("time steps per second: 11.111");
+            CheckLastMessageContains("cycles and total time steps: 2, 1000");
+            CheckLastMessageContains("fluid sites: 1234");
+            CheckLastMessageContains("topology depths checked: 3");
+            CheckLastMessageContains("threads: 5, machines checked: 4");
           }
 
-          void table_expectation(unsigned int row)
+        private:
+
+          void CheckRepeatedCallIncrementsCounter(std::mem_fun_t<void, ReporterMock> method
+                                                  ,const std::string &message)
+          {
+            for (int times = 0; times < 5; times++)
+            {
+              method(reporter);
+              std::stringstream expectation;
+              expectation << message << ": " << times + 1 << std::endl;
+              CheckLastMessageContains(expectation.str());
+            }
+          }
+
+          void CheckLastMessageContains(const std::string &expectation)
+          {
+            std::string &actual = reporter->Results().back();
+            CPPUNIT_ASSERT_MESSAGE(expectation + " : " + actual,
+                                   std::string::npos != actual.find(expectation));
+            reporter->Results().pop_back();
+          }
+
+          void CheckTimingsTable(unsigned int row)
           {
             std::stringstream expectation;
             expectation << std::setprecision(3);
-            double normn = 1.0;
+            double normalisation = 1.0;
             if (row == Timers::snapshot)
             {
-              normn = 4.0;
+              normalisation = 4.0;
             }
             if (row == Timers::visualisation)
             {
-              normn = 3.0;
+              normalisation = 3.0;
             }
             if (row == Timers::lb || row == Timers::mpiSend || row == Timers::mpiWait)
             {
-              normn = 2.0;
+              normalisation = 2.0;
             }
-            expectation << timerNames[row] << "\t\t" << row * 10.0 << "\t" << row * 15.0 / normn
-                << "\t" << row * 10.0 / normn << "\t" << row * 5.0 / normn << std::flush;
-            back_has_substring(expectation.str());
+            expectation << timerNames[row] << "\t\t" << row * 10.0 << "\t" << row * 15.0 / normalisation
+                << "\t" << row * 10.0 / normalisation << "\t" << row * 5.0 / normalisation << std::flush;
+            CheckLastMessageContains(expectation.str());
           }
 
         private:
