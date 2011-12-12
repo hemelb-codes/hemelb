34f9b4971e936b22eda49c164d5c2718416f10be
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-12 14:02:58 +0000

    Working towards nice clean fabric code

--- deploy/templates.py
+++ deploy/templates.py
@@ -1,10 +1,12 @@
-from machines import *
+from fabric.api import *
 from string import Template
 
-def fill_in_template(template_name,**binding):
+def fill_in_template(template_name,**arguments):
 	source=open(os.path.join(env.localroot,'deploy','templates',template_name))
+	binding=env.copy()
+	binding.update(arguments)
 	result=Template(source.read()).substitute(binding)
-	destname=os.path.join(env.localroot,'deploy','templates',"%s.sh"%binding['name'])
+	destname=os.path.join(env.localroot,'.jobscripts','templates',"%s.sh"%binding['name'])
 	target=open(destname,'w')
 	target.write(result)
 	return destname
