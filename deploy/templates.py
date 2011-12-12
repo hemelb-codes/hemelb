c68847114181f969d496de0e3cb887901e1a0418
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-12 14:29:55 +0000

    Fabric script work

--- deploy/templates.py
+++ deploy/templates.py
@@ -1,12 +1,13 @@
 from fabric.api import *
 from string import Template
+import os
 
 def fill_in_template(template_name,**arguments):
 	source=open(os.path.join(env.localroot,'deploy','templates',template_name))
 	binding=env.copy()
 	binding.update(arguments)
 	result=Template(source.read()).substitute(binding)
-	destname=os.path.join(env.localroot,'.jobscripts','templates',"%s.sh"%binding['name'])
+	destname=os.path.join(env.localroot,'deploy','.jobscripts',"%s.sh"%binding['name'])
 	target=open(destname,'w')
 	target.write(result)
 	return destname
