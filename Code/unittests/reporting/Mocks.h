39ca0b357b4f90c5e4026727815ba73e140d1bf2
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-15 15:25:32 +0000

    Use CTemplate to make reports - reports are dummies, need to be written

--- Code/unittests/reporting/Mocks.h
+++ Code/unittests/reporting/Mocks.h
@@ -65,48 +65,6 @@ namespace hemelb
         private:
           unsigned int calls;
       };
-
-      class WriterMock
-      {
-        public:
-          WriterMock(const std::string &path) :
-              results(0)
-          {
-            CPPUNIT_ASSERT_EQUAL(std::string("mock_path"), path);
-          }
-          ~WriterMock()
-          {
-          }
-          std::vector<std::string> & Results()
-          {
-           Stream();
-            return results;
-          }
-
-        protected:
-          void Print(const char * format, ...)
-          {
-            char buffer[1000];
-            std::va_list arg;
-            va_start(arg, format);
-            vsprintf(buffer, format, arg);
-            va_end(arg);
-            Stream() << std::string(buffer) << std::endl;
-          }
-
-          std::ostream & Stream()
-          {
-            if (buffer.str() != "")
-            {
-              results.push_back(buffer.str());
-              buffer.str("");
-            }
-            return buffer;
-          }
-        private:
-          std::vector<std::string> results;
-          std::stringstream buffer;
-      };
     }
   }
 }
