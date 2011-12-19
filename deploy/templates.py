3b52db1d976f0d723b68bb5806cf2757fe7f6483
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-19 17:49:16 +0000

    Document Fabric scripts

--- deploy/templates.py
+++ deploy/templates.py
@@ -1,8 +1,21 @@
+"""
+Use python standard library templates to allow strings to include $foo syntax to interpolate elements from the 
+Fabric environment dictionary, and to generate job queue submission scripts therefrom.
+
+Job-queue submission scripts should be stored in deploy/templates, with filenames like legion-hemelb (for a script used to
+launch hemelb jobs on legion, or hector-unittest, for a script used to launch unit-testing jobs on hector.)
+"""
 from fabric.api import *
 from string import Template
 import os
 
 def script_template(template_name):
+	"""
+	Load a template of the given name, and fill it in based on the Fabric environment dictionary,
+	storing the result in deploy/.scripts/job-name.sh
+	job-name is loaded from the environment dictionary.
+	Return value is the path of the generated script.
+	"""
 	source=open(os.path.join(env.localroot,'deploy','templates',template_name))
 	result=template(source.read())
 	destname=os.path.join(env.localroot,'deploy','.jobscripts',env['name']+'.sh')
