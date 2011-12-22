94ac96fb397c8c88fc24fd1640a6bd2f919de482
James Hetherington <j.hetherington@ucl.ac.uk>
2011-12-22 13:35:09 +0000

    Add option to permit Prefer static library for CTemplate for Hector

--- dependencies/Modules/FindCTemplate.cmake
+++ dependencies/Modules/FindCTemplate.cmake
@@ -12,10 +12,18 @@ IF( CTEMPLATE_INCLUDE_DIR )
 ENDIF( CTEMPLATE_INCLUDE_DIR )
 
 FIND_PATH( CTEMPLATE_INCLUDE_DIR "ctemplate/template.h")
+if(CTEMPLATE_USE_STATIC)
+set(__old_cmake_find_lib_suffixes ${CMAKE_FIND_LIBRARY_SUFFIXES})
+	set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
 
 FIND_LIBRARY( CTEMPLATE_LIBRARIES
               NAMES "ctemplate"
               PATH_SUFFIXES "ctemplate" )
+
+if(CTEMPLATE_USE_STATIC)
+	set(CMAKE_FIND_LIBRARY_SUFFIXES ${__old_cmake_find_lib_suffixes})
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and set CTEMPLATE_FOUND to TRUE if
 # all listed variables are TRUE
