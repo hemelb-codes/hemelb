64e39753d8fbbc611816add490a4c2f5a2066db5
Hywel Carver <hywelc@gmail.com>
2012-01-21 15:58:01 +0000

    Update the geometry parser as per the new format. #103
    --HG--
    branch : newGeometryFormat

--- Tools/hemeTools/parsers/__init__.py
+++ Tools/hemeTools/parsers/__init__.py
@@ -9,5 +9,34 @@ followed by another 4 bytes identifying the particular type/version,
 that number being terminated by the EOF character (0x04).
 """
 
+import numpy as np
+
 HemeLbMagicNumber = 0x686c6221
 GeometryMagicNumber = 0x676d7904
+D3Q27Directions = np.array([[-1,-1,-1],
+        [-1,-1, 0],
+        [-1,-1,+1],
+        [-1, 0,-1],
+        [-1, 0, 0],
+        [-1, 0,+1],
+        [-1,+1,-1],
+        [-1,+1, 0],
+        [-1,+1,+1],
+        [ 0,-1,-1],
+        [ 0,-1, 0],
+        [ 0,-1,+1],
+        [ 0, 0,-1],
+        #[ 0, 0, 0],
+        [ 0, 0,+1],
+        [ 0,+1,-1],
+        [ 0,+1, 0],
+        [ 0,+1,+1],
+        [+1,-1,-1],
+        [+1,-1, 0],
+        [+1,-1,+1],
+        [+1, 0,-1],
+        [+1, 0, 0],
+        [+1, 0,+1],
+        [+1,+1,-1],
+        [+1,+1, 0],
+        [+1,+1,+1]])
