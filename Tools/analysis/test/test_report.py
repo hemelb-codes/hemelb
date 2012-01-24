40b8037b0b1f92a89c4f26517edb4d5a5470aeb0
James Hetherington <j.hetherington@ucl.ac.uk>
2012-01-24 12:39:10 +0000

    Fix name of file in generated docstrings re #114

--- Tools/analysis/test/test_report.py
+++ Tools/analysis/test/test_report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-test_analysis.py
+test_report.py
 
 Created by James Hetherington on 2012-01-23.
 Copyright (c) 2012 __MyCompanyName__. All rights reserved.
