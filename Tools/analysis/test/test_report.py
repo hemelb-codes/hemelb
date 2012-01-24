23ece73539b8e49d9b644de88dbe453cd5c2077a
James Hetherington <j.hetherington@ucl.ac.uk>
2012-01-24 18:29:36 +0000

    Allow to write csv file re #114

--- Tools/analysis/test/test_report.py
+++ Tools/analysis/test/test_report.py
@@ -9,18 +9,28 @@ Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 
 import os
 import unittest
+import tempfile
 
 from ..report import Report
 from ..graph import Graph
+from ..results_collection import ResultsCollection
 import fixtures
 
 class TestReport(unittest.TestCase):
     def setUp(self):
         self.graphs={'performance_versus_cores': Graph(fixtures.GraphConfig('performance_versus_cores'))}
+        self.r=Report(fixtures.ReportConfig('planck_performance'),self.graphs)
+        self.results=ResultsCollection(fixtures.Results('cylinders').path,fixtures.ResultsConfig('example'))
     def test_construct(self):
-        r=Report(fixtures.ReportConfig('planck_performance'),self.graphs)
+        r=self.r
         self.assertEqual('Performance on planck',r.name)
         self.assertEqual({'type':'hemelb','machine':'planck'},r.graphs['performance_versus_cores'].select)
         self.assertEqual(['cores'],r.graphs['performance_versus_cores'].curves)
         self.assertEqual(['total'],r.graphs['performance_versus_cores'].dependent)
         self.assertEqual(['steps'],r.graphs['performance_versus_cores'].independent)
+    def test_prepare(self):
+        self.r.prepare(self.results.results)
+    def test_write(self):
+        self.r.prepare(self.results.results)
+        self.r.write(tempfile.mkdtemp())
+        
