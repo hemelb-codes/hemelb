4acb72885bf12aa98206c1f56b667528146d3f2a
James Hetherington <j.hetherington@ucl.ac.uk>
2012-01-24 12:37:59 +0000

    Additional tests for report, re #114

--- Tools/analysis/test/test_report.py
+++ Tools/analysis/test/test_report.py
@@ -15,10 +15,12 @@ from ..graph import Graph
 import fixtures
 
 class TestReport(unittest.TestCase):
-	def setUp(self):
-	    self.graphs={'performance_versus_cores': Graph(fixtures.GraphConfig('performance_versus_cores'))}
-	def test_construct(self):
-		r=Report(fixtures.ReportConfig('planck_performance'),self.graphs)
-		self.assertEqual('Performance on planck',r.name)
-		
-		
+    def setUp(self):
+        self.graphs={'performance_versus_cores': Graph(fixtures.GraphConfig('performance_versus_cores'))}
+    def test_construct(self):
+        r=Report(fixtures.ReportConfig('planck_performance'),self.graphs)
+        self.assertEqual('Performance on planck',r.name)
+        self.assertEqual({'type':'hemelb','machine':'planck'},r.graphs['performance_versus_cores'].select)
+        self.assertEqual(['cores'],r.graphs['performance_versus_cores'].curves)
+        self.assertEqual(['total'],r.graphs['performance_versus_cores'].dependent)
+        self.assertEqual(['steps'],r.graphs['performance_versus_cores'].independent)
