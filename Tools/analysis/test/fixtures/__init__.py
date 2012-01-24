04ac4c5f3389a0ad4f6083830b211aa4637ede85
James Hetherington <j.hetherington@ucl.ac.uk>
2012-01-24 12:47:30 +0000

    Additional tests re #114

--- Tools/analysis/test/fixtures/__init__.py
+++ Tools/analysis/test/fixtures/__init__.py
@@ -5,6 +5,9 @@ fixtures_path=os.path.dirname(os.path.abspath(__file__))
 class Results(object):
     def __init__(self,path):
         self.path=os.path.join(fixtures_path,'results',path)
+        self.results=os.listdir(self.path)
+    def result_path(self,index):
+        return os.path.join(self.path,self.results[index])
         
 class GraphConfig(dict):
     def __init__(self,label):
