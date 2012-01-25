60bdb0731f01a4c36099caaf50341c29e8ffed78
James Hetherington <j.hetherington@ucl.ac.uk>
2012-01-25 17:28:44 +0000

    #114, add curve values to results table

--- Tools/analysis/graph.py
+++ Tools/analysis/graph.py
@@ -39,10 +39,10 @@ class Graph(object):
             return tuple([getattr(result,prop) for prop in self.curves])
         self.curve_results={key:list(group) for key,group in itertools.groupby(sorted(self.filtered_results,key=curve_key),curve_key)}
         # for now, support only a single dependent and a single independent variable on each curve
-        self.curve_data={curve:[ (getattr(result,self.dependent[0]),getattr(result,self.independent[0])) for result in results ] for curve,results in self.curve_results.iteritems()}
+        self.curve_data={curve:[ (getattr(result,self.independent[0]),getattr(result,self.dependent[0]))+curve for result in results ] for curve,results in self.curve_results.iteritems()}
     def write_data(self,csv_file):
         writer=csv.writer(csv_file)
-        fieldnames=[self.dependent[0],self.independent[0]]
+        fieldnames=[self.independent[0],self.dependent[0]]+self.curves
         csv_file.write("#CSV File produced by hemelb reporter\n")
         csv_file.write("#Groups are separated by "+str(tuple(self.curves))+"\n#")
         writer.writerow(fieldnames)
