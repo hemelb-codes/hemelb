d4c413d0e18fdaae0626ce2e96af9c020b305f70
James Hetherington <j.hetherington@ucl.ac.uk>
2012-01-25 16:53:29 +0000

    Re #114, tests to exercise master analysis object, and fixes to make them pass.

--- Tools/analysis/test/test_analysis.py
+++ Tools/analysis/test/test_analysis.py
@@ -15,13 +15,21 @@ import fixtures
 import tempfile 
 
 class TestAnalysis(unittest.TestCase):
-	def setUp(self):
-	    self.config={
-	        'graphs': {'performance_versus_cores':fixtures.GraphConfig('performance_versus_cores')},
-	        'reports': {'planck_performance':fixtures.ReportConfig('planck_performance')},
-	        'results_path': fixtures.Results('cylinders').path,
-	        'reports_path': tempfile.mkdtemp(),
-	        'results': fixtures.ResultsConfig('example')
-	    }
-	def test_construct(self):
-		a=Analysis(self.config)
+    def setUp(self):
+        self.config={
+            'graphs': {'performance_versus_cores':fixtures.GraphConfig('performance_versus_cores')},
+            'reports': {'planck_performance':fixtures.ReportConfig('planck_performance')},
+            'results_path': fixtures.Results('cylinders').path,
+            'reports_path': fixtures.ReportOutput('reports').path,
+            'results': fixtures.ResultsConfig('example')
+        }
+        self.a=Analysis(self.config)
+    def test_construct(self):
+        self.assertEqual(Analysis,type(self.a))
+    def test_prepare(self):
+        self.a.load_data()
+        self.a.prepare()
+    def test_write(self):
+        self.a.load_data()
+        self.a.prepare()
+        self.a.write()
