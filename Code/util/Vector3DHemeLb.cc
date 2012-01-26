5f852600ff5a0f92336edbbae03ad5c0761986c3
Rupert Nash <rupert.nash@ed.ac.uk>
2012-01-26 18:44:54 +0000

    Factor util/Vector3D.h so it's ready for use by the setuptool (#115)
    The setuptool doesn't use MPI, so here I put the necessary template
    specialisations in util/Vector3DHemeLb.cc. Factor the index error
    handling into a base class, with a static method as the handler. This
    delegates to a class static member variable (function pointer) that is
    initialised (in HemeLB) in util/Vector3DHemeLb.cc
    
    The setuptool's Vec3 had some methods that were not implemented in
    Vector3D, so implement them.
    
    Give proper Doxygen comments to everything.
    
    Rename a few methods and make the appropriate updates in HemeLB.

---
+++ Code/util/Vector3DHemeLb.cc
@@ -0,0 +1,52 @@
+#include "util/Vector3D.h"
+#include "mpiInclude.h"
+#include "units.h"
+#include <cassert>
+#include "log/Logger.h"
+
+namespace hemelb
+{
+  template<typename vectorType>
+  MPI_Datatype GenerateTypeForVector()
+  {
+    const int typeCount = 1;
+    int blocklengths[typeCount] = { 3 };
+
+    MPI_Datatype types[typeCount] = { MpiDataType<vectorType> () };
+
+    MPI_Aint displacements[typeCount] = { 0 };
+
+    MPI_Datatype ret;
+
+    MPI_Type_struct(typeCount, blocklengths, displacements, types, &ret);
+
+    MPI_Type_commit(&ret);
+    return ret;
+  }
+
+  template<>
+  MPI_Datatype MpiDataTypeTraits<hemelb::util::Vector3D<float> >::RegisterMpiDataType()
+  {
+    return GenerateTypeForVector<float> ();
+  }
+
+  template<>
+  MPI_Datatype MpiDataTypeTraits<hemelb::util::Vector3D<site_t> >::RegisterMpiDataType()
+  {
+    return GenerateTypeForVector<site_t> ();
+  }
+  namespace util
+  {
+    namespace
+    {
+      void DefaultHandlerFunction(int direction)
+      {
+        // TODO need to find a way of handling this case better.
+        log::Logger::Log<log::Info, log::OnePerCore>("Failed while accessing a direction in Vector3D.");
+        assert(false);
+      }
+    }
+    Vector3DBase::HandlerFunction* Vector3DBase::handler = DefaultHandlerFunction;
+
+  }
+}
