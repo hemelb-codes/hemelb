6624923c2fcbbe53e4b81dd3bd87ac06b60b9445
James Hetherington <j.hetherington@ucl.ac.uk>
2012-02-03 11:58:40 +0000

    Fix indentation CR #114

--- Tools/analysis/test/test_curate.py
+++ Tools/analysis/test/test_curate.py
@@ -14,14 +14,13 @@ import StringIO
 from ..curate import Curation
 
 class TestCuration(unittest.TestCase):
-	def setUp(self):
-	    self.buff=StringIO.StringIO()
-	    self.rc=Curation(fixtures.Results('cylinders').path,fixtures.ResultsConfig('example'),['myprog','name'],self.buff)
-	def test_construct(self):
-	    self.assertEqual(27,len(self.rc.filtered_results))
-	    self.assertEqual(self.rc.action.action,'name')
-	def test_name(self):
-	    print self.buff.getvalue()
-	    self.rc.act()
-	    self.assertEqual(self.buff.getvalue().split('\n')[0],'cylinder_0_001_1000_3_546058666e20_planck_1')
-		
+    def setUp(self):
+        self.buff=StringIO.StringIO()
+        self.rc=Curation(fixtures.Results('cylinders').path,fixtures.ResultsConfig('example'),['myprog','name'],self.buff)
+    def test_construct(self):
+        self.assertEqual(27,len(self.rc.filtered_results))
+        self.assertEqual(self.rc.action.action,'name')
+    def test_name(self):
+        print self.buff.getvalue()
+        self.rc.act()
+        self.assertEqual(self.buff.getvalue().split('\n')[0],'cylinder_0_001_1000_3_546058666e20_planck_1')
