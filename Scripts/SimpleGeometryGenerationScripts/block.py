0e1b70e37989f741627ca5a583822c30b8a94e98
Rupert Nash <rupert.nash@ed.ac.uk>
2012-02-10 17:01:52 +0000

    #104, #101: four_cube produces compressed blocks with correct fluid site count
    --HG--
    branch : newGeometryFormat

--- Scripts/SimpleGeometryGenerationScripts/block.py
+++ Scripts/SimpleGeometryGenerationScripts/block.py
@@ -1,6 +1,14 @@
 from functools import reduce
 from Site import Site
+
 class Block(object):
     def __init__(self,sites):
         self.sites=sites
-        self.bytes=sum(map(Site.bytes,self.sites))
+
+    def get_number_of_fluid_sites(self):
+        n = 0
+        for site in self.sites:
+            if site.site_type == Site.fluid_site:
+                n += 1
+        return n
+    
