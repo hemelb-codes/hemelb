028d1b62ae5c91f6f932210ae9e741ac34fa89bb
James Hetherington <j.hetherington@ucl.ac.uk>
2012-02-29 14:11:24 +0000

    Indentation and space changes, per CR of #142
    --HG--
    branch : newGeometryFormat

--- Code/geometry/Decomposition.h
+++ Code/geometry/Decomposition.h
@@ -8,32 +8,38 @@ namespace hemelb
   namespace geometry
   {
     /**
-    Class defining HemeLB domain decomposition
-    Used by geometry reader to know where to send which blocks.
-    **/
+     Class defining HemeLB domain decomposition
+     Used by geometry reader to know where to send which blocks.
+     **/
     /*
-    JH Note: Class created to make the new decomposition communication strategy in #142 testable.
-    Will not initially contain all decomposition-related code, will gradually add this.
-    Eventually, will do the decomposition, and have clean interfaces to provide each site's owned and needed blocks during geometry reading.
-    Site ownership during simulation will not be managed by this class, but by the lattice data as currently.
-    */
+     JH Note: Class created to make the new decomposition communication strategy in #142 testable.
+     Will not initially contain all decomposition-related code, will gradually add this.
+     Eventually, will do the decomposition, and have clean interfaces to provide each site's owned and needed blocks during geometry reading.
+     Site ownership during simulation will not be managed by this class, but by the lattice data as currently.
+     */
     template<class Net> class DecompositionBase
     {
-    public:
-      DecompositionBase(const site_t BlockCount, bool *readBlock, const proc_t areadingGroupSize, Net &anet, MPI_Comm comm,
-        const proc_t rank, const proc_t size); // Temporarily during the refactor, constructed just to abstract the block sharing bit
-      const std::vector<proc_t> & ProcessorsNeedingBlock(const site_t &block) const {
-        return procsWantingBlocksBuffer[block];
-      }
-      
-      proc_t GetReadingCoreForBlock(const site_t blockNumber) const;
-    private:
-      std::vector<std::vector<proc_t> > procsWantingBlocksBuffer;
-      Net &net;
-      MPI_Comm decompositionCommunicator;
-      const proc_t decompositionCommunicatorRank;
-      const proc_t decompositionCommunicatorSize;
-      const proc_t readingGroupSize;
+      public:
+        DecompositionBase(const site_t BlockCount,
+                          bool *readBlock,
+                          const proc_t areadingGroupSize,
+                          Net &anet,
+                          MPI_Comm comm,
+                          const proc_t rank,
+                          const proc_t size); // Temporarily during the refactor, constructed just to abstract the block sharing bit
+        const std::vector<proc_t> & ProcessorsNeedingBlock(const site_t &block) const
+        {
+          return procsWantingBlocksBuffer[block];
+        }
+
+        proc_t GetReadingCoreForBlock(const site_t blockNumber) const;
+      private:
+        std::vector<std::vector<proc_t> > procsWantingBlocksBuffer;
+        Net &net;
+        MPI_Comm decompositionCommunicator;
+        const proc_t decompositionCommunicatorRank;
+        const proc_t decompositionCommunicatorSize;
+        const proc_t readingGroupSize;
     };
     typedef DecompositionBase<net::Net> Decomposition;
   }
