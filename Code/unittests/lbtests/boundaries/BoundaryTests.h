825249ecb815edadc9d990a242cf490e0640e348
James Hetherington <j.hetherington@ucl.ac.uk>
2012-03-14 16:43:48 +0000

    Tests for boundaryValues collection, re #150

--- Code/unittests/lbtests/boundaries/BoundaryTests.h
+++ Code/unittests/lbtests/boundaries/BoundaryTests.h
@@ -28,21 +28,58 @@ namespace hemelb
         {
           CPPUNIT_TEST_SUITE(BoundaryTests);
               CPPUNIT_TEST(TestConstruct);
+              CPPUNIT_TEST(TestUpdate);
             CPPUNIT_TEST_SUITE_END();
           public:
             void setUp()
             {
               FourCubeBasedTestFixture::setUp();
+
+              inlets = new BoundaryValues(hemelb::geometry::INLET_TYPE,
+                                          latDat,
+                                          simConfig->Inlets,
+                                          simState,
+                                          unitConverter);
             }
             void tearDown()
             {
+              delete inlets;
               FourCubeBasedTestFixture::tearDown();
             }
           private:
             void TestConstruct()
             {
-              BoundaryValues inlets(hemelb::geometry::INLET_TYPE, latDat, simConfig->Inlets, simState, unitConverter);
+              double targetStartDensity = pressureToDensity(80.0-1.0);
+              CPPUNIT_ASSERT_EQUAL(targetStartDensity, inlets->GetBoundaryDensity(0));
             }
+            void TestUpdate()
+            {
+              inlets->RequestComms();
+              double targetStartDensity = pressureToDensity(80.0 - 1.0);
+              CPPUNIT_ASSERT_EQUAL(targetStartDensity, inlets->GetBoundaryDensity(0));
+              for (unsigned int step = 0; step < simState->GetTotalTimeSteps() / 2; step++)
+              {
+                simState->Increment();
+              }
+              inlets->RequestComms();
+              double targetMidDensity = pressureToDensity(80.0 + 1.0);
+              CPPUNIT_ASSERT_EQUAL(targetMidDensity, inlets->GetBoundaryDensity(0));
+              for (unsigned int step = 0; step < simState->GetTotalTimeSteps() / 2; step++)
+              {
+                simState->Increment();
+              }
+              inlets->RequestComms();
+              double targetEndDensity = pressureToDensity(80.0 - 1.0);
+              CPPUNIT_ASSERT_EQUAL(targetEndDensity, inlets->GetBoundaryDensity(0));
+            }
+            double pressureToDensity(double pressure)
+            {
+              double inverse_velocity = simState->GetTimeStepLength() / latDat->GetVoxelSize();
+              return 1
+                  + ( pressure - REFERENCE_PRESSURE_mmHg) * mmHg_TO_PASCAL * inverse_velocity * inverse_velocity
+                      / (Cs2 * BLOOD_DENSITY_Kg_per_m3);
+            }
+            BoundaryValues *inlets;
         };
         //BoundaryTests
         CPPUNIT_TEST_SUITE_REGISTRATION(BoundaryTests);
