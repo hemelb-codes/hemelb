934e4d58bef7ead43422ec5d4c7ba0306656b1c5
James Hetherington <j.hetherington@ucl.ac.uk>
2012-03-15 21:02:54 +0000

    Re #151, create multiscale iolets and multiscale simulation master, with test

--- Code/unittests/multiscale/MockIntercommunicatorTests.h
+++ Code/unittests/multiscale/MockIntercommunicatorTests.h
@@ -1,10 +1,10 @@
-#ifndef HEMELB_UNITTEST_TEST_INTERCOMMUNICATOR_EXAMPLE_H
-#define HEMELB_UNITTEST_TEST_INTERCOMMUNICATOR_EXAMPLE_H
+#ifndef HEMELB_UNITTEST_MULTISCALE_MOCKINTERCOMMUNICATORTESTS_H
+#define HEMELB_UNITTEST_MULTISCALE_MOCKINTERCOMMUNICATORTESTS_H
 #include "unittests/multiscale/MockIntercommunicand.h"
 #include "resources/Resource.h"
 #include "unittests/multiscale/MockIntercommunicator.h"
+#include "multiscale/MultiscaleSimulationMaster.h"
 
-using namespace hemelb::unittest::multiscale;
 #include <iostream>
 
 namespace hemelb
@@ -20,15 +20,15 @@ namespace hemelb
               inlet(), outlet(), inoutlettype("inoutlet"), intercomms(buffer), time_resolution(atime_resolution), space_resolution(aspace_resolution), current_time(0)
           {
             // The intercommunicators have a shared buffer which represents imaginary communication
-            inoutlettype.RegisterSharedValue<double>("pressure");
-            inoutlettype.RegisterSharedValue<double>("velocity");
+            inoutlettype.template RegisterSharedValue<double>("pressure");
+            inoutlettype.template RegisterSharedValue<double>("velocity");
             intercomms.RegisterIntercommunicand(inoutlettype, inlet, "boundary1");
             intercomms.RegisterIntercommunicand(inoutlettype, outlet, "boundary2");
           }
           MockIntercommunicand inlet;
           MockIntercommunicand outlet;
-          MockIntercommunicator::IntercommunicandTypeT inoutlettype;
-          MockIntercommunicator intercomms;
+          typename IntercommuniatorImplementation::IntercommunicandTypeT inoutlettype;
+          IntercommuniatorImplementation intercomms;
           double time_resolution;
           double space_resolution;
           double current_time;
@@ -60,15 +60,15 @@ namespace hemelb
               inlet(), outlet(), inoutlettype("inoutlet"), intercomms(buffer), time_resolution(atime_resolution), space_resolution(aspace_resolution), current_time(0)
           {
             // The intercommunicators have a shared buffer which represents imaginary communication
-            inoutlettype.RegisterSharedValue<double>("pressure");
-            inoutlettype.RegisterSharedValue<double>("velocity");
+            inoutlettype.template RegisterSharedValue<double>("pressure");
+            inoutlettype.template RegisterSharedValue<double>("velocity");
             intercomms.RegisterIntercommunicand(inoutlettype, inlet, "boundary2");
             intercomms.RegisterIntercommunicand(inoutlettype, outlet, "boundary1");
           }
           MockIntercommunicand inlet;
           MockIntercommunicand outlet;
-          MockIntercommunicator::IntercommunicandTypeT inoutlettype;
-          MockIntercommunicator intercomms;
+          typename IntercommuniatorImplementation::IntercommunicandTypeT inoutlettype;
+          IntercommuniatorImplementation intercomms;
           double time_resolution;
           double space_resolution;
           double current_time;
@@ -94,28 +94,29 @@ namespace hemelb
 
       };
 
-      class MockIntercommunicatorTests : public CppUnit::TestFixture
+      class MockIntercommunicatorTests : public FolderTestFixture
       {
           CPPUNIT_TEST_SUITE(MockIntercommunicatorTests);
           CPPUNIT_TEST(TestCRRun);
-          CPPUNIT_TEST_SUITE_END();
+          CPPUNIT_TEST(TestCHemeRun);CPPUNIT_TEST_SUITE_END();
         public:
           void setUp()
           {
             pbuffer = new std::map<std::string, double>();
             std::map<std::string, double> &buffer = *pbuffer;
-            buffer["boundary1_pressure"] = 120.0;
-            buffer["boundary2_pressure"] = 80.0;
+            buffer["boundary1_pressure"] = 81.0;
+            buffer["boundary2_pressure"] = 79.0;
             buffer["boundary1_velocity"] = 0.0;
             buffer["boundary2_velocity"] = 0.0;
             buffer["shared_time"] = 0.0;
-            mockheme = new MockHemeLB<MockIntercommunicator>(25.0, 25.0, buffer);
-            zerod = new Mock0DModel<MockIntercommunicator>(10.0, 10.0, buffer);
+            mockheme = new MockHemeLB<MockIntercommunicator>(25.0, 0.2, buffer);
+            zerod = new Mock0DModel<MockIntercommunicator>(10.0, 0.5, buffer);
 
           }
           void tearDown()
           {
-
+            delete zerod;
+            delete mockheme;
           }
         private:
           void TestCRRun()
@@ -134,9 +135,44 @@ namespace hemelb
               //    << " O: p: " << zerod->outlet.GetDensity() << " v:" << zerod->outlet.GetVelocity() << std::endl;
             }
 
-            CPPUNIT_ASSERT_EQUAL(mockheme->current_time, zerod->current_time);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(mockheme->current_time, zerod->current_time,1e-6);
+          }
+          void TestCHemeRun()
+          {
+            int argc;
+            const char* argv[9];
+            argc = 9;
+            argv[0] = "hemelb";
+            argv[2] = "four_cube_multiscale.xml";
+            argv[1] = "-in";
+            argv[3] = "-i";
+            argv[4] = "1";
+            argv[5] = "-s";
+            argv[6] = "1";
+            argv[7] = "-ss";
+            argv[8] = "1111";
+            FolderTestFixture::setUp();
+            CopyResourceToTempdir("four_cube_multiscale.xml");
+            CopyResourceToTempdir("four_cube.gmy");
+            hemelb::configuration::CommandLine options(argc, argv);
+            MockIntercommunicator intercomms(*pbuffer);
+            heme = new MultiscaleSimulationMaster<MockIntercommunicator>(options,intercomms);
+            // Mock out the behaviour of the simulation master iteration, but with the other model linked in.
+            while (heme->GetState()->GetTimeStepsPassed() <= heme->GetState()->GetTotalTimeSteps())
+            {
+              zerod->Simulate();
+              heme->DoTimeStep();
+              //std::cout << "Heme: I: p:" << heme->inlet.GetDensity() << " v:" << heme->inlet.GetVelocity() << " O: p: "
+              //    << heme->outlet.GetDensity() << " v:" << heme->outlet.GetVelocity() << std::endl;
+              std::cout << "Zerod: @" << zerod->current_time << " I: p:"  << zerod->inlet.GetDensity() << " v:" << zerod->inlet.GetVelocity()
+                  << " O: p: " << zerod->outlet.GetDensity() << " v:" << zerod->outlet.GetVelocity() << std::endl;
+            }
+            heme->Finalise();
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(heme->GetState()->GetTime(), zerod->current_time,1e-6);
+            delete heme;
           }
           MockHemeLB<MockIntercommunicator> *mockheme;
+          MultiscaleSimulationMaster<MockIntercommunicator> *heme;
           Mock0DModel<MockIntercommunicator> *zerod;
           std::map<std::string, double> *pbuffer;
       };
@@ -145,4 +181,4 @@ namespace hemelb
     } //multiscale
   } //unittests
 } //hemelb
-#endif // HEMELB_UNITTEST_TEST_INTERCOMMUNICATOR_EXAMPLE_H
+#endif  HEMELB_UNITTEST_MULTISCALE_MOCKINTERCOMMUNICATORTESTS_H
