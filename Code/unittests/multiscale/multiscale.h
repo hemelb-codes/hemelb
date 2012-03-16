a82ea089bbcf38abfedd4db686684f78e41f47c7
James Hetherington <j.hetherington@ucl.ac.uk>
2012-03-16 11:00:53 +0000

    Once guard bug fix #151

--- Code/unittests/multiscale/multiscale.h
+++ Code/unittests/multiscale/multiscale.h
@@ -1,1 +1,4 @@
+#ifndef HEMELB_UNITTESTS_MULTISCALE_MULTISCALE_H
+#define HEMELB_UNITTESTS_MULTISCALE_MULTISCALE_H
 #include "unittests/multiscale/MockIntercommunicatorTests.h"
+#endif // HEMELB_UNITTESTS_MULTISCALE_MULTISCALE_H
