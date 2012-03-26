67604638d7e4ba6da9ad94ed023805476ce5b339
James Hetherington <j.hetherington@ucl.ac.uk>
2012-03-26 18:25:38 +0100

    Changes per CR #193

--- Code/util/UnitConverter.cc
+++ Code/util/UnitConverter.cc
@@ -6,46 +6,46 @@ namespace hemelb
   namespace util
   {
 
-    UnitConverter::UnitConverter(lb::LbmParameters* iParams, lb::SimulationState* iState, double voxelSize) :
-      mParams(iParams), mState(iState), voxel_size(voxelSize), CharacteristicVelocity(voxel_size/ mState->GetTimeStepLength())
+    UnitConverter::UnitConverter(lb::LbmParameters* params, lb::SimulationState* state, double voxelSize) :
+      lbmParameters(params), simulationState(state), voxelSize(voxelSize), latticeSpeed(voxelSize/ simulationState->GetTimeStepLength())
     {
 
     }
 
     LatticeDensity UnitConverter::ConvertPressureToLatticeUnits(PhysicalPressure pressure) const
     {
-      return Cs2 + ConvertPressureGradToLatticeUnits(pressure - REFERENCE_PRESSURE_mmHg);
+      return Cs2 + ConvertPressureDifferenceToLatticeUnits(pressure - REFERENCE_PRESSURE_mmHg);
     }
 
     PhysicalPressure UnitConverter::ConvertPressureToPhysicalUnits(LatticePressure pressure) const
     {
-      return REFERENCE_PRESSURE_mmHg + ConvertPressureGradToPhysicalUnits(pressure  - Cs2);
+      return REFERENCE_PRESSURE_mmHg + ConvertPressureDifferenceToPhysicalUnits(pressure  - Cs2);
     }
 
-    LatticeDensity UnitConverter::ConvertPressureGradToLatticeUnits(double pressure_grad) const
+    LatticeDensity UnitConverter::ConvertPressureDifferenceToLatticeUnits(double pressure_grad) const
     {
-      return pressure_grad * mmHg_TO_PASCAL  / (CharacteristicVelocity * CharacteristicVelocity * BLOOD_DENSITY_Kg_per_m3);
+      return pressure_grad * mmHg_TO_PASCAL  / (latticeSpeed * latticeSpeed * BLOOD_DENSITY_Kg_per_m3);
     }
 
-    double UnitConverter::ConvertPressureGradToPhysicalUnits(LatticePressure pressure_grad) const
+    double UnitConverter::ConvertPressureDifferenceToPhysicalUnits(LatticePressure pressure_grad) const
     {
-      return pressure_grad * BLOOD_DENSITY_Kg_per_m3 * CharacteristicVelocity * CharacteristicVelocity / mmHg_TO_PASCAL;
+      return pressure_grad * BLOOD_DENSITY_Kg_per_m3 * latticeSpeed * latticeSpeed / mmHg_TO_PASCAL;
     }
 
     LatticeVelocity UnitConverter::ConvertVelocityToLatticeUnits(PhysicalVelocity velocity) const
     {
-      return velocity / CharacteristicVelocity;
+      return velocity / latticeSpeed;
     }
 
     LatticeStress UnitConverter::ConvertStressToLatticeUnits(PhysicalStress stress) const
     {
-      return stress / (CharacteristicVelocity*CharacteristicVelocity*BLOOD_DENSITY_Kg_per_m3);
+      return stress / (latticeSpeed*latticeSpeed*BLOOD_DENSITY_Kg_per_m3);
     }
 
     PhysicalStress UnitConverter::ConvertStressToPhysicalUnits(PhysicalStress stress) const
     {
       // convert stress from lattice units to physical units (Pa)
-      return stress * (CharacteristicVelocity*CharacteristicVelocity*BLOOD_DENSITY_Kg_per_m3);
+      return stress * (latticeSpeed*latticeSpeed*BLOOD_DENSITY_Kg_per_m3);
       // stress=Force per unit area=mass * length / time^2 / length^2=mass / length * time^2
       // = mass * (length/time)^2 / length^3
     }
