ec7d22cbb8730198134c6713431a8f4f3c9f238a
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2012-04-04 13:41:57 +0100

    #138, tidy up and document

--- Code/util/Vector3DArithmeticTraits.h
+++ Code/util/Vector3DArithmeticTraits.h
@@ -16,22 +16,21 @@ namespace hemelb
     struct Vector3DArithmeticTraits
     {
         /*
-         * One would like to write BOOST_STATIC_ASSERT_MSG(false,"..."), however if the
+         * One would like to write HEMELB_STATIC_ASSERT(false), however if the
          * static assertion is not dependent upon one or more template parameters, then
          * the compiler is permitted to evaluate the static assertion at the point it is
          * first seen, irrespective of whether the template is ever instantiated.
          *
-         * If the C++0x static_assert feature is not available, BOOST_STATIC_ASSERT_MSG(x, msg)
-         * will be treated as BOOST_STATIC_ASSERT(x) and unfortunately you won't see the message
-         *
          * See http://www.boost.org/doc/libs/1_49_0/doc/html/boost_staticassert.html
          */
-
-        ///@todo 138, reenable once BOOST is usable in other machines
-        //BOOST_STATIC_ASSERT_MSG(sizeof(T1) == 0, "Vector3D has not been tested with this combination of types");
         HEMELB_STATIC_ASSERT(sizeof(T1) == 0);
-        ///@todo 138, remove once BOOST is usable in other machines
-        //typedef T1 type;
+
+        /*
+         * Boost alternative including an error message. If the C++0x static_assert feature is
+         * not available, BOOST_STATIC_ASSERT_MSG(x, msg) will be treated as BOOST_STATIC_ASSERT(x)
+         * and unfortunately you won't see the message.
+         */
+        //BOOST_STATIC_ASSERT_MSG(sizeof(T1) == 0, "Vector3D has not been tested with this combination of types");
     };
 
     // Trivial case: both arguments share type.
