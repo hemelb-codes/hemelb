ea2e27f5c78a8d4bab9dbc6794010a1f1a8f774e
dholmes <unknown>
2012-04-11 11:08:02 +0100

    #264: obtain neighbour-list for ColloidController

--- Code/colloids/ColloidController.h
+++ Code/colloids/ColloidController.h
@@ -1,8 +1,10 @@
 #ifndef HEMELB_COLLOIDS_COLLOIDCONTROLLER_H
 #define HEMELB_COLLOIDS_COLLOIDCONTROLLER_H
 
+#include <vector>
 #include "net/net.h"
 #include "geometry/LatticeData.h"
+#include "util/Vector3D.h"
 #include "geometry/ReadResult.h"
 
 namespace hemelb
@@ -18,16 +20,33 @@ namespace hemelb
         // holds fluid information for local sites, i.e. the velocity distribution values
         geometry::LatticeData* mLatDat;
 
+        proc_t mLocalRank;
+
+        // a vector of the processors that might be interested in
+        // particles near the edge of this processor's sub-domain
+        std::vector<proc_t> mNeighbourProcessors;
+
+        typedef std::vector<util::Vector3D<site_t> > neighbourhood_t;
+        neighbourhood_t GetNeighbourhoodVectors(site_t distance);
+
+        void InitialiseNeighbourList(geometry::GeometryReadResult* gmyResult,
+                                     neighbourhood_t neighbourhood);
+
+        bool GetLocalInformationForGlobalSite(geometry::GeometryReadResult* gmyResult,
+                                              util::Vector3D<site_t> globalLocationForSite,
+                                              site_t* blockIdForSite,
+                                              site_t* localSiteIdForSite,
+                                              proc_t* ownerRankForSite);
+
       public:
         // constructor - called by SimulationMaster::Initialise()
         ColloidController(net::Net* net,
-            geometry::LatticeData* latDat,
-            geometry::GeometryReadResult* gmyResult);
+                          geometry::LatticeData* latDatLBM,
+                          geometry::GeometryReadResult* gmyResult);
 
         // destructor - called by SimulationMaster::~SimulationMaster()
         ~ColloidController();
     };
-
   }
 }
 
