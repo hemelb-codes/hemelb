2d5d85aa5031d352c769522524074638c4eb6122
James Hetherington <j.hetherington@ucl.ac.uk>
2012-04-16 18:35:05 +0100

    #162, property representing old-value-storing steered parameter

--- Tools/steering/python/hemelb_steering/paged_socket.py
+++ Tools/steering/python/hemelb_steering/paged_socket.py
@@ -1,14 +1,20 @@
 import socket
 
+#Model of the way HemeLB steering treats socket-comms.
+#On send, is a known-length buffer of float values.
 class PagedSocket(object):
     def __init__(self,address,port):
         self.address=address
         self.port=port
         self.socket=socket.socket()
-        
         self.socket.connect(address,port)
-    def send(self,page):
-        self.socket.send(page)
+    def send(self,vals):
+        totalsent = 0
+        while totalsent < len(vals):
+            sent = self.socket.send(str(bytearray(vals[totalsent:])))
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent += sent
     def receive(self):
         contents=self.socket.recv(1024)
         if contents=='':
