938e8bb0ad35dcd780476efce89ecdd70cf5731c
James Hetherington <j.hetherington@ucl.ac.uk>
2012-04-17 16:58:22 +0100

    #162 test image receipt, simple image and pixel models

--- Tools/steering/python/hemelb_steering/remote_hemelb.py
+++ Tools/steering/python/hemelb_steering/remote_hemelb.py
@@ -1,6 +1,8 @@
 from paged_socket import PagedSocket
 from steered_parameter import SteeredParameter
+from image import Image
 from config import config
+import xdrlib
 
 class RemoteHemeLB(object):
     # Metaprogram property access       
@@ -8,16 +10,44 @@ class RemoteHemeLB(object):
         self.address=address
         self.port=port
         self.steering_id=steering_id
-        self.socket=PagedSocket(address=self.address,port=self.port)
+        self.socket=PagedSocket(address=self.address,
+            port=self.port,
+            receive_length=3*RemoteHemeLB.xdr_int_bytes,
+            additional_receive_length_function=self._calculate_receive_length)
         self.latitude=0
         self.image=None
         for steered_parameter in self.steered_parameters:
             steered_parameter.initialise_in_instance(self)
 
+    xdr_int_bytes=4
+    xdr_double_bytes=8
+
     def step(self):
+        self.receive()
         self.image=self.socket.receive() or self.image
         if any([par.changed(self) for par in self.steered_parameters]):
             self.socket.send([par.value(self) for par in self.steered_parameters])
+            
+    def _calculate_receive_length(header):
+        unpacker=xdrlib.Unpacker(header)
+        width=unpacker.unpack_int()
+        height=unpacker.unpack_int()
+        frame=unpacker.unpack_int()
+        return frame+3*RemoteHemeLB.xdr_double_bytes+3*RemoteHemeLB.xdr_int_bytes
+        
+    def receive(self):
+        page=self.socket.receive()
+        unpacker=xdrlib.Unpacker(page)
+        self.width=unpacker.unpack_int()
+        self.height=unpacker.unpack_int()
+        self.frame=unpacker.unpack_int()
+        self.image=Image(self.frame/Image.bytes_per_pixel,unpacker)
+        self.step=unpacker.unpack_int()
+        self.time=unpacker.unpack_double()
+        unpacker.unpack_int() # throw away cycle
+        self.inlets=unpacker.unpack_int()
+        self.mouse_pressure=unpacker.unpack_double()
+        self.mouse_stress=unpacker.unpack_double()
 
 #Metaprogram property access
 RemoteHemeLB.steered_parameters=[]        
