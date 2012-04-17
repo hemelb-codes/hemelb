657caea43976b6fb2d4c2648b4994583f8366708
James Hetherington <j.hetherington@ucl.ac.uk>
2012-04-17 11:57:09 +0100

    #162 Receive header functionality

--- Tools/steering/python/hemelb_steering/paged_socket.py
+++ Tools/steering/python/hemelb_steering/paged_socket.py
@@ -2,12 +2,15 @@ import socket
 
 #Model of the way HemeLB steering treats socket-comms.
 #On send, is a known-length buffer of float values.
+#On receive, there is a header, which is used to determine the length of the rest of the message
 class PagedSocket(object):
-    def __init__(self,address,port):
+    def __init__(self,address,port,receive_length,additional_receive_length_function=lambda header:0):
         self.address=address
         self.port=port
         self.socket=socket.socket()
         self.socket.connect(address,port)
+        self.receive_length=receive_length
+        self.additional_receive_length_function=additional_receive_length_function
     def send(self,vals):
         totalsent = 0
         while totalsent < len(vals):
@@ -16,7 +19,14 @@ class PagedSocket(object):
                 raise RuntimeError("socket connection broken")
             totalsent += sent
     def receive(self):
-        contents=self.socket.recv(1024)
-        if contents=='':
-            return None
-        return contents
+        header=self._receive_fixed_length(self.receive_length)
+        additional_length=self.additional_receive_length_function(header)
+        return header+self._receive_fixed_length(additional_length)
+    def _receive_fixed_length(self,length):
+        msg = ''
+        while len(msg) < length:
+            chunk = self.socket.recv(length-len(msg))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            msg = msg + chunk
+        return bytearray(msg)
--- Tools/steering/python/hemelb_steering/test/test_paged_socket.py
+++ Tools/steering/python/hemelb_steering/test/test_paged_socket.py
@@ -16,18 +16,15 @@ class TestPagedSocket(unittest.TestCase):
     def setUp(self):
         with mock.patch('socket.socket') as mock_socket_maker:
             self.mockSocket=mock_socket_maker.return_value
-            self.ps=PagedSocket(address='fibble',port=8080)
+            self.ps=PagedSocket(address='fibble',port=8080,receive_length=9)
             mock_socket_maker.assert_called_once_with()
             self.mockSocket.connect.assert_called_once_with('fibble',8080)
     def test_connect(self):
         pass #The mocks defined in setUp are sufficient here
     def test_receive(self):
         self.mockSocket.recv.return_value='dummyPage'
-        self.assertEqual('dummyPage',self.ps.receive())
-        self.mockSocket.recv.assert_called_once_with(1024)
-        self.mockSocket.recv.return_value=''
-        self.assertEqual(None,self.ps.receive())
-        self.mockSocket.recv.assert_has_calls([mock.call(1024),mock.call(1024)])
+        self.assertEqual(bytearray('dummyPage'),self.ps.receive())
+        self.mockSocket.recv.assert_called_once_with(9)
     def test_send(self):
         payload=[1,2,3,4,5]
         payload_string=str(bytearray(payload))
@@ -39,4 +36,13 @@ class TestPagedSocket(unittest.TestCase):
         self.mockSocket.send.side_effect=[3,2]
         self.ps.send(payload)
         self.mockSocket.send.assert_has_calls([mock.call(str(bytearray([1,2,3,4,5]))),mock.call(str(bytearray([4,5])))])
+    def test_variable_length_receive(self):
+        def _dummy_additional_receive(header):
+            return header[0]
+        self.ps.additional_receive_length_function=_dummy_additional_receive
+        self.mockSocket.recv.side_effect=[str(bytearray([5,2,3,4,5,6,7,8,9])),str(bytearray([10,11,12,13,14]))]
+        self.assertEqual(bytearray([5,2,3,4,5,6,7,8,9,10,11,12,13,14]),self.ps.receive())
+        self.mockSocket.recv.assert_has_calls([mock.call(9),mock.call(5)])
+        
+        
         
