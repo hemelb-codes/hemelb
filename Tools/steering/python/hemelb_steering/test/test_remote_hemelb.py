2709f8c2405cfc1d40bc8deb359d671560bee278
James Hetherington <j.hetherington@ucl.ac.uk>
2012-04-17 16:58:22 +0100

    #162 test image receipt, simple image and pixel models

--- Tools/steering/python/hemelb_steering/test/test_remote_hemelb.py
+++ Tools/steering/python/hemelb_steering/test/test_remote_hemelb.py
@@ -9,7 +9,7 @@ Copyright (c) 2012 University College London. All rights reserved.
 
 import unittest
 import mock
-
+import xdrlib
 from hemelb_steering.remote_hemelb import RemoteHemeLB 
 
 
@@ -18,20 +18,30 @@ class TestRemoteHemeLB(unittest.TestCase):
 	    with mock.patch('hemelb_steering.remote_hemelb.PagedSocket') as mockPagedSocket:
 	        self.mockSocket=mockPagedSocket.return_value
 	        self.rhlb=RemoteHemeLB(address='fibble',port=8080,steering_id=1111)     
-	        mockPagedSocket.assert_called_once_with(address='fibble',port=8080)
+	        mockPagedSocket.assert_called_once_with(address='fibble',
+	            port=8080,
+	            receive_length=12,
+	            additional_receive_length_function=self.rhlb._calculate_receive_length)
+            fixture=xdrlib.Packer()
+    	    fixture.pack_int(4) #x
+    	    fixture.pack_int(4) #y
+    	    pixels=16
+    	    bytes_per_pixel=4+12#each three colors with four sub-images per color and two two-byte coordinates
+    	    image_size=pixels*bytes_per_pixel
+    	    fixture.pack_int(image_size)
+    	    for _ in xrange(pixels):
+    	        fixture.pack_fopaque(bytes_per_pixel,'abcdefghijklmnop')
+    	    fixture.pack_int(7) # step
+    	    fixture.pack_double(0.7) #time
+    	    fixture.pack_int(0) #cycle
+    	    fixture.pack_int(2) #inlets
+    	    fixture.pack_double(0) #mouse stress
+    	    fixture.pack_double(0) #mouse pressure
+    	    self.mockSocket.receive.return_value=str(bytearray(fixture.get_buffer()))
 	def test_receive_image(self):
-	    self.mockSocket.receive.return_value='dummyPage'
 	    self.rhlb.step()
-	    self.assertEqual('dummyPage',self.rhlb.image)
-	def test_two_step_one_image(self):
-	    self.mockSocket.receive.return_value='dummyPage'
-	    self.rhlb.step()
-	    self.assertEqual('dummyPage',self.rhlb.image)
-	    self.mockSocket.receive.assert_called_once_with()
-	    self.mockSocket.receive.return_value=None
-	    self.rhlb.step()
-	    self.assertEqual('dummyPage',self.rhlb.image)
-	    self.mockSocket.receive.assert_has_calls([mock.call(),mock.call()])
+	    self.assertEqual(7,self.rhlb.step)
+	    self.assertEqual(0.7,self.rhlb.time)
 	def test_no_change_no_send(self):
 	    self.rhlb.step()
 	    self.assertEqual(self.mockSocket.send.mock_calls,[])
