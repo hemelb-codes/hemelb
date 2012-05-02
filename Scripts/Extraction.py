e564cc78e2b5d3e827f937a88ec6698a5be58a59
Rupert Nash <rupert.nash@ed.ac.uk>
2012-05-02 14:22:23 +0100

    #233 Incorporate Hywel's CR comments, mainly updating Scripts/Extraction.py

--- Scripts/Extraction.py
+++ Scripts/Extraction.py
@@ -1,50 +1,30 @@
 #! /usr/bin/env python
 import xdrlib
 import sys
-from hemeTools.parsers import HemeLbMagicNumber
-
-ExtractionMagicNumber = 0x78747278
+from hemeTools.parsers.extraction import ExtractedProperty
 
 def unpack(filename):
-  file = open(filename, 'r')
-  x = xdrlib.Unpacker(file.read())
+    propFile = ExtractedProperty(filename)
 
-  hlbMagicNumber = x.unpack_uint()
-  extractionMagicNumber = x.unpack_uint()
-  extractionVersionNumber = x.unpack_uint()
+    print '# Dump of file "{}"'.format(filename)
+    print '# File has {} sites.'.format(propFile.siteCount)
+    print '# File has {} fields:'.format(propFile.fieldCount)
+    for name, xdrType, memType, length, offset in propFile._fieldSpec:
+        print '#     "{0}", length {1}'.format(name, length)
+    print '# Geometry origin = {} m'.format(propFile.originMetres)
+    print '# Voxel size = {} m'.format(propFile.voxelSizeMetres)
 
-  if hlbMagicNumber != HemeLbMagicNumber:
-    print "Expected HemeLb magic number %i, got %i" % (HemeLbMagicNumber, hlbMagicNumber)
-    return
-  
-  if extractionMagicNumber != ExtractionMagicNumber:
-    print "Expected Extraction magic number %i, got %i" % (ExtractionMagicNumber, extractionMagicNumber)
-    return
+    header  = '# '+ ', '.join(name for name, xdrType, memType, length, offset in propFile._fieldSpec)
+    print header
+    
+    for t in propFile.times:
+        fields = propFile.GetByTimeStep(t)
+        print "# Timestep {:d}".format(t)
+        
+        for row in fields:
+            print ', '.join(str(getattr(row, name)) for name, xdrType, memType, length, offset in propFile._fieldSpec)
 
-  print "Extraction version number %i" % extractionVersionNumber
-
-  fieldCount = x.unpack_uint()
-  fieldNames = [x.unpack_string() for fieldNum in xrange(fieldCount)]
-  fieldLengths = [x.unpack_uint() for fieldNum in xrange(fieldCount)]
-
-  siteCount = x.unpack_uhyper()
-  voxelSize = x.unpack_float()
-  origin = [x.unpack_float() for fieldNum in xrange(3)]
-
-  print "-----\nOpened file \"%s\"...\n-----" % filename
-  print "File has %s fields: %s, for %s sites" % (fieldCount, fieldNames, siteCount)
-  print "Each of these fields has %s floats" % fieldLengths
-  print "Geometry origin: %s, voxel size: %s" % (origin, voxelSize)
- 
-  while x.get_position() < (len(x.get_buffer())):
-    iterationNumber = x.unpack_hyper()
-
-    print "\nIteration %s\n-----" % iterationNumber
-
-    for siteNum in xrange(siteCount):
-      position = [x.unpack_uhyper() for fieldNum in xrange(3)]
-      field = [[x.unpack_float() for dimension in xrange(fieldLengths[fieldNum])] for fieldNum in xrange(fieldCount)]
-      print "Site at %s: %s = %s" % (position, fieldNames, field)
+        print ""
 
 if __name__ == "__main__":
   unpack(sys.argv[1])
