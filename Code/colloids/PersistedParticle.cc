8f81ccaaafecf1de29774ede49c3da0a678131f6
dholmes <unknown>
2012-05-15 11:36:55 +0100

    #351: Adding Colloid config - version 1

---
+++ Code/colloids/PersistedParticle.cc
@@ -0,0 +1,19 @@
+#include "colloids/PersistedParticle.h"
+
+namespace hemelb
+{
+  namespace colloids
+  {
+    PersistedParticle::PersistedParticle(configuration::XmlAbstractionLayer& xml)
+    {
+      // assume we are currently at a <SubgridParticle> node
+
+      bool ok = true;
+      ok &= xml.GetUnsignedLongValue("ParticleId", particleId);
+      ok &= xml.GetDoubleValue("InputRadiusA0", smallRadius_a0);
+      ok &= xml.GetDoubleValue("HydrostaticRadiusAh", largeRadius_ah);
+      ok &= xml.GetDoubleVector("initialPosition", globalPosition);
+    };
+
+  }
+}
