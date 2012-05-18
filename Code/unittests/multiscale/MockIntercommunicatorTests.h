a80a5f6e4f2ef7205963bd6afec635a67187794a
James Hetherington <j.hetherington@ucl.ac.uk>
2012-05-18 14:07:46 +0100

    Implement #361, exchange instead of get/set for multiscale

--- Code/unittests/multiscale/MockIntercommunicatorTests.h
+++ Code/unittests/multiscale/MockIntercommunicatorTests.h
@@ -20,14 +20,15 @@ namespace hemelb
       template<class IntercommuniatorImplementation> class MockHemeLB
       {
         public:
-          MockHemeLB(double spaceResolution, double timeResolution, std::map<std::string, double> & buffer) :
-              inlet(), outlet(), inOutLetType("inoutlet"), intercomms(buffer), timeResolution(timeResolution), spaceResolution(spaceResolution), currentTime(0)
+          MockHemeLB(double spaceResolution, double timeResolution, std::map<std::string, double> & buffer,std::map<std::string,bool> &orchestration) :
+              inlet(81.0,0.1), outlet(79.0,0.1), inOutLetType("inoutlet"), intercomms(buffer,orchestration), timeResolution(timeResolution), spaceResolution(spaceResolution), currentTime(0)
           {
             // The intercommunicators have a shared buffer which represents imaginary communication
             inOutLetType.template RegisterSharedValue<double>("pressure");
             inOutLetType.template RegisterSharedValue<double>("velocity");
             intercomms.RegisterIntercommunicand(inOutLetType, inlet, "boundary1");
             intercomms.RegisterIntercommunicand(inOutLetType, outlet, "boundary2");
+            intercomms.ShareInitialConditions();
           }
           MockIntercommunicand inlet;
           MockIntercommunicand outlet;
@@ -46,15 +47,13 @@ namespace hemelb
 
           void Simulate()
           {
-            if (!intercomms.ShouldAdvance())
-              return;
-            intercomms.GetFromMultiscale();
-            DoLB();
-            currentTime += timeResolution;
-            intercomms.AdvanceTime(currentTime);
-            intercomms.SendToMultiscale();
+            if (intercomms.DoMultiscale(currentTime))
+            {
+              std::cerr << "MH: " << currentTime << std::endl;
+              DoLB();
+              currentTime += timeResolution;
+            }
           }
-
       };
       /***
        * Mock intercommunicating entity which is a tank of water.
@@ -63,14 +62,15 @@ namespace hemelb
       template<class IntercommuniatorImplementation> class Mock0DModel
       {
         public:
-          Mock0DModel(double spaceResolution, double timeResolution, std::map<std::string, double> & buffer) :
-              inlet(), outlet(), inOutLetType("inoutlet"), intercomms(buffer), timeResolution(timeResolution), spaceResolution(spaceResolution), currentTime(0)
+          Mock0DModel(double spaceResolution, double timeResolution, std::map<std::string, double> & buffer, std::map<std::string,bool> &orchestration) :
+              inlet(79.0,0.1), outlet(81.0,0.1), inOutLetType("inoutlet"), intercomms(buffer,orchestration), timeResolution(timeResolution), spaceResolution(spaceResolution), currentTime(0)
           {
             // The intercommunicators have a shared buffer which represents imaginary communication
             inOutLetType.template RegisterSharedValue<double>("pressure");
             inOutLetType.template RegisterSharedValue<double>("velocity");
             intercomms.RegisterIntercommunicand(inOutLetType, inlet, "boundary2");
             intercomms.RegisterIntercommunicand(inOutLetType, outlet, "boundary1");
+            intercomms.ShareInitialConditions();
           }
           MockIntercommunicand inlet;
           MockIntercommunicand outlet;
@@ -83,6 +83,10 @@ namespace hemelb
           {
             return outlet.GetPressure();
           }
+          double GetTime()
+          {
+            return currentTime;
+          }
           void Do1D()
           {
             double capacitance = 10.0;
@@ -93,13 +97,12 @@ namespace hemelb
 
           void Simulate()
           {
-            if (!intercomms.ShouldAdvance())
-              return;
-            intercomms.GetFromMultiscale();
-            Do1D();
-            currentTime += timeResolution;
-            intercomms.AdvanceTime(currentTime);
-            intercomms.SendToMultiscale();
+            if (intercomms.DoMultiscale(currentTime))
+            {
+              Do1D();
+              currentTime += timeResolution;
+              std::cerr << "0D: " << currentTime << " " << GetOutletPressure() << std::endl;
+            }
           }
 
       };
@@ -127,13 +130,23 @@ namespace hemelb
           {
             pbuffer = new std::map<std::string, double>();
             std::map<std::string, double> &buffer = *pbuffer;
-            buffer["boundary1_pressure"] = 81.0;
-            buffer["boundary2_pressure"] = 79.0;
-            buffer["boundary1_velocity"] = 0.1;
-            buffer["boundary2_velocity"] = 0.1;
-            buffer["shared_time"] = 0.0;
-            mockheme = new MockHemeLB<MockIntercommunicator>(25.0, 0.2, buffer);
-            zerod = new Mock0DModel<MockIntercommunicator>(10.0, 0.5, buffer);
+
+            orchestrationOD=new std::map<std::string,bool>();
+            orchestrationLB=new std::map<std::string,bool>();
+            std::map<std::string,bool> &rorchestrationLB=*orchestrationLB;
+            std::map<std::string,bool> &rorchestration0D=*orchestrationOD;
+            rorchestrationLB["boundary1_pressure"] = false;
+            rorchestrationLB["boundary2_pressure"] = false;
+            rorchestrationLB["boundary1_velocity"] = true;
+            rorchestrationLB["boundary2_velocity"] = true;
+            rorchestration0D["boundary1_pressure"] = true;
+            rorchestration0D["boundary2_pressure"] = true;
+            rorchestration0D["boundary1_velocity"] = false;
+            rorchestration0D["boundary2_velocity"] = false;
+
+
+            mockheme = new MockHemeLB<MockIntercommunicator>(25.0, 0.2, buffer,rorchestrationLB);
+            zerod = new Mock0DModel<MockIntercommunicator>(10.0, 0.5, buffer,rorchestration0D);
 
           }
           void tearDown()
@@ -141,6 +154,7 @@ namespace hemelb
             delete zerod;
             delete mockheme;
             delete pbuffer;
+            //delete orchestrationLB;
           }
         private:
           void TestCRRun()
@@ -155,8 +169,9 @@ namespace hemelb
             }
             // The asserted value here is a result of discretised exponential decay.
             // I have not thought it worth determining this by a formula.
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(80.3379435171394, zerod->GetOutletPressure(), 1e-6);
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(mockheme->currentTime, zerod->currentTime, 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(80.3258457641491, zerod->GetOutletPressure(), 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(mockheme->currentTime, 20.0, 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(zerod->currentTime, 20.5, 1e-6);
           }
           void TestCHemeRun()
           {
@@ -176,27 +191,30 @@ namespace hemelb
             CopyResourceToTempdir("four_cube_multiscale.xml");
             CopyResourceToTempdir("four_cube.gmy");
             hemelb::configuration::CommandLine options(argc, argv);
-            MockIntercommunicator intercomms(*pbuffer);
+            MockIntercommunicator intercomms(*pbuffer,*orchestrationLB);
             heme = new MultiscaleSimulationMaster<MockIntercommunicator>(options, intercomms);
             // Mock out the behaviour of the simulation master iteration, but with the other model linked in.
-            while (heme->GetState()->GetTimeStep() <= heme->GetState()->GetTotalTimeSteps())
+            while (heme->GetState()->GetTime() < 20.0 || zerod->GetTime()< 20.0)
             {
-              zerod->Simulate();
               heme->DoTimeStep();
+              zerod->Simulate();
             }
             // In advancing 100 time steps, at 0.2 s per time step, with a 0d model at 0.5s per time step
-            // the 0d model with execute 100*2/5=40 times.
+            // the 0d model with execute 100*2/5=40 times, plus one more step, the one where it communicates the previous step.
             // Each time, the pressure difference will drop by 0.1*0.1=0.01 mmHg.
-            // So the final pressure will be 81.0-40*0.01=80.6 mmHg
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(80.6, zerod->GetOutletPressure(), 1e-6);
+            // So the final pressure will be 81.0-41*0.01=80.59 mmHg
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(80.59, zerod->GetOutletPressure(), 1e-6);
             heme->Finalise();
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(heme->GetState()->GetTime(), zerod->currentTime, 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(heme->GetState()->GetTime(), 20.0, 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(zerod->currentTime, 20.5, 1e-6); // does one more step, where it sets the shared time.
             delete heme;
           }
           MockHemeLB<MockIntercommunicator> *mockheme;
           MultiscaleSimulationMaster<MockIntercommunicator> *heme;
           Mock0DModel<MockIntercommunicator> *zerod;
           std::map<std::string, double> *pbuffer;
+          std::map<std::string,bool> *orchestrationLB;
+          std::map<std::string,bool> *orchestrationOD;
       };
       //class
       CPPUNIT_TEST_SUITE_REGISTRATION(MockIntercommunicatorTests);
