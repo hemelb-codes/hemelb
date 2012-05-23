345c0806ecf6e7bb7e6cfeb394e56cdcbdadc9f2
James Hetherington <j.hetherington@ucl.ac.uk>
2012-05-23 16:59:33 +0100

    #377 Separate net coalesced communicator functionality into mixins (broken)

---
+++ Code/net/mixins/mixins.h
@@ -0,0 +1,7 @@
+#ifndef HEMELB_NET_MIXINS_MIXINS_H
+#define HEMELB_NET_MIXINS_MIXINS_H
+#include "net/mixins/CoalescePointPoint.h"
+#include "net/mixins/StoringNet.hpp"
+#include "net/mixins/SeparatedGathers.h"
+#include "net/mixins/CppInterfaceDelegationNet.hpp"
+#endif
