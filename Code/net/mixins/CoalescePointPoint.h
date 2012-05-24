b982c5b1e4d8520c5bf6a530ba77fe45e5992bb6
James Hetherington <j.hetherington@ucl.ac.uk>
2012-05-24 17:22:13 +0100

    #337, restore formerly rolled-back buggy coalesce mixins

---
+++ Code/net/mixins/CoalescePointPoint.h
@@ -0,0 +1,34 @@
+#ifndef HEMELB_NET_MIXINS_COALESCEPOINTPOINT_H
+#define HEMELB_NET_MIXINS_COALESCEPOINTPOINT_H
+#include "net/BaseNet.h"
+#include "net/mixins/StoringNet.hpp"
+namespace hemelb
+{
+  namespace net
+  {
+    class CoalescePointPoint : public virtual StoringNet
+    {
+      public:
+        CoalescePointPoint():sendReceivePrepped(false),mRequests(),mStatuses(){}
+        void Wait();
+      protected:
+        void ReceivePointToPoint();
+        void EnsurePreparedToSendReceive();
+        void SendPointToPoint();
+        ~CoalescePointPoint();
+      private:
+        void EnsureEnoughRequests(size_t count);
+
+        bool sendReceivePrepped;
+
+        // Requests and statuses available for general communication within the Net object (both
+        // initialisation and during each iteration). Code using these must make sure
+        // there are enough available. We do this in a way to minimise the number created
+        // on each core, but also to minimise creation / deletion overheads.
+        std::vector<MPI_Request> mRequests;
+        std::vector<MPI_Status> mStatuses;
+    };
+  }
+}
+
+#endif
