2a2bd4320306c1effa6edc3d1795aada96a1cc4e
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2012-05-30 15:53:18 +0100

    #372 moving script that generates the pipe to functional test folder and packaging generator as a Python module

--- Tools/hemeTools/surfacegenerator/CylinderGenerator.py
+++ Tools/hemeTools/surfacegenerator/CylinderGenerator.py
@@ -1,26 +1,28 @@
-#!/usr/bin/env python
 import sys
-try:
-    r = float(sys.argv[1])
-    length = float(sys.argv[2])
-    n = int(sys.argv[3])
-    outfile = sys.argv[4]
-except:
-    print "Usage:\n%s radius(in mm) length(in mm) resolution outfile" % sys.argv[0]
-    raise SystemExit
-
 from TriangulatedCylinderSource import TriangulatedCylinderSource
 from vtk import vtkSTLWriter
 
-tcs = TriangulatedCylinderSource()
-w = vtkSTLWriter()
-w.SetInputConnection(tcs.GetOutputPort())
+def CylinderGenerator(radius, length, resolution, outfile):
+    tcs = TriangulatedCylinderSource()
+    w = vtkSTLWriter()
+    w.SetInputConnection(tcs.GetOutputPort())
 
-tcs.SetRadius(r)
-tcs.SetResolution(n)
-tcs.SetHeight(length)
-tcs.CappingOff()
-w.SetFileName(outfile)
+    tcs.SetRadius(radius)
+    tcs.SetResolution(resolution)
+    tcs.SetHeight(length)
+    tcs.CappingOff()
+    w.SetFileName(outfile)
 
-w.Write()
+    w.Write()
 
+if __name__ == "__main__":
+    try:
+        radius = float(sys.argv[1])
+        length = float(sys.argv[2])
+        resolution = int(sys.argv[3])
+        outfile = sys.argv[4]
+    except:
+        print "Usage:\n%s radius(in mm) length(in mm) resolution outfile" % sys.argv[0]
+        raise SystemExit
+
+    CylinderGenerator(radius, length, resolution, outfile)
