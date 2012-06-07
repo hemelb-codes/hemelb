026fc04a5789dcbe461f25c708855fdd0511ece6
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-07 16:45:35 +0100

    #162 - whitespace changes for code review.

--- Tools/steering/python/hemelb_steering/remote_hemelb.py
+++ Tools/steering/python/hemelb_steering/remote_hemelb.py
@@ -11,21 +11,21 @@ class RemoteHemeLB(object):
     e.g. myheme.Latitude=50
     """
    
-    def __init__(self,address,port,steering_id):
-        self.address=address
-        self.port=port
-        self.steering_id=steering_id
-        self.socket=PagedSocket(address=self.address,
+    def __init__(self, address, port, steering_id):
+        self.address = address
+        self.port = port
+        self.steering_id = steering_id
+        self.socket = PagedSocket(address=self.address,
             port=self.port,
-            receive_length=3*RemoteHemeLB.xdr_int_bytes,
+            receive_length= 3 * RemoteHemeLB.xdr_int_bytes,
             additional_receive_length_function=RemoteHemeLB._calculate_receive_length)
-        self.latitude=0
-        self.image=None
+        self.latitude = 0
+        self.image = None
         for steered_parameter in self.steered_parameters:
-            steered_parameter.initialise_in_instance(self,config['steered_parameter_defaults'][steered_parameter.name])
+            steered_parameter.initialise_in_instance(self, config['steered_parameter_defaults'][steered_parameter.name])
 
-    xdr_int_bytes=4
-    xdr_double_bytes=8
+    xdr_int_bytes = 4
+    xdr_double_bytes = 8
 
     def step(self):
         self.receive()
@@ -33,35 +33,35 @@ class RemoteHemeLB(object):
         
     def send(self):
         if any([par.changed(self) for par in self.steered_parameters]):
-            vals=xdrlib.Packer()
+            vals = xdrlib.Packer()
             for par in self.steered_parameters:
                 vals.pack_float(float(par.value(self)))
             self.socket.send(vals.get_buffer())
     
     @staticmethod
     def _calculate_receive_length(header):
-        unpacker=xdrlib.Unpacker(header)
-        width=unpacker.unpack_int()
-        height=unpacker.unpack_int()
-        frame=unpacker.unpack_int()
-        return frame+3*RemoteHemeLB.xdr_double_bytes+3*RemoteHemeLB.xdr_int_bytes
+        unpacker = xdrlib.Unpacker(header)
+        width = unpacker.unpack_int()
+        height = unpacker.unpack_int()
+        frame = unpacker.unpack_int()
+        return frame + 3*RemoteHemeLB.xdr_double_bytes + 3*RemoteHemeLB.xdr_int_bytes
         
     def receive(self):
-        page=self.socket.receive()
-        unpacker=xdrlib.Unpacker(page)
-        self.width=unpacker.unpack_int()
-        self.height=unpacker.unpack_int()
-        self.frame=unpacker.unpack_int()
-        self.image=Image(self.width,self.height,self.frame/Image.bytes_per_pixel,unpacker)
-        self.time_step=unpacker.unpack_int()
-        self.time=unpacker.unpack_double()
+        page = self.socket.receive()
+        unpacker = xdrlib.Unpacker(page)
+        self.width = unpacker.unpack_int()
+        self.height = unpacker.unpack_int()
+        self.frame = unpacker.unpack_int()
+        self.image = Image(self.width, self.height, self.frame / Image.bytes_per_pixel, unpacker)
+        self.time_step = unpacker.unpack_int()
+        self.time = unpacker.unpack_double()
         unpacker.unpack_int() # throw away cycle
-        self.inlets=unpacker.unpack_int()
-        self.mouse_pressure=unpacker.unpack_double()
-        self.mouse_stress=unpacker.unpack_double()
+        self.inlets = unpacker.unpack_int()
+        self.mouse_pressure = unpacker.unpack_double()
+        self.mouse_stress = unpacker.unpack_double()
         
     def __str__(self):
-        return "HemeLB at %s: Step %s" % (self.address,self.time_step)
+        return "HemeLB at %s: Step %s" % (self.address, self.time_step)
 
 # Metaprogram property access
 # We use metaprogramming techniques to define the properties which can be steered,
@@ -69,7 +69,8 @@ class RemoteHemeLB(object):
 # We use the SteeredParameter python "descriptor" class to define these attributes.
 # See the descriptors discussion in http://docs.python.org/reference/datamodel.html#new-style-and-classic-classes if you never tried these.
 RemoteHemeLB.steered_parameters=[]        
+
 for index,name in enumerate(config['steered_parameters']):
-    steered_parameter=SteeredParameter(index,name)
-    setattr(RemoteHemeLB,name,steered_parameter)
+    steered_parameter = SteeredParameter(index,name)
+    setattr(RemoteHemeLB, name, steered_parameter)
     RemoteHemeLB.steered_parameters.append(steered_parameter)
