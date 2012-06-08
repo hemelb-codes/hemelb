76962245f5a320aa9a7668ced07c0b8008542685
Derek Groen <d.groen@ucl.ac.uk>
2012-06-08 15:53:37 +0100

    Added in semi-sensible multiscale code to mainMultiscale.cc. Also made some corrections in the structure of the Intercommunicators. Still have to move the MPWide Icomm files to a suitable location.

--- Code/unittests/multiscale/mpwide/MPWideIntercommunicatorTests.h
+++ Code/unittests/multiscale/mpwide/MPWideIntercommunicatorTests.h
@@ -4,7 +4,7 @@
 #include "unittests/multiscale/MockIntercommunicand.h"
 #include <resources/Resource.h>
 #include "unittests/multiscale/MPWideIntercommunicator.h"
-#include <multiscale/MultiscaleSimulationMaster.h>
+#include "multiscale/MultiscaleSimulationMaster.h"
 
 
 namespace hemelb
