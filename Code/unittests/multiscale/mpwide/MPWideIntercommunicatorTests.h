6528de8a03baec37a46d950ac06c03ef0b76a6dd
Derek Groen <d.groen@ucl.ac.uk>
2012-06-08 16:11:58 +0100

    Moved MPWideIntercommunicator.h from unittests to the main source tree. However, it still refers to "MockMPWide.h" to ensure proper functioning of the unit tests. We will have to revise that in the near future.

--- Code/unittests/multiscale/mpwide/MPWideIntercommunicatorTests.h
+++ Code/unittests/multiscale/mpwide/MPWideIntercommunicatorTests.h
@@ -3,7 +3,7 @@
 #include "unittests/multiscale/MockMPWide.h"
 #include "unittests/multiscale/MockIntercommunicand.h"
 #include <resources/Resource.h>
-#include "unittests/multiscale/MPWideIntercommunicator.h"
+#include "multiscale/mpwide/MPWideIntercommunicator.h"
 #include "multiscale/MultiscaleSimulationMaster.h"
 
 
