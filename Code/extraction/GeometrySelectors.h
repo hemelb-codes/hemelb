571ee154287e461ad6246afaa019448266d46953
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2012-06-10 16:31:20 +0100

    #271, adding functionality to XML config file

---
+++ Code/extraction/GeometrySelectors.h
@@ -0,0 +1,9 @@
+#ifndef HEMELB_EXTRACTION_GEOMETRYSELECTORS_H
+#define HEMELB_EXTRACTION_GEOMETRYSELECTORS_H
+
+#include "extraction/StraightLineGeometrySelector.h"
+#include "extraction/PlaneGeometrySelector.h"
+#include "extraction/WholeGeometrySelector.h"
+#include "extraction/GeometrySurfaceSelector.h"
+
+#endif /* HEMELB_EXTRACTION_GEOMETRYSELECTORS_H */
