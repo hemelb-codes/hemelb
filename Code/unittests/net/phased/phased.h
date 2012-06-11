50c576a17239d915877b029374035d330cb2ef69
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-11 15:02:51 +0100

    #416, basic empty skeleton

---
+++ Code/unittests/net/phased/phased.h
@@ -0,0 +1,7 @@
+#ifndef HEMELB_UNITTESTS_NET_PHASED_PHASED_H
+#define HEMELB_UNITTESTS_NET_PHASED_PHASED_H
+
+#include "unittests/net/phased/StepManagerTests.h"
+#include "unittests/net/phased/ConcernTests.h"
+
+#endif //ONCE
