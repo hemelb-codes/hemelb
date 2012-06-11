2ab498658e1248b402cafdc3decfe4f7133fc4ce
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-11 15:02:51 +0100

    #416, basic empty skeleton

---
+++ Code/net/phased/Concern.h
@@ -0,0 +1,10 @@
+#ifndef HEMELB_NET_PHASED_CONCERN_H
+#define HEMELB_NET_PHASED_CONCERN_H
+namespace hemelb{
+  namespace net{
+    namespace phased{
+
+    }
+  }
+}
+#endif //ONCE
---
+++ Code/net/phased/StepManager.h
@@ -0,0 +1,10 @@
+#ifndef HEMELB_NET_PHASED_STEPMANAGER_H
+#define HEMELB_NET_PHASED_STEPMANAGER_H
+namespace hemelb{
+  namespace net{
+    namespace phased{
+
+    }
+  }
+}
+#endif //ONCE
