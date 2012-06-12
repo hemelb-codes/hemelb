ff042d8d3c7123aa5087e04b6505b75b0629bc74
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-12 17:49:15 +0100

    #417 skeleton files for neighbouring data manager

---
+++ Code/geometry/neighbouring/NeighbouringSite.h
@@ -0,0 +1,13 @@
+#ifndef HEMELB_GEOMETRY_NEIGHBOURING_NEIGHBOURINGSITE_H
+#define HEMELB_GEOMETRY_NEIGHBOURING_NEIGHBOURINGSITE_H
+namespace hemelb{
+  namespace geometry{
+    namespace neighbouring{
+      class NeighbouringSite : public Site {
+
+      };
+    }
+  }
+}
+
+#endif
