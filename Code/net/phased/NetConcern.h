69245b70ee4766a3f9b3f13f639d6115ee4d3da1
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-12 15:43:52 +0100

    #428, #416, implement wrapper for net as a concern.

---
+++ Code/net/phased/NetConcern.h
@@ -0,0 +1,45 @@
+#ifndef HEMELB_NET_PHASED_NETCONCERN_H
+#define HEMELB_NET_PHASED_NETCONCERN_H
+
+#include "net/phased/Concern.h"
+#include "net/phased/steps.h"
+
+namespace hemelb
+{
+  namespace net
+  {
+    namespace phased
+    {
+      class NetConcern : public Concern
+      {
+        public:
+          NetConcern(net::BaseNet & net) :
+              net(net)
+          {
+          }
+          bool CallAction(int action)
+          {
+            switch (static_cast<phased::steps::Step>(action))
+            {
+              case phased::steps::Send:
+                net.Receive();
+                return true;
+              case phased::steps::Receive:
+                net.Send();
+                return true;
+              case phased::steps::Wait:
+                net.Wait();
+                return true;
+
+              default:
+                return false;
+            }
+          }
+        private:
+          net::BaseNet & net;
+      };
+    }
+  }
+}
+
+#endif
--- Code/unittests/net/phased/StepManagerTests.h
+++ Code/unittests/net/phased/StepManagerTests.h
@@ -4,6 +4,7 @@
 #include "unittests/net/NetMock.h"
 #include "unittests/net/phased/MockConcern.h"
 #include "unittests/net/phased/MockIteratedAction.h"
+#include "net/phased/NetConcern.h"
 #include <cppunit/TestFixture.h>
 
 namespace hemelb
@@ -19,15 +20,24 @@ namespace hemelb
         {
             CPPUNIT_TEST_SUITE (StepManagerTests);
             CPPUNIT_TEST (TestConstruct);
+
             CPPUNIT_TEST (TestRegisterActor);
+
             CPPUNIT_TEST (TestRegisterAction);
             CPPUNIT_TEST (TestRegisterTwoConcerns);
+            CPPUNIT_TEST (TestCallNonCommsActions);
+
+            CPPUNIT_TEST (TestRegisterCommsConcern);
+            CPPUNIT_TEST (TestCallCommsActions);
+
+            CPPUNIT_TEST (TestRegisterThreeConcerns);
             CPPUNIT_TEST (TestCallActions);
+
             CPPUNIT_TEST_SUITE_END();
 
           public:
             StepManagerTests() :
-                netMock(NULL), communicatorMock(NULL), stepManager(NULL), action(NULL), concern(NULL)
+                netMock(NULL), communicatorMock(NULL), stepManager(NULL), action(NULL), concern(NULL), netConcern(NULL)
             {
             }
 
@@ -82,7 +92,51 @@ namespace hemelb
               CPPUNIT_ASSERT_EQUAL(stepManager->ActionCount(), 7u);
             }
 
-            void TestCallActions()
+            void TestRegisterCommsConcern()
+            {
+              SetupMocks(4, 1);
+              stepManager->RegisterCommsSteps(*netConcern, 0);
+              CPPUNIT_ASSERT_EQUAL(stepManager->ConcernCount(), 1u);
+              CPPUNIT_ASSERT_EQUAL(stepManager->ActionCount(), 3u);
+            }
+
+            void TestRegisterThreeConcerns()
+            {
+              action = new MockIteratedAction("mockOne");
+              concern = new MockConcern("mockTwo");
+              SetupMocks(4, 1);
+
+              stepManager->RegisterCommsSteps(*netConcern, 0);
+              stepManager->RegisterIteratedActorSteps(*action);
+              stepManager->Register(0, steps::PreSend, *concern, 0);
+              stepManager->Register(0, steps::EndPhase, *concern, 1);
+
+              CPPUNIT_ASSERT_EQUAL(stepManager->ConcernCount(), 3u);
+              CPPUNIT_ASSERT_EQUAL(stepManager->ActionCount(), 10u);
+            }
+
+            void TestCallCommsActions()
+            {
+              SetupMocks(4, 1);
+              int payload0 = 0;
+              int payload1 = 1;
+
+              int payload0Expectation = 5;
+              int payload1Expectation = 1;
+              netMock->RequestSendR(payload1, 1);
+              netMock->RequestReceiveR(payload0, 1);
+              netMock->RequireSend(&payload1Expectation, 1, 1);
+              netMock->RequireReceive(&payload0Expectation, 1, 1);
+
+              stepManager->RegisterCommsSteps(*netConcern, 0);
+
+              stepManager->CallActionsForPhase(0);
+
+              CPPUNIT_ASSERT_EQUAL(payload0, 5);
+              netMock->ExpectationsAllCompleted();
+            }
+
+            void TestCallNonCommsActions()
             {
               action = new MockIteratedAction("mockOne");
               concern = new MockConcern("mockTwo");
@@ -103,11 +157,48 @@ namespace hemelb
                                    action->CallsSoFar());
             }
 
+            void TestCallActions()
+            {
+
+              SetupMocks(4, 1);
+              int payload0 = 0;
+              int payload1 = 1;
+
+              int payload0Expectation = 5;
+              int payload1Expectation = 1;
+              netMock->RequestSendR(payload1, 1);
+              netMock->RequestReceiveR(payload0, 1);
+              netMock->RequireSend(&payload1Expectation, 1, 1);
+              netMock->RequireReceive(&payload0Expectation, 1, 1);
+
+              action = new MockIteratedAction("mockOne");
+              concern = new MockConcern("mockTwo");
+
+              stepManager->RegisterIteratedActorSteps(*action);
+              stepManager->Register(0, steps::PreSend, *concern, 0);
+              stepManager->Register(0, steps::EndPhase, *concern, 1);
+              stepManager->RegisterCommsSteps(*netConcern, 0);
+
+              stepManager->CallActionsForPhase(0);
+
+              std::vector<int> shouldHaveCalled;
+
+              shouldHaveCalled.push_back(0);
+              shouldHaveCalled.push_back(1);
+
+              CPPUNIT_ASSERT_EQUAL(shouldHaveCalled, concern->ActionsCalled());
+
+              CPPUNIT_ASSERT_EQUAL(std::string("RequestComms, PreSend, PreReceive, PostReceive, "),
+                                   action->CallsSoFar());
+              CPPUNIT_ASSERT_EQUAL(payload0, 5);
+              netMock->ExpectationsAllCompleted();
+            }
+
             void SetupMocks(const proc_t core_count, const proc_t current_core)
             {
               communicatorMock = new topology::Communicator(current_core, core_count);
               netMock = new net::NetMock(*communicatorMock);
-
+              netConcern = new NetConcern(*netMock);
             }
 
           private:
@@ -117,6 +208,7 @@ namespace hemelb
             StepManager *stepManager;
             MockIteratedAction *action;
             MockConcern *concern;
+            NetConcern *netConcern;
         };
 
         CPPUNIT_TEST_SUITE_REGISTRATION (StepManagerTests);
