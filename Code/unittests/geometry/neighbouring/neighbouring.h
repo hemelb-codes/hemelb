249ad95c96bb901bb863ab1f11cc88b0c41fa5f5
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-12 17:49:15 +0100

    #417 skeleton files for neighbouring data manager

---
+++ Code/unittests/geometry/neighbouring/neighbouring.h
@@ -0,0 +1,8 @@
+#ifndef HEMELB_UNITTESTS_GEOMETRY_NEIGHBOURING_NEIGHBOURING_H
+#define HEMELB_UNITTESTS_GEOMETRY_NEIGHBOURING_NEIGHBOURING_H
+
+#include "unittests/geometry/neighbouring/NeighbouringDataManagerTests.h"
+#include "unittests/geometry/neighbouring/NeighbouringSiteTests.h"
+#include "unittests/geometry/neighbouring/RequiredFieldInformationTests.h"
+
+#endif // ONCE
