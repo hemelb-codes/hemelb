c1dc044262dfebfdbf71e4e148dc9441c293991e
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-12 12:06:46 +0100

    #416, refactor cppunit compare vectors for reuse

---
+++ Code/unittests/helpers/CppUnitCompareVectors.h
@@ -0,0 +1,28 @@
+#ifndef HEMELB_UNITTESTS_HELPERS_CPPUNITCOMPAREVECTORS_H
+#define HEMELB_UNITTESTS_HELPERS_CPPUNITCOMPAREVECTORS_H
+#include <cppunit/TestFixture.h>
+
+namespace CPPUNIT_NS
+{
+  template<class T> struct assertion_traits<std::vector<T> >
+  {
+      static bool equal(const std::vector<T>& x, const std::vector<T>& y)
+      {
+        return x == y;
+      }
+
+      // Note this vector print doesn't visually distinguish between ("1" "2" "3") and("1, 2" "3").
+      static std::string toString(const std::vector<T>& values)
+      {
+        std::stringstream output;
+        output << "[ " << std::flush;
+        for (typename std::vector<T>::const_iterator value = values.begin(); value != values.end(); value++)
+        {
+          output << *value << ", " << std::flush;
+        }
+        output << "]" << std::flush;
+        return output.str();
+      }
+  };
+}
+#endif
