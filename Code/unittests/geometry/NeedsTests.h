2b13caee17341e2df33ffe28447eb112dce1fcdc
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-12 12:06:46 +0100

    #416, refactor cppunit compare vectors for reuse

--- Code/unittests/geometry/NeedsTests.h
+++ Code/unittests/geometry/NeedsTests.h
@@ -2,33 +2,12 @@
 #define HEMELB_UNITTESTS_GEOMETRY_NEEDSTESTS_H
 #include "geometry/needs/Needs.hpp"
 #include "unittests/net/NetMock.h"
+#include "unittests/helpers/CppUnitCompareVectors.h"
 #include <cppunit/TestFixture.h>
 
 // Let vectors be output to an ostream, so that CPPUNIT can assert equality on them
 
-namespace CPPUNIT_NS
-{
-  template<class T> struct assertion_traits<std::vector<T> >
-  {
-      static bool equal(const std::vector<T>& x, const std::vector<T>& y)
-      {
-        return x == y;
-      }
 
-      // Note this vector print doesn't visually distinguish between ("1" "2" "3") and("1, 2" "3").
-      static std::string toString(const std::vector<T>& values)
-      {
-        std::stringstream output;
-        output << "[ " << std::flush;
-        for (typename std::vector<T>::const_iterator value = values.begin(); value != values.end(); value++)
-        {
-          output << *value << ", " << std::flush;
-        }
-        output << "]" << std::flush;
-        return output.str();
-      }
-  };
-}
 
 namespace hemelb
 {
