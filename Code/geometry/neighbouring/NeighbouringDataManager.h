2944d52c4afa71c31e9973842f3566637b0e6fcd
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-13 19:27:57 +0100

    #417 Use partially implemented alltoall to share needs -- partially implemented

--- Code/geometry/neighbouring/NeighbouringDataManager.h
+++ Code/geometry/neighbouring/NeighbouringDataManager.h
@@ -4,6 +4,7 @@
 #include "geometry/neighbouring/NeighbouringLatticeData.h"
 #include "net/net.h"
 #include <vector>
+#include <map>
 namespace hemelb
 {
   namespace geometry
@@ -15,14 +16,20 @@ namespace hemelb
       {
         public:
           NeighbouringDataManager(const LatticeData & localLatticeData,
-                                  NeighbouringLatticeData & neighbouringLatticeData,net::InterfaceDelegationNet & net);
+                                  NeighbouringLatticeData & neighbouringLatticeData,
+                                  net::InterfaceDelegationNet & net);
           void RegisterNeededSite(site_t globalId);
+          void ShareNeeds();
+          std::multimap<site_t, proc_t> & GetProcsNeedingEachSite(){return procsNeedingEachSite;}
+
         private:
           const LatticeData & localLatticeData;
-          const NeighbouringLatticeData & neighbouringLatticeData;
-          const net::InterfaceDelegationNet & net;
+          NeighbouringLatticeData & neighbouringLatticeData;
+          net::InterfaceDelegationNet & net;
 
           std::vector<site_t> neededSites;
+          std::multimap<site_t, proc_t> procsNeedingEachSite;
+
       };
 
     }
