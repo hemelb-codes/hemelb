c607a4981114eea529c4e4304c6ecbb10e2df1b0
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-13 17:47:08 +0100

    #433 Simplify use of Net bases, makes testing easier

--- Code/net/mixins/GathersViaPointPoint.cc
+++ Code/net/mixins/GathersViaPointPoint.cc
@@ -16,7 +16,7 @@ namespace hemelb
         {
           // The below use of unsigned char is not formally correct (due to the possibility of char not having alignment 1)
           // But we cannot currently see a better solution to avoid compiler warnings from void* arithmetic.
-          RequestReceive(static_cast<unsigned char *>(receive_it->Pointer) + size * source_rank,
+          RequestReceiveImpl(static_cast<unsigned char *>(receive_it->Pointer) + size * source_rank,
                          1,
                          source_rank,
                          receive_it->Type);
@@ -33,7 +33,7 @@ namespace hemelb
 
         for (GatherProcComms::iterator req = send_it->second.begin(); req != send_it->second.end(); req++)
         {
-          RequestSend(req->Pointer, 1, send_it->first, req->Type);
+          RequestSendImpl(req->Pointer, 1, send_it->first, req->Type);
         }
 
       }
@@ -56,7 +56,7 @@ namespace hemelb
           // Note that MPI Displacements are given in the arithmetic appropriate to the MPI_Datatype, not void*, i.e. in units of the size
           // It will also potentially fail, if the MPI_Datatype used, is a sparse (strided) type.
           // This class is intended for timing and testing use, not production use.
-          RequestReceive(static_cast<unsigned char *>(receive_it->Pointer)
+          RequestReceiveImpl(static_cast<unsigned char *>(receive_it->Pointer)
                              + receive_it->Displacements[source_rank] * size,
                          receive_it->Counts[source_rank],
                          source_rank,
@@ -74,7 +74,7 @@ namespace hemelb
 
         for (ProcComms::iterator req = send_it->second.begin(); req != send_it->second.end(); req++)
         {
-          RequestSend(req->Pointer, req->Count, send_it->first, req->Type);
+          RequestSendImpl(req->Pointer, req->Count, send_it->first, req->Type);
         }
 
       }
