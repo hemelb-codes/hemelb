379d233f3c6241b9412fa20d307940fdd967c447
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-13 18:19:14 +0100

    #417 add ability to get proc from global id

--- Code/unittests/geometry/LatticeDataTests.h
+++ Code/unittests/geometry/LatticeDataTests.h
@@ -10,55 +10,62 @@ namespace hemelb
     namespace geometry
     {
 
-        using namespace hemelb::geometry;
-        class NeighbouringLatticeDataTests : public FourCubeBasedTestFixture
-        {
-            CPPUNIT_TEST_SUITE (NeighbouringLatticeDataTests);
-            CPPUNIT_TEST (TestConstruct);
-            CPPUNIT_TEST (TestConvertGlobalId);
+      using namespace hemelb::geometry;
+      class NeighbouringLatticeDataTests : public FourCubeBasedTestFixture
+      {
+          CPPUNIT_TEST_SUITE (NeighbouringLatticeDataTests);
+          CPPUNIT_TEST (TestConstruct);
+          CPPUNIT_TEST (TestConvertGlobalId);
+          CPPUNIT_TEST (TestGetProcFromGlobalId);
 
-            CPPUNIT_TEST_SUITE_END();
+          CPPUNIT_TEST_SUITE_END();
 
-          public:
-            NeighbouringLatticeDataTests()
-            {
-            }
+        public:
+          NeighbouringLatticeDataTests()
+          {
+          }
 
-            void setUp()
-            {
-              FourCubeBasedTestFixture::setUp();
+          void setUp()
+          {
+            FourCubeBasedTestFixture::setUp();
 
-            }
+          }
 
-            void tearDown()
-            {
-              FourCubeBasedTestFixture::tearDown();
+          void tearDown()
+          {
+            FourCubeBasedTestFixture::tearDown();
 
-            }
+          }
 
-            void TestConstruct()
-            {
-              // PASS -- just verify setUp and tearDown
-            }
+          void TestConstruct()
+          {
+            // PASS -- just verify setUp and tearDown
+          }
 
-            void TestConvertGlobalId()
-            {
-              // not really a very good test to use a one-proc geometry
-              // we need to create a sixteen-cube lattice data test fixture to test this kind of thing properly
-              // but we can at least smoke test things with four cube.
-              util::Vector3D<site_t> exampleCoord(1,2,3);
-              site_t id = latDat->GetGlobalNoncontiguousSiteIdFromGlobalCoords(exampleCoord);
-              CPPUNIT_ASSERT_EQUAL(id,static_cast<site_t>(1*16+2*4+3));
-              util::Vector3D<site_t> resultCoord;
-              latDat->GetGlobalCoordsFromGlobalNoncontiguousSiteId(id,resultCoord);
-              CPPUNIT_ASSERT_EQUAL(resultCoord,exampleCoord);
-            }
+          void TestConvertGlobalId()
+          {
+            // not really a very good test to use a one-proc geometry
+            // we need to create a sixteen-cube lattice data test fixture to test this kind of thing properly
+            // but we can at least smoke test things with four cube.
+            util::Vector3D<site_t> exampleCoord(1, 2, 3);
+            site_t id = latDat->GetGlobalNoncontiguousSiteIdFromGlobalCoords(exampleCoord);
+            CPPUNIT_ASSERT_EQUAL(id, static_cast<site_t>(1 * 16 + 2 * 4 + 3)); //43
+            util::Vector3D<site_t> resultCoord;
+            latDat->GetGlobalCoordsFromGlobalNoncontiguousSiteId(id, resultCoord);
+            CPPUNIT_ASSERT_EQUAL(resultCoord, exampleCoord);
+          }
 
-          private:
-        };
-        CPPUNIT_TEST_SUITE_REGISTRATION (NeighbouringLatticeDataTests);
-      }
+          void TestGetProcFromGlobalId()
+          {
+            // Again, we need to work up a way to mock a multi-processor situation to test this properly.
+            CPPUNIT_ASSERT_EQUAL(latDat->ProcProvidingSiteByGlobalNoncontiguousId(43),0);
+          }
+
+        private:
+      };
+      CPPUNIT_TEST_SUITE_REGISTRATION (NeighbouringLatticeDataTests);
     }
   }
+}
 
 #endif
