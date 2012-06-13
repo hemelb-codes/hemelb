918032e91d116d7d7f499da05d0163f06bdc9a23
James Hetherington <j.hetherington@ucl.ac.uk>
2012-06-13 17:47:08 +0100

    #433 Simplify use of Net bases, makes testing easier

--- Code/net/mixins/mixins.h
+++ Code/net/mixins/mixins.h
@@ -4,7 +4,7 @@
 #include "net/mixins/CoalescePointPoint.h"
 #include "net/mixins/StoringNet.h"
 #include "net/mixins/SeparatedGathers.h"
-#include "net/mixins/InterfaceDelegationNet.hpp"
+#include "net/mixins/InterfaceDelegationNet.h"
 #include "net/mixins/GathersViaPointPoint.h"
 
 #endif
