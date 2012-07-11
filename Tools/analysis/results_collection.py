1a490fae1c5ad3cc5e3a278b870f0c4d95ca5ec2
James Hetherington <j.hetherington@ucl.ac.uk>
2012-07-11 15:38:40 +0100

    #465

--- Tools/analysis/results_collection.py
+++ Tools/analysis/results_collection.py
@@ -17,6 +17,9 @@ class ResultsCollection(object):
         results=os.listdir(os.path.expanduser(source_path))
         Result=result.result_model(config)
         self.results=[Result(os.path.join(source_path,res)) for res in results]
+        if 'optional_properties' in config:
+          for optional_test,optional_config in config['optional_properties'].iteritems():
+            self.results=[aresult.upgrade(optional_config,optional_test) for aresult in self.results]
     def filter(self,selection,invert=False,latest=False):
         def filtration(result):
             ok=all([result.query(prop,value) for prop,value in selection.iteritems()])
