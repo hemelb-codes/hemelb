1f8846050b92246dd4ed6759103f7aab9b9a2006
James Hetherington <j.hetherington@ucl.ac.uk>
2012-07-16 14:56:56 +0100

    Add copyright text #473

--- Code/net/BaseNet.cc
+++ Code/net/BaseNet.cc
@@ -1,3 +1,12 @@
+// 
+// Copyright (C) University College London, 2007-2012, all rights reserved.
+// 
+// This file is part of HemeLB and is CONFIDENTIAL. You may not work 
+// with, install, use, duplicate, modify, redistribute or share this
+// file, or any part thereof, other than as allowed by any agreement
+// specifically made by you with University College London.
+// 
+
 /*! \file net.cc
  \brief In this file the functions useful to discover the topology used and
  to create and delete the domain decomposition and the various
