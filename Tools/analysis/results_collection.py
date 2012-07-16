ed252d701000a6621dcd0a6a4cd612ba939fdf1e
James Hetherington <j.hetherington@ucl.ac.uk>
2012-07-16 14:56:56 +0100

    Add copyright text #473

--- Tools/analysis/results_collection.py
+++ Tools/analysis/results_collection.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# 
+# Copyright (C) University College London, 2007-2012, all rights reserved.
+# 
+# This file is part of HemeLB and is CONFIDENTIAL. You may not work 
+# with, install, use, duplicate, modify, redistribute or share this
+# file, or any part thereof, other than as allowed by any agreement
+# specifically made by you with University College London.
+# 
+
 # encoding: utf-8
 """
 results_collection.py
@@ -24,4 +33,3 @@ class ResultsCollection(object):
         results=filter(filtration,self.results)
         if latest:
             results=sorted(results,key=lambda result: result.report_created_timestamp)[-1*latest:]
-        return results
