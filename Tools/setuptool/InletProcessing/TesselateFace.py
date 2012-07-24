150c3767b89ee17a2f24524b846aabe95d3f9e90
Rupert Nash <rupert.nash@ed.ac.uk>
2012-07-24 16:42:27 +0100

    Refactor the TransformAndTesselate method into an object
    --HG--
    branch : physics_validation

--- Tools/setuptool/InletProcessing/TesselateFace.py
+++ Tools/setuptool/InletProcessing/TesselateFace.py
@@ -1,62 +1,80 @@
 import numpy as np
-from vtk import vtkDelaunay2D, vtkTransformPolyDataFilter
+from vtk import vtkDelaunay2D, vtkTransformPolyDataFilter, vtkPoints, \
+    vtkPolyData, vtkTransform
 
-def TransformAndTesselate(inlet, intersectionPD, positionsNP):
-    trans = CreateTransformToInletCoords(inlet)
-    mergedPD = MergePoints(intersectionPD, positionsNP)
-    
-    tf = vtkTransformPolyDataFilter()
-    tf.SetTransform(trans)
-    tf.SetInput(mergedPD)
-    
-    tesselator = vtkDelaunay2D()
-    tesselator.SetSource(intersectionPD)
-    tesselator.SetInputConnection(tf.GetOutputPort())
-    
-    tesselator.Update()
-    return tesselator.GetOutput()
-
-from vtk import vtkPoints, vtkPolyData
-def MergePoints(intersectionPD, positionsNP):
-    """Create a vtkPolyData containing all the points, with the ones defining 
-    the perimeter first and in the same order as the input.
+class Tesselator(object):
+    """Object to transform the iolet to the X-Y plane and tesselate the 
+    interior with a triangular mesh.
     """
-    nI = intersectionPD.GetNumberOfPoints()
-    nP = len(positionsNP)
-    
-    outPD = vtkPolyData()
-    outPoints = vtkPoints()
-    outPoints.DeepCopy(intersectionPD.GetPoints())
-    
-    outPoints.SetNumberOfPoints(nI + nP)
-    for iPos, iOut in enumerate(xrange(nI, nI + nP)):
-        outPoints.SetPoint(iOut, *positionsNP[iPos])
-        continue
-    
-    outPD.SetPoints(outPoints)
-    return outPD
 
-from vtk import vtkTransform
-def CreateTransformToInletCoords(inlet):
-    """Create a vtkTransform which will rotate the coordinates such that
-    inlet.Normal is oriented in the z-direction and origin is inlet.Centre
-    """
-    n = inlet.Normal
-    n = np.array([n.x, n.y, n.z])
+    def __init__(self, inlet, intersectionPD, positionsNP):
+        """Create the object that will tesselate the face.
+        
+        inlet -- the HemeLbSetupTool.Model.Iolets.Iolet object
+        
+        intersectionPD -- vtkPolyData representing the intersection between the 
+            iolet and the surface
+            
+        positionsNP -- numpy array of the positions of the LB sites that are
+            adjacent to the iolet
+        """
+        self.trans = self._CreateTransformToInletCoords(inlet)
+        mergedPD = self._MergePoints(intersectionPD, positionsNP)
+        
+        tf = vtkTransformPolyDataFilter()
+        tf.SetTransform(self.trans)
+        tf.SetInput(mergedPD)
+        
+        self.tesselator = vtkDelaunay2D()
+        self.tesselator.SetSource(intersectionPD)
+        self.tesselator.SetInputConnection(tf.GetOutputPort())
+        return
     
-    z = np.array([0., 0., 1.])
+    def __call__(self):
+        self.tesselator.Update()
+        return self.tesselator.GetOutput()
     
-    theta = np.arccos(np.dot(n,z))
-    axis  = np.cross(n, z)
+    @staticmethod
+    def _MergePoints(intersectionPD, positionsNP):
+        """Create a vtkPolyData containing all the points, with the ones defining 
+        the perimeter first and in the same order as the input.
+        """
+        nI = intersectionPD.GetNumberOfPoints()
+        nP = len(positionsNP)
+        
+        outPD = vtkPolyData()
+        outPoints = vtkPoints()
+        outPoints.DeepCopy(intersectionPD.GetPoints())
+        
+        outPoints.SetNumberOfPoints(nI + nP)
+        for iPos, iOut in enumerate(xrange(nI, nI + nP)):
+            outPoints.SetPoint(iOut, *positionsNP[iPos])
+            continue
+        
+        outPD.SetPoints(outPoints)
+        return outPD
     
-    norm = np.sqrt(np.dot(axis, axis))
-    axis /= norm
-    
-    trans = vtkTransform()
-    trans.Scale(1., 1., 0.)
-    trans.RotateWXYZ(180. * theta / np.pi, axis[0], axis[1], axis[2])
-    
-    r = inlet.Centre
-    trans.Translate(r.x, r.y, r.z)
-    
-    return trans
+    @staticmethod
+    def _CreateTransformToInletCoords(inlet):
+        """Create a vtkTransform which will rotate the coordinates such that
+        inlet.Normal is oriented in the z-direction and origin is inlet.Centre
+        """
+        n = inlet.Normal
+        n = np.array([n.x, n.y, n.z])
+        
+        z = np.array([0., 0., 1.])
+        
+        theta = np.arccos(np.dot(n,z))
+        axis  = np.cross(n, z)
+        
+        norm = np.sqrt(np.dot(axis, axis))
+        axis /= norm
+        
+        trans = vtkTransform()
+        trans.Scale(1., 1., 0.)
+        trans.RotateWXYZ(180. * theta / np.pi, axis[0], axis[1], axis[2])
+        
+        r = inlet.Centre
+        trans.Translate(r.x, r.y, r.z)
+        
+        return trans
