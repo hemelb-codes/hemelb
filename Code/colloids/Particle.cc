39a06432ae7c89a1527227b3d7cc152c02abfcee
dholmes <unknown>
2012-08-01 16:28:02 +0100

    #400: colloid communication - position exchange works

--- Code/colloids/Particle.cc
+++ Code/colloids/Particle.cc
@@ -25,6 +25,14 @@ namespace hemelb
       globalPosition -= latDatLBM.GetOrigin();
       globalPosition /= latDatLBM.GetVoxelSize();
 
+      // updating position with zero velocity and zero body force is necessary
+      // because of the side-effect that sets owner rank from the new position
+      ownerRank = BIG_NUMBER2;
+      velocity *= 0.0;
+      bodyForces *= 0.0;
+      UpdatePosition(latDatLBM);
+
+      OutputInformation();
       if (log::Logger::ShouldDisplay<log::Debug>())
         log::Logger::Log<log::Debug, log::OnePerCore>(
           "In colloids::Particle::ctor, id: %i, a0: %g, ah: %g, position: {%g,%g,%g}\n",
@@ -32,7 +40,33 @@ namespace hemelb
           globalPosition.x, globalPosition.y, globalPosition.z);
     }
 
-    const void Particle::UpdatePosition()
+    const bool Particle::operator<(const Particle& other) const
+    {
+      // ORDER BY isLocal, ownerRank, particleId
+      if (ownerRank == other.ownerRank)
+        return (particleId < other.particleId);
+      else if (ownerRank == topology::NetworkTopology::Instance()->GetLocalRank())
+        return true;
+      else if (other.ownerRank == topology::NetworkTopology::Instance()->GetLocalRank())
+        return false;
+      else
+        return (ownerRank < other.ownerRank);
+    }
+
+    const bool Particle::IsOwnerRankKnown(std::map<proc_t, unsigned int> map) const
+    {
+      return map.count(ownerRank)>0;
+    }
+
+    const void Particle::OutputInformation() const
+    {
+        log::Logger::Log<log::Info, log::OnePerCore>(
+          "In colloids::Particle::OutputInformation, id: %i, owner: %i, position: {%g,%g,%g}, velocity: {%g,%g,%g}, bodyForces: {%g,%g,%g}\n",
+          particleId, ownerRank, globalPosition.x, globalPosition.y, globalPosition.z,
+          velocity.x, velocity.y, velocity.z, bodyForces.x, bodyForces.y, bodyForces.z);
+    }
+
+    const void Particle::UpdatePosition(const geometry::LatticeData& latDatLBM)
     {
       if (log::Logger::ShouldDisplay<log::Debug>())
         log::Logger::Log<log::Debug, log::OnePerCore>(
@@ -42,6 +76,34 @@ namespace hemelb
 
       globalPosition += velocity + bodyForces;
 
+      // round the global position of the particle to the nearest site coordinates
+      const util::Vector3D<site_t> siteGlobalPosition(
+                                     (site_t)(0.5+globalPosition.x),
+                                     (site_t)(0.5+globalPosition.y),
+                                     (site_t)(0.5+globalPosition.z));
+
+      // convert the site coordinates into a local site index and find owner rank
+      proc_t procId = latDatLBM.GetProcIdFromGlobalCoords(siteGlobalPosition);
+      isValid = (procId != BIG_NUMBER2);
+      if (isValid && (ownerRank != procId))
+      {
+        log::Logger::Log<log::Debug, log::OnePerCore>(
+          "Changing owner of particle %i from %i to %i - %s\n",
+          particleId, ownerRank, procId, isValid ? "valid" : "INVALID");
+        ownerRank = procId;
+      }
+/*
+      site_t siteId;
+      isValid = latDatLBM.GetContiguousSiteId(siteGlobalPosition, procId, siteId);
+      if (isValid)
+      {
+        if (ownerRank != procId)
+          log::Logger::Log<log::Debug, log::OnePerCore>(
+            "Changing owner of particle %i from %i to %i\n",
+            particleId, ownerRank, procId);
+        ownerRank = procId;
+      }
+*/
       if (log::Logger::ShouldDisplay<log::Debug>())
         log::Logger::Log<log::Debug, log::OnePerCore>(
           "In colloids::Particle::UpdatePosition, id: %i, position is now: {%g,%g,%g}\n",
@@ -65,22 +127,23 @@ namespace hemelb
 
       // we have chosen to make each block of fields contain a single field
       // so, the number of field blocks is the same as the number of fields
-      int numberOfFieldBlocks = 4;
+      int numberOfFieldBlocks = 5;
 
       // and the length of every field block is one
-      int lengthOfEachFieldBlock[] = {1, 1, 1, 1};
+      int lengthOfEachFieldBlock[] = {1, 1, 1, 1, 1};
 
       // there is no guarantee that the fields will be contiguous, so
       // the displacement of each field must be determined separately
       MPI::Aint displacementOfEachFieldBlock[numberOfFieldBlocks];
       displacementOfEachFieldBlock[0] = MPI::Get_address(&(temp.particleId)) - baseAddress; 
-      displacementOfEachFieldBlock[1] = MPI::Get_address(&(temp.smallRadius_a0)) - baseAddress; 
-      displacementOfEachFieldBlock[2] = MPI::Get_address(&(temp.largeRadius_ah)) - baseAddress; 
-      displacementOfEachFieldBlock[3] = MPI::Get_address(&(temp.globalPosition)) - baseAddress; 
+      displacementOfEachFieldBlock[1] = MPI::Get_address(&(temp.ownerRank)) - baseAddress; 
+      displacementOfEachFieldBlock[2] = MPI::Get_address(&(temp.smallRadius_a0)) - baseAddress; 
+      displacementOfEachFieldBlock[3] = MPI::Get_address(&(temp.largeRadius_ah)) - baseAddress; 
+      displacementOfEachFieldBlock[4] = MPI::Get_address(&(temp.globalPosition)) - baseAddress; 
 
       // the built-in MPI datatype of each field must match the C++ type
       MPI::Datatype datatypeOfEachFieldBlock[] =
-        {MPI::UNSIGNED_LONG, MPI::DOUBLE, MPI::DOUBLE, MpiDataType<util::Vector3D<double> >()};
+        {MPI::UNSIGNED_LONG, MPI::INT, MPI::DOUBLE, MPI::DOUBLE, MpiDataType<util::Vector3D<double> >()};
 
       // create a first draft of the MPI datatype for a Particle
       // the lower bound and displacements of fields are correct
@@ -172,42 +235,122 @@ namespace hemelb
       // determine the global coordinates of the next neighbour site:
       // nested loop - x, y, z directions semi-open interval [-2, +2)
       for (site_t x = ((site_t)globalPosition.x)-1;
-               x < ((site_t)globalPosition.x)+2; x++)
+                  x < ((site_t)globalPosition.x)+3; x++)
         for (site_t y = ((site_t)globalPosition.y)-1;
-                 y < ((site_t)globalPosition.y)+2; y++)
+                    y < ((site_t)globalPosition.y)+3; y++)
           for (site_t z = ((site_t)globalPosition.z)-1;
-                   z < ((site_t)globalPosition.z)+2; z++)
+                      z < ((site_t)globalPosition.z)+3; z++)
           {
             const util::Vector3D<site_t> siteGlobalPosition(x, y, z);
 
             // convert the global coordinates of the site into a local site index
             proc_t procId;
             site_t siteId;
-            bool isValid = latDatLBM.GetContiguousSiteId(siteGlobalPosition, procId, siteId);
-            bool isLocal = (procId == topology::NetworkTopology::Instance()->GetLocalRank());
+            bool isSiteValid = latDatLBM.GetContiguousSiteId(siteGlobalPosition, procId, siteId);
+            bool isSiteLocal = (procId == topology::NetworkTopology::Instance()->GetLocalRank());
 
             if (log::Logger::ShouldDisplay<log::Debug>())
             {
+              log::Logger::Log<log::Debug, log::OnePerCore>("WAIT A MINUTE 1 ...\n");
+
+              int globalStatus = 0; // valid && local fluid
+              int blockStatus = 0; // valid && non-empty
+              int siteStatus = 0; // valid && fluid
+
               util::Vector3D<site_t> blockCoords, localSiteCoords;
               latDatLBM.GetBlockAndLocalSiteCoords(siteGlobalPosition, blockCoords, localSiteCoords);
+              log::Logger::Log<log::Debug, log::OnePerCore>("WAIT A MINUTE 2 ...\n");
+              if (!latDatLBM.IsValidBlock(blockCoords))
+                blockStatus = 1; // invalid - out of range
+              else if (latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).IsEmpty())
+                blockStatus = 2; // empty - entire block is solid
 
+              log::Logger::Log<log::Debug, log::OnePerCore>("WAIT A MINUTE 3 ...\n");
+
+              if (!latDatLBM.IsValidLatticeSite(localSiteCoords))
+                siteStatus = 1; // invalid - out of range
+              else if (blockStatus == 2)
+                siteStatus = 2; // deemed solid because block is empty
+              else if (blockStatus == 0)
+                if (latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).GetProcessorRankForSite(latDatLBM.GetLocalSiteIdFromLocalSiteCoords(localSiteCoords)) == BIG_NUMBER2)
+                  siteStatus = 3; // individual site is not simulated, i.e. must be solid
+                else if (latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).GetProcessorRankForSite(latDatLBM.GetLocalSiteIdFromLocalSiteCoords(localSiteCoords)) == topology::NetworkTopology::Instance()->GetLocalRank())
+                  if (latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).SiteIsSolid(latDatLBM.GetLocalSiteIdFromLocalSiteCoords(localSiteCoords)))
+                  siteStatus = 4; // individual site is local but solid (should not happen?)
+
+              log::Logger::Log<log::Debug, log::OnePerCore>("WAIT A MINUTE 4 ...\n");
+
+              if (blockStatus == 1 || (siteStatus == 1))
+                globalStatus = 1; // invalid - out of range
+              else if (blockStatus == 2 || (siteStatus == 3) | (siteStatus == 4))
+                globalStatus = 2; // solid
+              else if (latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).GetProcessorRankForSite(latDatLBM.GetLocalSiteIdFromLocalSiteCoords(localSiteCoords)) != topology::NetworkTopology::Instance()->GetLocalRank())
+                globalStatus = 3; // remote fluid
+
+              log::Logger::Log<log::Debug, log::OnePerCore>("ABOUT TO DO STUFF\n");
+/*
               log::Logger::Log<log::Debug, log::OnePerCore>(
-                "In colloids::Particle::InterpolateFluidVelocity, particleId: %i,\nsiteGlobalPosition: {%i,%i,%i}\nblockCoords: {%i,%i,%i} - %s\nlocalSiteCoords: {%i,%i,%i} - %s\nprocId: %i, siteId: %i\n",
-                particleId,
-                siteGlobalPosition.x, siteGlobalPosition.y, siteGlobalPosition.z,
-                blockCoords.x, blockCoords.y, blockCoords.z,
-                !latDatLBM.IsValidBlock(blockCoords) ? "INVALID" :
-                (latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).IsEmpty() ?
-                "SOLID" : "valid"),
-                localSiteCoords.x, localSiteCoords.y, localSiteCoords.z,
-                !latDatLBM.IsValidLatticeSite(localSiteCoords) ? "INVALID" :
-                (!isValid ? "SOLID" : (!isLocal ? "REMOTE" : "local fluid")),
-                procId, siteId);
+                "In colloids::Particle::InterpolateFluidVelocity, particleId: %i, siteGlobalPosition: {%i,%i,%i} - %s, blockCoords: {%i,%i,%i} - %s, localSiteCoords: {%i,%i,%i} - %s\n",
+                particleId, siteGlobalPosition.x, siteGlobalPosition.y, siteGlobalPosition.z,
+                globalStatus == 0 ? "valid && local fluid" :
+                (globalStatus == 1 ? "invalid - out-of-range" :
+                (globalStatus == 2 ? "solid" :
+                (globalStatus == 3 ? "remote fluid" : "unknown status"))),
+                particleId, blockCoords.x, blockCoords.y, blockCoords.z,
+                blockStatus == 0 ? "valid && non-empty" :
+                (blockStatus == 1 ? "invalid - out-of-range" :
+                (blockStatus == 2 ? "empty - entire block is solid" : "unknown status")),
+                particleId, localSiteCoords.x, localSiteCoords.y, localSiteCoords.z,
+                siteStatus == 0 ? "valid && fluid" :
+                (siteStatus == 1 ? "invalid - out of range" :
+                (siteStatus == 2 ? "deemed solid because block is empty" :
+                (siteStatus == 3 ? "individual site is solid" : "unknown status"))));
+*/
+//*
+              log::Logger::Log<log::Debug, log::OnePerCore>(
+                "In colloids::Particle::InterpolateFluidVelocity, particleId: %i, siteGlobalPosition: {%i,%i,%i} - %s \n",
+                particleId, siteGlobalPosition.x, siteGlobalPosition.y, siteGlobalPosition.z,
+                globalStatus == 0 ? "valid && local fluid" :
+                (globalStatus == 1 ? "invalid - out-of-range" :
+                (globalStatus == 2 ? "solid" :
+                (globalStatus == 3 ? "remote fluid" : "unknown status"))));
+              log::Logger::Log<log::Debug, log::OnePerCore>("ABOUT TO DO MORE STUFF\n");
+              log::Logger::Log<log::Debug, log::OnePerCore>(
+                "In colloids::Particle::InterpolateFluidVelocity, particleId: %i, blockCoords: {%i,%i,%i} - %s\n",
+                particleId, blockCoords.x, blockCoords.y, blockCoords.z,
+                blockStatus == 0 ? "valid && non-empty" :
+                (blockStatus == 1 ? "invalid - out-of-range" :
+                (blockStatus == 2 ? "empty - entire block is solid" : "unknown status")));
+              log::Logger::Log<log::Debug, log::OnePerCore>("ABOUT TO DO EVEN MORE STUFF\n");
+              log::Logger::Log<log::Debug, log::OnePerCore>(
+                "In colloids::Particle::InterpolateFluidVelocity, particleId: %i, localSiteCoords: {%i,%i,%i} - %s\n",
+                particleId, localSiteCoords.x, localSiteCoords.y, localSiteCoords.z,
+                siteStatus == 0 ? "valid && fluid" :
+                (siteStatus == 1 ? "invalid - out of range" :
+                (siteStatus == 2 ? "deemed solid because block is empty" :
+                (siteStatus == 3 ? "deemed solid because not simulated" :
+                (siteStatus == 4 ? "local but solid (should not happen?)" : "unknown status")))));
+//*/
+              if (siteStatus == 0)
+              log::Logger::Log<log::Debug, log::OnePerCore>("ABOUT TO DO ONE LAST THING\n");
+              if (siteStatus == 0)
+              log::Logger::Log<log::Debug, log::OnePerCore>(
+                "In colloids::Particle::InterpolateFluidVelocity, particleId: %i, procIdForSite: %i\n",
+                particleId, latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).GetProcessorRankForSite(latDatLBM.GetLocalSiteIdFromLocalSiteCoords(localSiteCoords)));
+              log::Logger::Log<log::Debug, log::OnePerCore>("DONE LOTS OF STUFF\n");
+
+              if (!isSiteValid || !isSiteLocal)
+                if (globalStatus == 0)
+                {
+                  log::Logger::Log<log::Debug, log::OnePerCore>("MAJOR PROBLEM! isValid: %s, isLocal: %s, procIdForSite: %i, localRank: %i\n", isSiteValid ? "T": "F", isSiteLocal ? "T" : "F",
+                    latDatLBM.GetBlock(latDatLBM.GetBlockIdFromBlockCoords(blockCoords)).GetProcessorRankForSite(latDatLBM.GetLocalSiteIdFromLocalSiteCoords(localSiteCoords)),
+                    topology::NetworkTopology::Instance()->GetLocalRank());
+                }
             }
 
             /** TODO: implement communication for remote fluid sites */
             /** TODO: implement boundary conditions for invalid/solid sites */
-            if (!isValid || !isLocal)
+            if (!isSiteValid || !isSiteLocal)
               continue;
 
             // read value of velocity for site index from macroscopic cache
