b20f08ef8b1ed2e1b4e3f4248fa04944379d866d
Rupert Nash <rupert.nash@ed.ac.uk>
2012-08-09 19:03:11 +0100

    #507 Bug fix the extraction -> VTK converter, also improve performance by caching the indexes
    --HG--
    branch : physics_validation

--- Tools/hemeTools/converters/ExtractedPropertyUnstructuredGridReader.py
+++ Tools/hemeTools/converters/ExtractedPropertyUnstructuredGridReader.py
@@ -7,7 +7,20 @@ script with no arguments.
 """
 
 import vtk
+from vtk.util import numpy_support
+import numpy as np
 from hemeTools.parsers.extraction import ExtractedProperty
+
+# Work around limitations of the VTK bindings support (enum wrapping added in 5.8)
+v = vtk.vtkVersion()
+if v.GetVTKMajorVersion() >5 or \
+    (v.GetVTKMajorVersion()==5 and v.GetVTKMinorVersion() >6):
+    CELL = vtk.vtkSelectionNode.CELL
+    INDICES = vtk.vtkSelectionNode.INDICES
+else:
+    CELL = 0
+    INDICES = 4
+del v
 
 class ExtractedPropertyUnstructuredGridReader(vtk.vtkProgrammableFilter):
     """VTK-style filter for reading HemeLB extracted property files as VTK 
@@ -25,12 +38,17 @@ class ExtractedPropertyUnstructuredGridReader(vtk.vtkProgrammableFilter):
         self.SetExecuteMethod(self._Execute)
         self.Extracted = None
         self.Time = None
+        self.Skeleton = None
+        self.OutputCellIdsByInputIndex = None
+        self.InputMTime = -1
         return
     
     def SetExtraction(self, extracted):
         """The parsed extracted property file.
         """
         self.Extracted = extracted
+        self.Skeleton = None
+        self.OutputCellIdsByInputIndex = None
         self.Modified() 
         return
 
@@ -43,9 +61,14 @@ class ExtractedPropertyUnstructuredGridReader(vtk.vtkProgrammableFilter):
             self.Modified()
         return    
 
-    def _Execute(self):
-        """Private method that actually does the reading. Called by the VTK
-        API.
+    def _CreateSkeleton(self):
+        """Create the structure of the output vtkUnstructuredGrid and a map
+        from the index of a point in the extraction file to the corresponding
+        cellId in the skeleton.
+        
+        This method should only be called if the extraction object this 
+        instance is working on has changed since the last time this method was
+        called.
         """
         input = self.GetUnstructuredGridInput()
         
@@ -58,104 +81,118 @@ class ExtractedPropertyUnstructuredGridReader(vtk.vtkProgrammableFilter):
         locator.SetDataSet(centers.GetOutput())
         locator.BuildLocator()
         # Should be fine enough
-        locator.SetTolerance(0.1 * extraction.voxelSizeMetres)
+        locator.SetTolerance(0.1 * self.Extracted.voxelSizeMetres)
+
+        # Get the first set of extracted data from the file. We don't care
+        # which as we only want to use the positions.
+        extracted_positions = self.Extracted.GetByIndex(0).position
+        nExtractedPoints = len(extracted_positions)
         
-        # Copy the structure to output.
-        grid = self.GetUnstructuredGridOutput()
-
-        # Get the data we want to visualise, Note that this is likely to 
-        # involve a subset of the points in the geometry
-        extracted_data = self.Extracted.GetByTimeStep(self.Time)
-
-        # Make a list of the cell ids to keep
-        cellIds = vtk.vtkIdTypeArray()
-        cellIds.SetNumberOfComponents(1)
- 
-        for point in extracted_data:
+        # Make a list of the cell ids to keep; i.e. the cell in the input 
+        # (whole geometry) with ID cellIdsGmy[i] contains  the point given by
+        # extracted_positions[i]
+        gmyCellIdsByInput = vtk.vtkIdTypeArray()
+        gmyCellIdsByInput.SetNumberOfComponents(1)
+        gmyCellIdsByInput.SetNumberOfTuples(nExtractedPoints)
+        
+        gmyCellIdsByInputNp = numpy_support.vtk_to_numpy(gmyCellIdsByInput)
+         
+        for i, point in enumerate(extracted_positions):
             # Get cell in geometry corresponding to the point
-            cellId = locator.FindClosestPoint(point.position)
+            cellId = locator.FindClosestPoint(point)
 
             if cellId == -1:
-                raise ValueError("Can't find cell for point at " + str(point.position))
+                raise ValueError("Can't find cell for point at " + str(point))
 
-            cellIds.InsertNextValue(cellId)
-
+            gmyCellIdsByInputNp[i] = cellId
+        
         # Make an object to select only the cell ids we want
         selector = vtk.vtkSelectionNode()
-        selector.SetFieldType(vtk.vtkSelectionNode.CELL)
-        selector.SetContentType(vtk.vtkSelectionNode.INDICES)
-        selector.SetSelectionList(cellIds)
-
+        selector.SetFieldType(CELL)
+        selector.SetContentType(INDICES)
+        selector.SetSelectionList(gmyCellIdsByInput)
+        
         # Make an object to hold the selector
         selectors = vtk.vtkSelection()
         selectors.AddNode(selector)
-
+        
         # Perform the selection
         extractSelection = vtk.vtkExtractSelectedIds()
         extractSelection.SetInput(0, input)
         extractSelection.SetInput(1, selectors)
         extractSelection.Update()
-
-        # Copy the result into our grid, and get the lookup array from the new cell ids
-        # to the old cell ids.
-        grid.ShallowCopy(extractSelection.GetOutput())
-
-        originalCellIdLookup = grid.GetCellData().GetArray('vtkOriginalCellIds')
-
-        nCells = grid.GetNumberOfCells()
-
-        # Create the field datasets to write into
+        
+        gmyCellIdsByOutput = extractSelection.GetOutput().GetCellData().GetArray('vtkOriginalCellIds')
+        gmyCellIdsByOutputNp = numpy_support.vtk_to_numpy(gmyCellIdsByOutput)
+        
+        outCellIdsByInputNp = np.empty_like(gmyCellIdsByOutputNp)
+        
+        for iExtracted in xrange(nExtractedPoints):
+            for iSearch in xrange(nExtractedPoints):
+                if gmyCellIdsByOutputNp[iSearch] == gmyCellIdsByInputNp[iExtracted]:
+                    outCellIdsByInputNp[iExtracted] = iSearch
+                    break
+        
+        self.OutputCellIdsByInputIndex = outCellIdsByInputNp
+        
+        # Create the skeleton data object and only copy in the structure.
+        self.Skeleton = vtk.vtkUnstructuredGrid()
+        self.Skeleton.CopyStructure(extractSelection.GetOutput())
+        return
+    
+    def _Execute(self):
+        """Private method that actually does the reading. Called by the VTK
+        API.
+        """
+        # Check if we have a skeleton and if so, if it is up-to-date.
+        if self.Skeleton is None or (self.GetInput().GetMTime() > self.InputMTime):
+            self._CreateSkeleton()
+            self.InputMTime = self.GetInput().GetMTime()
+        
+        # Copy the structure to output.
+        output = self.GetUnstructuredGridOutput()
+        output.CopyStructure(self.Skeleton)
+        
+        # Get the data we want to visualise, Note that this is likely to 
+        # involve a subset of the points in the geometry
+        extracted_data = self.Extracted.GetByTimeStep(self.Time)
+        nExtractedPoints = len(extracted_data)
+        
+        # Create the arrays to store field data
         field_dict = {}
-  
+        
         for name, xdrType, memType, length, offset in self.Extracted.GetFieldSpec():
-            # Skip the grid.
+            # Skip the grid (i.e. 3d-index)
             if name == 'grid':
                 continue
-
-            # Create a VTK object for storing the data.
+            
+            # Set up the VTK array
             field = vtk.vtkDoubleArray()
             if isinstance(length, tuple):
                 length = length[0]
             field.SetNumberOfComponents(length)
-            field.SetNumberOfTuples(nCells)
+            field.SetNumberOfTuples(nExtractedPoints)
             field.SetName(name)
 
             # Insert it into the dictionary
             field_dict[name] = field
 
-        # The hard work bit.
-        for point in extracted_data:
-            # Get cell in geometry corresponding to the point
-            oldCellId = locator.FindClosestPoint(point.position)
-            if oldCellId == -1:
-                raise ValueError("Can't find cell for point at " + str(point.position))
+        # Copy the data into the correct position in the output array
+        for i, point in enumerate(extracted_data):
+            cellId = self.OutputCellIdsByInputIndex[i]
 
-            cellId = -1
-
-            # Translate to new grid.
-            for i in range(0, originalCellIdLookup.GetSize()):
-                if originalCellIdLookup.GetComponent(0, i) == oldCellId:
-                    cellId = i
-                    break
-
-            if cellId == -1:
-                raise ValueError("Can't translate old cellId %i to new grid for point at %s" % (oldCellId, str(point.position)))
-
-            # For each field considered, set the property in the associated VTK array.
+            # TODO: this needs another case for the stress. We should check
+            # what representation VTK uses for rank 3 tensors.            
             for field_name,field in field_dict.items():
                 if field.GetNumberOfComponents() == 3:
                     field.SetTuple3(cellId, *getattr(point, field_name))
                 else:
                     field.SetTuple1(cellId, getattr(point, field_name))
         
-        # Add the arrays to the output
+        # Add the arrays to the output data object
         for field in field_dict:
-            grid.GetCellData().AddArray(field_dict[field])
-
-        # Remove the arrays added during the translation of the grid.
-        grid.GetCellData().RemoveArray('vtkOriginalCellIds')
-        grid.GetPointData().RemoveArray('vtkOriginalPointIds')
-
+            output.GetCellData().AddArray(field_dict[field])
+            
         return
     pass
 
@@ -192,21 +229,23 @@ if __name__ == '__main__':
 
     # For each extracted file given, parse the file...    
     for extractedFile in args.extracted:
-        extraction=ExtractedProperty(extractedFile)
-
-        # For each timestamp in that file, create a reader...
-        for time in extraction.times:
-            converter = ExtractedPropertyUnstructuredGridReader()
-            converter.SetInputConnection(reader.GetOutputPort())
-            converter.SetExtraction(extraction)
-            converter.SetTime(time)
-    
-            # And a writer...
-            writer = vtk.vtkXMLUnstructuredGridWriter()
-            writer.SetInputConnection(converter.GetOutputPort())
+        extraction = ExtractedProperty(extractedFile)
+        converter = ExtractedPropertyUnstructuredGridReader()
+        converter.SetInputConnection(reader.GetOutputPort())
+        converter.SetExtraction(extraction)
+            
+        # Create a writer...
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        writer.SetInputConnection(converter.GetOutputPort())
         
+        # For each timestamp in that file
+        for time in extraction.times:
+            # Choose the time
+            converter.SetTime(time)
+            
             # And give the output a unique filename based on the extracted
             # property file name and the timestamp.
             base, ext = os.path.splitext(extractedFile)
             writer.SetFileName('%s_%s.vtu' % (base, str(time)))
+            # trigger the update
             writer.Write()
