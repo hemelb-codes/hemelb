8629637580867286b1cceec5a1e8cef0e901653e
Miguel O. Bernabeu <unknown>
2012-08-30 14:07:24 +0100

    #493 Added analytical expression for Womersley velocity and modified analysis tool to get geometry characteristics from .smy file

--- Tools/analysis/helpers.py
+++ Tools/analysis/helpers.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import scipy.special
 
 def space(grid_coords,voxel_size,origin):
   return [grid_coord*voxel_size+origin for grid_coord in grid_coords] 
@@ -32,3 +33,13 @@ def frobenius_norm(symmetric_matrix):
   # Compute the Frobenius norm of symmetric_matrix. Off-diagonal matrix elements are added twice to account for the not stored strictly lower part of the matrix.
   norm = math.sqrt(sum(squared_symmetric_matrix) + np.ma.sum(np.ma.array(squared_symmetric_matrix, mask=off_diagonal_mask)))
   return norm
+
+def womersley_velocity(radial_position, time, womersley, pipe_radius, pressure_amplitude, pressure_period, density):
+  print radial_position, time, womersley, pipe_radius, pressure_amplitude, pressure_period, density
+  # Eq. (5.1) in Formaggia et al. "Cardiovascular Mathematics"
+  bessel_numerator = scipy.special.jn(0, pow(1j, 3.0/2.0)*womersley*(radial_position/pipe_radius))
+  bessel_denominator = scipy.special.jn(0, pow(1j, 3.0/2.0)*womersley)
+  sol = (((pressure_amplitude * pressure_period) / (2j * np.pi * density))
+         * (1 - bessel_numerator/bessel_denominator)
+         * np.exp(2j * np.pi * (time/pressure_period))).real
+  return sol
