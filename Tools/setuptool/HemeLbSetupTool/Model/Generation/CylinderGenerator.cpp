477a96a71bebe26addc529f34e4b9268b13d458f
Rupert Nash <rupert.nash@ed.ac.uk>
2012-08-30 12:19:25 +0100

    Change the logic for determining fluidness. Rewrite intersection method to ensure exactly one intersection point.

--- Tools/setuptool/HemeLbSetupTool/Model/Generation/CylinderGenerator.cpp
+++ Tools/setuptool/HemeLbSetupTool/Model/Generation/CylinderGenerator.cpp
@@ -123,127 +123,57 @@ void CylinderGenerator::WriteFluidSite(BlockWriter& blockWriter, Site& site) {
 	}
 }
 
-/*
- * Given a site with known fluidness, examine the links to not-yet-visited
- * neighbouring sites. If the neighbours have unknown fluidness, set that.
- *
- * Since we wish to examine each link only once, this will set link properties
- * from neighbour => site as well as site => neighbour
- *
- */
-void CylinderGenerator::ClassifySite(Site& site) {
-
-	for (LaterNeighbourIterator neighIt = site.begin(); neighIt != site.end(); ++neighIt) {
-		Site& neigh = *neighIt;
-		unsigned int iNeigh = neighIt.GetNeighbourIndex();
-		vtkIdType nHits;
-
-		if (!neigh.IsFluidKnown) {
-			// Neighbour unknown, must always intersect
-			nHits = this->ComputeIntersections(site, neigh);
-
-			if (nHits % 2 == 0) {
-				// Even # hits, hence neigh has same type as site
-				neigh.IsFluid = site.IsFluid;
-			} else {
-				// Odd # hits, neigh is opposite type to site
-				neigh.IsFluid = !site.IsFluid;
-			}
-
-			if (neigh.IsFluid)
-				neigh.CreateLinksVector();
-
-			neigh.IsFluidKnown = true;
+bool IsInsideCylinder(CylinderData* cyl, Vector& pt) {
+	Vector x = pt - cyl->Centre;
+	double z = Vector::Dot(x, cyl->Axis);
+	if (z > -0.5 * cyl->Length && z < 0.5 * cyl->Length) {
+		double rsq = Vector::Dot(x, x) - z*z;
+		if (rsq < cyl->Radius * cyl->Radius) {
+			return true;
 		} else {
-			// We know the fluidness of neigh, maybe don't need to intersect
-			if (site.IsFluid != neigh.IsFluid) {
-				// Only in the case of difference must we intersect.
-				nHits = this->ComputeIntersections(site, neigh);
-				if (nHits % 2 != 1) {
-					throw InconsistentFluidnessError(site, neigh, nHits);
-				}
-			}
+			return false;
 		}
-
-		// Four cases: fluid-fluid, solid-solid, fluid-solid and solid-fluid.
-		// Will handle the last two together.
-		if (site.IsFluid == neigh.IsFluid) {
-			if (site.IsFluid) {
-				// Fluid-fluid, must set CUT_NONE for both
-				site.Links[iNeigh].Type = geometry::CUT_NONE;
-				neigh.Links[Neighbours::inverses[iNeigh]].Type
-						= geometry::CUT_NONE;
-			} else {
-				// solid-solid, nothing to do.
-			}
-		} else {
-			// They differ, figure out which is fluid and which is solid.
-			Site* fluid;
-			Site* solid;
-
-			// Index of the solid site from the fluid site.
-			int iSolid;
-			// Index of the point in this->hitPoints we're considering as the
-			// hit of interest (i.e. the one closest to the fluid site).
-			int iHit;
-
-			if (site.IsFluid) {
-				fluid = &site;
-				solid = &neigh;
-				iSolid = iNeigh;
-				iHit = 0;
-			} else {
-				fluid = &neigh;
-				solid = &site;
-				iSolid = Neighbours::inverses[iNeigh];
-				iHit = nHits - 1;
-			}
-
-			Vector hitPoint;
-			this->hitPoints->GetPoint(iHit, &hitPoint[0]);
-			LinkData& link = fluid->Links[iSolid];
-
-			// This is set in any solid case
-			link.Distance = (hitPoint - fluid->Position).GetMagnitude();
-			// The distance is in voxels but must be output as a fraction of
-			// the lattice vector. Scale it.
-			link.Distance /= Neighbours::norms[iSolid];
-
-			// The value associated with that cell, which identifies what was hit.
-			int ioletId = this->hitCellIds->GetId(iHit);
-
-			if (ioletId < 0) {
-				// -1 => we hit a wall
-				link.Type = geometry::CUT_WALL;
-			} else {
-				// We hit an inlet or outlet
-				Iolet* iolet = this->Iolets[ioletId];
-				if (iolet->IsInlet) {
-					link.Type = geometry::CUT_INLET;
-				} else {
-					link.Type = geometry::CUT_OUTLET;
-				}
-				// Set the Id
-				link.IoletId = iolet->Id;
-			}
-		}
-
+	} else {
+		return false;
 	}
 }
 
-int CylinderGenerator::ComputeIntersections(Site& from, Site& to) {
-	int nHits = 0;
-	this->hitPoints->SetNumberOfPoints(0);
-	this->hitCellIds->SetNumberOfIds(0);
+#define TOL 1e-6
 
+struct Hit {
+	double t;
+	Vector pt;
+	int cellId;
+	bool operator<(const Hit& rhs) const {
+		// Note this goes the wrong way! It's meant to.
+		return (t > rhs.t);
+	}
+};
+
+#include <queue>
+typedef std::priority_queue<Hit> HitList;
+/**
+ * Compute the intersection between the line segment and the capped cylinder.
+ *
+ * Site from is ALWAYS fluid
+ * Site to is ALWAYS solid
+ *
+ * Therefore we expect exactly one intersection. Due to floating point errors,
+ * we can't guarantee this. So, we search for intersections with some
+ * tolerance (macro TOL) and pick the closest to fluid site (as given by the
+ * parametic line coordinate t. We use a priority_queue to keep the hits in
+ * order.
+ */
+Hit ComputeIntersection(CylinderData* cyl, std::vector<Iolet*>& iolets, Site& from, Site& to) {
+	HitList hitList = HitList();
 	/*
 	 * The equation of the line segment is:
 	 * x(t) = a + t (b - a)		t E (0, 1)
 	 */
-	Vector& n = this->Cylinder->Axis;
-	double& r = this->Cylinder->Radius;
-	Vector& c = this->Cylinder->Centre;
-	double& h = this->Cylinder->Length;
+	Vector& n = cyl->Axis;
+	double& r = cyl->Radius;
+	Vector& c = cyl->Centre;
+	double& h = cyl->Length;
 
 	{
 		/*
@@ -287,12 +217,13 @@ int CylinderGenerator::ComputeIntersections(Site& from, Site& to) {
 					// Now check if it's on the finite cylinder. This requires that
 					// x.n E (-h/2, h/2)
 					double xDOTn = aDOTn + t * b_aDOTn;
-					if (xDOTn >= -0.5 * h && xDOTn <= 0.5 * h) {
+					if (xDOTn >= -0.5 * h - TOL && xDOTn <= 0.5 * h + TOL) {
 						// Real cylinder hit!
-						Vector hit = from.Position + b_a * t;
-						++nHits;
-						this->hitPoints->InsertNextPoint(hit.x, hit.y, hit.z);
-						this->hitCellIds->InsertNextId(-1);
+						Hit hit;
+						hit.t = t;
+						hit.pt = from.Position + b_a * t;
+						hit.cellId = -1;
+						hitList.push(hit);
 					}
 				}
 			}
@@ -304,8 +235,8 @@ int CylinderGenerator::ComputeIntersections(Site& from, Site& to) {
 	Vector& a = from.Position;
 	Vector& b = to.Position;
 	Vector b_a = b - a;
-	for (std::vector<Iolet*>::iterator iIt = this->Iolets.begin(); iIt
-			!= this->Iolets.end(); ++iIt) {
+	for (std::vector<Iolet*>::iterator iIt = iolets.begin(); iIt
+			!= iolets.end(); ++iIt) {
 		Iolet* iolet = *iIt;
 		/*
 		 * Plane equation is x.p = q.p (p = plane normal, q = point on plane)
@@ -321,13 +252,101 @@ int CylinderGenerator::ComputeIntersections(Site& from, Site& to) {
 			Vector x_c = x - c;
 			double x_cDOTn = Vector::Dot(x_c, n);
 			Vector radial = x_c - n * x_cDOTn;
-			if (radial.GetMagnitudeSquared() <= r * r) {
+			if (radial.GetMagnitudeSquared() < (r + TOL) * (r + TOL)) {
 				// Within the cap
-				++nHits;
-				this->hitPoints->InsertNextPoint(x.x, x.y, x.z);
-				this->hitCellIds->InsertNextId(iIt - this->Iolets.begin());
+				Hit hit;
+				hit.t = t;
+				hit.pt = x;
+				hit.cellId = iIt - iolets.begin();
+				hitList.push(hit);
 			}
 		}
 	}
-	return nHits;
+	// We know there SHOULD be exactly one intersection. Take the closest.
+	if (hitList.size() == 0)
+		throw InconsistentFluidnessError(from, to, hitList.size());
+	return hitList.top();
+}
+
+/*
+ * Given a site with known fluidness, examine the links to not-yet-visited
+ * neighbouring sites. If the neighbours have unknown fluidness, set that.
+ *
+ * Since we wish to examine each link only once, this will set link properties
+ * from neighbour => site as well as site => neighbour
+ *
+ */
+void CylinderGenerator::ClassifySite(Site& site) {
+
+	for (LaterNeighbourIterator neighIt = site.begin(); neighIt != site.end(); ++neighIt) {
+		Site& neigh = *neighIt;
+		unsigned int iNeigh = neighIt.GetNeighbourIndex();
+		vtkIdType nHits;
+
+		if (!neigh.IsFluidKnown) {
+			neigh.IsFluidKnown = true;
+			neigh.IsFluid = IsInsideCylinder(this->Cylinder, neigh.Position);
+			if (neigh.IsFluid)
+				neigh.CreateLinksVector();
+		}
+
+		// Four cases: fluid-fluid, solid-solid, fluid-solid and solid-fluid.
+		// Will handle the last two together.
+		if (site.IsFluid == neigh.IsFluid) {
+			if (site.IsFluid) {
+				// Fluid-fluid, must set CUT_NONE for both
+				site.Links[iNeigh].Type = geometry::CUT_NONE;
+				neigh.Links[Neighbours::inverses[iNeigh]].Type
+						= geometry::CUT_NONE;
+			} else {
+				// solid-solid, nothing to do.
+			}
+		} else {
+			// They differ, figure out which is fluid and which is solid.
+			Site* fluid;
+			Site* solid;
+
+			// Index of the solid site from the fluid site.
+			int iSolid;
+
+			if (site.IsFluid) {
+				fluid = &site;
+				solid = &neigh;
+				iSolid = iNeigh;
+			} else {
+				fluid = &neigh;
+				solid = &site;
+				iSolid = Neighbours::inverses[iNeigh];
+			}
+
+			// This uses special knowledge about the fact that we're working
+			// on a cylinder. Since the first point is always fluid, and the
+			// second always solid, we must get exactly one hit.
+			Hit hit = ComputeIntersection(this->Cylinder, this->Iolets, *fluid, *solid);
+
+			LinkData& link = fluid->Links[iSolid];
+
+			// This is set in any solid case
+			link.Distance = (hit.pt - fluid->Position).GetMagnitude();
+			// The distance is in voxels but must be output as a fraction of
+			// the lattice vector. Scale it.
+			link.Distance /= Neighbours::norms[iSolid];
+
+			if (hit.cellId < 0) {
+				// -1 => we hit a wall
+				link.Type = geometry::CUT_WALL;
+			} else {
+				// We hit an inlet or outlet
+				Iolet* iolet = this->Iolets[hit.cellId];
+				if (iolet->IsInlet) {
+					link.Type = geometry::CUT_INLET;
+				} else {
+					link.Type = geometry::CUT_OUTLET;
+				}
+				// Set the Id
+				link.IoletId = iolet->Id;
+			}
+		}
+
+	}
 }
