64bc705d61a559b0c5c00420afdf381e37e9f7d3
Derek Groen <d.groen@ucl.ac.uk>
2012-09-05 12:17:54 +0100

    BROKEN VERSION: Merged the multiscale ehad with the old MS enhancements. I will now proceed to make this compileable.

---
+++ Code/lb/boundaries/iolets/InOutLetVelocityAware.cc
@@ -0,0 +1,14 @@
+/*
+ * InOutLetVelocityAware.cc
+ *
+ *  Created on: 3 Jul 2012
+ *      Author: derek
+ */
+
+#include "lb/boundaries/iolets/InOutLetVelocityAware.h"
+#include "configuration/SimConfig.h"
+#include "topology/NetworkTopology.h"
+
+
+
+
