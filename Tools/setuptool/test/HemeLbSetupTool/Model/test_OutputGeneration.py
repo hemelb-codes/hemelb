c1bc4c5d9fc64970e762ebce4fa95695f18624be
Rupert Nash <rupert.nash@ed.ac.uk>
2012-09-05 16:46:21 +0100

    Add a regression test for the gmy generators that use polydata

--- Tools/setuptool/test/HemeLbSetupTool/Model/test_OutputGeneration.py
+++ Tools/setuptool/test/HemeLbSetupTool/Model/test_OutputGeneration.py
@@ -1,10 +1,40 @@
 import os.path
 import numpy as np
-from HemeLbSetupTool.Model.OutputGeneration import CylinderGenerator
+
+from HemeLbSetupTool.Model import OutputGeneration
+from HemeLbSetupTool.Model.Profile import Profile
 from hemeTools.parsers.geometry.simple import ConfigLoader
+
+class TestPolyDataGenerator:
+    def test_regression(self, tmpdir):
+        """Generate a gmy from a stored profile and check that the output is
+        identical.
+        """
+        dataDir = os.path.join(os.path.split(__file__)[0], 'data')
+        proFileName = os.path.join(dataDir, 'test.pro')
+        
+        p = Profile()
+        p.LoadFromFile(proFileName)
+        # Change the output to the tmpdir
+        basename = tmpdir.join('test').strpath
+        outGmyFileName = basename + '.gmy'
+        outXmlFileName = basename + '.xml'
+        p.OutputGeometryFile = outGmyFileName
+        p.OutputXmlFile = outXmlFileName
+        
+        generator = OutputGeneration.PolyDataGenerator(p)
+        generator.Execute()
+        
+        import filecmp
+        assert filecmp.cmp(outGmyFileName, os.path.join(dataDir, 'test.gmy'))
+        assert filecmp.cmp(outXmlFileName, os.path.join(dataDir, 'test.xml'))
 
 class TestCylinderGenerator:
     def test_regression(self, tmpdir):
+        """Generate a small cylinder GMY with a random orientation. Then check 
+        that the output is correct by running it through a custom subclass of 
+        a ConfigLoader. 
+        """
         rng = np.random.RandomState()
         
         basename = tmpdir.join('cyl')
@@ -17,7 +47,7 @@ class TestCylinderGenerator:
         LengthMetres = 1.32
         RadiusMetres = 0.74
         
-        generator = CylinderGenerator(OutputGeometryFile, OutputXmlFile,
+        generator = OutputGeneration.CylinderGenerator(OutputGeometryFile, OutputXmlFile,
                                       VoxelSizeMetres, Axis, LengthMetres,
                                       RadiusMetres)
         generator.Execute()
@@ -38,7 +68,7 @@ class CylinderTestingGmyParser(ConfigLoader):
         self.Radius = Radius
         return
     
-    def IsInsideCylinder(self, x):
+    def IsInside(self, x):
         xDOTn = np.dot(x, self.Axis)
         if xDOTn < -0.5 * self.Length or xDOTn > 0.5 * self.Length:
             return False
@@ -53,9 +83,9 @@ class CylinderTestingGmyParser(ConfigLoader):
         
     def OnEndSite(self, block, site):
         if site.IsFluid:
-            assert self.IsInsideCylinder(site.Position)
+            assert self.IsInside(site.Position)
         else:
-            assert not self.IsInsideCylinder(site.Position)
+            assert not self.IsInside(site.Position)
         return
     
     def OnEndBlock(self, bIdx, bIjk):
