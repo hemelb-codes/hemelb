94edebcec80d7efb167f855de61a2597e007cc0d
James Hetherington <j.hetherington@ucl.ac.uk>
2012-10-19 09:54:44 +0100

    Extract index matching algo from extracted property reader, test and optimise

---
+++ Tools/hemeTools/utils/test/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by James Hetherington on 2012-10-18.
+Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
