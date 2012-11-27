6357d4c328136125b6bafdf585cf23bb24ef8e8a
Rupert Nash <rupert.nash@ed.ac.uk>
2012-11-27 15:27:10 +0000

    #537 Basic tidying of the analysis tool: tabs/spacing, PEP8, a few renames to be more pythonic

--- Tools/analysis/extraction.py
+++ Tools/analysis/extraction.py
@@ -10,22 +10,26 @@ import xdrlib
 import sys
 from hemeTools.parsers.extraction import ExtractedProperty
 
-header_fields=['siteCount','fieldCount','voxelSizeMetres','originMetres', 'times']
+header_fields = ['siteCount',
+                 'fieldCount',
+                 'voxelSizeMetres',
+                 'originMetres',
+                 'times']
 
 def extraction_loader(filename):
-  return ExtractedProperty(filename)
+    return ExtractedProperty(filename)
 
-def extraction_parser(content,pattern):
-  if pattern in header_fields:
-    return getattr(content,pattern)
-  if type(pattern)==str:
-    time='final'
-    name=pattern
-  else:
-    time,name=pattern
-    if type(time) == str and time[0] == 'i':
-      time = content.times[int(time[1:])]
-  if time=='final':
-    time=content.times[-1]
-  fields=content.GetByTimeStep(time)
-  return fields.field(name)
+def extraction_parser(content, pattern):
+    if pattern in header_fields:
+        return getattr(content, pattern)
+    if isinstance(pattern, str):
+        time = 'final'
+        name = pattern
+    else:
+        time, name = pattern
+        if isinstance(time, str) and time[0] == 'i':
+            time = content.times[int(time[1:])]
+    if time == 'final':
+        time = content.times[-1]
+    fields = content.GetByTimeStep(time)
+    return fields.field(name)
--- Tools/analysis/report.py
+++ Tools/analysis/report.py
@@ -10,26 +10,29 @@
 import os
 
 class Report(object):
-    def __init__(self,config,graphs):
-        self.name=config['name']
-        self.graphs={label:graphs[label].specialise(config['defaults'],specialisation) for label,specialisation in config['graphs'].iteritems()}
+    def __init__(self, config, graphs):
+        self.name = config['name']
+        self.graphs = {
+            label: graphs[label].specialise(config['defaults'], specialisation)
+            for label, specialisation in config['graphs'].iteritems()
+        }
 
-    def prepare(self,results):
+    def prepare(self, results):
         for graph in self.graphs.values():
             graph.prepare(results)
     
     def write_datafiles(self):
-        data_files_path=os.path.join(self.path,'data_files')
+        data_files_path = os.path.join(self.path, 'data_files')
         try:
             os.makedirs(data_files_path)
         except OSError:
             pass
-        for label,graph in self.graphs.iteritems():
-            out_file = open(os.path.join(data_files_path,label)+'.csv','w')
+        for label, graph in self.graphs.iteritems():
+            out_file = open(os.path.join(data_files_path, label) + '.csv', 'w')
             graph.write_data(out_file)
             out_file.close()
     
-    def write(self,report_path):
-        self.path=report_path
+    def write(self, report_path):
+        self.path = report_path
         self.write_datafiles()
         
