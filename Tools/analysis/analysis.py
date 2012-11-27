1c7165bae6fd72d632003c53daedd61ba0ccb44a
Rupert Nash <rupert.nash@ed.ac.uk>
2012-11-27 15:27:10 +0000

    #537 Basic tidying of the analysis tool: tabs/spacing, PEP8, a few renames to be more pythonic

--- Tools/analysis/analysis.py
+++ Tools/analysis/analysis.py
@@ -17,29 +17,32 @@ from report import Report
 
 class Analysis(object):
     def __init__(self,config):
-        self.results_path=config['results_path']
-        self.reports_path=config['reports_path']
-        self.graph_configuration=config['graphs']
-        self.report_configuration=config['reports']
-        self.result_configuration=config['results']
-        self.graphs={label:Graph(data) for label,data in self.graph_configuration.iteritems()}
-        self.reports={label:Report(data,self.graphs) for label,data in self.report_configuration.iteritems()}
+        self.results_path = config['results_path']
+        self.reports_path = config['reports_path']
+        self.graph_configuration = config['graphs']
+        self.report_configuration = config['reports']
+        self.result_configuration = config['results']
+        self.graphs = {label: Graph(data)
+                       for label, data in self.graph_configuration.iteritems()}
+        self.reports = {label: Report(data, self.graphs)
+                        for label, data in self.report_configuration.iteritems()}
     
     def load_data(self):
-        self.results=ResultsCollection(self.results_path,self.result_configuration)
+        self.results = ResultsCollection(self.results_path,
+                                         self.result_configuration)
         
     def prepare(self):
         for report in self.reports.itervalues():
             report.prepare(self.results)
             
     def write(self):
-        for label,report in self.reports.iteritems():
-            report.write(os.path.expanduser(os.path.join(self.reports_path,label)))
-            print "Report %s written to %s" % (report.name,report.path)
+        for label, report in self.reports.iteritems():
+            report.write(os.path.expanduser(os.path.join(self.reports_path, label)))
+            print "Report %s written to %s" % (report.name, report.path)
 
 def main():
     print 'Loading environment'
-    analysis=Analysis(environment.config)
+    analysis = Analysis(environment.config)
     print 'Loading data'
     analysis.load_data()
     print 'Preparing data'
