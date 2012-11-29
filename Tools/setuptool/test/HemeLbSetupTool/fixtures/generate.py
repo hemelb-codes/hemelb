8b25d3f4369b4a9532863aeb74a799c77a585f37
James Hetherington <j.hetherington@ucl.ac.uk>
2012-11-29 17:06:12 +0000

    Work around known bug in VTK STL ASCII Reader http://paraview.org/Bug/view.php?id=13160

--- Tools/setuptool/test/HemeLbSetupTool/fixtures/generate.py
+++ Tools/setuptool/test/HemeLbSetupTool/fixtures/generate.py
@@ -4,6 +4,7 @@ from .load import fixture_path
 def write_stl(input,label):
     writer = vtk.vtkSTLWriter()
     writer.SetInputConnection(input.GetOutputPort())
+    writer.SetFileTypeToBinary()
     writer.SetFileName(fixture_path(label))
     writer.Write()
 
