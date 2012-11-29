ee684adcac5b2e47c3144f1b585bd16a8969ac6b
James Hetherington <j.hetherington@ucl.ac.uk>
2012-11-29 17:39:16 +0000

    Additional simple test for geometry generation based on a cube

--- Tools/setuptool/test/HemeLbSetupTool/fixtures/load.py
+++ Tools/setuptool/test/HemeLbSetupTool/fixtures/load.py
@@ -5,7 +5,17 @@ def fixture_path(name):
     here = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(here,name+".stl")
 
-def cube():
-    # Create a profile which is a simple cube
+def temporary_profile(tmpdir,name):
     result=Profile()
+    basename = tmpdir.join(name).strpath
+    outGmyFileName = basename + '.gmy'
+    outXmlFileName = basename + '.xml'
+    result.OutputGeometryFile = outGmyFileName
+    result.OutputXmlFile = outXmlFileName
+    return result
+
+def cube(tmpdir):
+    # Create a profile which is a simple cube
+    result=temporary_profile(tmpdir,'cube')
     result.StlFile=fixture_path('cube')
+    return result
