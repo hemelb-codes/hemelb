01476e6a01f22bd2e0be6a53ea728999eab6b77c
Rupert Nash <rupert.nash@ed.ac.uk>
2013-03-11 16:13:07 +0000

    #604 Make unit tests gracefully fail on when LaddBB is enabled (used to segfault)

---
+++ Code/unittests/helpers/LaddFail.h
@@ -0,0 +1,19 @@
+//
+// Copyright (C) University College London, 2007-2012, all rights reserved.
+//
+// This file is part of HemeLB and is CONFIDENTIAL. You may not work
+// with, install, use, duplicate, modify, redistribute or share this
+// file, or any part thereof, other than as allowed by any agreement
+// specifically made by you with University College London.
+//
+
+#ifndef HEMELB_UNITTESTS_HELPERS_LADDFAIL_H
+#define HEMELB_UNITTESTS_HELPERS_LADDFAIL_H
+
+#define HEMELB_UNITTESTS_HELPERS_LADDFAIL_Q(str) #str
+#define HEMELB_UNITTESTS_HELPERS_LADDFAIL_QQ(str) HEMELB_UNITTESTS_HELPERS_LADDFAIL_Q(str)
+
+#define LADD_FAIL() if (std::strcmp(HEMELB_UNITTESTS_HELPERS_LADDFAIL_QQ(HEMELB_IOLET_BOUNDARY), "LADDIOLET") == 0) CPPUNIT_ASSERT_MESSAGE("This test has been marked as fatal when HemeLB is built with LADDIOLET. See ticket #605.", 1 == 0)
+
+
+#endif /* HEMELB_UNITTESTS_HELPERS_LADDFAIL_H */
