d7063cb26487b4a47a18cfea5a669b07d3011b91
Rupert Nash <rupert.nash@ed.ac.uk>
2013-03-25 15:26:43 +0000

    Factor the GuoZhengShi boundary condition into the new streamers

--- Code/lb/kernels/BaseKernel.h
+++ Code/lb/kernels/BaseKernel.h
@@ -75,9 +75,30 @@ namespace hemelb
             return f_eq;
           }
 
+          inline void SetFEq(Direction i, distribn_t val)
+          {
+            f_eq[i] = val;
+          }
+
+          inline distribn_t* GetFEqPtr()
+          {
+            return f_eq.f;
+          }
+
           inline const FVector<LatticeType>& GetFNeq() const
           {
             return f_neq;
+          }
+          // This is necessary as some of the streamers need the post-collision distribution.
+          // It is calculated by collisions and kernels.
+          inline void SetFNeq(Direction direction, distribn_t value)
+          {
+            f_neq[direction] = value;
+          }
+
+          inline distribn_t* GetFNeqPtr()
+          {
+            return f_neq.f;
           }
 
           // This is necessary as some of the streamers need the post-collision distribution.
