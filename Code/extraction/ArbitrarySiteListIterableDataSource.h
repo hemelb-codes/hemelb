9b80343669e7b5baf72859751b84a06b50624c54
Derek Groen <d.groen@ucl.ac.uk>
2013-04-25 17:35:02 +0100

    Fixed include guards and added the proper HemeLB header.

--- Code/extraction/ArbitrarySiteListIterableDataSource.h
+++ Code/extraction/ArbitrarySiteListIterableDataSource.h
@@ -1,9 +1,11 @@
-/*
- * ArbitrarySiteListIterableDataSource.h
- *
- *  Created on: 26 Jun 2012
- *      Author: derek
- */
+// 
+// Copyright (C) University College London, 2007-2012, all rights reserved.
+// 
+// This file is part of HemeLB and is CONFIDENTIAL. You may not work 
+// with, install, use, duplicate, modify, redistribute or share this
+// file, or any part thereof, other than as allowed by any agreement
+// specifically made by you with University College London.
+// 
 
 #ifndef HEMELB_EXTRACTION_ARBITRARYSITELISTITERABLEDATASOURCE_H
 #define HEMELB_EXTRACTION_ARBITRARYSITELISTITERABLEDATASOURCE_H
