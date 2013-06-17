23e751f1fcf81fdcc59cb6013f9060adbad8b9d2
Rupert Nash <rupert.nash@ed.ac.uk>
2013-06-17 17:03:02 +0100

    Make the hemelb debugger launch script work on KDE as well as Gnome

--- Code/debug/linux/launchGdbs.sh
+++ Code/debug/linux/launchGdbs.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-debuggerCommandFile=$1
-binary=$2
-debugger="gdb -q -x $debuggerCommandFile $binary"
-
-shift 2 
-# Remove the first to params.
-# Positional params now hold the process ids we
-# want to attach to.
-    
-command="gnome-terminal"
-rank=0
-
-until [ -z "$1" ] # all params used up
-do
-    pid=$1
-    shift
-    
-    if [ "$rank" -eq "0" ]; then
-	command="${command} --window -e '${debugger} $pid'"
-    else
-	command="${command} --tab -e '${debugger} $pid'"
-    fi
-    
-    ((rank++))
-done
-
-echo $command
-eval $command
-
+DIR=$(cd $(dirname "$0"); pwd)
+# Try to guess which environment to run appropriate debugger script
+if [ "$(pidof ksmserver)" ]; then
+	# KDE
+	$DIR/launchKonsole.sh "$@"
+elif [ "$(pidof gnome-session)" ]; then
+    # GNOME
+    $DIR/launchGnomeTerminal.sh "$@"
+else
+	# Panic
+	echo "Can't figure out environment"
+fi
