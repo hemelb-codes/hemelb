16c044db3dacf74f314af18eff21cf98592b9958
Sebastian Schmieschek <sebastian@schmie.net>
2013-11-28 18:25:49 +0000

    Introduced pointer to force vector data into hydroVars and adjusted constructor calls in streamers accordingly.

--- Code/lb/streamers/GuoZhengShiDelegate.h
+++ Code/lb/streamers/GuoZhengShiDelegate.h
@@ -13,6 +13,7 @@
 #include "lb/streamers/BaseStreamerDelegate.h"
 #include "geometry/neighbouring/RequiredSiteInformation.h"
 #include "geometry/neighbouring/NeighbouringDataManager.h"
+#include "util/Vector3D.h"
 
 namespace hemelb
 {
@@ -147,7 +148,8 @@ namespace hemelb
             // Then 0 = velocityWall * wallDistance + velocityFluid * (1 - wallDistance)
             // Hence velocityWall = velocityFluid * (1 - 1/wallDistance)
             distribn_t fWall[LatticeType::NUMVECTORS];
-            kernels::HydroVars<typename CollisionType::CKernel> hydroVarsWall(fWall);
+            util::Vector3D<distribn_t> force;
+            kernels::HydroVars<typename CollisionType::CKernel> hydroVarsWall(fWall,&force);
 
             hydroVarsWall.density = hydroVars.density;
             hydroVarsWall.momentum = hydroVars.momentum * (1. - 1. / wallDistance);
