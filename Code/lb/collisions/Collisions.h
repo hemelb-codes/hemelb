ffae2371452f370ede88d579172f56e0ca563498
Rupert Nash <rupert.nash@ed.ac.uk>
2014-04-21 18:07:58 +0100

    Remove the unphysical Mazzeo collisions used in his BCs

--- Code/lb/collisions/Collisions.h
+++ Code/lb/collisions/Collisions.h
@@ -10,9 +10,6 @@
 #ifndef HEMELB_LB_COLLISIONS_COLLISIONS_H
 #define HEMELB_LB_COLLISIONS_COLLISIONS_H
 
-#include "lb/collisions/NonZeroVelocityEquilibriumFixedDensity.h"
 #include "lb/collisions/Normal.h"
-#include "lb/collisions/ZeroVelocityEquilibrium.h"
-#include "lb/collisions/ZeroVelocityEquilibriumFixedDensity.h"
 
 #endif /* HEMELB_LB_COLLISIONS_COLLISIONS_H */
