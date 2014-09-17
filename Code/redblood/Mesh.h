48c1342e68ba099cf190bb95e5e40eb848f6bb3e
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-09-17 10:21:57 +0100

    Add topology functions/data structure over mesh
    These functions find neighboring facets and create a map from vertices to facets.

--- Code/redblood/Mesh.h
+++ Code/redblood/Mesh.h
@@ -11,27 +11,63 @@
 #define HEMELB_REDBLOOD_MESH_H
 
 #include <boost/shared_ptr.hpp>
+#include <boost/array.hpp>
 #include <vector>
+#include <set>
 #include <string>
+#include <array>
 #include "util/Vector3D.h"
 
 namespace hemelb { namespace redblood {
 
+//! Type of 3D vectors over real space
+typedef util::Vector3D<double> t_Real3D;
+//! Type of containers over indices
+typedef std::set<unsigned int> t_Indices;
+
 //! Holds raw mesh data
 //! Data is separated into vertices and triangular facets
 struct MeshData {
-    std::vector< util::Vector3D<double> > vertices;
-    std::vector< util::Vector3D<unsigned int> > facets;
+    //! Facet container type
+    typedef std::vector<t_Indices> t_Facets;
+    //! Vertex container type
+    typedef std::vector<t_Real3D> t_Vertices;
+    //! Vertex container
+    t_Vertices vertices;
+    //! Facet container
+    t_Facets facets;
 };
 
+//! Holds data about facet-facet connections, e.g. neighbors
+struct MeshTopology {
+    //! For each vertex, lists the facet indices
+    std::vector< std::set<unsigned int> > vertex_to_facets;
+    //! For each facet, lists the neighboring facets
+    std::vector< boost::array<unsigned int, 3> > facet_neighbors;
+
+    // Creates mesh topology from mesh data
+    MeshTopology(MeshData const &_mesh);
+};
+
+//! Performs 
 class Mesh {
 
-    public:
-    Mesh(boost::shared_ptr<MeshData> const & _data) : data_(_data) {};
+public:
+    //! Initializes mesh from mesh data
+    Mesh   (boost::shared_ptr<MeshData> const & _mesh)
+         : mesh_(_mesh), connectivity_(new MeshTopology(*_mesh)) {}
 
-    protected:
-    //! Holds actual data
-    boost::shared_ptr<MeshData> data_;
+    //! Determines barycenter of mesh
+    t_Real3D barycenter() const;
+    //! Connectivity data
+    boost::shared_ptr<const MeshTopology> connectivity() const
+      { return connectivity_; }
+
+protected:
+    //! Holds actual data about the mesh
+    boost::shared_ptr<MeshData> mesh_;
+    //! Holds connectivity information;
+    boost::shared_ptr<MeshTopology> connectivity_;
 };
 
 //! Read mesh from file
--- Code/unittests/redblood/Mesh.h
+++ Code/unittests/redblood/Mesh.h
@@ -21,47 +21,131 @@ namespace hemelb { namespace unittests {
 class RedBloodMeshTests : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(RedBloodMeshTests);
-    CPPUNIT_TEST(TestReadMesh);
+    CPPUNIT_TEST(testReadMesh);
     CPPUNIT_TEST_SUITE_END();
   public:
     void setUp() {
         std::string filename = Resource("red_blood_cell.txt").Path();
-        data = redblood::read_mesh(filename);
+        mesh = redblood::read_mesh(filename);
     }
 
     void tearDown() {}
 
-    void TestReadMesh()
+    void testReadMesh()
     {
-        CPPUNIT_ASSERT(data);
-        CPPUNIT_ASSERT(data->vertices.size() == 812);
-        CPPUNIT_ASSERT(data->facets.size() == 1620);
+        CPPUNIT_ASSERT(mesh);
+        CPPUNIT_ASSERT(mesh->vertices.size() == 812);
+        CPPUNIT_ASSERT(mesh->facets.size() == 1620);
 
         typedef util::Vector3D<double> Vector3d;
         Vector3d vfirst(0.850650808352040, 0.525731112119134, 0.0);
         Vector3d vlast(0.298722767882436, -0.095229509306174,
                 -0.186475800096805);
-        CPPUNIT_ASSERT(compare(data->vertices.front() - vfirst));
-        CPPUNIT_ASSERT(compare(data->vertices.back() - vlast));
+        CPPUNIT_ASSERT(compare(mesh->vertices.front() - vfirst));
+        CPPUNIT_ASSERT(compare(mesh->vertices.back() - vlast));
 
-        typedef util::Vector3D<unsigned int> Vector3u;
-        Vector3u ffirst(0, 12, 20);
-        Vector3u flast(100, 811, 244);
-        CPPUNIT_ASSERT(compare(data->facets.front() - ffirst));
-        CPPUNIT_ASSERT(compare(data->facets.back() - flast));
+        CPPUNIT_ASSERT(mesh->facets.front().count(0) == 1);
+        CPPUNIT_ASSERT(mesh->facets.front().count(12) == 1);
+        CPPUNIT_ASSERT(mesh->facets.front().count(20) == 1);
+        CPPUNIT_ASSERT(mesh->facets.back().count(100) == 1);
+        CPPUNIT_ASSERT(mesh->facets.back().count(811) == 1);
+        CPPUNIT_ASSERT(mesh->facets.back().count(244) == 1);
     }
 
     static bool compare(util::Vector3D<double> const &_in) {
         return _in.GetMagnitudeSquared() < 1e-8;
     }
-    static bool compare(util::Vector3D<unsigned int> const &_in) {
-        return _in.x == 0 and _in.y == 0 and _in.z == 0;
-    }
 
   private:
-    boost::shared_ptr<hemelb::redblood::MeshData> data;
+    boost::shared_ptr<hemelb::redblood::MeshData> mesh;
 };
+
+class TopologyTests : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(TopologyTests);
+    CPPUNIT_TEST(testNodeToVertex);
+    CPPUNIT_TEST(testFacetNeighbors);
+    CPPUNIT_TEST_SUITE_END();
+
+  public:
+    void setUp() {
+        std::string filename = Resource("red_blood_cube.txt").Path();
+        mesh = redblood::read_mesh(filename);
+        // Checks the mesh input makes sense
+        CPPUNIT_ASSERT(mesh->vertices.size() == 8);
+        CPPUNIT_ASSERT(mesh->facets.size() == 12);
+
+        // Creates topology
+        topo = boost::shared_ptr<hemelb::redblood::MeshTopology>(
+            new hemelb::redblood::MeshTopology(*mesh)
+        );
+    }
+
+    void tearDown() {}
+
+    void testNodeToVertex() {
+        CPPUNIT_ASSERT(topo->vertex_to_facets.size() == 8);
+
+        // expected[vertex] = {nfacets, facet indices}
+        unsigned int expected[8][6] = {
+            {4, 0, 1, 6, 9},
+            {5, 0, 2, 3, 8, 9},
+            {4, 0, 1, 3, 10},
+            {5, 1, 6, 7, 10, 11},
+            {5, 4, 6, 7, 8, 9},
+            {4, 2, 4, 5, 8},
+            {5, 2, 3, 5, 10, 11},
+            {4, 4, 5, 7, 11},
+        };
+        for(unsigned vertex(0); vertex < 8; ++vertex) {
+            std::set<unsigned int> facets = topo->vertex_to_facets[vertex];
+            CPPUNIT_ASSERT(facets.size() == expected[vertex][0]);
+            for(unsigned facet(1); facet <= expected[vertex][0]; ++facet) {
+                CPPUNIT_ASSERT(facets.count(expected[vertex][facet]) == 1);
+            }
+        }
+    }
+
+    void testFacetNeighbors() {
+        CPPUNIT_ASSERT(topo->facet_neighbors.size() == 12);
+
+        // expected[facet] = {nneighbors, neighbor indices}
+        unsigned int expected[12][3] = {
+            { 1, 3,  9},
+            { 0, 6, 10},
+            { 3, 5,  8},
+            { 0, 2, 10},
+            { 5, 7,  8},
+            { 4, 2, 11},
+            { 1, 7,  9},
+            { 6, 4, 11},
+            { 9, 4,  2},
+            { 0, 6,  8},
+            { 1, 3, 11},
+            {10, 5,  7},
+        };
+        for(unsigned facet(0); facet < 12; ++facet) {
+            boost::array<unsigned int, 3> const &neighs
+                = topo->facet_neighbors[facet];
+            CPPUNIT_ASSERT(neighs.size() == 3);
+            for(unsigned neigh(0); neigh < 3; ++neigh)
+                CPPUNIT_ASSERT(contains(neighs, expected[facet][neigh]));
+        }
+    }
+
+    template<class T, unsigned long N>
+        static bool contains(boost::array<T, N> const &_facets, T _value) {
+           for(unsigned i(0); i < N; ++i)
+               if(_facets[i] == _value) return true;
+           return false;
+        }
+
+  private:
+    boost::shared_ptr<hemelb::redblood::MeshData> mesh;
+    boost::shared_ptr<hemelb::redblood::MeshTopology> topo;
+};
+
 CPPUNIT_TEST_SUITE_REGISTRATION(RedBloodMeshTests);
+CPPUNIT_TEST_SUITE_REGISTRATION(TopologyTests);
 }}
 
 #endif // ONCE
