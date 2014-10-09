5f31691155162e2b08cf1dcaf1d6813939d442d5
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-10-09 12:35:31 +0100

    cppcheck corrections

--- Code/unittests/redblood.h
+++ Code/unittests/redblood.h
@@ -1,3 +1,14 @@
+//
+// Copyright (C) University College London, 2007-2012, all rights reserved.
+//
+// This file is part of HemeLB and is CONFIDENTIAL. You may not work 
+// with, install, use, duplicate, modify, redistribute or share this
+// file, or any part thereof, other than as allowed by any agreement
+// specifically made by you with University College London.
+//
+#ifndef HEMELB_UNITTESTS_REDBLOOD_H
+#define HEMELB_UNITTESTS_REDBLOOD_H
 #include "unittests/redblood/Mesh.h"
 #include "unittests/redblood/ParticleImpl.h"
 #include "unittests/redblood/Particle.h"
+#endif
