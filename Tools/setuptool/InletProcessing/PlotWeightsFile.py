146d24f5b4daf7bf131b39f4f740cfe71925d9d4
Derek Groen <d.groen@ucl.ac.uk>
2015-01-05 13:48:27 +0000

    This vInlet code fails on fewer lattice sites, but is still very slow and does not account for the normal.

--- Tools/setuptool/InletProcessing/PlotWeightsFile.py
+++ Tools/setuptool/InletProcessing/PlotWeightsFile.py
@@ -3,28 +3,60 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import sys
 
+
+#420.500000 297.500000 215.000000
+
+def filterLowX(xyzv):
+    deleteList = []
+    for i in xrange(0, len(xyzv)):
+        if xyzv[i,3] > 0.99:
+            deleteList.append(i)
+            continue
+        if xyzv[i,0] > 423 or xyzv[i,0] < 418:
+            deleteList.append(i)
+            continue
+        if xyzv[i,1] > 300 or xyzv[i,1] < 295:
+            deleteList.append(i)
+            continue
+        if xyzv[i,2] > 219 or xyzv[i,2] < 212:
+            deleteList.append(i)
+            continue
+
+    deleteList.sort(reverse=True)
+
+    for i in deleteList:
+        xyzv = np.delete(xyzv,i,0)
+
+    return xyzv
+
+
+
 if __name__ == "__main__":
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    xyzv = np.loadtxt(sys.argv[1])
-
-    mode = "xyz"
-
-    if len(sys.argv)>2:
-        mode = sys.argv[2]
-
-    if mode == "xyz":
-        ax.scatter(xyzv[:,0], xyzv[:,1], xyzv[:,2])
-    elif mode == "xyv":
-        ax.scatter(xyzv[:,0], xyzv[:,1], xyzv[:,3])
-    elif mode == "yzv":
-        ax.scatter(xyzv[:,1], xyzv[:,2], xyzv[:,3])
-    elif mode == "xzv":
-        ax.scatter(xyzv[:,0], xyzv[:,2], xyzv[:,3])
-    
-
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
+    for i in xrange(1,len(sys.argv)):
+        xyzv = filterLowX(np.loadtxt(sys.argv[i]))
+      
+        mode = "xyz"
+        cl='red'
+        if(i == 2):
+            cl='blue'
+
+        if len(sys.argv)>2 and len(sys.argv[-1]) == 3:
+            mode = sys.argv[-1]
+
+        if mode == "xyz":
+            ax.scatter(xyzv[:,0], xyzv[:,1], xyzv[:,2], color=cl)
+        elif mode == "xyv":
+            ax.scatter(xyzv[:,0], xyzv[:,1], xyzv[:,3], color=cl)
+        elif mode == "yzv":
+            ax.scatter(xyzv[:,1], xyzv[:,2], xyzv[:,3], color=cl)
+        elif mode == "xzv":
+            ax.scatter(xyzv[:,0], xyzv[:,2], xyzv[:,3], color=cl)
+    
     plt.show()
