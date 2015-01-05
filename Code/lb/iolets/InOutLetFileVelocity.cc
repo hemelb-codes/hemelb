87d0f14785aa08d19cf7be745b36ea27d9e2cc03
Derek Groen <d.groen@ucl.ac.uk>
2015-01-05 16:23:29 +0000

    Implemented a new algorithm, which uses a nearest neighbour approach. This one should be faster and more predictable.

--- Code/lb/iolets/InOutLetFileVelocity.cc
+++ Code/lb/iolets/InOutLetFileVelocity.cc
@@ -16,6 +16,7 @@
 #include "util/utilityStructs.h"
 #include "configuration/SimConfig.h"
 #include <math.h>
+#include <algorithm>
 
 namespace hemelb
 {
@@ -115,92 +116,153 @@ namespace hemelb
         }
         else
         {
-          bool logging = false;
-          /*if (402.9 < x.x && x.x < 403.1 && 312.9 < x.y && x.y < 313.1 && 160.4 < x.z && x.z < 160.6)
-          {
-            logging = true;
-          }*/
+          /* These absolute normal values can still be negative here,
+           * but are corrected below to become positive.
+           * */
+          double abs_normal[3] = {normal.x, normal.y, normal.z};
+
+          /* Prevent division by 0 errors if the normals are 0.0. */
+          if(normal.x < 0.0000001) { abs_normal[0] = 0.0000001; }
+          if(normal.y < 0.0000001) { abs_normal[1] = 0.0000001; }
+          if(normal.z < 0.0000001) { abs_normal[2] = 0.0000001; }
+
+          /*bool logging = false;
+          if (402.9 < x.x && x.x < 403.1 && 312.9 < x.y && x.y < 313.1 && 160.4 < x.z && x.z < 160.6)
+           {
+           logging = true;
+           }
 
           if (logging)
           {
             log::Logger::Log<log::Warning, log::OnePerCore>("%f %f %f", x.x, x.y, x.z);
-          }
+          }*/
+
+          int xyz_directions[3] = { 1, 1, 1 };
+
           std::vector<int> xyz;
           xyz.push_back(0);
           xyz.push_back(0);
           xyz.push_back(0);
 
-          int whole[3] = { 1, 1, 1 };
-          int xint = int(x.x - 0.9999999);
-          int yint = int(x.y - 0.9999999);
-          int zint = int(x.z - 0.9999999);
+          double xyz_residual[3] = {0.0, 0.0, 0.0};
+          /* The residual values increase by the normal values at every time step. When they hit >1.0, then
+           * xyz is incremented and a new grid point is attempted.
+           * In addition, the specific residual value is decreased by 1.0. */
 
-          if (x.x - floor(x.x) > 0.1)
+          if (normal.x < 0.0)
           {
-            whole[0] = 0;
-            xint += 1;
-          }
-          if (x.y - floor(x.y) > 0.1)
-          {
-            whole[1] = 0;
-            yint += 1;
-          }
-          if (x.z - floor(x.z) > 0.1)
-          {
-            whole[2] = 0;
-            zint += 1;
+            xyz_directions[0] = -1;
+            xyz[0] = floor(x.x);
+            abs_normal[0] = -abs_normal[0];
+            /* Start with a negative residual because we already moved partially in this direction. */
+            xyz_residual[0] = -(x.x - floor(x.x));
+          } else {
+            xyz[0] = ceil(x.x);
+            xyz_residual[0] = -(ceil(x.x) - x.x);
           }
 
-          //log::Logger::Log<log::Warning, log::OnePerCore>("%f %f", x.x, ((float) xint));
-
-          std::vector<double> v;
-          v.reserve(27);
-          std::vector<bool> dist_flag;
-          dist_flag.reserve(27);
-
-          int sizes[3] = { whole[0] + 2, whole[1] + 2, whole[2] + 2 };
-          int v_tot = 0;
-
-          for (int i = 0; i < sizes[0]; i++)
+          if (normal.y < 0.0)
           {
-            for (int j = 0; j < sizes[1]; j++)
+            xyz_directions[1] = -1;
+            xyz[1] = floor(x.y);
+            abs_normal[1] = -abs_normal[1];
+            xyz_residual[1] = -(x.y - floor(x.y));
+          } else {
+            xyz[1] = ceil(x.y);
+            xyz_residual[1] = -(ceil(x.y) - x.y);
+          }
+
+          if (normal.z < 0.0)
+          {
+            xyz_directions[2] = -1;
+            xyz[2] = floor(x.z);
+            abs_normal[2] = -abs_normal[2];
+            xyz_residual[2] = -(x.z - floor(x.z));
+          } else {
+            xyz[2] = ceil(x.z);
+            xyz_residual[2] = -(ceil(x.z) - x.z);
+          }
+
+          double v_tot = 0;
+          int iterations = 0;
+
+          while (iterations < 3)
+          {
+            if (weights_table.count(xyz) > 0)
             {
+              v_tot = weights_table.at(xyz);
+              //log::Logger::Log<log::Warning, log::OnePerCore>("%f %f %f %f",
+              //                                                              x.x,
+              //                                                              x.y,
+              //                                                              x.z, v_tot);
+              return v_tot;
+            }
 
-              for (int k = 0; k < sizes[2]; k++)
-              {
+            /*if (logging)
+            {
+              log::Logger::Log<log::Warning, log::OnePerCore>("%f %f %f %d %d %d",
+                                                              x.x,
+                                                              x.y,
+                                                              x.z,
+                                                              xyz[0],
+                                                              xyz[1],
+                                                              xyz[2]);
+            }*/
 
-                xyz[0] = xint + i;
-                xyz[1] = yint + j;
-                xyz[2] = zint + k;
+            /* Propagate residuals to the move to the next grid point. */
+            double xstep = (1.0 - xyz_residual[0]) / abs_normal[0];
+            double ystep = (1.0 - xyz_residual[1]) / abs_normal[1];
+            double zstep = (1.0 - xyz_residual[2]) / abs_normal[2];
 
-                if (logging)
-                {
-                  log::Logger::Log<log::Warning, log::OnePerCore>("%d %d %d %d %d %d", i, j, k, xyz[0], xyz[1], xyz[2]);
-                }
+            //log::Logger::Log<log::Warning, log::OnePerCore>("%f %f %f", xstep, ystep, zstep);
 
-                if (weights_table.count(xyz) > 0)
-                {
-                  //int is_dist = (whole[0] * (1-(i%2))) + (whole[1] * (1-(j%2))) + (whole[2] * (1-(k%2)));
-                  v.push_back(weights_table.at(xyz));
-                  //dist_flag.push_back(is_dist);
-                  v_tot += weights_table.at(xyz);
-                  //log::Logger::Log<log::Warning, log::OnePerCore>("%d %d %d OK.", xyz[0], xyz[1], xyz[2]);
+            double all_step = 0.0;
+            int xyz_change = 0;
+
+            if(xstep < ystep) {
+              if (xstep < zstep) {
+                all_step = xstep;
+                xyz_change = 0;
+              } else {
+                if (ystep < zstep) {
+                  all_step = ystep;
+                  xyz_change = 1;
+                } else {
+                  all_step = zstep;
+                  xyz_change = 2;
                 }
               }
+
+            } else {
+              if (ystep < zstep) {
+                all_step = ystep;
+                xyz_change = 1;
+              } else {
+                all_step = zstep;
+                xyz_change = 2;
+              }
             }
-          }
-          //if(logging) {
-          //  log::Logger::Log<log::Warning, log::Singleton>("%f %f %f %d", x.x, x.y, x.z, v.size());
-          //}
 
-          if (v.size() == 0)
-          {
-            log::Logger::Log<log::Warning, log::Singleton>("%f %f %f %d", x.x, x.y, x.z, v.size());
-            /* local interpolation did not work, so we adopt a wider range. */
-            return 0.0;
+            xyz_residual[0] += abs_normal[0] * all_step;
+            xyz_residual[1] += abs_normal[1] * all_step;
+            xyz_residual[2] += abs_normal[2] * all_step;
+
+            xyz[xyz_change] += xyz_directions[xyz_change];
+
+            //if(xyz_residual[xyz_change] < 1.0) {
+            //  log::Logger::Log<log::Error, log::Singleton>("ERROR: Residual bug in vInlet: %f %f %f %f", x.x, x.y, x.z, xyz_residual[xyz_change]);
+            //}
+
+            xyz_residual[xyz_change] -= 1.0;
+
+            iterations++;
           }
 
-          return v_tot / v.size();
+          /* Lists the sites which should be in the wall, outside of the main inlet.
+           * If you are unsure, you can increase the log level of this, run HemeLb
+           * for 1 time step, and plot these points out. */
+          log::Logger::Log<log::Trace, log::OnePerCore>("%f %f %f", x.x, x.y, x.z);
+          return 0.0;
         }
 
       }
