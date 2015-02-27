a6b75fa115cb8de29257e3006ce0a345fc27af09
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-02-27 15:45:31 +0000

    Adding function to compute size of mpi message

--- Code/unittests/util/PackerTests.h
+++ Code/unittests/util/PackerTests.h
@@ -33,16 +33,21 @@ namespace hemelb
 
           void TestInteger()
           {
+            using namespace hemelb::util;
             int64_t const i0 = 20000000;
             int16_t const j0 = 2;
             int64_t i = i0;
             int16_t j = j0;
-            hemelb::util::Packer send;
-            send << i << j;
+
+            Packer send;
+            send << i;
+            CPPUNIT_ASSERT_EQUAL(send.messageSize(), packerMessageSize(i));
+            send << j;
+            CPPUNIT_ASSERT_EQUAL(send.messageSize(), packerMessageSize(i, j));
             CPPUNIT_ASSERT_EQUAL(sizeof(int64_t) + sizeof(int16_t), send.size());
             i = 0; j = 0;
 
-            hemelb::util::Packer receive(send);
+            Packer receive(send);
             receive.reset_read();
             CPPUNIT_ASSERT(i0 != i);
             CPPUNIT_ASSERT(j0 != j);
@@ -52,12 +57,14 @@ namespace hemelb
           }
           void TestDouble()
           {
+            using namespace hemelb::util;
             double const d0 = 3.14;
             float const f0 = 3.1;
             double d = d0;
             float f = f0;
-            hemelb::util::Packer send;
+            Packer send;
             send << d << f;
+            CPPUNIT_ASSERT_EQUAL(send.messageSize(),  packerMessageSize(d, f));
             send.reset_read();
             d = 0e0; f = 0e0;
             send >> d >> f;
@@ -66,10 +73,14 @@ namespace hemelb
           }
           void TestVector3D()
           {
-            hemelb::util::Vector3D<int> const v0 = {0, 1, 2};
-            hemelb::util::Vector3D<int> v = {2, 1, 0};
-            hemelb::util::Packer send;
+            using namespace hemelb::util;
+            Vector3D<int> const v0 = {0, 1, 2};
+            Vector3D<int> v = {2, 1, 0};
+            Packer send;
+
             send << v0;
+            CPPUNIT_ASSERT_EQUAL(send.messageSize(), packerMessageSize(v0));
+
             send.reset_read();
             send >> v;
             CPPUNIT_ASSERT_EQUAL(v0.x, v.x);
@@ -78,10 +89,13 @@ namespace hemelb
           }
           void TestVector()
           {
-            std::vector<hemelb::util::Vector3D<int>> const v0 = {{0, 1, 2}, {2, 3, 4}, {5, 6, 7}};
-            std::vector<hemelb::util::Vector3D<int>> v = v0;
-            hemelb::util::Packer send;
+            using namespace hemelb::util;
+            std::vector<Vector3D<int>> const v0 = {{0, 1, 2}, {2, 3, 4}, {5, 6, 7}};
+            std::vector<Vector3D<int>> v = v0;
+            Packer send;
             send << v0;
+            CPPUNIT_ASSERT(packerMessageSize(v0) != 0);
+            CPPUNIT_ASSERT_EQUAL(send.messageSize(), packerMessageSize(v0));
             send.reset_read();
             send >> v;
             for(int i = 0; i < v0.size(); ++i)
@@ -94,10 +108,12 @@ namespace hemelb
 
           void TestMap()
           {
+            using namespace hemelb::util;
             std::map<int, std::string> const m0 = {{0, "zero"}, {1, "one"}};
             std::map<int, std::string> m;
-            hemelb::util::Packer packer;
+            Packer packer;
             packer << m0;
+            CPPUNIT_ASSERT_EQUAL(packer.messageSize(), packerMessageSize(m0));
             packer.reset_read();
             packer >> m;
             CPPUNIT_ASSERT_EQUAL(m0.size(), m.size());
