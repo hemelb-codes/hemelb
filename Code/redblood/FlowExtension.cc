0a47feb7e35ca994450b45d9777db4a36f8f8dd2
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-03-03 09:52:25 +0000

    Bettering flow extension
    - some defensive programming to check input assumptions
       * normal is normalized
       * length and radius of extension are stricly positive
    - simplified algorithm to avoid going to squares where not needed
    - added const to constant objects

--- Code/redblood/FlowExtension.cc
+++ Code/redblood/FlowExtension.cc
@@ -19,25 +19,28 @@ namespace hemelb
 
     //! Checks whether a cell is inside a flow extension
     bool contains(const FlowExtension & flowExt, const MeshData::Vertices::value_type & point) {
+      assert(std::abs(flowExt.normal.GetMagnitude() - 1e0) < 1e-8);
+      assert(flowExt.length > 1e-12);
+      assert(flowExt.radius > 1e-12);
       // Vector from centre of start to end of cylinder
-      util::Vector3D<LatticeDistance> d = flowExt.normal * flowExt.length;
+      util::Vector3D<LatticeDistance> const d = flowExt.normal;
 
       // Vector from centre of start of cylinder to point
-      util::Vector3D<LatticeDistance> pd = point - flowExt.position;
+      util::Vector3D<LatticeDistance> const pd = point - flowExt.position;
 
       // (Squared) distance from pos to point
-      LatticeDistance dot = pd.Dot(d);
+      LatticeDistance const dot = pd.Dot(d);
 
       // If the (squared) distance is less than 0 then the point is behind the
       // cylinder cap at pos.
       // If the (squared) distance is greater than the squared length of the
       // cylinder then the point is beyond the end of the cylinder.
-      if (dot < 0.0 || dot > flowExt.length * flowExt.length)
+      if (dot < 0.0 || dot > flowExt.length)
         return false;
 
       // Point lies between the parallel caps so calculate the (squared) distance
       // between the point and the line between a and b
-      LatticeDistance dist = pd.Dot(pd) - ((dot * dot) / (flowExt.length * flowExt.length));
+      LatticeDistance const dist = pd.GetMagnitudeSquared() - dot * dot;
       return (dist <= flowExt.radius * flowExt.radius);
     }
 
