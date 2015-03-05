aeefb317b70d6765b7fc6420801a6b3913487d5c
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-03-05 15:07:49 +0000

    Linear weight for points in flow extensions
    Added a function to compute linearly decreasing weight in flow extensions. The weights starts at one
    at the origin and reaches 0 at origin + normal * fadeLength. This means we want the origin at the
    interface between the vascular domain and the flow extension domain.

--- Code/redblood/FlowExtension.h
+++ Code/redblood/FlowExtension.h
@@ -10,7 +10,6 @@
 #ifndef HEMELB_REDBLOOD_FLOWEXTENSION_H
 #define HEMELB_REDBLOOD_FLOWEXTENSION_H
 
-#include <redblood/Mesh.h>
 #include "util/Vector3D.h"
 #include "units.h"
 
@@ -19,15 +18,30 @@ namespace hemelb
   namespace redblood
   {
 
+    //! Cylindrical extension at an in/outlet of the vascular system
     struct FlowExtension
     {
+      //! \brief Direction of the axis
+      //! \details Should be normalized
       util::Vector3D<Dimensionless> normal;
-      LatticePosition position;
-      LatticeDistance length, radius;
+      //! \brief Point on the axis giving the origin
+      //! \details Whether this is the left-most or right-most point on the axis depends on the
+      //! direction of the normal. In practice, it should be at the intersection between the
+      //! cylinder axis and the surface separating the extension from the real flow.
+      LatticePosition origin;
+      //! Length of the cylinder
+      LatticeDistance length;
+      //! Radius of the cylinder
+      LatticeDistance radius;
+      //! Distance within which to fade in/out
+      LatticeDistance fadeLength;
     };
 
     //! Checks whether a cell is inside a flow extension
-    bool contains(const FlowExtension &, const MeshData::Vertices::value_type &);
+    bool contains(const FlowExtension &, const LatticePosition &);
+
+    //! Linear weight associated with a point in the cylinder
+    Dimensionless linearWeight(FlowExtension const&, LatticePosition const&);
 
   } // namespace hemelb::redblood
 } // namespace hemelb
