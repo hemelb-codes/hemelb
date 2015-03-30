d1648f42e68b392dc60830fc31edcddb70c5b948
Rupert Nash <rupert.nash@ed.ac.uk>
2015-03-30 11:37:52 +0100

    Add class to classify nodes based on the found intersections and make PolyDataGenerator start using it.

--- Tools/setuptool/HemeLbSetupTool/Model/PolyDataGenerator.py
+++ Tools/setuptool/HemeLbSetupTool/Model/PolyDataGenerator.py
@@ -1,29 +1,35 @@
 import numpy as np
 from vtk import vtkTransform, vtkTransformFilter
+from vtk.util import numpy_support
+
 from .Vector import Vector
-from .GeometryGenerator import GeometryGenerator
+#from .GeometryGenerator import GeometryGenerator
 from .Clipper import Clipper
 
-import Generation
+import TriangleSorter
+from HitFinder import HitFinder
+from NodeClassifier import NodeClassifier
+
 import pdb
 
 np.seterr(divide='ignore')
 
-class PolyDataGenerator(GeometryGenerator):
+# class PolyDataGenerator(GeometryGenerator):
+class PolyDataGenerator(object):
 
     def __init__(self, profile):
         """Clip the STL and set attributes on the SWIG-proxied C++
         GeometryGenerator object.
         """
-        GeometryGenerator.__init__(self)
+        #GeometryGenerator.__init__(self)
         self._profile = profile
-        self.generator = Generation.PolyDataGenerator()
-        self._SetCommonGeneratorProperties()
-        self.generator.SetSeedPointWorking(
-            profile.SeedPoint.x / profile.VoxelSize,
-            profile.SeedPoint.y / profile.VoxelSize,
-            profile.SeedPoint.z / profile.VoxelSize)
-
+        #self.generator = Generation.PolyDataGenerator()
+        #self._SetCommonGeneratorProperties()
+        #self.generator.SetSeedPointWorking(
+        #    profile.SeedPoint.x / profile.VoxelSize,
+        #    profile.SeedPoint.y / profile.VoxelSize,
+        #    profile.SeedPoint.z / profile.VoxelSize)
+        
         # This will create the pipeline for the clipped surface
         clipper = Clipper(profile)
 
@@ -37,23 +43,62 @@ class PolyDataGenerator(GeometryGenerator):
         transformer.SetInputConnection(
             clipper.ClippedSurfaceSource.GetOutputPort())
 
-        # Uncomment this an insert the output path to debug pipeline construction
-        # write = StageWriter('/Users/rupert/working/compare/aneurysm').WriteOutput
-        # i = 0
-        # for alg in getpipeline(transformer):
-        #     print i
-        #     i += 1
-        #     print alg
-        #     write(alg)
-
         transformer.Update()
         self.ClippedSurface = transformer.GetOutput()
-        self.generator.SetClippedSurface(self.ClippedSurface)
+        #self.generator.SetClippedSurface(self.ClippedSurface)
         
-        originWorking, nSites = self._ComputeOriginWorking()
-        self.generator.SetOriginWorking(*(float(x) for x in originWorking))
-        self.generator.SetSiteCounts(*(int(x) for x in nSites))
-        self.OriginMetres = Vector(originWorking * self.VoxelSizeMetres) 
+        self._ComputeOriginWorking()
+        #self.generator.SetOriginWorking(*(float(x) for x in originWorking))
+        #self.generator.SetSiteCounts(*(int(x) for x in nSites))
+        self.OriginMetres = Vector(self.OriginWorking * self.VoxelSizeMetres) 
+        return
+    
+    def Execute(self):
+        # Just override the base's exec method for now...
+        
+        # Get the surface data into numpy arrays, assuming they're all triangles.
+        surf = self.ClippedSurface
+        tridata = numpy_support.vtk_to_numpy(surf.GetPolys().GetData())
+        tridata.shape = (surf.GetNumberOfPolys(), 4)
+        assert np.all(tridata[:,0]==3), "Non triangle in surface!"
+        triangles = tridata[:, 1:]
+        points = numpy_support.vtk_to_numpy(surf.GetPoints().GetData())
+        normals = self.ClippedSurface.GetCellData().GetNormals()
+        normals = numpy_support.vtk_to_numpy(normals)
+
+        # Sort the triangles to a sensible level on the tree
+        # Just pick the current 8x8x8 blocks for now
+        tri_box_pow = 3
+        tri_level = tri_box_pow
+        
+        self.tree = TriangleSorter.TrianglesToTree(self.TreeLevels, tri_level, points, triangles)
+        
+        hit_finder = HitFinder(points, triangles, normals)
+        classifier = NodeClassifier()
+        
+        # Iterate over all the nodes with triangles
+        for tri_node in self.tree.IterDepthFirst(tri_level, tri_level):
+            # At the moment, the finder modifies tri_node in place
+            nHits = hit_finder(tri_node)
+            if nHits > 0:
+                # OK, so we've done all the intersections.
+                # We now need to mark the nodes as solid/fluid
+                classifier(tri_node)
+            else:
+                # No hits, should delete tri_node from the tree
+                ind = tri_node.offset
+                # Zero out the parent-local bits
+                local_bits = ind & (1 << tri_level)
+                parent_ind = ind ^ (local_bits)
+                parent = self.tree.GetNode(tri_level + 1, parent_ind)
+                assert parent.children[tuple(local_bits>>tri_level)] is tri_node
+                parent.children[tuple(local_bits>>tri_level)] = None
+            continue
+        pdb.set_trace()
+        # Now classify the top of the tree
+        classifier(self.tree.root, tri_level+1)
+        self.tree.Write(self.OutputGeometryFile)
+        
         return
     
     def __getattr__(self, attr):
@@ -79,13 +124,13 @@ class PolyDataGenerator(GeometryGenerator):
         """
         SurfaceBoundsWorking = self.ClippedSurface.GetBounds()
         OriginWorking = np.zeros(3, dtype=float)
-        nSites = np.zeros(3, dtype=np.uint)
+        nSites = np.zeros(3, dtype=int)
         
         for i in xrange(3):
             # Bounds of the vtkPolyData
-            mins = SurfaceBoundsWorking[2 * i]
-            maxs = SurfaceBoundsWorking[2 * i + 1]
-            size = maxs - mins
+            bmin = SurfaceBoundsWorking[2 * i]
+            bmax = SurfaceBoundsWorking[2 * i + 1]
+            size = bmax - bmin
             
             nSites[i] = int(size)
             # Since int() truncates, we have:
@@ -105,13 +150,29 @@ class PolyDataGenerator(GeometryGenerator):
                 nSites[i] += 1
                 extra = (nSites[i] - 1) - size
                 pass
-
+            
+        # Get the biggest of the sides
+        nSites = int(nSites.max())
+        # Now round this up to the next power of two
+        nBits = nSites.bit_length()
+        if nSites > 2**(nBits -1):
+            nSites = 2**nBits
+            pass
+        
+        for i in xrange(3):
             # Now ensure this extra space is equally balanced before & after the
             # fluid region with the placement of the first site.
-            OriginWorking[i] = mins - 0.5 * extra
+            bmin = SurfaceBoundsWorking[2 * i]
+            bmax = SurfaceBoundsWorking[2 * i + 1]
+            size = bmax - bmin
+            extra = (nSites - 1) - size
+            OriginWorking[i] = bmin - 0.5 * extra
             continue
         
-        return OriginWorking, nSites
+        self.OriginWorking = OriginWorking
+        self.CubeSize = nSites
+        self.TreeLevels = nSites.bit_length() - 1
+        return
     pass
 
 
