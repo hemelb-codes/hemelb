f6a7d7af376c10d63fe5366429bea8fc64c7de69
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-08-19 16:20:12 +0100

    CellController instantiated from template parameter
    Means we can instiate simulation master with a given kernel, and CellController will get the same
    kernel.
    
    Closes #549.

--- Code/SimulationMaster.impl.h
+++ Code/SimulationMaster.impl.h
@@ -218,7 +218,7 @@ namespace hemelb
     if (simConfig->HasRBCSection())
     {
       hemelb::redblood::CellContainer cells;
-      typedef hemelb::redblood::CellController<hemelb::Traits<>::Kernel> Controller;
+      typedef hemelb::redblood::CellController<typename Traits::Kernel> Controller;
       auto const controller = std::make_shared<Controller>(*latticeData,
                                                            cells,
                                                            simConfig->GetBoxSize(),
