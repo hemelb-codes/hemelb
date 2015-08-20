aa002b95c50c40128ac372c02b55ae361b5749b5
Gary Macindoe <gary.macindoe@ucl.ac.uk>
2015-08-20 17:05:58 +0100

    H5MD metadata class

--- Code/io/h5md/H5MD.h
+++ Code/io/h5md/H5MD.h
@@ -1,5 +1,16 @@
+//
+// Copyright (C) University College London, 2007-2012, all rights reserved.
+//
+// This file is part of HemeLB and is CONFIDENTIAL. You may not work
+// with, install, use, duplicate, modify, redistribute or share this
+// file, or any part thereof, other than as allowed by any agreement
+// specifically made by you with University College London.
+//
+
 #ifndef HEMELB_IO_H5MD_H5MD_H
 #define HEMELB_IO_H5MD_H5MD_H
+
+#include <string>
 
 namespace hemelb
 {
