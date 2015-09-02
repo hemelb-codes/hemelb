4a93db404859070cd5137f210affadcd787b073c
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-09-02 10:39:20 +0100

    Use same execution order as Timm's code
    Closes #569
    For technical reasons, the order we achieve is cyclic permutation of the one detailed by Timm in the
    ticket, which itself is a cyclic permutation of the one in his code.
    Point is, cyclic permutations are equivalent.

--- Code/redblood/CellController.h
+++ Code/redblood/CellController.h
@@ -41,14 +41,14 @@ namespace hemelb
           using namespace log;
           Logger::Log<Debug, Singleton>("Cell insertion");
           CellArmy<KERNEL>::CallCellInsertion();
+          Logger::Log<Debug, Singleton>("Fluid interaction with cells");
+          CellArmy<KERNEL>::Fluid2CellInteractions();
           Logger::Log<Debug, Singleton>("Cell interaction with fluid");
           CellArmy<KERNEL>::Cell2FluidInteractions();
         }
         void EndIteration() override
         {
           using namespace log;
-          Logger::Log<Debug, Singleton>("Fluid interaction with cells");
-          CellArmy<KERNEL>::Fluid2CellInteractions();
           Logger::Log<Debug, Singleton>("Checking whether cells have reached outlets");
           CellArmy<KERNEL>::CellRemoval();
           Logger::Log<Debug, Singleton>("Notify cell listeners");
