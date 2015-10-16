2efe8c600de8e3e1596e48e1f23b770badc8f328
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-16 19:59:58 +0100

    Make FigureNearness accessible throughout the code

--- Code/redblood/Borders.h
+++ Code/redblood/Borders.h
@@ -11,6 +11,7 @@
 #define HEMELB_REDBLOOD_BORDERS_INTERACTION_H
 
 #include <cassert>
+#include "redblood/DivideConquer.h"
 #include "units.h"
 
 namespace hemelb
@@ -34,6 +35,13 @@ namespace hemelb
     template<class T> util::Vector3D<T> direction(Borders border);
     //! Converts border enum to actual lattice direction
     template<class T> util::Vector3D<T> direction(size_t border);
+    //! \brief Encodes how close a vertex is to neighboring boxes
+    //! \param[in] dnc Divide and conquer boxes
+    //! \param[in] vertex Position to investigate
+    //! \param[in] haloLenght the meaning of close
+    template<class T>
+    size_t figureNearness(
+        DivideConquer<T> &dnc, LatticePosition const &vertex, LatticeDistance const &haloLength);
 
     //! \brief Iterates only over nearest borders
     class BorderBoxIterator
@@ -98,6 +106,39 @@ namespace hemelb
           b == Borders::SOUTH or b == Borders::EAST or b == Borders::WEST);
       return direction<T>(b);
     }
+
+    template<class T>
+    size_t figureNearness(DivideConquer<T> &dnc, LatticeVector const &key,
+                       LatticePosition const &vertex, LatticeDistance const &haloLength)
+    {
+      if (haloLength + haloLength > dnc.GetBoxSize())
+      {
+        return static_cast<size_t>(Borders::CENTER);
+      }
+
+      size_t result = 0;
+      typedef LatticePosition::value_type Distance;
+
+      for (size_t d(1); d < (1 << 6); d <<= 1)
+      {
+        Borders const border(static_cast<Borders>(d));
+        LatticePosition const translated(direction<Distance>(border) * haloLength);
+
+        if (not (key == dnc.DowngradeKey(vertex + translated)))
+        {
+          result |= d;
+        }
+      }
+
+      return result bitor static_cast<size_t>(Borders::CENTER);
+    }
+    template<class T>
+    size_t figureNearness(
+        DivideConquer<T> &dnc, LatticePosition const &vertex, LatticeDistance const &haloLength)
+    {
+      return figureNearness<T>(dnc, dnc.DowngradeKey(vertex), vertex, haloLength);
+    }
+
   }
 } // hemelb::redblood
 
