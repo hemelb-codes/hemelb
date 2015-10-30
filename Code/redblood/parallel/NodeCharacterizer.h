bbc77364020ebe56a458aec5ff18d82659932d50
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-30 16:15:08 +0000

    NodeCharacterizers maps process -> nodes
    Used to map nodes -> process.
    I think this should make reductions and spreading easier.

--- Code/redblood/parallel/NodeCharacterizer.h
+++ Code/redblood/parallel/NodeCharacterizer.h
@@ -11,6 +11,7 @@
 
 #include <set>
 #include <memory>
+#include <functional>
 
 #include "util/Packer.h"
 #include "redblood/cell.h"
@@ -29,6 +30,10 @@ namespace hemelb
     {
       namespace details
       {
+        //! Maps process indices to a sequence of node indices
+        typedef std::map<proc_t, std::set<MeshData::Vertices::size_type>> Process2NodesMap;
+        //! Functions returning the set of affected procs for a given node
+        typedef std::function<std::set<proc_t>(LatticePosition const&)> AssessNodeRange;
         //! Set of procs affected by this position
         //! \param[in] latDat will tell us which site belongs to which proc
         //! \param[in] iterator a  stencil iterator going over affected lattice points
@@ -48,30 +53,31 @@ namespace hemelb
       {
         public:
           //! set of processors affected by a node
-          typedef std::set<proc_t> ProcessorSet;
-          //! set of processors affected by nodes
-          typedef std::vector<ProcessorSet> ProcessorSets;
+          typedef details::Process2NodesMap Process2NodesMap;
+          //! Indices of the nodes
+          typedef Process2NodesMap::value_type::second_type::size_type Index;
+          //! A function to assess which processes a node may affect
+          typedef details::AssessNodeRange AssessNodeRange;
 
           //! Constructs object from prior knowledge of how processors are affected
-          NodeCharacterizer(ProcessorSets const &affectedProcs) : affectedProcs(affectedProcs)
+          NodeCharacterizer(Process2NodesMap const &affectedProcs)
+            : affectedProcs(affectedProcs)
           {
           }
           //! Constructs object from prior knowledge of how processors are affected
-          NodeCharacterizer(ProcessorSets &&affectedProcs) : affectedProcs(affectedProcs)
+          NodeCharacterizer(Process2NodesMap &&affectedProcs) : affectedProcs(affectedProcs)
           {
           }
           //! Constructs object using stencil as range object
           //! In practice, this will be the main constructor. Others are there for testing and
           //! convenience.
           NodeCharacterizer(
-              std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-              MeshData::Vertices const &vertices);
+              AssessNodeRange const &assessNodeRange, MeshData::Vertices const &vertices);
           //! Constructs object using stencil as range object
           //! In practice, this will be the main constructor. Others are there for testing and
           //! convenience.
           NodeCharacterizer(
-              std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-              std::shared_ptr<CellBase const> cell);
+              AssessNodeRange const &assessNodeRange, std::shared_ptr<CellBase const> cell);
 
           //! Constructs object using a custom assessor function
           template<class ... ARGS>
@@ -79,133 +85,150 @@ namespace hemelb
               geometry::LatticeData const &latDat,
               std::shared_ptr<CellBase const> cell,
               Traits<ARGS...> const& )
-          : NodeCharacterizer(
-                [&latDat](LatticePosition const& position)
-                {
-                  typedef typename Traits<ARGS...>::Stencil Stencil;
-                  return details::positionAffectsProcs<Stencil>(latDat, position);
-                },
-                cell)
+          : NodeCharacterizer(AssessMPIFunction<typename Traits<ARGS...>::Stencil>(latDat), cell)
           {
           }
 
 
           //! Whether the node affects more than one processor
-          bool IsMidDomain(int i) const
-          {
-            return affectedProcs[i].size() == 1;
-          }
+          bool IsMidDomain(Index index) const;
           //! Whether the node affects more than one processor
-          bool IsBoundary(int i) const
+          bool IsBoundary(Index index) const
           {
-            return not IsMidDomain(i);
+            return not IsMidDomain(index);
           }
-          //! The processors affected by a given node
-          std::set<proc_t> const & operator[](int i) const
+          //! \brief The processors affected by a given node
+          std::set<Process2NodesMap::key_type> AffectedProcs(Index i) const;
+
+          Process2NodesMap::mapped_type const & operator[](Process2NodesMap::key_type proc) const
           {
-            return affectedProcs[i];
+            auto result = affectedProcs.find(proc);
+            assert(result != affectedProcs.end());
+            return result->second;
           }
+
+          //! Updates node characterization and return change in ownership
+          void Reindex(AssessNodeRange const& assessNodeRange, MeshData::Vertices const &vertices);
+          //! Reindex with normal mpi function
+          template<class STENCIL>
+          void Reindex(geometry::LatticeData const &latDat, std::shared_ptr<CellBase const> cell)
+          {
+            Reindex(AssessMPIFunction<STENCIL>(latDat), cell->GetVertices());
+          }
+
+          //! Consolidates result from another proc into an input array
+          // void ReduceFrom(
+          //     MeshData::Vertices const &consolidated,
+          //     proc_t node, MeshData::Vertices const& incoming) const;
 
         protected:
+          template<class STENCIL>
+            static AssessNodeRange AssessMPIFunction(geometry::LatticeData const &latDat)
+            {
+              return std::bind(
+                details::positionAffectsProcs<STENCIL>,
+                latDat, std::placeholders::_1
+              );
+            }
           //! Processes affected by a given processor
-          ProcessorSets affectedProcs;
+          Process2NodesMap affectedProcs;
       };
 
-      //! An object for nodes owned by this process
-      class MeshOwner : public NodeCharacterizer
-      {
-        public:
-          MeshOwner(
-              std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-              std::shared_ptr<CellBase> cell)
-            : NodeCharacterizer(assessNodeRange, cell), cell(cell)
-          {
-          }
-          //! Constructs object using a custom assessor function
-          template<class ... ARGS>
-          MeshOwner(
-              geometry::LatticeData const &latDat,
-              std::shared_ptr<CellBase> cell,
-              redblood::stencil::types stencil=redblood::stencil::types::FOUR_POINT)
-            : NodeCharacterizer(latDat, cell, Traits<ARGS...>()), cell(cell)
-          {
-          }
+      // //! An object for nodes owned by this process
+      // class MeshOwner : public NodeCharacterizer
+      // {
+      //   public:
+      //     MeshOwner(
+      //         AssessNodeRange const& assessNodeRange,
+      //         std::shared_ptr<CellBase> cell)
+      //       : NodeCharacterizer(assessNodeRange, cell), cell(cell)
+      //     {
+      //     }
+      //     //! Constructs object using a custom assessor function
+      //     template<class ... ARGS>
+      //     MeshOwner(
+      //         geometry::LatticeData const &latDat,
+      //         std::shared_ptr<CellBase> cell,
+      //         redblood::stencil::types stencil=redblood::stencil::types::FOUR_POINT)
+      //       : NodeCharacterizer(latDat, cell, Traits<ARGS...>()), cell(cell)
+      //     {
+      //     }
+      //
+      //     //! Apply functor to mid-domain nodes
+      //     template<class APPLY> void ApplyToMidDomainNodes(APPLY apply, proc_t proc) const;
+      //     //! Update mid-domain positions by computing velocities
+      //     template <typename KERNEL> void UpdateLocalMidDomainPositions(
+      //         geometry::LatticeData const &latDat,
+      //         stencil::types stencil = stencil::types::FOUR_POINT) const;
+      //
+      //     //! Recomputes node locations and packs diffs for other procs
+      //     void UpdateNodeCharacterization(
+      //         AssessNodeRange const& assessNodeRange,
+      //         proc_t const localRank,
+      //         std::map<proc_t, util::Packer> &packer);
+      //
+      //   protected:
+      //     using NodeCharacterizer::affectedProcs;
+      //     //! Reference to vertices characterized by this object
+      //     std::shared_ptr<CellBase> cell;
+      // };
 
-          //! Apply functor to mid-domain nodes
-          template<class APPLY> void ApplyToMidDomainNodes(APPLY apply, proc_t proc) const;
-          //! Update mid-domain positions by computing velocities
-          template <typename KERNEL> void UpdateLocalMidDomainPositions(
-              geometry::LatticeData const &latDat,
-              stencil::types stencil = stencil::types::FOUR_POINT) const;
+      // //! \brief An object for nodes owned by another processor
+      // //! \details Internally, there are two kind of nodes: mid-domain nodes and boundary nodes.
+      // //! The former do not interact with sites owned by other procs. The latter do. The interaction
+      // //! range is determined by the size of the stencil for velocity interpolation and force
+      // //! spreading. (Though a larger stencil could also be used).
+      // class DistributedNodes
+      // {
+      //   public:
+      //     //! type of the vector of nodes for each proc
+      //     typedef std::vector<LatticePosition> Nodes;
+      //     //! constructor
+      //     DistributedNodes()
+      //     {
+      //     }
+      //     //! Explicit construction from a map of proc -> vector of nodes
+      //     DistributedNodes(std::map<proc_t, Nodes> && mid, std::map<proc_t, Nodes> && bound)
+      //       : midDomain(mid), boundary(bound)
+      //     {
+      //     }
+      //     //! destructor
+      //     virtual ~DistributedNodes();
+      //
+      //     //! Apply functor to mid-domain nodes
+      //     template<class KERNEL> void UpdateMidDomainPositions(
+      //         geometry::LatticeData const &latDat,
+      //         stencil::types stencil = stencil::types::FOUR_POINT);
+      //     //! Send new node positions to owner
+      //     void PackMidDomainNodes(util::Packer &packer, proc_t proc) const
+      //     {
+      //       auto const nodes = midDomain.find(proc);
+      //       assert(nodes != midDomain.end());
+      //       packer << nodes->second;
+      //     }
+      //     //! Send partial boundary velocity to owner
+      //     // void ComputeAndPackBoundaryDomainNodes(util::Packer &packer, proc_t proc) const;
+      //
+      //   protected:
+      //     //! MidDomain nodes and the processor they are owned by
+      //     std::map<proc_t, Nodes> midDomain;
+      //     //! Boundary nodes and the processor they are owned by
+      //     std::map<proc_t, Nodes> boundary;
+      // };
 
-          //! Recomputes node locations and packs diffs for other procs
-          void UpdateNodeCharacterization(
-              std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-              proc_t const localRank,
-              std::map<proc_t, util::Packer> &packer);
-
-        protected:
-          using NodeCharacterizer::affectedProcs;
-          //! Reference to vertices characterized by this object
-          std::shared_ptr<CellBase> cell;
-      };
-
-      //! \brief An object for nodes owned by another processor
-      //! \details Internally, there are two kind of nodes: mid-domain nodes and boundary nodes.
-      //! The former do not interact with sites owned by other procs. The latter do. The interaction
-      //! range is determined by the size of the stencil for velocity interpolation and force
-      //! spreading. (Though a larger stencil could also be used).
-      class DistributedNodes
-      {
-        public:
-          //! type of the vector of nodes for each proc
-          typedef std::vector<LatticePosition> Nodes;
-          //! constructor
-          DistributedNodes()
-          {
-          }
-          //! Explicit construction from a map of proc -> vector of nodes
-          DistributedNodes(std::map<proc_t, Nodes> && mid, std::map<proc_t, Nodes> && bound)
-            : midDomain(mid), boundary(bound)
-          {
-          }
-          //! destructor
-          virtual ~DistributedNodes();
-
-          //! Apply functor to mid-domain nodes
-          template<class KERNEL> void UpdateMidDomainPositions(
-              geometry::LatticeData const &latDat,
-              stencil::types stencil = stencil::types::FOUR_POINT);
-          //! Send new node positions to owner
-          void PackMidDomainNodes(util::Packer &packer, proc_t proc) const
-          {
-            auto const nodes = midDomain.find(proc);
-            assert(nodes != midDomain.end());
-            packer << nodes->second;
-          }
-          //! Send partial boundary velocity to owner
-          // void ComputeAndPackBoundaryDomainNodes(util::Packer &packer, proc_t proc) const;
-
-        protected:
-          //! MidDomain nodes and the processor they are owned by
-          std::map<proc_t, Nodes> midDomain;
-          //! Boundary nodes and the processor they are owned by
-          std::map<proc_t, Nodes> boundary;
-      };
-
-      template<class APPLY> void MeshOwner::ApplyToMidDomainNodes(APPLY apply, proc_t proc) const
-      {
-        auto i_procs = affectedProcs.cbegin();
-        auto const i_procs_end = affectedProcs.cend();
-        auto i_vertex = cell->GetVertices().begin();
-        for(; i_procs != i_procs_end; ++i_procs, ++i_vertex)
-        {
-          if(i_procs->count(proc) == 1 and i_procs->size() == 1)
-          {
-            apply(*i_vertex);
-          }
-        }
-      }
+      // template<class APPLY> void MeshOwner::ApplyToMidDomainNodes(APPLY apply, proc_t proc) const
+      // {
+      //   auto i_procs = affectedProcs.cbegin();
+      //   auto const i_procs_end = affectedProcs.cend();
+      //   auto i_vertex = cell->GetVertices().begin();
+      //   for(; i_procs != i_procs_end; ++i_procs, ++i_vertex)
+      //   {
+      //     if(i_procs->count(proc) == 1 and i_procs->size() == 1)
+      //     {
+      //       apply(*i_vertex);
+      //     }
+      //   }
+      // }
 
       // template<class APPLY>
       //   void DistributedNodes::ApplyToMidDomainNodes(APPLY apply, proc_t proc) const
@@ -219,16 +242,16 @@ namespace hemelb
       //   }
       // }
 
-      template<class KERNEL> void MeshOwner::UpdateLocalMidDomainPositions(
-          geometry::LatticeData const &latDat, stencil::types stencil) const
-      {
-        auto velocity = [&latDat, &stencil](LatticePosition &position)
-        {
-          position += interpolateVelocity<KERNEL>(latDat, position, stencil);
-        };
-        ApplyToMidDomainNodes(velocity, latDat.GetLocalRank());
-      }
-
+      // template<class KERNEL> void MeshOwner::UpdateLocalMidDomainPositions(
+      //     geometry::LatticeData const &latDat, stencil::types stencil) const
+      // {
+      //   auto velocity = [&latDat, &stencil](LatticePosition &position)
+      //   {
+      //     position += interpolateVelocity<KERNEL>(latDat, position, stencil);
+      //   };
+      //   ApplyToMidDomainNodes(velocity, latDat.GetLocalRank());
+      // }
+      //
       // template<class KERNEL> void DistributedNodes::UpdateLocalMidDomainPositions(
       //     geometry::LatticeData const &latDat, stencil::types stencil)
       // {
--- Code/unittests/redblood/parallel/NodeParallelizationTests.h
+++ Code/unittests/redblood/parallel/NodeParallelizationTests.h
@@ -13,8 +13,6 @@
 #include <cppunit/TestFixture.h>
 #include "unittests/redblood/Fixtures.h"
 #include "redblood/parallel/NodeCharacterizer.h"
-// to unittest helper functions in in anonymous namespace
-// #include "redblood/parallel/NodeCharacterizer.cc"
 #include <algorithm>
 
 namespace hemelb
@@ -30,19 +28,19 @@ namespace hemelb
           CPPUNIT_TEST_SUITE (NodeParallelizationTests);
           CPPUNIT_TEST (testProperties);
           CPPUNIT_TEST (testConstruction);
-          CPPUNIT_TEST (testApplyMidDomain);
+          // CPPUNIT_TEST (testApplyMidDomain);
           CPPUNIT_TEST_SUITE_END();
 
         public:
 
           void testProperties();
           void testConstruction();
-          void testApplyMidDomain();
+          // void testApplyMidDomain();
       };
 
       void NodeParallelizationTests::testProperties()
       {
-        NodeCharacterizer mm({{0}, {1}, {0, 1}});
+        NodeCharacterizer mm({{0, {0, 2}}, {1, {1, 2}}, {}});
         CPPUNIT_ASSERT_EQUAL(mm.IsMidDomain(0), true);
         CPPUNIT_ASSERT_EQUAL(mm.IsMidDomain(1), true);
         CPPUNIT_ASSERT_EQUAL(mm.IsMidDomain(2), false);
@@ -53,7 +51,7 @@ namespace hemelb
 
       void NodeParallelizationTests::testConstruction()
       {
-        auto func = [](LatticePosition const &position) -> NodeCharacterizer::ProcessorSet
+        auto func = [](LatticePosition const &position) -> std::set<proc_t>
         {
           bool isZero = std::abs(position.x) < 1e1 and std::abs(position.y) < 1e1
             and std::abs(position.z) < 1e1;
@@ -66,48 +64,49 @@ namespace hemelb
         NodeCharacterizer mm(func,
             {LatticePosition(0, 0, 0), LatticePosition(1e2, 0, 0), LatticePosition(7e0, 0, 0)});
 
-        CPPUNIT_ASSERT(NodeCharacterizer::ProcessorSet{0} == mm[0]);
-        CPPUNIT_ASSERT(NodeCharacterizer::ProcessorSet{1} == mm[1]);
-        CPPUNIT_ASSERT(NodeCharacterizer::ProcessorSet({0, 1}) == mm[2]);
+        CPPUNIT_ASSERT(mm[0].count(0));
+        CPPUNIT_ASSERT(mm[0].count(2));
+        CPPUNIT_ASSERT(mm[1].count(1));
+        CPPUNIT_ASSERT(mm[1].count(2));
       }
 
-      void NodeParallelizationTests::testApplyMidDomain()
-      {
-        auto const cell = std::make_shared<Cell>(pancakeSamosa());
-        auto positionOnProc = [](LatticePosition const &position) -> std::set<proc_t>
-        {
-          bool isZero = position.GetMagnitude() < 1e-8;
-          bool isSecond = (position - LatticePosition{1, 0, 1}).GetMagnitude() < 1e-8;
-          return isZero ?
-            std::set<proc_t>{0}: (isSecond ? std::set<proc_t>{1}: std::set<proc_t>{0, 1, 2});
-        };
-        MeshOwner const mm(positionOnProc, cell);
-        size_t nbcalls(0);
-        LatticePosition position(10, 10, 10);
-        auto apply = [&position, &nbcalls](LatticePosition &input)
-        {
-          ++nbcalls;
-          position = input;
-        };
-
-        // Finally, makes a call: there should be one node on proc 0
-        mm.ApplyToMidDomainNodes(apply, 0);
-        CPPUNIT_ASSERT_EQUAL(1ul, nbcalls);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.x, 1e-8);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.y, 1e-8);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.z, 1e-8);
-
-        // there should be one node on proc 1
-        mm.ApplyToMidDomainNodes(apply, 1);
-        CPPUNIT_ASSERT_EQUAL(2ul, nbcalls);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(1, position.x, 1e-8);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.y, 1e-8);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(1, position.z, 1e-8);
-
-        // The node on proc 2 is shared (not mid-domain)
-        mm.ApplyToMidDomainNodes(apply, 2);
-        CPPUNIT_ASSERT_EQUAL(2ul, nbcalls);
-      }
+      // void NodeParallelizationTests::testApplyMidDomain()
+      // {
+      //   auto const cell = std::make_shared<Cell>(pancakeSamosa());
+      //   auto positionOnProc = [](LatticePosition const &position) -> std::set<proc_t>
+      //   {
+      //     bool isZero = position.GetMagnitude() < 1e-8;
+      //     bool isSecond = (position - LatticePosition{1, 0, 1}).GetMagnitude() < 1e-8;
+      //     return isZero ?
+      //       std::set<proc_t>{0}: (isSecond ? std::set<proc_t>{1}: std::set<proc_t>{0, 1, 2});
+      //   };
+      //   // MeshOwner const mm(positionOnProc, cell);
+      //   // size_t nbcalls(0);
+      //   // LatticePosition position(10, 10, 10);
+      //   // auto apply = [&position, &nbcalls](LatticePosition &input)
+      //   // {
+      //   //   ++nbcalls;
+      //   //   position = input;
+      //   // };
+      //   //
+      //   // // Finally, makes a call: there should be one node on proc 0
+      //   // mm.ApplyToMidDomainNodes(apply, 0);
+      //   // CPPUNIT_ASSERT_EQUAL(1ul, nbcalls);
+      //   // CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.x, 1e-8);
+      //   // CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.y, 1e-8);
+      //   // CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.z, 1e-8);
+      //   //
+      //   // // there should be one node on proc 1
+      //   // mm.ApplyToMidDomainNodes(apply, 1);
+      //   // CPPUNIT_ASSERT_EQUAL(2ul, nbcalls);
+      //   // CPPUNIT_ASSERT_DOUBLES_EQUAL(1, position.x, 1e-8);
+      //   // CPPUNIT_ASSERT_DOUBLES_EQUAL(0, position.y, 1e-8);
+      //   // CPPUNIT_ASSERT_DOUBLES_EQUAL(1, position.z, 1e-8);
+      //   //
+      //   // // The node on proc 2 is shared (not mid-domain)
+      //   // mm.ApplyToMidDomainNodes(apply, 2);
+      //   // CPPUNIT_ASSERT_EQUAL(2ul, nbcalls);
+      // }
 
       CPPUNIT_TEST_SUITE_REGISTRATION (NodeParallelizationTests);
     }
