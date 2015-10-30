11abe603c91e2f3ecd9fbc64c72093ff30e56290
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-10-30 16:15:08 +0000

    NodeCharacterizers maps process -> nodes
    Used to map nodes -> process.
    I think this should make reductions and spreading easier.

--- Code/redblood/parallel/NodeCharacterizer.cc
+++ Code/redblood/parallel/NodeCharacterizer.cc
@@ -9,6 +9,7 @@
 
 #include <set>
 #include <numeric>
+#include "util/Iterator.h"
 #include "redblood/parallel/NodeCharacterizer.h"
 
 namespace hemelb
@@ -36,48 +37,91 @@ namespace hemelb
 
 
         template<class FUNCTION>
-          NodeCharacterizer::ProcessorSets meshMessenger(
+          NodeCharacterizer::Process2NodesMap meshMessenger(
               FUNCTION assessNodeRange, MeshData::Vertices const &vertices)
         {
-          NodeCharacterizer::ProcessorSets result(vertices.size());
-          std::transform(vertices.begin(), vertices.end(), result.begin(), assessNodeRange);
+          NodeCharacterizer::Process2NodesMap result;
+          for(auto const vertex: util::enumerate(vertices))
+          {
+            for(auto const &process: assessNodeRange(vertex.value))
+            {
+              auto const i_proc = result.find(process);
+              if(i_proc == result.end())
+              {
+                result[process] = {vertex.index};
+              }
+              else
+              {
+                result[process].insert(vertex.index);
+              }
+            }
+          }
           return result;
         }
       }
 
-#     ifndef HEMELB_DOING_UNITTESTS
       NodeCharacterizer::NodeCharacterizer(
-              std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-              MeshData::Vertices const &vertices)
+              AssessNodeRange const& assessNodeRange, MeshData::Vertices const &vertices)
         : NodeCharacterizer(details::meshMessenger(assessNodeRange, vertices))
         {
         }
 
       NodeCharacterizer :: NodeCharacterizer(
-          std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-          std::shared_ptr<CellBase const> cell)
+          AssessNodeRange const & assessNodeRange, std::shared_ptr<CellBase const> cell)
         : NodeCharacterizer(assessNodeRange, cell->GetVertices())
         {
         }
 
-      void MeshOwner::UpdateNodeCharacterization(
-          std::function<ProcessorSet(LatticePosition const&)> assessNodeRange,
-          proc_t const localRank,
-          std::map<proc_t, util::Packer> &packers)
+      void NodeCharacterizer :: Reindex(
+          AssessNodeRange const & assessor, MeshData::Vertices const & vertices)
       {
-        MeshData::Vertices :: const_iterator vertex = cell->GetVertices().begin();
-        MeshData::Vertices :: const_iterator const vertex_end = cell->GetVertices().end();
-        ProcessorSets::iterator procs = affectedProcs.begin();
-        assert(affectedProcs.size() == cell->GetVertices().size());
-        for(; vertex != vertex_end; ++vertex, ++procs)
-        {
-          auto const newProcs = assessNodeRange(*vertex);
-          parallel::details::UpdateNodeCharacterization(
-              newProcs, *procs, localRank, *vertex, packers);
-          *procs = std::move(newProcs);
-        }
+        affectedProcs = details::meshMessenger(assessor, vertices);
       }
-#     endif
+
+      bool NodeCharacterizer::IsMidDomain(Index index) const
+      {
+        int found (0);
+        for(auto const process: affectedProcs)
+        {
+          if(process.second.count(index) and ++found > 1)
+          {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      std::set<NodeCharacterizer::Process2NodesMap::key_type>
+        NodeCharacterizer::AffectedProcs(Index index) const
+      {
+        std::set<NodeCharacterizer::Process2NodesMap::key_type> result;
+        for(auto const process: affectedProcs)
+        {
+          if(process.second.count(index))
+          {
+            result.insert(process.first);
+          }
+        }
+        return result;
+      }
+
+      // void MeshOwner::UpdateNodeCharacterization(
+      //     AssessNodeRange const& assessNodeRange,
+      //     proc_t const localRank,
+      //     std::map<proc_t, util::Packer> &packers)
+      // {
+      //   MeshData::Vertices :: const_iterator vertex = cell->GetVertices().begin();
+      //   MeshData::Vertices :: const_iterator const vertex_end = cell->GetVertices().end();
+      //   ProcessorSets::iterator procs = affectedProcs.begin();
+      //   assert(affectedProcs.size() == cell->GetVertices().size());
+      //   for(; vertex != vertex_end; ++vertex, ++procs)
+      //   {
+      //     auto const newProcs = assessNodeRange(*vertex);
+      //     parallel::details::UpdateNodeCharacterization(
+      //         newProcs, *procs, localRank, *vertex, packers);
+      //     *procs = std::move(newProcs);
+      //   }
+      // }
     } // parallel
   } // redblood
 }  // hemelb
