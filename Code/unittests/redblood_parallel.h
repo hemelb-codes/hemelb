89e0131969cae6372dbdd8f56f5c8fc537fe6781
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-11-04 12:52:55 +0000

    Adds header guards

--- Code/unittests/redblood_parallel.h
+++ Code/unittests/redblood_parallel.h
@@ -7,5 +7,9 @@
 // specifically made by you with University College London.
 //
 
+#ifndef HEMELB_UNITTEST_REDBLOOD_PARALLEL_H
+#define HEMELB_DOING_UNITTESTS
 #include <unittests/redblood/parallel/ParticleShufflerTests.h>
 #include <unittests/redblood/parallel/NodeParallelizationTests.h>
+#undef HEMELB_DOING_UNITTESTS
+#endif
