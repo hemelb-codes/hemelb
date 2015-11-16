0a649b197dc5a6f19a5474007e10dc5a646667b9
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-11-16 15:09:47 +0000

    Non-blocking neighberhood collectives with variable message size

--- Code/unittests/net/NeighborCommTests.h
+++ Code/unittests/net/NeighborCommTests.h
@@ -16,6 +16,7 @@
 
 #include "net/MpiCommunicator.h"
 #include "net/INeighborAllToAll.h"
+#include "net/INeighborAllToAllV.h"
 #include "util/Iterator.h"
 
 namespace hemelb
@@ -28,8 +29,9 @@ namespace hemelb
       class NeighborCommTests : public CppUnit::TestFixture
       {
           CPPUNIT_TEST_SUITE (NeighborCommTests);
-          CPPUNIT_TEST(testInteger);
-          CPPUNIT_TEST(testDoubles);
+          // CPPUNIT_TEST(testInteger);
+          // CPPUNIT_TEST(testDoubles);
+          CPPUNIT_TEST(testVariableInts);
           CPPUNIT_TEST_SUITE_END();
 
         public:
@@ -38,6 +40,9 @@ namespace hemelb
           void testInteger();
           //! Sends several doubles per proc
           void testDoubles();
+          //! \brief Sends different number of ints from each proc
+          //! \details Each proc expects to know how many it is gettin from each before hand
+          void testVariableInts();
 
         protected:
           net::MpiCommunicator graph;
@@ -125,6 +130,65 @@ namespace hemelb
           CPPUNIT_ASSERT_EQUAL(message(item.value, graph.Rank(), 1), received[item.index * 3 + 1]);
           CPPUNIT_ASSERT_EQUAL(message(item.value, graph.Rank(), 2), received[item.index * 3 + 2]);
         }
+      }
+
+      void NeighborCommTests::testVariableInts()
+      {
+        if(not graph)
+        {
+          return;
+        }
+        // Just a message such what i sends to j is different from what j sends to i
+        auto const message = [](int proci, int procj, int n = 0)
+        {
+          return n + proci + procj + (proci > procj ? 100: proci < procj ? -100: 0);
+        };
+        // If X sends to Y, then how many it does send
+        auto const XSendsToY = [](int proci, int procj)
+        {
+          return proci >= 4 or procj >= 4 ? 0:
+              proci > procj ? proci + procj: proci + procj + 10;
+        };
+
+        // Prepares arrays how many elements are sent from each to each
+        auto const neighbors = graph.GetNeighbors();
+        std::vector<int> sendCounts, recvCounts;
+        int nReceived = 0;
+        for(auto neighbor: neighbors)
+        {
+          sendCounts.push_back(XSendsToY(graph.Rank(), neighbor));
+          recvCounts.push_back(XSendsToY(neighbor, graph.Rank()));
+          nReceived += recvCounts.back();
+        }
+
+        // Prepares array with elements to send
+        INeighborAllToAllV<int> all2all(graph, sendCounts, recvCounts);
+        for(auto const neighbor: neighbors)
+        {
+          std::vector<int> stuff;
+          for(int i(0); i < XSendsToY(graph.Rank(), neighbor); ++i)
+          {
+            stuff.push_back(message(graph.Rank(), neighbor, i));
+          }
+          all2all.insertSend(neighbor, stuff);
+        }
+
+        // Send message
+        all2all.send();
+        // Wait for end of request and check received lengths
+        all2all.receive();
+
+        // Check result
+        CPPUNIT_ASSERT_EQUAL(size_t(nReceived), all2all.GetReceiveBuffer().size());
+        auto received = all2all.GetReceiveBuffer().begin();
+        for(auto const item: util::zip(neighbors, recvCounts))
+        {
+          for(int i(0); i < std::get<1>(item); ++i, ++received)
+          {
+            CPPUNIT_ASSERT_EQUAL(message(std::get<0>(item), graph.Rank(), i), *received);
+          }
+        }
+
       }
 
       CPPUNIT_TEST_SUITE_REGISTRATION (NeighborCommTests);
