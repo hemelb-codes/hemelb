03a61ca28bd73105eb4300fab29c455acc8f1b97
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-11-17 10:21:54 +0000

    Add mpi tests to ctest config

---
+++ Code/unittests/redblood_mpi.h
@@ -0,0 +1,15 @@
+//
+// Copyright (C) University College London, 2007-2012, all rights reserved.
+//
+// This file is part of HemeLB and is CONFIDENTIAL. You may not work
+// with, install, use, duplicate, modify, redistribute or share this
+// file, or any part thereof, other than as allowed by any agreement
+// specifically made by you with University College London.
+//
+
+#ifndef HEMELB_UNITTEST_REDBLOOD_PARALLEL_H
+#define HEMELB_DOING_UNITTESTS
+#include <unittests/redblood/parallel/CellParallelizationTests.h>
+#include <unittests/net/NeighborCommTests.h>
+#undef HEMELB_DOING_UNITTESTS
+#endif
