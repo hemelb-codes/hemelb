cc8655b5175ce29eee799936995b318e3d73c89e
Rupert Nash <rupert.nash@ed.ac.uk>
2015-12-04 09:19:42 +0000

    Remove images from Fabric et al

--- deploy/fab.py
+++ deploy/fab.py
@@ -498,7 +498,6 @@ def hemelb(config, **args):
     config : config directory to use to define geometry, e.g. config=cylinder
     Keyword arguments:
             cores : number of compute cores to request
-            images : number of images to take
             steering : steering session i.d.
             wall_time : wall-time job limit
             memory : memory per node
@@ -506,7 +505,7 @@ def hemelb(config, **args):
     with_config(config)
     execute(put_configs, config)
     job(dict(script='hemelb',
-            cores=4, images=10, steering=1111, wall_time='0:15:0', memory='2G'), args)
+            cores=4, steering=1111, wall_time='0:15:0', memory='2G'), args)
     if args.get('steer', False):
         execute(steer, env.name, retry=True, framerate=args.get('framerate'), orbit=args.get('orbit'))
 
@@ -518,7 +517,6 @@ def multiscale_hemelb(config,**args):
     config : config directory to use to define geometry, e.g. config=cylinder
     Keyword arguments:
             cores : number of compute cores to request
-            images : number of images to take
             steering : steering session i.d.
             wall_time : wall-time job limit
             memory : memory per node
@@ -526,7 +524,7 @@ def multiscale_hemelb(config,**args):
     with_config(config)
     execute(put_configs,config)
     job(dict(script='multiscale_hemelb',
-            cores=4,images=10, steering=1111, wall_time='0:15:0',memory='2G'),args)
+            cores=4, steering=1111, wall_time='0:15:0',memory='2G'),args)
     if args.get('steer',False):
         execute(steer,env.name,retry=True,framerate=args.get('framerate'),orbit=args.get('orbit'))
 
@@ -546,7 +544,7 @@ def multijob(*names, **args):
     env.jobstorun = "\n".join(jobscriptpaths)
     # And then, submit it
     job(dict(script='multijob', job_name_template='multijob',
-            cores=4, images=10, steering=1111, wall_time='0:15:0', memory='2G'), args)
+            cores=4, steering=1111, wall_time='0:15:0', memory='2G'), args)
 
 @task
 def hemelbs(config, **args):
@@ -557,7 +555,6 @@ def hemelbs(config, **args):
     config : config directory to use to define geometry, e.g. config=cylinder
     Keyword arguments:
             cores : number of compute cores to request
-            images : number of images to take
             steering : steering session i.d.
             wall_time : wall-time job limit
             memory : memory per node
@@ -578,7 +575,7 @@ def hemelb_benchmark(config, min_cores, max_cores, **args):
         with_config(config)
         execute(put_configs, config)
         job(dict(script='hemelb',
-            cores=cores_used, images=10, steering=1111, wall_time='0:15:0', memory='2G'), args)
+            cores=cores_used, steering=1111, wall_time='0:15:0', memory='2G'), args)
         cores_used *= 2
 
 @task(alias='regress')
@@ -588,7 +585,7 @@ def regression_test(**args):
     execute(copy_regression_tests)
     execute(build_python_tools)
     job(dict(job_name_template='regression_${build_number}_${machine_name}', cores=3,
-            wall_time='0:20:0', memory='2G', images=0, steering=1111, script='regression'), args)
+            wall_time='0:20:0', memory='2G', steering=1111, script='regression'), args)
 
 def calc_nodes():
   # If we're not reserving whole nodes, then if we request less than one node's worth of cores, need to keep N<=n
