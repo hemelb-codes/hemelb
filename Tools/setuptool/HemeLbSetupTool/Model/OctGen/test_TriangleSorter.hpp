bda791c102ef0d0a45ac7990c6576eeb464a3ba4
Rupert Nash <rupert.nash@ed.ac.uk>
2016-06-13 16:53:13 +0100

    get test_TriangleSorter passing the sphere test

--- Tools/setuptool/HemeLbSetupTool/Model/OctGen/test_TriangleSorter.hpp
+++ Tools/setuptool/HemeLbSetupTool/Model/OctGen/test_TriangleSorter.hpp
@@ -2,6 +2,7 @@
 #define HEMELBSETUPTOOL_TEST_TRIANGLESORTER_HPP
 
 #include <cppunit/extensions/HelperMacros.h>
+#include <set>
 #include "TestResources/Meshes.hpp"
 #include "TriangleSorter.h"
 
@@ -69,32 +70,55 @@ public:
     CPPUNIT_ASSERT(ids[1] == 1);
   }
 
+  template <class T>
+  bool overlap1d(T aMin, T aMax, T bMin, T bMax) {
+    return (aMin < bMax) && (aMax > bMin);
+  }
+
+  bool overlap3d(const Vector& aMin, const Vector& aMax,
+		 const Vector& bMin, const Vector& bMax) {
+    bool ans = true;
+    for (auto d =0; d<3; ++d)
+      ans &= overlap1d(aMin[d], aMax[d], bMin[d], bMax[d]);
+    
+    return ans;
+  }
+  
   void Sphere() {
     TriTree::Int levels = 5;
     TriTree::Int tri_level = 3;
     
     auto sphere = SimpleMeshFactory::MkSphere();
-    // tree = TriangleSorter.TrianglesToTree(levels, tri_level, points, triangles)
-    
-    // seen_tris = set()
-    // for node in tree.IterDepthFirst(tri_level, tri_level):
-    //     n = len(node.triIds)
-    //     node_points = np.zeros((n, 3, 3))
-    //     node_point_ids = triangles[node.triIds]
-    //     for iPt in xrange(3):
-    //         node_points[:,iPt,:] = points[node_point_ids[:,iPt]]
-        
-    //     tri_min = node_points.min(axis=1)
-    //     tri_max = node_points.max(axis=1)
-        
-    //     nd_min = (node.offset - 1)[np.newaxis,:]
-    //     nd_max = (node.offset + 2**tri_level + 1)[np.newaxis,:]
-        
-    //     assert np.all(overlap3d(tri_min, tri_max, nd_min, nd_max))
-            
-    //     seen_tris.update(node.triIds)
-    // allids = sorted(seen_tris)
-    // assert np.all(np.equal(allids, np.arange(len(triangles))))
+    auto tree = TrianglesToTree(levels, tri_level, sphere->points, sphere->triangles);
+
+    std::set<int> seen_tris;
+
+    tree.IterDepthFirst(tri_level, tri_level, [&](TriTree::Node& node) {
+	const auto& triIds = node.Data();
+	const Vector offset(node.X(), node.Y(), node.Z());
+	auto node_min = offset - 1;
+	auto node_max = offset + (1 << tri_level) + 1;
+	
+	for (auto iTri: triIds) {
+	  Vector tri_min(std::numeric_limits<double>::infinity());
+	  Vector tri_max(0);
+	  for (auto iPt: sphere->triangles[iTri]) {
+	    tri_min.UpdatePointwiseMin(sphere->points[iPt]);
+	    tri_max.UpdatePointwiseMax(sphere->points[iPt]);
+	  }
+
+	  CPPUNIT_ASSERT(overlap3d(tri_min, tri_max, node_min, node_max));
+	  seen_tris.insert(triIds.begin(), triIds.end());
+	}
+      });
+
+    // Assert that we have seen all the triangles in the input
+    CPPUNIT_ASSERT(seen_tris.size() == sphere->triangles.size());
+    int i = 0;
+    for (auto seen_id : seen_tris) {
+      CPPUNIT_ASSERT(seen_id == i);
+      ++i;
+    }
   }
     
 };
