5d634cb97b86046a07df7b3997b8f4d92cbbcdfb
Ulf Schiller <ulf@lattice-boltzmann.de>
2016-08-03 20:42:52 -0400

    Add cubic velocity terms to D3Q19 equilibrium. D3Q19 basis comprises six non-degenerate third rank Hermite polynomials. The added terms are the projection of Maxwell-Boltzmann onto these polynomials. Note this does not affect hydrodynamics nor overall accuracy, but the improved kinetic projection may influence stability.

--- Code/lb/lattices/D3Q19.h
+++ Code/lb/lattices/D3Q19.h
@@ -38,7 +38,48 @@ namespace hemelb
 
           // The index of the inverse direction of each discrete velocity vector
           static const Direction INVERSEDIRECTIONS[NUMVECTORS];
-      };
+
+	  /**
+           * Calculate Feq, the orginal version
+           * @param density
+           * @param momentum_x
+           * @param momentum_y
+           * @param momentum_z
+           * @param f_eq
+           */
+          inline static void CalculateFeq(const distribn_t &density,
+                                          const distribn_t &momentum_x,
+                                          const distribn_t &momentum_y,
+                                          const distribn_t &momentum_z,
+                                          distribn_t f_eq[])
+          {
+	    const distribn_t u_x = momentum_x/density;
+	    const distribn_t u_y = momentum_y/density;
+	    const distribn_t u_z = momentum_z/density;
+	    const distribn_t u2 = u_x * u_x + u_y * u_y + u_z * u_z;
+
+            for (Direction i = 0; i < NUMVECTORS; ++i)
+            {
+              const distribn_t u_dot_ei = D3Q19::CX[i] * u_x + D3Q19::CY[i] * u_y + D3Q19::CZ[i] * u_z;
+
+              f_eq[i] = D3Q19::EQMWEIGHTS[i] * density
+		* (1.
+		   + 3. * u_dot_ei
+		   + (9. / 2.) * u_dot_ei * u_dot_ei - (3. / 2.) * u2
+		   + (27. / 2.) * D3Q19::CX[i] * u_x
+		   * (  u_y * u_y * (D3Q19::CY[i] * D3Q19::CY[i] - (1./3.) )
+			+ u_z * u_z * (D3Q19::CZ[i] * D3Q19::CZ[i] - (1./3.) ) )
+		   + (27. / 2.) * D3Q19::CY[i] * u_y
+		   * (  u_z * u_z * (D3Q19::CZ[i] * D3Q19::CZ[i] - (1./3.))
+			+ u_x * u_x * (D3Q19::CX[i] * D3Q19::CX[i] - (1./3.)) )
+		   + (27. / 2.) * D3Q19::CZ[i] * u_z
+		   * (  u_x * u_x * (D3Q19::CX[i] * D3Q19::CX[i] - (1./3.))
+			+ u_y * u_y * (D3Q19::CY[i] * D3Q19::CY[i] - (1./3.)) ) );
+
+            }
+          }
+
+     };
     }
   }
 }
