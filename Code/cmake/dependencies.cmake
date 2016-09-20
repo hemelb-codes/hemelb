f51217afdf598a19aec96ec52c1eeef8e837860e
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-09-20 14:03:40 +0100

    Avoids warnings from system libraries

--- Code/cmake/dependencies.cmake
+++ Code/cmake/dependencies.cmake
@@ -1,6 +1,6 @@
 #------Parmetis  ------------
 find_package(Parmetis REQUIRED)
-include_directories(${PARMETIS_INCLUDE_DIRS})
+include_directories(SYSTEM ${PARMETIS_INCLUDE_DIRS})
 
 #------TinyXML ----------------
 find_package(TinyXML REQUIRED)
@@ -8,21 +8,21 @@ OPTION(TIXML_USE_STL "Use STL with TIXML" ON)
 if(TIXML_USE_STL)
 	add_definitions(-DTIXML_USE_STL)
 endif()
-include_directories(${TINYXML_INCLUDE_DIR})
+include_directories(SYSTEM ${TINYXML_INCLUDE_DIR})
 #------BOOST ------------------
 find_package(Boost 1.54 REQUIRED)
-include_directories(${Boost_INCLUDE_DIRS})
+include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
 add_definitions(-DHEMELB_USE_BOOST)
 #------CTemplate ----------------
 find_package(CTemplate REQUIRED)
-include_directories(${CTEMPLATE_INCLUDE_DIR})
+include_directories(SYSTEM ${CTEMPLATE_INCLUDE_DIR})
 if(HEMELB_BUILD_MULTISCALE)
   #------MPWide ----------------
   find_package(MPWide REQUIRED)
-  include_directories(${MPWide_INCLUDE_DIR})
+  include_directories(SYSTEM ${MPWide_INCLUDE_DIR})
   add_definitions(-DHEMELB_BUILD_MULTISCALE)
 endif()
 
 #------zlib ----------------
 find_package(ZLIB REQUIRED)
-include_directories(${ZLIB_INCLUDE_DIR})
+include_directories(SYSTEM ${ZLIB_INCLUDE_DIR})
