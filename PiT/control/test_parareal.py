e07c009428e0a2e20547ca4df3e709b5161aa913
Rupert Nash <rupert.nash@ed.ac.uk>
2018-05-24 14:29:30 +0100

    get tests passing again

--- PiT/control/test_parareal.py
+++ PiT/control/test_parareal.py
@@ -4,27 +4,11 @@ import luigi
 
 Coarse = parareal.Resolution.Coarse
 Fine = parareal.Resolution.Fine
-    
-def master_mock(num_time_slices = 1, num_parareal_iters = 1):
-    def get_master():
-        params = {'b': 0.1, 'omega': 1.0}
-        initial = {
-            'x': 1.0,
-            'v': 0.0
-        }
-        time = parareal.Interval(2.0 * np.pi/1000, 0, 1000)
-        return PararealParameters(params, initial, time, num_parareal_iters, num_time_slices)
-    return get_master
-
-@pytest.fixture
-def master_1_1(monkeypatch):
-    monkeypatch.setattr(parareal, 'get_master', master_mock())
-    return
 
 @pytest.mark.parametrize("i", [1,2,5])
 def test_y0_i_is_made_by_coarse(i):
     t0 = parareal.y.producer(Coarse, i, 0)
-    assert isinstance(t0, parareal.Linker)
+    assert isinstance(t0, parareal.Assignment)
     
     t1 = t0.requires()
     assert isinstance(t1, parareal.SolutionExtractor)
@@ -37,7 +21,7 @@ def test_y0_i_is_made_by_coarse(i):
 @pytest.mark.parametrize("i", [1,2,5])
 def test_yi_i_is_made_by_fine(i):
     t0 = parareal.y.producer(Fine, i, i)
-    assert isinstance(t0, parareal.Linker)
+    assert isinstance(t0, parareal.Assignment)
     
     t1 = t0.requires()
     assert isinstance(t1, parareal.SolutionExtractor)
@@ -64,7 +48,6 @@ def gather_deps(tsk):
     return ans
 
 def test_32_deps(monkeypatch):
-    monkeypatch.setattr(parareal, 'get_master', master_mock(3,2))
     tsk = parareal.y.producer(Fine, 3, 2)
     deps = gather_deps(tsk)
     assert len(deps) == 34
