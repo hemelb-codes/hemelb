86c0dd920333c9fe734196879752f37910935b2c
Rupert Nash <rupert.nash@ed.ac.uk>
2018-06-07 12:09:56 +0100

    testing that parareal k=i is equiv to serial

---
+++ PiT/control/serial_equiv.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import py.path
+import luigi
+
+from test_hm import test_44_equiv_serial
+import interval
+
+
+def analytic(t):
+    ''' x(t) = (A sin wt + B cos wt) exp(-bt/2)
+
+    where (to satisfy x(0) = 1, v(0) = 0):
+        A = b/(2w)
+        B = 1
+    
+    '''
+    beta = 0.1
+    omega0 = 1.0
+    
+    omega = 0.5 * np.sqrt(4.0 * omega0**2 - beta**2)
+    
+    A = beta / (2.0*omega)
+    sin_wt = np.sin(omega*t)
+    cos_wt = np.cos(omega*t)
+    e_bt2 = np.exp(-0.5 * beta * t)
+    x = (A * sin_wt + cos_wt) * e_bt2
+    v = -0.5 * beta * x + (A * omega * cos_wt - omega * sin_wt) * e_bt2
+    return x, v
+
+class Run:
+    def __init__(self, data):
+        self.data = data
+        self.t, self.x, self.v = data.transpose()
+
+if __name__ == '__main__':
+    rundir = py.path.local('serial_equivalence')
+    if rundir.exists():
+        rundir.remove()
+        pass
+    rundir.mkdir()
+
+    test_44_equiv_serial(rundir, luigi, plot_mode=True)
+
+    with rundir.as_cwd():
+        serial = Run(np.loadtxt('serial.txt'))
+        parareal = Run(np.loadtxt('parareal.txt'))
+
+    time = interval.Interval(2.0 * np.pi/1000,0.0, 1000)
+    sub_times = time.subdivide(4)
+    
+    assert np.all(parareal.t == serial.t)
+
+    t = parareal.t
+    exact_x, exact_v = analytic(t)
+    delta_x = parareal.x - serial.x
+    delta_v = parareal.v - serial.v
+
+    class Plotter(object):
+        def __init__(self, x):
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(111)
+            self.x = x
+            
+            self.ylim = np.array([np.inf, -np.inf])
+            
+        def _update(self, y):
+            self.ylim[0] = min(self.ylim[0], np.min(y))
+            self.ylim[1] = max(self.ylim[1], np.max(y))
+            
+        def plot(self, y, **kwargs):
+            self.ax.plot(self.x, y, **kwargs)
+            self._update(y)
+        def scatter(self, y, **kwargs):
+            self.ax.scatter(self.x, y, **kwargs)
+            self._update(y)
+            
+        def show_or_save(self, fname=None):
+            pows = np.log10(np.abs(self.ylim))
+            pow10 = np.floor(pows.max())
+            base = 10**pow10
+            ylim = self.ylim / base
+            ymin = np.floor(ylim[0]) * base
+            ymax = np.ceil(ylim[1]) * base
+
+            self.ax.set_ylim(ymin, ymax)
+            
+            if fname is None:
+                self.fig.show()
+            else:
+                self.fig.savefig(fname)
+    
+    def plt_x(fname=None):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(t, exact_x)
+        ax.scatter(t, serial.x)
+        ax.scatter(t, parareal.x)
+        show_or_save(fig, fname)
+
+    def plt_v(fname=None):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(t, exact_v)
+        ax.scatter(t, serial.v)
+        ax.scatter(t, parareal.v)
+        show_or_save(fig, fname)
+
+    def plt_x_err(fname=None):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(t, serial.x-exact_x)
+        ax.scatter(t, parareal.x-exact_x)
+        show_or_save(fig, fname)
+
+    def plt_v_err(fname=None):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(t, serial.v-exact_v)
+        ax.scatter(t, parareal.v-exact_v)
+        show_or_save(fig, fname)
+
+    def plot2(func, fname=None):
+        p = Plotter(t)
+        p.scatter(func(serial))
+        p.scatter(func(parareal))
+        p.show_or_save(fname)
+        
+    def plt_err(fname=None):
+        def err(run):
+            return 
+        p = Plotter(t)
+        
+        p.scatter(err(serial))
+        p.scatter(err(parareal))
+        p.show_or_save(fname)
+        
+    plot2(lambda run: np.sqrt((run.x - exact_x)**2 + (run.v - exact_v)**2),
+              'rms_err.pdf')
+    
+# #aplot = ax.plot(t, exact_x)
+# splot = ax.scatter(t, serial.x-exact_x)
+# pplot = ax.scatter(t, parareal.x-exact_x)
+# #dplot = ax.scatter(t, delta_x)
+# plt.show()
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+
+# #ax.plot(t, exact_v)
+# ax.scatter(t, serial.v-exact_v)
+# ax.scatter(t, parareal.v-exact_v)
+# #dplot = ax.scatter(t, delta_x)
+# plt.show()
---
+++ PiT/control/test_hm.py
@@ -0,0 +1,122 @@
+import io
+import pytest
+import numpy as np
+
+import parareal
+import interval
+import hm
+
+@pytest.fixture
+def clean_luigi(monkeypatch):
+    '''Luigi has singletons for various configuration objects and an
+    instance cache of horror. Here we monkey patch those back to the
+    initial state so it's like we have just done `import luigi`
+    '''
+    import luigi
+
+    monkeypatch.setattr(luigi.configuration.LuigiConfigParser,
+                            '_instance',
+                            None)
+    monkeypatch.setattr(luigi.task_register.Register,
+                            '_Register__instance_cache',
+                            {})
+    monkeypatch.setattr(luigi.cmdline_parser.CmdlineParser,
+                            '_instance',
+                            None)
+
+    monkeypatch.setattr(luigi.interface.setup_interface_logging,
+                            'has_run',
+                            False,
+                            raising=False)
+    return luigi
+
+def set_params(clean_luigi, params):
+    cfg = clean_luigi.configuration.LuigiConfigParser.instance()
+    
+    cfg.set('core', 'workers', '1')
+    cfg.set('core', 'no_configure_logging', 'True')
+    cfg.set('core', 'local_scheduler', 'True')
+    
+    params.to_file('tmp.yml')
+    cfg.set('parareal', 'filename', 'tmp.yml')
+    return cfg
+
+def test_simple_runs(tmpdir, clean_luigi):
+    params = parareal.PararealParameters(
+        {'b': 0.1, 'omega': 1.0},
+        {'x': 1.0, 'v': 0.0},
+        interval.Interval(2.0 * np.pi/1000,0.0, 1000),
+        2, 4)
+    
+    with tmpdir.as_cwd() as old:
+        set_params(clean_luigi, params)
+        tsk = params.final_task()
+        assert not tsk.output().exists()
+        ran_ok = clean_luigi.build([tsk])
+        assert ran_ok
+        assert tsk.output().exists()
+
+def merge_trajectories(traj_lst):
+    chunk_lst = []
+    n_rows = 0
+    for t in traj_lst:
+        chunk = np.loadtxt(t.path)
+        try:
+            last = chunk_lst[-1]
+            assert np.all(last[-1] == chunk[0])
+            chunk = chunk[1:]
+        except IndexError:
+            # we must be first
+            pass
+        chunk_lst.append(chunk)
+        continue
+    ans = np.concatenate(chunk_lst, axis=0)
+    np.savetxt('parareal.txt', ans)
+    return ans
+
+def run_serial(parareal_params):
+    prob = hm.Problem(parareal_params.sim_params,
+                          parareal_params.initial_conditions,
+                          parareal_params.time,
+                          'fine')
+    fn = 'serial.txt'
+    prob.run(fn)
+    return np.loadtxt(fn)
+    
+    
+def test_44_equiv_serial(tmpdir, clean_luigi, plot_mode=False):
+    i_max = 4
+    
+    params = parareal.PararealParameters(
+        {'b': 0.1, 'omega': 1.0},
+        {'x': 1.0, 'v': 0.0},
+        interval.Interval(2.0 * np.pi/1000,0.0, 1000),
+        i_max, i_max)
+    
+    with tmpdir.as_cwd() as old:
+        set_params(clean_luigi, params)
+        tsk = params.final_task()
+        
+        ran_ok = clean_luigi.build([tsk])
+        assert ran_ok
+        
+        assert tsk.output().exists()
+
+        overall = merge_trajectories(
+            parareal.Trajectory(parareal.Resolution.Fine, i, i)
+            for i in range(i_max)
+            )
+        serial = run_serial(params)
+
+        delta = serial - overall
+        
+        dt, dx, dv = delta.transpose()
+        
+        if not plot_mode:
+            # We have to choose these tolerances so skip this to get
+            # the data out and plotted.
+            assert np.all(dt == 0)
+
+            e2 = np.sqrt(dx**2 + dv**2)
+            assert np.all(np.absolute(e2) < 5e-4)
+            
