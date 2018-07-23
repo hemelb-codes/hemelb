74738475327c0d00a3cf495f1b1e3881b3012290
Rupert Nash <r.nash@epcc.ed.ac.uk>
2018-07-23 11:16:37 +0100

    add make_unique in c++11 compatible way

---
+++ Code/util/unique.h
@@ -0,0 +1,22 @@
+// This file is part of HemeLB and is Copyright (C)
+// the HemeLB team and/or their institutions, as detailed in the
+// file AUTHORS. This software is provided under the terms of the
+// license in the file LICENSE.
+
+#ifndef HEMELB_UTIL_UNIQUE_H
+#define HEMELB_UTIL_UNIQUE_H
+
+#include <memory>
+
+#ifndef __cpp_lib_make_unique
+namespace std {
+  template<typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+}
+
+#endif
+
+#endif 
