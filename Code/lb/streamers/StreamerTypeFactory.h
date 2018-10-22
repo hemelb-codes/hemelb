08f5947defda855ae21c794a7d3e94ffd73dd7eb
Ben Shealy <bentshermann@gmail.com>
2018-10-22 19:43:32 -0400

    Added initial CUDA kernel

--- Code/lb/streamers/StreamerTypeFactory.h
+++ Code/lb/streamers/StreamerTypeFactory.h
@@ -10,6 +10,7 @@
 #include "lb/kernels/BaseKernel.h"
 #include "lb/streamers/BaseStreamer.h"
 #include "lb/streamers/SimpleCollideAndStreamDelegate.h"
+#include <cuda_runtime.h>
 
 namespace hemelb
 {
@@ -17,6 +18,17 @@ namespace hemelb
   {
     namespace streamers
     {
+      __global__ void WallStreamerTypeFactory_DoStreamAndCollideKernel(
+        site_t firstIndex,
+        site_t siteCount,
+        distribn_t lbmParams_tau,
+        distribn_t lbmParams_omega,
+        const site_t* neighbourIndices,
+        const unsigned* wallIntersections,
+        const distribn_t* fOld,
+        distribn_t* fNew
+      );
+
       /**
        * Template to produce Streamers that can cope with fluid-fluid and
        * fluid-wall links. Requires two classes as arguments: 1) the Collision
@@ -52,40 +64,56 @@ namespace hemelb
                                          geometry::LatticeData* latDat,
                                          lb::MacroscopicPropertyCache& propertyCache)
           {
-            for (site_t siteIndex = firstIndex; siteIndex < (firstIndex + siteCount); siteIndex++)
+            static bool init = true;
+            static site_t* neighbourIndices_dev;
+            static unsigned* wallIntersections_dev;
+            static distribn_t* fOld_dev;
+            static distribn_t* fNew_dev;
+
+            if ( init )
             {
-              geometry::Site<geometry::LatticeData> site = latDat->GetSite(siteIndex);
+              unsigned numVectors = latDat->GetLatticeInfo().GetNumVectors();
 
-              const distribn_t* fOld = site.GetFOld<LatticeType> ();
+              cudaMalloc(&neighbourIndices_dev, numVectors * LatticeType::NUMVECTORS * sizeof(site_t));
+              cudaMalloc(&wallIntersections_dev, numVectors * sizeof(unsigned));
+              cudaMalloc(&fOld_dev, numVectors * LatticeType::NUMVECTORS * sizeof(distribn_t));
+              cudaMalloc(&fNew_dev, numVectors * LatticeType::NUMVECTORS * sizeof(distribn_t));
 
-              kernels::HydroVars<typename CollisionType::CKernel> hydroVars(fOld);
+              std::vector<unsigned> wallIntersections(numVectors);
 
-              ///< @todo #126 This value of tau will be updated by some kernels within the collider code (e.g. LBGKNN). It would be nicer if tau is handled in a single place.
-              hydroVars.tau = lbmParams->GetTau();
-
-              collider.CalculatePreCollision(hydroVars, site);
-
-              collider.Collide(lbmParams, hydroVars);
-
-              for (Direction ii = 0; ii < LatticeType::NUMVECTORS; ii++)
+              for ( site_t siteIndex = 0; siteIndex < numVectors; ++siteIndex )
               {
-                if (site.HasWall(ii))
-                {
-                  wallLinkDelegate.StreamLink(lbmParams, latDat, site, hydroVars, ii);
-                }
-                else
-                {
-                  bulkLinkDelegate.StreamLink(lbmParams, latDat, site, hydroVars, ii);
-                }
+                wallIntersections[siteIndex] = latDat->GetSite(siteIndex).GetSiteData().GetWallIntersectionData();
               }
 
-              //TODO: Necessary to specify sub-class?
-              BaseStreamer<WallStreamerTypeFactory>::template UpdateMinsAndMaxes<tDoRayTracing>(site,
-                                                                                                hydroVars,
-                                                                                                lbmParams,
-                                                                                                propertyCache);
+              cudaMemcpy(neighbourIndices_dev, latDat->GetNeighbourIndices().data(), numVectors * LatticeType::NUMVECTORS * sizeof(site_t), cudaMemcpyHostToDevice);
+              cudaMemcpy(wallIntersections_dev, wallIntersections.data(), numVectors * sizeof(unsigned), cudaMemcpyHostToDevice);
+
+              init = false;
             }
+
+            // copy fOld from host to device
+            cudaMemcpy(fOld_dev, latDat->GetSite(firstIndex).GetFOld<LatticeType>(), siteCount * LatticeType::NUMVECTORS * sizeof(distribn_t), cudaMemcpyHostToDevice);
+
+            // launch WallStreamer_DoStreamAndCollide kernel
+            const int BLOCK_SIZE = 256;
+            const int GRID_SIZE = (siteCount + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+            WallStreamerTypeFactory_DoStreamAndCollideKernel<<<GRID_SIZE, BLOCK_SIZE>>>(
+              firstIndex,
+              siteCount,
+              lbmParams->GetTau(),
+              lbmParams->GetOmega(),
+              neighbourIndices_dev,
+              wallIntersections_dev,
+              fOld_dev,
+              fNew_dev
+            );
+
+            // copy fNew from device to host
+            cudaMemcpy(latDat->GetFNew(firstIndex), fNew_dev, siteCount * LatticeType::NUMVECTORS * sizeof(distribn_t), cudaMemcpyDeviceToHost);
           }
+
           template<bool tDoRayTracing>
           inline void DoPostStep(const site_t firstIndex,
                                  const site_t siteCount,
