26ab5a1105450c54c662fb3615a90a79f39b887e
Miguel O. Bernabeu <miguel.bernabeu@ed.ac.uk>
2020-04-17 14:31:54 +0100

    #734 compile VTK from source when not available, needs bumping up cmake version used in ARCHER

--- Code/cmake/dependencies.cmake
+++ Code/cmake/dependencies.cmake
@@ -1,6 +1,6 @@
 #------Parmetis  ------------
 find_package(Parmetis REQUIRED)
-include_directories(SYSTEM ${PARMETIS_INCLUDE_DIRS})
+include_directories(${PARMETIS_INCLUDE_DIRS})
 
 #------TinyXML ----------------
 find_package(TinyXML REQUIRED)
@@ -8,25 +8,25 @@ OPTION(TIXML_USE_STL "Use STL with TIXML" ON)
 if(TIXML_USE_STL)
 	add_definitions(-DTIXML_USE_STL)
 endif()
-include_directories(SYSTEM ${TINYXML_INCLUDE_DIR})
+include_directories(${TINYXML_INCLUDE_DIR})
 #------BOOST ------------------
 find_package(Boost 1.54 REQUIRED)
-include_directories(SYSTEM ${Boost_INCLUDE_DIRS})
+include_directories(${Boost_INCLUDE_DIRS})
 add_definitions(-DHEMELB_USE_BOOST)
 #------CTemplate ----------------
 find_package(CTemplate REQUIRED)
-include_directories(SYSTEM ${CTEMPLATE_INCLUDE_DIR})
+include_directories(${CTEMPLATE_INCLUDE_DIR})
 if(HEMELB_BUILD_MULTISCALE)
   #------MPWide ----------------
   find_package(MPWide REQUIRED)
-  include_directories(SYSTEM ${MPWide_INCLUDE_DIR})
+  include_directories(${MPWide_INCLUDE_DIR})
   add_definitions(-DHEMELB_BUILD_MULTISCALE)
 endif()
 
 #------zlib ----------------
 find_package(ZLIB REQUIRED)
-include_directories(SYSTEM ${ZLIB_INCLUDE_DIR})
+include_directories(${ZLIB_INCLUDE_DIR})
 
 #------VTK ----------------
-find_package(VTK REQUIRED)
-include_directories(SYSTEM ${VTK_INCLUDE_DIRS})
+find_package(VTK REQUIRED NO_MODULE)
+include_directories(${VTK_INCLUDE_DIRS})
