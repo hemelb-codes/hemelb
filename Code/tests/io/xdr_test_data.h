efac3f1ec285e1e4551d6d520640ec3c0cd39c19
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-09-03 14:26:34 +0100

    move tests io/XdrReader and XdrWriter

---
+++ Code/tests/io/xdr_test_data.h
@@ -0,0 +1,22 @@
+// -*- mode: c++; -*-
+// This file is part of HemeLB and is Copyright (C)
+// the HemeLB team and/or their institutions, as detailed in the
+// file AUTHORS. This software is provided under the terms of the
+// license in the file LICENSE.
+
+#ifndef HEMELB_TESTS_IO_XDRTESTDATA_H
+#define HEMELB_TESTS_IO_XDRTESTDATA_H
+#include <vector>
+
+namespace hemelb
+{
+  namespace tests
+  {
+    template <typename T>
+    struct test_data {
+      static const std::vector<T>& unpacked();
+      static const std::vector<char>& packed();
+    };
+  }
+}
+#endif
