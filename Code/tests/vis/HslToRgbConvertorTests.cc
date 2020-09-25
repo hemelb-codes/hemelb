a0fc25562d68986d11566b558c959b8a17932115
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-09-25 13:18:48 +0100

    move vis test

---
+++ Code/tests/vis/HslToRgbConvertorTests.cc
@@ -0,0 +1,44 @@
+
+// This file is part of HemeLB and is Copyright (C)
+// the HemeLB team and/or their institutions, as detailed in the
+// file AUTHORS. This software is provided under the terms of the
+// license in the file LICENSE.
+
+#include <catch2/catch.hpp>
+
+#include "vis/rayTracer/HSLToRGBConverter.h"
+
+namespace hemelb
+{
+  namespace tests
+  {
+    namespace {
+      void DoColourTest(float iHue,
+			float iSaturation,
+			float iLightness,
+			unsigned char iExpectedRed,
+			unsigned char iExpectedGreen,
+			unsigned char iExpectedBlue)
+      {
+	unsigned char lRGB[3];
+
+	vis::raytracer::HSLToRGBConverter::Convert(iHue, iSaturation, iLightness, lRGB);
+
+	UNSCOPED_INFO("HslRgbConvertor test differed on red channel");
+	REQUIRE(iExpectedRed == lRGB[0]);
+	UNSCOPED_INFO("HslRgbConvertor test differed on green channel");
+	REQUIRE(iExpectedGreen == lRGB[1]);
+	UNSCOPED_INFO("HslRgbConvertor test differed on blue channel");
+	REQUIRE(iExpectedBlue == lRGB[2]);
+      }
+    }
+
+    TEST_CASE("HslToRgbConvertorTests") {
+      DoColourTest(0, 0, 0, 0, 0, 0);
+      DoColourTest(1, 1, 1, 255, 255, 255);
+      DoColourTest(250, 0.5, 0.5, 84, 63, 191);
+    }
+
+  }
+}
+
--- Code/unittests/main.cc
+++ Code/unittests/main.cc
@@ -11,7 +11,6 @@
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <stdexcept>
-#include "unittests/vistests/vistests.h"
 #include "unittests/net/net.h"
 #include "unittests/multiscale/multiscale.h"
 #ifdef HEMELB_BUILD_MULTISCALE
