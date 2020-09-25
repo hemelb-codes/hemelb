0694e4c2a4638bc34f8dc93d8c8ffb0a2bc926af
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-09-25 14:23:12 +0100

    move net/phased tests

---
+++ Code/tests/net/phased/MockConcern.h
@@ -0,0 +1,35 @@
+
+// This file is part of HemeLB and is Copyright (C)
+// the HemeLB team and/or their institutions, as detailed in the
+// file AUTHORS. This software is provided under the terms of the
+// license in the file LICENSE.
+
+#ifndef HEMELB_TESTS_NET_PHASED_MOCKCONCERN_H
+#define HEMELB_TESTS_NET_PHASED_MOCKCONCERN_H
+
+#include <string>
+#include <vector>
+
+#include "net/phased/Concern.h"
+
+namespace hemelb
+{
+  namespace tests
+  {
+    class MockConcern : public net::phased::Concern
+    {
+    public:
+      MockConcern(const std::string &name);
+
+      bool CallAction(int action);
+
+      std::vector<int> const & ActionsCalled() const;
+
+    private:
+      std::vector<int> calls;
+      std::string name;
+    };
+
+  }
+}
+#endif
--- Code/unittests/net/net.h
+++ Code/unittests/net/net.h
@@ -7,7 +7,6 @@
 #ifndef HEMELB_UNITTESTS_NET_NET_H
 #define HEMELB_UNITTESTS_NET_NET_H
 
-#include "unittests/net/phased/phased.h"
 #include "unittests/net/MpiTests.h"
 
 #endif
