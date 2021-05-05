2cd325e471f6dbcb52f89853324408a8c666c26e
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-05-05 09:39:41 +0100

    CMake: modernise cppunit stuff a bit after build probs

--- dependencies/Modules/FindCPPUnit.cmake
+++ dependencies/Modules/FindCPPUnit.cmake
@@ -5,33 +5,49 @@
 
 include_guard()
 
-FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/Test.h)
-option(CPPUNIT_USE_STATIC "Prefer Static CPPUNIT library" OFF)
-if(CPPUNIT_USE_STATIC)
-set(__old_cmake_find_lib_suffixes ${CMAKE_FIND_LIBRARY_SUFFIXES})
-	set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+find_package(PkgConfig QUIET)
+if (PkgConfig_FOUND)
+  pkg_check_modules(_CPPUnit cppunit)
+  if (_CPPUnit_FOUND)
+    # Forward consistently
+    set(CPPUnit_FOUND "${_CPPUnit_FOUND}")
+    set(CPPUnit_INCLUDE_DIR "${_CPPUnit_INCLUDE_DIRS}")
+    set(CPPUnit_LIBRARY "${_CPPUnit_LINK_LIBRARIES}")
+    add_library(CPPUnit::CPPUnit INTERFACE IMPORTED)
+    set_target_properties(CPPUnit::CPPUnit PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${_CPPUnit_INCLUDE_DIRS}"
+      INTERFACE_LINK_LIBRARIES "${_CPPUnit_LINK_LIBRARIES}"
+      )
+  endif()
 endif()
-FIND_LIBRARY(CPPUNIT_LIBRARY NAMES cppunit)
-if(CPPUNIT_USE_STATIC)
-	set(CMAKE_FIND_LIBRARY_SUFFIXES ${__old_cmake_find_lib_suffixes})
+
+if (NOT PkgConfig_FOUND OR NOT _CPPUnit_FOUND)
+  find_path(CPPUnit_INCLUDE_DIR cppunit/Test.h)
+
+  option(CPPUnit_USE_STATIC "Prefer Static CPPUnit library" OFF)
+  if(CPPUnit_USE_STATIC)
+    set(__old_cmake_find_lib_suffixes ${CMAKE_FIND_LIBRARY_SUFFIXES})
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+  endif()
+
+  find_library(CPPUnit_LIBRARY NAMES cppunit)
+  if(CPPUnit_USE_STATIC)
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ${__old_cmake_find_lib_suffixes})
+  endif()
+
+  add_library(CPPUnit::CPPUnit INTERFACE IMPORTED)
+  set_target_properties(CPPUnit::CPPUnit PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${CPPUnit_INCLUDE_DIR}"
+    INTERFACE_LINK_LIBRARIES "${CPPUnit_LIBRARY}"
+    )
+
 endif()
 
-IF (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
-   SET(CPPUNIT_FOUND TRUE)
-ENDIF (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
+# handle the QUIETLY and REQUIRED arguments
+include( "FindPackageHandleStandardArgs" )
+find_package_handle_standard_args(
+  "CPPUnit"
+  DEFAULT_MSG
+  CPPUnit_INCLUDE_DIR CPPUnit_LIBRARY
+  )
 
-IF (CPPUNIT_FOUND)
-
-   # show which CppUnit was found only if not quiet
-   IF (NOT CppUnit_FIND_QUIETLY)
-      MESSAGE(STATUS "Found CppUnit: ${CPPUNIT_LIBRARY}")
-   ENDIF (NOT CppUnit_FIND_QUIETLY)
-
-ELSE (CPPUNIT_FOUND)
-
-   # fatal error if CppUnit is required but not found
-   IF (CppUnit_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find CppUnit")
-   ENDIF (CppUnit_FIND_REQUIRED)
-
-ENDIF (CPPUNIT_FOUND)
