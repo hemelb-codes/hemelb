1dd8493d314cdecc5781de89374dff6011c360c6
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-09-21 11:46:42 +0100

    python-tools: add basic parser for offset files

---
+++ python-tools/tests/test_parser_offset.py
@@ -0,0 +1,15 @@
+# This file is part of HemeLB and is Copyright (C)
+# the HemeLB team and/or their institutions, as detailed in the
+# file AUTHORS. This software is provided under the terms of the
+# license in the file LICENSE.
+
+from pathlib import Path
+import numpy as np
+from hlb.parsers.offset import OffsetFile
+
+
+def test_load_off(diffTestDir):
+    off_path = Path(diffTestDir) / "CleanExtracted" / "flow_snapshot.off"
+    off = OffsetFile(off_path)
+    assert off.NumberOfRanks == 3
+    assert off.Data.shape == (4,)
