517b91c6e9d48a48ae954e92d0ba818c865c81fa
Rupert Nash <r.nash@epcc.ed.ac.uk>
2023-01-27 10:17:40 +0000

    modernise and tidy util::enumerate/zip templates

--- Code/util/Iterator.h
+++ Code/util/Iterator.h
@@ -6,43 +6,49 @@
 #ifndef HEMELB_UTIL_ITERATOR_H
 #define HEMELB_UTIL_ITERATOR_H
 
-#include <iterator>
 #include <iostream>
 #include <tuple>
+#include <compare>
 
-namespace hemelb
+namespace hemelb::util
 {
-  namespace util
-  {
     //! For-ranged loop plus enumeration
-    template<class ITERATOR> class Enumerate
+    template<class WrappedIterT, class CounterT = std::iter_difference_t<WrappedIterT>>
+    class Enumerate
     {
+    protected:
+        WrappedIterT first;
+        WrappedIterT last;
+
         //! Holds info about enumeration
         struct EnumerateItem
         {
-          size_t index;
-          typename ITERATOR::reference value;
+            CounterT index;
+            typename WrappedIterT::reference value;
         };
 
-      public:
+    public:
         class iterator
         {
-          public:
-            iterator(ITERATOR const &iter) : index(0), iter(iter)
+        protected:
+            CounterT index;
+            WrappedIterT iter;
+        public:
+            explicit iterator(WrappedIterT const &iter) : index(0), iter(iter)
             {
             }
-            iterator(ITERATOR const &iter, size_t index) : index(index), iter(iter)
+            iterator(WrappedIterT const &iter, size_t index) : index(index), iter(iter)
             {
             }
-            iterator & operator++()
+            iterator& operator++()
             {
               ++index;
               ++iter;
               return *this;
             }
-            iterator operator++(int)
+            iterator operator++(int) &
             {
-              iterator const result(iter.iter);
+              iterator result(iter.iter);
               iter++;
               return result;
             }
@@ -50,22 +56,109 @@ namespace hemelb
             {
               return {index, *iter};
             }
-            bool operator==(iterator const &b) const
-            {
-              return b.iter == iter;
-            }
-            bool operator!=(iterator const &b) const
-            {
-              return b.iter != iter;
-            }
-          protected:
-            size_t index;
-            ITERATOR iter;
+            friend auto operator<=>(const iterator&, const iterator&) = default;
         };
 
-        Enumerate(ITERATOR const &first, ITERATOR const &last) : first(first), last(last)
+        Enumerate(WrappedIterT const &first, WrappedIterT const &last) : first(first), last(last)
         {
+        }
+        iterator begin() const
+        {
+          return {first, 0};
+        }
+        iterator end() const
+        {
+          return iterator(last, std::distance(first, last));
+        }
+    };
+
+    //! Ranged-for loops with enumeration
+    template<class C>
+    auto enumerate(C&& c) -> Enumerate<decltype(begin(std::forward<C>(c)))>
+    {
+      return {begin(std::forward<C>(c)), end(std::forward<C>(c))};
+    }
+
+    //! Ranged-for loops with enumeration
+    template<class CONTAINER>
+    auto cenumerate(CONTAINER const &x) -> Enumerate<decltype(begin(x))>
+    {
+      return {begin(x), end(x)};
+    }
+
+    //! For-ranged loop over several containers in parallel
+    template<class... ContainerTs>
+    class Zip
+    {
+    public:
+        //! Iterates over all objects that are being zipped
+        template<class ... ITERATOR>
+        class zip_iterator
+        {
+            static constexpr auto N = sizeof...(ITERATOR);
+        public:
+            using reference = std::tuple<typename std::iterator_traits<ITERATOR>::reference...>;
+            //! Inherits from all iterator types
+            zip_iterator(ITERATOR const & ... args) : iter(args...)
+            {
+            }
+            //! Inherits from all iterator types
+            zip_iterator(ITERATOR && ... args) : iter(std::move(args)...)
+            {
+            }
+            zip_iterator& operator++()
+            {
+                increment(std::make_index_sequence<N>());
+                return *this;
+            }
+            reference operator*() const
+            {
+                return deref(std::make_index_sequence<N>());
+            }
+
+            //! True if any of the iterator is equal to the corresponding on in b
+            friend bool operator==(zip_iterator const &a, zip_iterator const &b)
+            {
+                return IsEqual<0>(a, b);
+            }
+            friend bool operator!=(zip_iterator const& a, zip_iterator const &b)
+            {
+                return !(a == b);
+            }
+
+        protected:
+            // For each of these helpers, we use an index sequence
+            // as the argument to deduce the template arguments `Is`.
+            // Should be called with `std::make_index_sequence<N>()`.
+
+            //! Increment
+            // Note folding over the comma operator
+            template <std::size_t... Is>
+            void increment(std::index_sequence<Is...>) {
+                (++std::get<Is>(iter), ...);
+            }
+            //! Comparison
+            // Do a fold over logical and
+            template<std::size_t... Is>
+            friend bool IsEqual(zip_iterator const& a, zip_iterator const &b)
+            {
+                return ((std::get<Is>(a.iter) == std::get<Is>(b.iter)) && ...);
+            }
+            //! Derefence
+            template <std::size_t... Is>
+            reference deref(std::index_sequence<Is...>) const {
+                return {*std::get<Is>(iter)...};
+            }
+
+            //! Holds each iterator
+            std::tuple<ITERATOR...> iter;
         };
+
+        using iterator = zip_iterator<decltype(std::declval<ContainerTs>().begin())...>;
+
+        Zip(ContainerTs && ... cont) : first(cont.begin()...), last(cont.end()...)
+        {
+        }
         iterator begin() const
         {
           return first;
@@ -75,117 +168,8 @@ namespace hemelb
           return last;
         }
       protected:
-        ITERATOR const first;
-        ITERATOR const last;
-    };
-
-    //! Ranged-for loops with enumeration
-    template<class CONTAINER> auto enumerate(CONTAINER &&x) -> Enumerate<decltype(begin(x))>
-    {
-      return {begin(x), end(x)};
-    }
-
-    //! Ranged-for loops with enumeration
-    template<class CONTAINER> auto cenumerate(CONTAINER const &x) -> Enumerate<decltype(begin(x))>
-    {
-      return {begin(x), end(x)};
-    }
-
-    //! For-ranged loop over several containers in parallel
-    template<class ... CONTAINER> class Zip
-    {
-      public:
-        //! Iterates over all objects that are being zipped
-        template<class ... ITERATOR> class iterator
-        {
-          public:
-            typedef std::tuple<typename std::iterator_traits<ITERATOR>::reference...> reference;
-            //! Inherits from all iterator types
-            iterator(ITERATOR const & ... args) : iter(args...)
-            {
-            }
-            //! Inherits from all iterator types
-            iterator(ITERATOR && ... args) : iter(std::move(args)...)
-            {
-            }
-            iterator& operator++()
-            {
-              increment<0>();
-              return *this;
-            }
-            reference operator*() const
-            {
-              return deref<sizeof...(ITERATOR) - 1, sizeof...(ITERATOR) - 1>();
-            }
-
-            //! True if any of the iterator is equal to the corresponding on in b
-            bool operator==(iterator const &b) const
-            {
-              return IsEqual<0>(b);
-            }
-            bool operator!=(iterator const &b) const
-            {
-              return not operator==(b);
-            }
-
-          protected:
-            //! Increments by recurrence
-            template<size_t N>
-              typename std::enable_if<N < sizeof...(ITERATOR)>::type increment()
-              {
-                ++std::get<N>(iter);
-                increment<N+1>();
-              }
-            //! Final for increment by recurrence
-            template<size_t N>
-              typename std::enable_if<N >= sizeof...(ITERATOR)>::type increment()
-              {
-              }
-            //! Comparison by recurrence
-            template<size_t N>
-              typename std::enable_if<N + 1 < sizeof...(ITERATOR), bool>::type
-              IsEqual(iterator const &b) const
-              {
-                return std::get<N>(iter) == std::get<N>(b.iter) or IsEqual<N+1>(b);
-              }
-            //! Final for comparison by recurrence
-            template<size_t N>
-              typename std::enable_if<N + 1 == sizeof...(ITERATOR), bool>::type
-              IsEqual(iterator const &b) const
-              {
-                return std::get<N>(iter) == std::get<N>(b.iter);
-              }
-            //! Comparison by recurrence
-            template<size_t i, size_t ... N>
-              typename std::enable_if<i == 0, reference>::type deref() const
-              {
-                return std::tie(*std::get<N>(iter)...);
-              }
-            //! Comparison by recurrence
-            template<size_t i, size_t ... N>
-              typename std::enable_if<i != 0, reference>::type deref() const
-              {
-                return deref<i - 1, i - 1, N...>();
-              }
-
-            //! Holds each iterator
-            std::tuple<ITERATOR...> iter;
-        };
-
-        Zip(CONTAINER && ... cont) : first(cont.begin()...), last(cont.end()...)
-        {
-        };
-        iterator<decltype(std::declval<CONTAINER>().begin())...> begin() const
-        {
-          return first;
-        }
-        iterator<decltype(std::declval<CONTAINER>().end())...> end() const
-        {
-          return last;
-        }
-      protected:
-        iterator<decltype(std::declval<CONTAINER>().begin())...> const first;
-        iterator<decltype(std::declval<CONTAINER>().begin())...> const last;
+        iterator first;
+        iterator last;
     };
 
     //! \brief Ranged-for loops over sets of containers in parallel
@@ -196,7 +180,8 @@ namespace hemelb
     //!   std::get<1>(item);
     //! }
     //! \endcode
-    template<class ... CONTAINER> auto zip(CONTAINER && ... x) -> Zip<CONTAINER...>
+    template<class ... CONTAINER>
+    auto zip(CONTAINER && ... x) -> Zip<CONTAINER...>
     {
       return {x...};
     }
@@ -208,12 +193,12 @@ namespace hemelb
     //!   std::get<1>(item);
     //! }
     //! \endcode
-    template<class ... CONTAINER> auto czip(CONTAINER const & ... x) -> Zip<CONTAINER const &...>
+    template<class ... CONTAINER>
+    auto czip(CONTAINER const & ... x) -> Zip<CONTAINER const &...>
     {
       return {x...};
     }
 
-  }
 }
 
-#endif /* HEMELB_UTIL_CACHE_H */
+#endif
